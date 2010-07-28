3d3f25a8625d7d4ccbc504f218771a946d228bd6
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2010-07-28 20:23:09 +0000

    Fixed issue #1056 and #1057, migrated star rating to standard JSF 2.0 ajax apis, also added widgetVar option.

--- primefaces/trunk/src/main/java-templates/org/primefaces/component/rating/RatingTemplate.java
+++ primefaces/trunk/src/main/java-templates/org/primefaces/component/rating/RatingTemplate.java
@@ -1,12 +1,11 @@
-import javax.faces.event.ActionEvent;
 
-	public void broadcast(javax.faces.event.FacesEvent event) throws javax.faces.event.AbortProcessingException {
-		super.broadcast(event);
-		
-		FacesContext facesContext = FacesContext.getCurrentInstance();
-		MethodExpression me = getRateListener();
-		
-		if (me != null) {
-			me.invoke(facesContext.getELContext(), new Object[] {event});
-		}
-	}
+    public void broadcast(javax.faces.event.FacesEvent event) throws javax.faces.event.AbortProcessingException {
+        super.broadcast(event);
+
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        MethodExpression me = getRateListener();
+
+        if (me != null && event instanceof org.primefaces.event.RateEvent) {
+            me.invoke(facesContext.getELContext(), new Object[] {event});
+        }
+    }
