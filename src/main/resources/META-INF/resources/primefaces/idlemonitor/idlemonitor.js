163ac1a368f1c42d1304ff20896bee0ad6f2106f
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2010-07-28 19:36:08 +0000

    Fixed issue #1054 and #1055, migrated idleMonitor to standard JSF 2.0 ajax apis.

--- src/main/resources/META-INF/resources/primefaces/idlemonitor/idlemonitor.js
+++ src/main/resources/META-INF/resources/primefaces/idlemonitor/idlemonitor.js
@@ -1,31 +1,38 @@
-PrimeFaces.widget.IdleMonitor = function(clientId, cfg) {
-	
-	jQuery(document).bind("idle.idleTimer", function(){
-		
-		if(cfg.hasIdleListener) {
-			var xhrOptions = {formId:cfg.formId};
-			
-			if(cfg.onidle) {
-				xhrOptions.oncomplete = cfg.onidle;
-			}
-			
-			var params = {};
-			params[clientId] = clientId;
-			params[PrimeFaces.PARTIAL_PROCESS_PARAM] = clientId;
-			params[PrimeFaces.PARTIAL_UPDATE_PARAM] = cfg.update;
+PrimeFaces.widget.IdleMonitor = function(id, cfg) {
+    this.id = id;
+    this.cfg = cfg;
 
-			PrimeFaces.ajax.AjaxRequest(cfg.actionURL, xhrOptions, params);
-			
-		} else if(cfg.onidle) {
-			cfg.onidle();
-		}
-	});
+    var _self = this;
 	
-	jQuery(document).bind("active.idleTimer", function(){
-		if(cfg.onactive) {
-			cfg.onactive();
-		}
-	});
+    jQuery(document).bind("idle.idleTimer", function(){
+
+        if(_self.cfg.onidle) {
+            _self.cfg.onidle.call(_self);
+        }
+		
+        if(_self.cfg.hasIdleListener) {
+            var options = {
+                source: _self.id,
+                process: _self.id,
+                formId: _self.cfg.formId
+            };
+
+            if(_self.cfg.update) {
+                options.update = _self.cfg.update;
+            }
+
+            var params = {};
+            params[_self.id] = _self.id;
+
+            PrimeFaces.ajax.AjaxRequest(_self.cfg.url, options, params);
+        }
+    });
+	
+    jQuery(document).bind("active.idleTimer", function(){
+        if(_self.cfg.onactive) {
+            _self.cfg.onactive.call(this);
+        }
+    });
     
-    jQuery.idleTimer(cfg.timeout);
+    jQuery.idleTimer(this.cfg.timeout);
 }
