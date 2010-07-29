25014b7f59804d87448741876fd79f1968f42e60
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2010-07-29 08:58:03 +0000

    Fixed issue #1059, migrated calendar ajax date selection to jsf 2 ajax apis

--- primefaces/trunk/src/main/resources/META-INF/resources/primefaces/calendar/calendar.js
+++ primefaces/trunk/src/main/resources/META-INF/resources/primefaces/calendar/calendar.js
@@ -1,51 +1,49 @@
 PrimeFaces.widget.Calendar = function(id, cfg) {
-	this.id = id;
-	this.cfg = cfg;
-	this.jqId = PrimeFaces.escapeClientId(id);
-	this.jq = this.cfg.popup ? this.jqId + '_input' : this.jqId + '_inline';
+    this.id = id;
+    this.cfg = cfg;
+    this.jqId = PrimeFaces.escapeClientId(id);
+    this.jq = this.cfg.popup ? this.jqId + '_input' : this.jqId + '_inline';
 	
-	this.configureOnSelectHandler();
-	this.configureLocale();
+    this.configureOnSelectHandler();
+    this.configureLocale();
 	
-	if(!this.cfg.disabled) {
-		jQuery(this.jq).datepicker(this.cfg);
-	}
+    if(!this.cfg.disabled) {
+        jQuery(this.jq).datepicker(this.cfg);
+    }
 }
 
 PrimeFaces.widget.Calendar.prototype.configureLocale = function() {
-	jQuery.datepicker.setDefaults(jQuery.datepicker.regional['']);
-	var regional = jQuery.datepicker.regional;
-	var localeSettings = jQuery.datepicker.regional[this.cfg.locale];
+    jQuery.datepicker.setDefaults(jQuery.datepicker.regional['']);
+    var localeSettings = jQuery.datepicker.regional[this.cfg.locale];
 	
-	if(localeSettings) {
-		for(var setting in localeSettings) {
-			this.cfg[setting] = localeSettings[setting];
-		}
-	}
+    if(localeSettings) {
+        for(var setting in localeSettings) {
+            this.cfg[setting] = localeSettings[setting];
+        }
+    }
 }
 
 PrimeFaces.widget.Calendar.prototype.configureOnSelectHandler = function() {
-	var _self = this;
+    var _self = this;
 	
-	this.cfg.onSelect = function(dateText, input) {
+    this.cfg.onSelect = function(dateText, input) {
 		
-		if(!_self.cfg.popup) {
-			jQuery(_self.jqId + '_input').val(dateText);
-		}
+        if(!_self.cfg.popup) {
+            jQuery(_self.jqId + '_input').val(dateText);
+        }
 		
-		if(_self.cfg.hasSelectListener) {
-			var params = {};
-			params[PrimeFaces.PARTIAL_PROCESS_PARAM] = _self.id;
-			
-			if(_self.cfg.onSelectUpdate) {
-				params[PrimeFaces.PARTIAL_UPDATE_PARAM] = _self.cfg.onSelectUpdate;
-			}
-			
-			PrimeFaces.ajax.AjaxRequest(_self.cfg.url, {
-					formId:_self.cfg.formId,
-					global:true
-				},
-				params);
-		}
-	}
+        if(_self.cfg.hasSelectListener) {
+            var options = {
+                source: _self.id,
+                process: _self.id,
+                formId: _self.cfg.formId
+            };
+
+            if(_self.cfg.onSelectUpdate) {
+                options.update = _self.cfg.onSelectUpdate;
+            }
+	
+            PrimeFaces.ajax.AjaxRequest(_self.cfg.url, options);
+        }
+    }
 }
