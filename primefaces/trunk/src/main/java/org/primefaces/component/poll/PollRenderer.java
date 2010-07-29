d352780af0f8d29ed0a61e85b17d3a2dd52b28aa
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2010-07-29 19:04:08 +0000

    Code refactor&cosmetics on p:poll

--- primefaces/trunk/src/main/java/org/primefaces/component/poll/PollRenderer.java
+++ primefaces/trunk/src/main/java/org/primefaces/component/poll/PollRenderer.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2010 Prime Technology.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.primefaces.component.poll;
 
 import java.io.IOException;
@@ -10,40 +25,43 @@ import javax.faces.event.ActionEvent;
 
 import org.primefaces.renderkit.CoreRenderer;
 import org.primefaces.util.ComponentUtils;
+
 public class PollRenderer extends CoreRenderer {
 
-	public void decode(FacesContext facesContext, UIComponent component) {
-		Poll poll = (Poll) component;
-	
-		if(facesContext.getExternalContext().getRequestParameterMap().containsKey(poll.getClientId(facesContext))) {
-			poll.queueEvent(new ActionEvent(poll));
-		}
-	}
-	
-	public void encodeEnd(FacesContext facesContext, UIComponent component) throws IOException {
-		ResponseWriter writer = facesContext.getResponseWriter();
-		Poll poll = (Poll) component;
-		String pollVar = createUniqueWidgetVar(facesContext, poll);
-		String clientId = poll.getClientId(facesContext);
-		UIComponent form = ComponentUtils.findParentForm(facesContext, poll);
-		if(form == null) {
-			throw new FacesException("Poll:" + clientId + " needs to be enclosed in a form component");
-		}
-		
-		writer.startElement("script", null);
-		writer.writeAttribute("type", "text/javascript", null);
-		
-		writer.write("YAHOO.util.Event.addListener(window, 'load', function() {");
-		
-		writer.write(pollVar + "= new PrimeFaces.widget.Poll('" + clientId + "', {");
-		writer.write("frequency:" + poll.getInterval());
-		writer.write(",autoStart:" + poll.isAutoStart());
-		writer.write(",fn: function() {");
-		writer.write(buildAjaxRequest(facesContext, poll, form.getClientId(facesContext), clientId));
-		writer.write("}");
-				
-		writer.write("});});");
-		
-		writer.endElement("script");
-	}
+    @Override
+    public void decode(FacesContext facesContext, UIComponent component) {
+        Poll poll = (Poll) component;
+
+        if (facesContext.getExternalContext().getRequestParameterMap().containsKey(poll.getClientId(facesContext))) {
+            poll.queueEvent(new ActionEvent(poll));
+        }
+    }
+
+    @Override
+    public void encodeEnd(FacesContext facesContext, UIComponent component) throws IOException {
+        ResponseWriter writer = facesContext.getResponseWriter();
+        Poll poll = (Poll) component;
+        String pollVar = createUniqueWidgetVar(facesContext, poll);
+        String clientId = poll.getClientId(facesContext);
+        UIComponent form = ComponentUtils.findParentForm(facesContext, poll);
+        if (form == null) {
+            throw new FacesException("Poll:" + clientId + " needs to be enclosed in a form component");
+        }
+
+        writer.startElement("script", null);
+        writer.writeAttribute("type", "text/javascript", null);
+
+        writer.write("jQuery(function() {");
+
+        writer.write(pollVar + "= new PrimeFaces.widget.Poll('" + clientId + "', {");
+        writer.write("frequency:" + poll.getInterval());
+        writer.write(",autoStart:" + poll.isAutoStart());
+        writer.write(",fn: function() {");
+        writer.write(buildAjaxRequest(facesContext, poll, form.getClientId(facesContext), clientId));
+        writer.write("}");
+
+        writer.write("});});");
+
+        writer.endElement("script");
+    }
 }
--- primefaces/trunk/src/main/resources/META-INF/resources/primefaces/poll/poll.js
+++ primefaces/trunk/src/main/resources/META-INF/resources/primefaces/poll/poll.js
@@ -1,15 +1,16 @@
 PrimeFaces.widget.Poll = function(id, cfg) {
-	this.id = id;
-	this.cfg = cfg;
+    this.id = id;
+    this.cfg = cfg;
 	
-	if(this.cfg.autoStart)
-		this.start();
+    if(this.cfg.autoStart) {
+        this.start();
+    }
 }
 
 PrimeFaces.widget.Poll.prototype.start = function() {
-	this.timer = setInterval(this.cfg.fn, (this.cfg.frequency * 1000));
+    this.timer = setInterval(this.cfg.fn, (this.cfg.frequency * 1000));
 }
 
 PrimeFaces.widget.Poll.prototype.stop = function() {
-	clearInterval(this.timer);
+    clearInterval(this.timer);
 }
