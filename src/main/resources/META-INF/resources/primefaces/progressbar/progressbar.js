3d9fa121db619faf2f360db11ee710448171b587
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2010-07-29 18:18:43 +0000

    Fixed issue #1065 and #1066, migrated progress bar to standard jsf 2 apis

--- src/main/resources/META-INF/resources/primefaces/progressbar/progressbar.js
+++ src/main/resources/META-INF/resources/primefaces/progressbar/progressbar.js
@@ -1,79 +1,87 @@
 PrimeFaces.widget.ProgressBar = function(id, cfg) {
-	this.id = id;
-	this.cfg = cfg;
-	this.jqId = PrimeFaces.escapeClientId(id);
+    this.id = id;
+    this.cfg = cfg;
+    this.jqId = PrimeFaces.escapeClientId(id);
 	
-	jQuery(this.jqId).progressbar(this.cfg);
+    jQuery(this.jqId).progressbar(this.cfg);
 }
 
 PrimeFaces.widget.ProgressBar.prototype.setValue = function(value) {
-	jQuery(this.jqId).progressbar('value', value);
+    jQuery(this.jqId).progressbar('value', value);
 }
 
 PrimeFaces.widget.ProgressBar.prototype.getValue  = function() {
-	return jQuery(this.jqId).progressbar('value');
+    return jQuery(this.jqId).progressbar('value');
 }
 
 PrimeFaces.widget.ProgressBar.prototype.start = function() {
-	var p = this;
+    var _self = this;
 	
-	if(this.cfg.ajax) {
+    if(this.cfg.ajax) {
 		
-		this.progressPoll = setInterval(function() {
-			var params = {};
-			params[PrimeFaces.PARTIAL_PROCESS_PARAM] = p.id;
-			
-			PrimeFaces.ajax.AjaxRequest(p.cfg.url, {
-				formId:p.cfg.formId,
-				global:false,
-				async:true,
-				oncomplete:function(xhr, status, args) {
-						var value = args[p.id + '_value'];
-						p.setValue(value);
-						
-						//trigger close listener
-						if(value === 100) {
-							p.fireCloseEvent();
-						}
-					}
-				},
-				params);
-		}, this.cfg.interval);	
-	}
+        this.progressPoll = setInterval(function() {
+            var options = {
+                source: _self.id,
+                process: _self.id,
+                formId: _self.cfg._formId,
+                async: true,
+                oncomplete: function(xhr, status, args) {
+                    var value = args[_self.id + '_value'];
+                    _self.setValue(value);
+
+                    //trigger close listener
+                    if(value === 100) {
+                        _self.fireCompleteEvent();
+                    }
+                }
+            };
+
+            PrimeFaces.ajax.AjaxRequest(_self.cfg.url, options);
+            
+        }, this.cfg.interval);
+    }
 }
 
-PrimeFaces.widget.ProgressBar.prototype.fireCloseEvent = function() {
-	clearInterval(this.progressPoll);
-	
-	var params = {};
-	params[this.id + '_complete'] = true;
-	params[PrimeFaces.PARTIAL_PROCESS_PARAM] = this.id;
-	
-	if(this.cfg.onCompleteUpdate) {
-		params[PrimeFaces.PARTIAL_UPDATE_PARAM] = this.cfg.onCompleteUpdate;
-	}
+PrimeFaces.widget.ProgressBar.prototype.fireCompleteEvent = function() {
+    clearInterval(this.progressPoll);
 
-	PrimeFaces.ajax.AjaxRequest(this.cfg.url, {formId:this.cfg.formId, global:false, async:true}, params);
+    var options = {
+        source: this.id,
+        process: this.id,
+        formId: this.cfg.formId,
+        async: true
+    };
+
+    if(this.cfg.onCompleteUpdate) {
+        options.update = this.cfg.onCompleteUpdate;
+    }
+
+    var params = {};
+    params[this.id + '_complete'] = true;
+	
+    PrimeFaces.ajax.AjaxRequest(this.cfg.url, options, params);
 }
 
 PrimeFaces.widget.ProgressBar.prototype.cancel = function() {
-	clearInterval(this.progressPoll);
-	
-	var params = {};
-	params[this.id + '_cancel'] = true;
-	params[PrimeFaces.PARTIAL_PROCESS_PARAM] = this.id;
-	
-	if(this.cfg.onCancelUpdate) {
-		params[PrimeFaces.PARTIAL_UPDATE_PARAM] = this.cfg.onCancelUpdate;
-	}
+    clearInterval(this.progressPoll);
+    var _self = this;
 
-	var p = this;
-	PrimeFaces.ajax.AjaxRequest(this.cfg.url, {
-			formId:this.cfg.formId, 
-			global:false,
-			async:true,
-			oncomplete:function(xhr, status, args) {
-					p.setValue(0);
-				}
-			}, params);
+    var options = {
+        source: this.id,
+        process: this.id,
+        formId: this.cfg.formId,
+        async: true,
+        oncomplete:function(xhr, status, args) {
+            _self.setValue(0);
+        }
+    };
+
+    if(this.cfg.onCancelUpdate) {
+        options.update = this.cfg.onCancelUpdate;
+    }
+
+    var params = {};
+    params[this.id + '_cancel'] = true;
+
+    PrimeFaces.ajax.AjaxRequest(this.cfg.url, options, params);
 }
