520c9c07acfc7892e4e218bd9ed6b85a82b9f37e
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2010-07-30 10:24:06 +0000

    Fixed issue #1071, migrated dragdrop ajax listener to standard jsf 2.0 ajax

--- primefaces/trunk/src/main/java/org/primefaces/component/dnd/DraggableRenderer.java
+++ primefaces/trunk/src/main/java/org/primefaces/component/dnd/DraggableRenderer.java
@@ -27,75 +27,86 @@ import org.primefaces.renderkit.CoreRenderer;
 import org.primefaces.util.ComponentUtils;
 
 public class DraggableRenderer extends CoreRenderer {
-	
-	public void encodeEnd(FacesContext facesContext, UIComponent component) throws IOException {
-		ResponseWriter writer = facesContext.getResponseWriter();
-		Draggable draggable = (Draggable) component;
-		String var = createUniqueWidgetVar(facesContext, draggable);
-		String clientId = draggable.getClientId(facesContext);
-		String target = findTarget(facesContext, draggable);
-		String dashboard = draggable.getDashboard();
 
-		writer.startElement("script", draggable);
-		writer.writeAttribute("type", "text/javascript", null);
-		
-		writer.write("jQuery(function() {");
-		
-		writer.write(var + " = new PrimeFaces.widget.Draggable('" + clientId + "',");
-		
-		writer.write("{");
-		writer.write("target:'" + target + "'");
-		writer.write(",cursor:'" + draggable.getCursor() + "'");
-		
-		//Configuration
-		if(draggable.isDisabled()) writer.write(",disabled:true");
-		if(draggable.getAxis() != null) writer.write(",axis:'" + draggable.getAxis() + "'");
-		if(draggable.getContainment() != null) writer.write(",containment:'" + draggable.getContainment() + "'");
-		if(draggable.getHelper() != null) writer.write(",helper:'" + draggable.getHelper() + "'");
-		if(draggable.isRevert()) writer.write(",revert:'invalid'");
-		if(draggable.getZindex() != -1) writer.write(",zIndex:" + draggable.getZindex());
-		if(draggable.getHandle() != null) writer.write(",handle:'" + draggable.getHandle() + "'");
-		if(draggable.getOpacity() != 1.0) writer.write(",opacity:" + draggable.getOpacity());
-		if(draggable.getStack() != null) writer.write(",stack:'" + draggable.getStack() + "'");
-		if(draggable.getGrid() != null) writer.write(",grid:[" + draggable.getGrid() + "]");
-		if(draggable.getScope() != null) writer.write(",scope:'" + draggable.getScope() + "'");
-		
-		if(draggable.isSnap()) {
-			writer.write(",snap:true");
-			writer.write(",snapTolerance:" + draggable.getSnapTolerance());
-			if(draggable.getSnapMode() != null) 
-				writer.write(",snapMode:'" + draggable.getSnapMode() + "'");
-		}
+    @Override
+    public void encodeEnd(FacesContext facesContext, UIComponent component) throws IOException {
+        ResponseWriter writer = facesContext.getResponseWriter();
+        Draggable draggable = (Draggable) component;
+        String var = createUniqueWidgetVar(facesContext, draggable);
+        String clientId = draggable.getClientId(facesContext);
+        String target = findTarget(facesContext, draggable);
+        String dashboard = draggable.getDashboard();
 
-		
-		//Dashboard support
-		if(dashboard != null) {
-			Dashboard db = (Dashboard) draggable.findComponent(dashboard);
-			if(db == null) {
-				throw new FacesException("Cannot find dashboard \"" + dashboard + "\" in view");
-			}
-			
-			writer.write(",connectToSortable:'" + ComponentUtils.escapeJQueryId(db.getClientId(facesContext)) + " .ui-dashboard-column'");
-		}
+        writer.startElement("script", draggable);
+        writer.writeAttribute("type", "text/javascript", null);
 
-		writer.write("});");
-		
-		writer.write("});");
-		
-		writer.endElement("script");
-	}
-	
-	protected String findTarget(FacesContext facesContext, Draggable draggable) {
-		String _for = draggable.getFor();
-		
-		if(_for != null) {
-			UIComponent component = draggable.findComponent(_for);
-			if(component == null)
-				throw new FacesException("Cannot find component \"" + _for + "\" in view.");
-			else
-				return component.getClientId(facesContext);
-		} else {
-			return draggable.getParent().getClientId(facesContext);
-		}
-	}
+        writer.write("jQuery(function() {");
+
+        writer.write(var + " = new PrimeFaces.widget.Draggable('" + clientId + "',");
+
+        writer.write("{");
+        writer.write("target:'" + target + "'");
+        writer.write(",cursor:'" + draggable.getCursor() + "'");
+
+        //Configuration
+        if (draggable.isDisabled())
+            writer.write(",disabled:true");
+        if (draggable.getAxis() != null)
+            writer.write(",axis:'" + draggable.getAxis() + "'");
+        if (draggable.getContainment() != null)
+            writer.write(",containment:'" + draggable.getContainment() + "'");
+        if (draggable.getHelper() != null)
+            writer.write(",helper:'" + draggable.getHelper() + "'");
+        if (draggable.isRevert())
+            writer.write(",revert:'invalid'");
+        if (draggable.getZindex() != -1)
+            writer.write(",zIndex:" + draggable.getZindex());
+        if (draggable.getHandle() != null)
+            writer.write(",handle:'" + draggable.getHandle() + "'");
+        if (draggable.getOpacity() != 1.0)
+            writer.write(",opacity:" + draggable.getOpacity());
+        if (draggable.getStack() != null)
+            writer.write(",stack:'" + draggable.getStack() + "'");
+        if (draggable.getGrid() != null)
+            writer.write(",grid:[" + draggable.getGrid() + "]");
+        if (draggable.getScope() != null)
+            writer.write(",scope:'" + draggable.getScope() + "'");
+
+        if (draggable.isSnap()) {
+            writer.write(",snap:true");
+            writer.write(",snapTolerance:" + draggable.getSnapTolerance());
+            if (draggable.getSnapMode() != null)
+                writer.write(",snapMode:'" + draggable.getSnapMode() + "'");
+        }
+
+        //Dashboard support
+        if (dashboard != null) {
+            Dashboard db = (Dashboard) draggable.findComponent(dashboard);
+            if (db == null) {
+                throw new FacesException("Cannot find dashboard \"" + dashboard + "\" in view");
+            }
+
+            writer.write(",connectToSortable:'" + ComponentUtils.escapeJQueryId(db.getClientId(facesContext)) + " .ui-dashboard-column'");
+        }
+
+        writer.write("});");
+
+        writer.write("});");
+
+        writer.endElement("script");
+    }
+
+    protected String findTarget(FacesContext facesContext, Draggable draggable) {
+        String _for = draggable.getFor();
+
+        if (_for != null) {
+            UIComponent component = draggable.findComponent(_for);
+            if (component == null)
+                throw new FacesException("Cannot find component \"" + _for + "\" in view.");
+            else
+                return component.getClientId(facesContext);
+        } else {
+            return draggable.getParent().getClientId(facesContext);
+        }
+    }
 }
