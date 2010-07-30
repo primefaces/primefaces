46a71dffe615dbd162eb875594ffb3a17629ae94
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2010-07-30 16:15:19 +0000

    - Migrated terminal to standard JSF 2.0 ajax apis and themeroller integration, also added focus client side method to be used in cases like dialog integration.

--- src/main/java/org/primefaces/component/terminal/TerminalRenderer.java
+++ src/main/java/org/primefaces/component/terminal/TerminalRenderer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009 Prime Technology.
+ * Copyright 2010 Prime Technology.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,20 +23,23 @@ import javax.faces.context.FacesContext;
 import javax.faces.context.ResponseWriter;
 
 import org.primefaces.renderkit.CoreRenderer;
-import org.primefaces.renderkit.PartialRenderer;
 import org.primefaces.util.ComponentUtils;
 
-public class TerminalRenderer extends CoreRenderer implements PartialRenderer {
+public class TerminalRenderer extends CoreRenderer {
 
 	@Override
 	public void encodeEnd(FacesContext facesContext, UIComponent component) throws IOException {
 		Terminal terminal = (Terminal) component;
-		
-		encodeMarkup(facesContext, terminal);
-		encodeScript(facesContext, terminal);
+
+        if(facesContext.getExternalContext().getRequestParameterMap().containsKey(terminal.getClientId(facesContext))) {
+            handleCommand(facesContext, component);
+        } else {
+            encodeMarkup(facesContext, terminal);
+            encodeScript(facesContext, terminal);
+        }
 	}
 
-	private void encodeMarkup(FacesContext facesContext, Terminal terminal) throws IOException {
+	protected void encodeMarkup(FacesContext facesContext, Terminal terminal) throws IOException {
 		ResponseWriter writer = facesContext.getResponseWriter();
 		String clientId = terminal.getClientId(facesContext);
 		
@@ -45,31 +48,34 @@ public class TerminalRenderer extends CoreRenderer implements PartialRenderer {
 		writer.endElement("div");
 	}
 
-	private void encodeScript(FacesContext facesContext, Terminal terminal) throws IOException {
+	protected void encodeScript(FacesContext facesContext, Terminal terminal) throws IOException {
 		ResponseWriter writer = facesContext.getResponseWriter();
 		String clientId = terminal.getClientId(facesContext);
 		String var = createUniqueWidgetVar(facesContext, terminal);
 		
 		writer.startElement("script", null);
 		writer.writeAttribute("type", "text/javascript", null);
-
+        
 		writer.write(var + " = new PrimeFaces.widget.Terminal('" + clientId + "', {");
 		writer.write("PS1:'" + terminal.getPrompt() + "'");
-		writer.write(",clientId:'" + clientId + "'");
+		writer.write(",id:'" + clientId + "'");
 		writer.write(",url:'" + getActionURL(facesContext) + "'");
 		writer.write(",formId:'" + ComponentUtils.findParentForm(facesContext, terminal).getClientId(facesContext) + "'");
+
   		if(terminal.getWelcomeMessage() != null) writer.write(",WELCOME_MESSAGE:'" + terminal.getWelcomeMessage() + "'");
 		if(terminal.getWidth() != null) writer.write(",WIDTH:'" + terminal.getWidth() + "'");
 		if(terminal.getHeight() != null) writer.write(",HEIGHT:'" + terminal.getHeight() + "'");
+
 		writer.write("});");
 		
 		writer.endElement("script");
 	}
 
-	public void encodePartially(FacesContext facesContext, UIComponent component) throws IOException {
+	protected void handleCommand(FacesContext facesContext, UIComponent component) throws IOException {
 		ResponseWriter writer = facesContext.getResponseWriter();
 		Terminal terminal = (Terminal) component;
-		String argsParam = facesContext.getExternalContext().getRequestParameterMap().get("args");
+        String clientId = terminal.getClientId(facesContext);
+		String argsParam = facesContext.getExternalContext().getRequestParameterMap().get(clientId + "_args");
 		String tokens[] = argsParam.split(",");
 		String command = tokens[0];
 		String[] args;
@@ -79,8 +85,9 @@ public class TerminalRenderer extends CoreRenderer implements PartialRenderer {
 				args[t - 1] = tokens[t];
 			}
 		}
-		else
+		else {
 			args = new String[0];
+        }
 		
 		MethodExpression commandHandler = terminal.getCommandHandler();
 		String result = (String) commandHandler.invoke (facesContext.getELContext(), new Object[]{command, args});
