7ae9afe8240be60f2078321a5a800e71df36649b
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2010-08-04 20:22:06 +0000

    Migrated default skin to standard jsf 2.0 resource apis

--- primefaces/trunk/src/main/java-templates/org/primefaces/component/resource/ResourceTemplate.java
+++ primefaces/trunk/src/main/java-templates/org/primefaces/component/resource/ResourceTemplate.java
@@ -8,12 +8,3 @@ import org.primefaces.component.resource.Resource;
 	public String toString() {
 		return getName();
 	}
-	
-	@Override
-	public void processEvent(ComponentSystemEvent event) throws AbortProcessingException {
-		FacesContext facesContext = getFacesContext();
-		javax.faces.component.UIViewRoot viewroot = facesContext.getViewRoot();
-		if(!resourceExists(facesContext, getName())) {
-			viewroot.addComponentResource(facesContext, this, "head");
-		}
-	}
