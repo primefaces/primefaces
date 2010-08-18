e9d96a3026cfe2de2a4da4af3aa21e57d654933f
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2010-08-18 23:49:25 +0000

    Added scrollable datatable

--- primefaces/trunk/src/main/resources/META-INF/resources/primefaces/datatable/datatable.js
+++ primefaces/trunk/src/main/resources/META-INF/resources/primefaces/datatable/datatable.js
@@ -26,6 +26,10 @@ PrimeFaces.widget.DataTable = function(id, cfg) {
     if(this.cfg.expansion) {
         this.setupExpansionEvents();
     }
+
+    if(this.cfg.scrollable) {
+        this.setupScrolling();
+    }
 }
 
 /**
@@ -123,6 +127,79 @@ PrimeFaces.widget.DataTable.prototype.setupExpansionEvents = function() {
             });
 }
 
+/**
+ * Initialize data scrolling, for live scrolling listens scroll event to load data dynamically
+ */
+PrimeFaces.widget.DataTable.prototype.setupScrolling = function() {
+    this.scrollOffset = this.cfg.scrollStep;
+    this.shouldLiveScroll = true;
+    jQuery(this.jqId + ' table').tableScroll({height:this.cfg.height});
+
+    var _self = this;
+    if(this.cfg.liveScroll) {
+        
+        jQuery(this.jqId + ' .ui-scrollable-datatable-container').scroll(function() {
+
+            if(_self.shouldLiveScroll) {
+                var viewport = jQuery(this);
+                var scrollTop = viewport.attr('scrollTop'),
+                scrollHeight = viewport.attr('scrollHeight'),
+                viewportHeight = viewport.attr('clientHeight');
+
+                if(scrollTop >= (scrollHeight - (viewportHeight + 20))) {
+                    _self.loadLiveRows();
+                }
+            }
+            
+        });
+        
+    }
+}
+
+/**
+ * Loads rows on-the-fly when scrolling live
+ */
+PrimeFaces.widget.DataTable.prototype.loadLiveRows = function() {
+    var options = {
+        source: this.id,
+        process: this.id,
+        update: this.id,
+        formId: this.cfg.formId
+    },
+    _self = this;
+
+    options.onsuccess = function(responseXML) {
+        var xmlDoc = responseXML.documentElement,
+        updates = xmlDoc.getElementsByTagName("update");
+
+        for(var i=0; i < updates.length; i++) {
+            var id = updates[i].attributes.getNamedItem("id").nodeValue,
+            content = updates[i].firstChild.data;
+
+            if(id == _self.id){
+                jQuery(_self.jqId + ' table.ui-scrollable-datatable-body tr:last').after(content);
+
+                _self.scrollOffset += _self.cfg.scrollStep;
+
+                //Disable scroll if there is no more data left
+                if(_self.scrollOffset == _self.cfg.scrollLimit) {
+                    _self.shouldLiveScroll = false;
+                }
+            }
+            else {
+                PrimeFaces.ajax.AjaxUtils.updateElement(id, content, this.ajaxContext);
+            }
+        }
+
+        return false;
+    };
+
+    var params = {};
+    params[this.id + "_scrolling"] = true;
+    params[this.id + "_scrollOffset"] = this.scrollOffset;
+
+    PrimeFaces.ajax.AjaxRequest(this.cfg.url, options, params);
+}
 
 /**
  * Ajax pagination
@@ -543,3 +620,176 @@ PrimeFaces.widget.DataTable.prototype.clearSelection = function() {
     
     jQuery(this.selectionHolder).val('');
 }
+
+/*
+
+Copyright (c) 2009 Dimas Begunoff, http://www.farinspace.com
+
+Licensed under the MIT license
+http://en.wikipedia.org/wiki/MIT_License
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
+;(function($){
+
+	var scrollbarWidth = 0;
+
+	// http://jdsharp.us/jQuery/minute/calculate-scrollbar-width.php
+	function getScrollbarWidth()
+	{
+		if (scrollbarWidth) return scrollbarWidth;
+		var div = $('<div style="width:50px;height:50px;overflow:hidden;position:absolute;top:-200px;left:-200px;"><div style="height:100px;"></div></div>');
+		$('body').append(div);
+		var w1 = $('div', div).innerWidth();
+		div.css('overflow-y', 'auto');
+		var w2 = $('div', div).innerWidth();
+		$(div).remove();
+		scrollbarWidth = (w1 - w2);
+		return scrollbarWidth;
+	}
+
+	$.fn.tableScroll = function(options)
+	{
+		if (options == 'undo')
+		{
+			var container = $(this).parent().parent();
+			container.find('.ui-scrollable-datatable-header thead').prependTo(this);
+			container.find('.ui-scrollable-datatable-footer tfoot').appendTo(this);
+			container.before(this);
+			container.empty();
+			return;
+		}
+
+		var settings = $.extend({},$.fn.tableScroll.defaults,options);
+
+		settings.scrollbarWidth = getScrollbarWidth();
+
+		this.each(function()
+		{
+			var flush = settings.flush;
+
+			var tb = $(this).addClass('ui-scrollable-datatable-body');
+
+			var wrapper = $('<div class="ui-scrollable-datatable-container"></div>').insertBefore(tb).append(tb);
+
+			// check for a predefined container
+			if (!wrapper.parent('div').hasClass(settings.containerClass))
+			{
+				$('<div></div>').addClass(settings.containerClass).insertBefore(wrapper).append(wrapper);
+			}
+
+			var width = settings.width ? settings.width : tb.outerWidth();
+
+			wrapper.css
+			({
+				'width': width+'px',
+				'height': settings.height+'px',
+				'overflow': 'auto'
+			});
+
+			tb.css('width',width+'px');
+
+			// with border difference
+			var wrapper_width = wrapper.outerWidth();
+			var diff = wrapper_width-width;
+
+			// assume table will scroll
+			wrapper.css({width:((width-diff)+settings.scrollbarWidth)+'px'});
+			tb.css('width',(width-diff)+'px');
+
+			if (tb.outerHeight() <= settings.height)
+			{
+				wrapper.css({height:'auto',width:(width-diff)+'px'});
+				flush = false;
+			}
+
+			// using wrap does not put wrapper in the DOM right
+			// away making it unavailable for use during runtime
+			// tb.wrap(wrapper);
+
+			// possible speed enhancements
+			var has_thead = $('thead',tb).length ? true : false ;
+			var has_tfoot = $('tfoot',tb).length ? true : false ;
+			var thead_tr_first = $('thead tr:first',tb);
+			var tbody_tr_first = $('tbody tr:first',tb);
+			var tfoot_tr_first = $('tfoot tr:first',tb);
+
+			// remember width of last cell
+			var w = 0;
+
+			$('th, td',thead_tr_first).each(function(i)
+			{
+				w = $(this).width();
+
+				$('th:eq('+i+'), td:eq('+i+')',thead_tr_first).css('width',w+'px');
+				$('th:eq('+i+'), td:eq('+i+')',tbody_tr_first).css('width',w+'px');
+				if (has_tfoot) $('th:eq('+i+'), td:eq('+i+')',tfoot_tr_first).css('width',w+'px');
+			});
+
+			if (has_thead)
+			{
+				var tbh = $('<table class="ui-scrollable-datatable-header" cellspacing="0"></table>').insertBefore(wrapper).prepend($('thead',tb));
+			}
+
+			if (has_tfoot)
+			{
+				var tbf = $('<table class="ui-scrollable-datatable-footer" cellspacing="0"></table>').insertAfter(wrapper).prepend($('tfoot',tb));
+			}
+
+			if (tbh != undefined)
+			{
+				tbh.css('width',width+'px');
+
+				if (flush)
+				{
+					$('tr:first th:last, tr:first td:last',tbh).css('width',(w+settings.scrollbarWidth)+'px');
+					tbh.css('width',wrapper.outerWidth() + 'px');
+				}
+			}
+
+			if (tbf != undefined)
+			{
+				tbf.css('width',width+'px');
+
+				if (flush)
+				{
+					$('tr:first th:last, tr:first td:last',tbf).css('width',(w+settings.scrollbarWidth)+'px');
+					tbf.css('width',wrapper.outerWidth() + 'px');
+				}
+			}
+		});
+
+		return this;
+	};
+
+	// public
+	$.fn.tableScroll.defaults =
+	{
+		flush: true, // makes the last thead and tbody column flush with the scrollbar
+		width: null, // width of the table (head, body and foot), null defaults to the tables natural width
+		height: 100, // height of the scrollable area
+		containerClass: 'ui-scrollable-datatable' // the plugin wraps the table in a div with this css class
+	};
+
+})(jQuery);
