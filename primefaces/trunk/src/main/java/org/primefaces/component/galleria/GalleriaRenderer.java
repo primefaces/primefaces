dd3fc6218f1e82a03558b31275fd04e603687c14
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2010-08-27 13:48:51 +0000

    Implemented new galleria component, some skinning work will follow

---
+++ primefaces/trunk/src/main/java/org/primefaces/component/galleria/GalleriaRenderer.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2010 Prime Technology.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.primefaces.component.galleria;
+
+import java.io.IOException;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+import org.primefaces.renderkit.CoreRenderer;
+
+public class GalleriaRenderer extends CoreRenderer {
+
+    @Override
+    public void encodeBegin(FacesContext context, UIComponent component) throws IOException {
+        ResponseWriter writer = context.getResponseWriter();
+
+        writer.startElement("div", null);
+        writer.writeAttribute("id", component.getClientId(context), null);
+    }
+
+    @Override
+    public void encodeEnd(FacesContext context, UIComponent component) throws IOException {
+        ResponseWriter writer = context.getResponseWriter();
+        Galleria galleria = (Galleria) component;
+        String clientId = galleria.getClientId(context);
+
+        writer.endElement("div");
+
+        writer.startElement("script", component);
+        writer.writeAttribute("type", "text/javascript", null);
+
+		writer.write(galleria.resolveWidgetVar() + " = new PrimeFaces.widget.Galleria('" + clientId + "',{");
+
+        writer.write("});");
+        
+        writer.endElement("script");
+    }
+}
