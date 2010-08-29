161b55e6b1af0c240912714a7227e40533aa96d8
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2010-08-29 21:30:36 +0000

    Created isPagingRequest() method

--- src/main/java-templates/org/primefaces/component/datagrid/DataGridTemplate.java
+++ src/main/java-templates/org/primefaces/component/datagrid/DataGridTemplate.java
@@ -1,5 +1,9 @@
 	public static final String DATAGRID_CLASS = "ui-datagrid ui-widget";
-        public static final String CONTENT_CLASS = "ui-datagrid-content";
+    public static final String CONTENT_CLASS = "ui-datagrid-content";
 	public static final String TABLE_CLASS = "ui-datagrid-data";
 	public static final String TABLE_ROW_CLASS = "ui-datagrid-row";
 	public static final String TABLE_COLUMN_CLASS = "ui-datagrid-column";
+
+    public boolean isPagingRequest(FacesContext context) {
+        return context.getExternalContext().getRequestParameterMap().containsKey(getClientId(context) + "_ajaxPaging");
+    }
