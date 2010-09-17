3ef92a94533718b2345f6be89316eeb5112b099b
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2010-09-17 12:12:09 +0000

    I18N support for editor

--- primefaces/trunk/src/main/java/org/primefaces/component/editor/EditorRenderer.java
+++ primefaces/trunk/src/main/java/org/primefaces/component/editor/EditorRenderer.java
@@ -80,6 +80,7 @@ public class EditorRenderer extends CoreRenderer{
         if(editor.getHeight() != Integer.MIN_VALUE) writer.write(",height:" + editor.getHeight());
         if(editor.getStyleClass() != null) writer.write(",cssClass:'" + editor.getStyleClass() + "'");
         if(!editor.isSource()) writer.write(",allowSource:false");
+        if(editor.getLanguage() != null) writer.write(",lang:'" + editor.getLanguage() + "'");
 
 		writer.write("});});");
 		
