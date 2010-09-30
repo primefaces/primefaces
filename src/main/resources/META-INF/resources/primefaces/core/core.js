daf091ad3f83bc49413ae19ca24235b2594ac59b
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2010-09-30 13:00:20 +0000

    Fixed issue #1233

--- src/main/resources/META-INF/resources/primefaces/core/core.js
+++ src/main/resources/META-INF/resources/primefaces/core/core.js
@@ -44,6 +44,22 @@ PrimeFaces = {
             }
         }
     },
+
+    getCookie : function(name) {
+        var cookieValue = null;
+        if (document.cookie && document.cookie != '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = jQuery.trim(cookies[i]);
+
+                if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    },
 	
     PARTIAL_REQUEST_PARAM : "javax.faces.partial.ajax",
 
