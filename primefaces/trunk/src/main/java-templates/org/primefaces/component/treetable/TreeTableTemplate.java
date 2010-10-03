01c4e6529103e99806158ad9f4830c47df675d69
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2010-10-03 08:07:03 +0000

    Fixed issue #1296, improved treetable css structure

--- primefaces/trunk/src/main/java-templates/org/primefaces/component/treetable/TreeTableTemplate.java
+++ primefaces/trunk/src/main/java-templates/org/primefaces/component/treetable/TreeTableTemplate.java
@@ -3,8 +3,6 @@ import org.primefaces.model.TreeNode;
 import javax.faces.model.DataModel;
 
 	public final static String CONTAINER_CLASS = "ui-treetable ui-widget ui-widget-content";
-	public final static String HEADER_CLASS = "ui-treetable-header";
-	public final static String HEADER_LABEL_CLASS = "ui-treetable-header-label";
 	public final static String DATA_CLASS = "ui-treetable-data";
 
 	public String getEscapedClientId(FacesContext facesContext) {
