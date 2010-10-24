ac2781da2d5f38030c55123facb807265901c872
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2010-10-24 09:45:06 +0000

    Fixed issue #1229

--- primefaces/trunk/src/main/java/org/primefaces/component/datatable/DataTableRenderer.java
+++ primefaces/trunk/src/main/java/org/primefaces/component/datatable/DataTableRenderer.java
@@ -657,7 +657,7 @@ public class DataTableRenderer extends CoreRenderer {
             if(table.getOnRowSelectComplete() != null) writer.write(",onRowSelectComplete:function(xhr, status, args) {" + table.getOnRowSelectComplete() + "}");
         }
 
-        if(table.getRowSelectListener() != null) {
+        if(table.getRowUnselectListener() != null) {
             writer.write(",instantUnselect:true");
             
             if(table.getOnRowUnselectUpdate() != null) {
