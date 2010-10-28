6eb9ace17d310b797120a86b230f9d30dd7393e0
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2010-10-28 21:02:52 +0000

    Implemented #1363 and #1370

--- src/main/java/org/primefaces/component/slider/SliderRenderer.java
+++ src/main/java/org/primefaces/component/slider/SliderRenderer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009 Prime Technology.
+ * Copyright 2010 Prime Technology.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,17 +16,33 @@
 package org.primefaces.component.slider;
 
 import java.io.IOException;
-
+import java.util.Map;
+import javax.el.MethodExpression;
 import javax.faces.FacesException;
+
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.context.ResponseWriter;
+import org.primefaces.event.SlideEndEvent;
 
 import org.primefaces.renderkit.CoreRenderer;
 import org.primefaces.util.ComponentUtils;
 
 public class SliderRenderer extends CoreRenderer{
-	
+
+    @Override
+    public void decode(FacesContext context, UIComponent component) {
+        Map<String,String> params = context.getExternalContext().getRequestParameterMap();
+        Slider slider = (Slider) component;
+        String clientId = slider.getClientId(context);
+
+        if(params.containsKey(clientId + "_ajaxSlide")) {
+            int value = Integer.parseInt(params.get(clientId + "_ajaxSlideValue"));
+            slider.queueEvent(new SlideEndEvent(slider, value));
+        }
+    }
+
+    @Override
 	public void encodeEnd(FacesContext facesContext, UIComponent component) throws IOException {
 		Slider slider = (Slider) component;
 		
@@ -34,28 +50,26 @@ public class SliderRenderer extends CoreRenderer{
 		encodeScript(facesContext, slider);
 	}
 	
-	protected void encodeMarkup(FacesContext facesContext, Slider slider) throws IOException {
-		ResponseWriter writer = facesContext.getResponseWriter();
-		String clientId = slider.getClientId(facesContext);
+	protected void encodeMarkup(FacesContext context, Slider slider) throws IOException {
+		ResponseWriter writer = context.getResponseWriter();
+		String clientId = slider.getClientId(context);
 		
 		writer.startElement("div", slider);
 		writer.writeAttribute("id", clientId , "id");
-		if(slider.getStyle() != null) 
-			writer.writeAttribute("style", slider.getStyle() , null);
-		if(slider.getStyleClass() != null) 
-			writer.writeAttribute("class", slider.getStyleClass(), null);
+		if(slider.getStyle() != null)  writer.writeAttribute("style", slider.getStyle() , null);
+		if(slider.getStyleClass() != null) writer.writeAttribute("class", slider.getStyleClass(), null);
 		
 		writer.endElement("div");
 	}
 
-	protected void encodeScript(FacesContext facesContext, Slider slider) throws IOException {
-		ResponseWriter writer = facesContext.getResponseWriter();
-		String clientId = slider.getClientId(facesContext);
-		String input = getTarget(facesContext, slider, slider.getFor());
-		String output = getTarget(facesContext, slider, slider.getDisplay());
+	protected void encodeScript(FacesContext context, Slider slider) throws IOException {
+		ResponseWriter writer = context.getResponseWriter();
+		String clientId = slider.getClientId(context);
+		String input = getTarget(context, slider, slider.getFor());
+		String output = getTarget(context, slider, slider.getDisplay());
 		
 		int value = 0;
-		String stringValue = ComponentUtils.getStringValueToRender(facesContext, slider);
+		String stringValue = ComponentUtils.getStringValueToRender(context, slider);
 		if(!isValueBlank(stringValue)) {
 			value = Integer.parseInt(stringValue);
 		}
@@ -74,19 +88,43 @@ public class SliderRenderer extends CoreRenderer{
 		
 		if(slider.isDisabled()) writer.write(",disabled:true");
 		if(output != null) writer.write(",output:'" + output + "'");
+        if(slider.getOnSlideStart() != null) writer.write(",onSlideStart:function(event, ui) {" + slider.getOnSlideStart() + "}");
+        if(slider.getOnSlide() != null) writer.write(",onSlide:function(event, ui) {" + slider.getOnSlide() + "}");
+        if(slider.getOnSlideEnd() != null) writer.write(",onSlideEnd:function(event, ui) {" + slider.getOnSlideEnd() + "}");
+
+        //Ajax Slider configuration
+        MethodExpression me = slider.getSlideEndListener();
+
+        if(me != null) {
+            UIComponent form = ComponentUtils.findParentForm(context, slider);
+            if(form == null) {
+                throw new FacesException("Slider: '" + clientId + "' needs to be enclosed in a form when using a slideEndListener");
+            }
+
+            writer.write(",ajaxSlide:true");
+            writer.write(",formId:'" + form.getClientId(context) + "'");
+            writer.write(",url:'" + getActionURL(context) + "'");
+            
+            String onSlideEndUpdate = slider.getOnSlideEndUpdate();
+            if(onSlideEndUpdate != null)
+                writer.write(",onSlideEndUpdate:'" + ComponentUtils.findClientIds(context, slider, onSlideEndUpdate) + "'");
+        }
 		
 		writer.write("});");
 	
 		writer.endElement("script");
 	}
 	
-	protected String getTarget(FacesContext facesContext, Slider slider, String target) {
+	protected String getTarget(FacesContext context, Slider slider, String target) {
 		if(target == null) {
 			return null;
 		} else {
 			UIComponent targetComponent = slider.findComponent(target);
+            if(targetComponent == null) {
+                throw new FacesException("Cannot find slider target component '" + target + "' in view");
+            }
 			
-			return targetComponent.getClientId(facesContext);
+			return targetComponent.getClientId(context);
 		}
 	}
 }
