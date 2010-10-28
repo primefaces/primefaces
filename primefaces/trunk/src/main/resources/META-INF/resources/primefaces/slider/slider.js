4fdfc98968a253ad57a5d7a14f49603af37aea84
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2010-10-28 21:02:52 +0000

    Implemented #1363 and #1370

--- primefaces/trunk/src/main/resources/META-INF/resources/primefaces/slider/slider.js
+++ primefaces/trunk/src/main/resources/META-INF/resources/primefaces/slider/slider.js
@@ -1,23 +1,82 @@
+/**
+ * PrimeFaces Slider Widget
+ */
 PrimeFaces.widget.Slider = function(id, cfg) {
 	this.id = id;
 	this.cfg = cfg;
-	this.jqId = PrimeFaces.escapeClientId(id);
-	this.jqInput = PrimeFaces.escapeClientId(this.cfg.input);
+    this.jq = jQuery(PrimeFaces.escapeClientId(this.id));
+	this.input = jQuery(PrimeFaces.escapeClientId(this.cfg.input));
 	if(this.cfg.output) {
-		this.jqOutput = PrimeFaces.escapeClientId(this.cfg.output);
+		this.output = jQuery(PrimeFaces.escapeClientId(this.cfg.output));
 	}
-	
-	jQuery(this.jqId).slider(this.cfg);
-	
-	jQuery(this.jqId).bind("slide", {slider:this}, this.onSlide);
+    var _self = this;
+    
+    //Create slider
+	this.jq.slider(this.cfg);
+
+    //Slide handler
+	this.jq.bind('slide', function(event, ui) {
+        _self.onSlide(event, ui);
+    });
+
+    //Slide start handler
+    if(this.cfg.onSlideStart) {
+        this.jq.bind('slidestart', function(event, ui) {_self.cfg.onSlideStart.call(this, event, ui);});
+    }
+
+    //Slide end handler
+    this.jq.bind('slidestop', function(event, ui) {_self.onSlideEnd(event, ui);});
 }
 
 PrimeFaces.widget.Slider.prototype.onSlide = function(event, ui) {
-	var s = event.data.slider;
+    //User callback
+    if(this.cfg.onSlide) {
+        this.cfg.onSlide.call(this, event, ui);
+    }
+
+    //Update input and output(if defined)
+	this.input.val(ui.value);
 	
-	jQuery(s.jqInput).val(ui.value);
-	
-	if(s.jqOutput) {
-		jQuery(s.jqOutput).html(ui.value);
+	if(this.output) {
+		this.output.html(ui.value);
 	}
+}
+
+PrimeFaces.widget.Slider.prototype.onSlideEnd = function(event, ui) {
+    //User callback
+    if(this.cfg.onSlideEnd) {
+        this.cfg.onSlideEnd.call(this, event, ui);
+    }
+
+    var options = {
+        source : this.id,
+        process: this.id + ' ' + this.cfg.input,
+        formId: this.cfg.formId
+    };
+
+    if(this.cfg.onSlideEndUpdate) {
+        options.update = this.cfg.onSlideEndUpdate;
+    }
+
+    var params = {};
+    params[this.id + '_ajaxSlide'] = true;
+    params[this.id + '_ajaxSlideValue'] = ui.value;
+
+    PrimeFaces.ajax.AjaxRequest(this.cfg.url, options, params);
+}
+
+PrimeFaces.widget.Slider.prototype.getValue = function() {
+    return this.jq.slider('value');
+}
+
+PrimeFaces.widget.Slider.prototype.setValue = function(value) {
+    this.jq.slider('value', value);
+}
+
+PrimeFaces.widget.Slider.prototype.enable = function() {
+    this.jq.slider('enable');
+}
+
+PrimeFaces.widget.Slider.prototype.disable = function() {
+    this.jq.slider('disable');
 }
