8125d465ce0bd19e3f73a220cd5f2ae2924c656a
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2010-11-17 10:51:22 +0000

    Inplace refactor

--- src/main/resources/META-INF/resources/primefaces/inplace/inplace.js
+++ src/main/resources/META-INF/resources/primefaces/inplace/inplace.js
@@ -1,31 +1,53 @@
-PrimeFaces.widget.Inplace = function(clientId, options) {
-	var jqId = PrimeFaces.escapeClientId(clientId),
-	displayId = jqId + "_display",
-	contentId = jqId + "_content";
+/**
+ * PrimeFaces Inplace Widget
+ */
+PrimeFaces.widget.Inplace = function(id, cfg) {
+    this.id = id;
+    this.cfg = cfg;
+	this.jqId = PrimeFaces.escapeClientId(id);
+    this.display = jQuery(this.jqId + '_display');
+    this.content = jQuery(this.jqId + '_content');
+
+    var _self = this;
 	
-	if(!options.disabled) { 
-		jQuery(displayId).click(function(){
-			if(options.effect == 'fade') {
-				jQuery(displayId).fadeOut(options.effectSpeed, 
-						function(){
-							jQuery(contentId).fadeIn(options.effectSpeed);
-						});
-			} else if(options.effect == 'slide') {
-					jQuery(displayId).slideUp(options.effectSpeed, 
-						function(){
-							jQuery(contentId).slideDown(options.effectSpeed);
-					});
-			} else if(options.effect == 'none') {
-					jQuery(displayId).hide(); 
-					jQuery(contentId).show();
-			}
-			
-			});
-	
-		 	jQuery(displayId).mouseover(function(){
-				jQuery(this).toggleClass("pf-inplace-highlight");
-			}).mouseout(function(){
-				jQuery(this).toggleClass("pf-inplace-highlight");		
-			 });
+	if(!cfg.disabled) {
+		this.display.click(function(){
+            _self.show();
+        });
+        
+        this.display.mouseover(function(){
+            jQuery(this).toggleClass("ui-state-highlight");
+        }).mouseout(function(){
+            jQuery(this).toggleClass("ui-state-highlight");
+        });
 	}
+}
+
+PrimeFaces.widget.Inplace.prototype.show = function() {
+    this.toggle(this.content, this.display);
+}
+
+PrimeFaces.widget.Inplace.prototype.hide = function() {
+    this.toggle(this.display, this.content);
+}
+
+PrimeFaces.widget.Inplace.prototype.toggle = function(elToShow, elToHide) {
+    var _self = this;
+
+    if(this.cfg.effect == 'fade') {
+        elToHide.fadeOut(this.cfg.effectSpeed,
+            function(){
+                elToShow.fadeIn(_self.cfg.effectSpeed);
+            });
+    }
+    else if(this.cfg.effect == 'slide') {
+            elToHide.slideUp(this.cfg.effectSpeed,
+                function(){
+                    elToShow.slideDown(_self.cfg.effectSpeed);
+            });
+    }
+    else if(this.cfg.effect == 'none') {
+            elToHide.hide();
+            elToShow.show();
+    }
 }
