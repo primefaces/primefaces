d473c61376304f1fe815f8a66cf8bd4d6c5b54ab
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2011-02-07 11:39:32 +0000

    More options to scrollPanel

--- primefaces/trunk/src/main/resources/META-INF/resources/primefaces/scrollpanel/scrollpanel.js
+++ primefaces/trunk/src/main/resources/META-INF/resources/primefaces/scrollpanel/scrollpanel.js
@@ -5,5 +5,21 @@ PrimeFaces.widget.ScrollPanel = function(id, cfg) {
     this.jqId = PrimeFaces.escapeClientId(this.id);
     this.jq = jQuery(this.jqId);
 
+    this.cfg.hScroller = {
+        scrollMode : this.cfg.scrollMode
+    };
+
+    this.cfg.vScroller = {
+        scrollMode : this.cfg.scrollMode
+    };
+
+    this.cfg.animationOptions = {
+        easing : this.cfg.easing
+    };
+
     this.jq.wijsuperpanel(this.cfg);
+}
+
+PrimeFaces.widget.ScrollPanel.prototype.scrollTo = function(x, y) {
+    this.jq.wijsuperpanel('scrollTo', x, y);
 }
