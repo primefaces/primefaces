8f1a3324f6d8c6f030cb124ef63c6752f9b4b931
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2011-02-08 13:42:53 +0000

    Drop-in of new selectOneRadio component, quite functional, clientBehavior support pending.

---
+++ primefaces/trunk/src/main/java/org/primefaces/component/selectoneradio/SelectOneRadioRenderer.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2009-2010 Prime Technology.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.primefaces.component.selectoneradio;
+
+import java.io.IOException;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+import javax.faces.convert.Converter;
+import org.primefaces.renderkit.InputRenderer;
+
+public class SelectOneRadioRenderer extends InputRenderer {
+
+    @Override
+    public void decode(FacesContext context, UIComponent component) {
+        SelectOneRadio radio = (SelectOneRadio) component;
+
+        if(radio.isDisabled() || radio.isReadonly()) {
+            return;
+        }
+
+        decodeBehaviors(context, radio);
+
+        String clientId = radio.getClientId(context);
+        String value = context.getExternalContext().getRequestParameterMap().get(clientId);
+
+        if(value != null) {
+            radio.setSubmittedValue(value);
+        }
+    }
+
+    @Override
+    public void encodeEnd(FacesContext context, UIComponent component) throws IOException {
+        SelectOneRadio radio = (SelectOneRadio) component;
+
+        encodeMarkup(context, radio);
+        encodeScript(context, radio);
+    }
+
+    protected void encodeMarkup(FacesContext context, SelectOneRadio radio) throws IOException {
+        ResponseWriter writer = context.getResponseWriter();
+        String clientId = radio.getClientId(context);
+        String layout = radio.getLayout();
+        if(layout == null) {
+            layout = "lineDirection";
+        }
+
+        writer.startElement("span", radio);
+        writer.writeAttribute("id", clientId, "id");
+        if(layout.equals("lineDirection")) {
+            writer.writeAttribute("class", "wijmo-wijradio-horizontal", null);
+        }
+
+        encodeSelectItems(context, radio);
+
+        writer.endElement("span");
+    }
+
+    protected void encodeScript(FacesContext context, SelectOneRadio radio) throws IOException {
+        ResponseWriter writer = context.getResponseWriter();
+        String clientId = radio.getClientId(context);
+
+        writer.startElement("script", null);
+		writer.writeAttribute("type", "text/javascript", null);
+
+        writer.write(radio.resolveWidgetVar() + " = new PrimeFaces.widget.SelectOneRadio('" + clientId + "',{");
+
+        writer.write("});");
+
+        writer.endElement("script");
+    }
+
+    @Override
+    protected void encodeOption(FacesContext context, UIInput component, Object componentValue, Converter converter, String label, Object value) throws IOException {
+        ResponseWriter writer = context.getResponseWriter();
+        String formattedValue = formatOptionValue(context, component, converter, value);
+        String clientId = component.getClientId(context);
+        String containerClientId = component.getContainerClientId(context);
+
+        writer.startElement("input", null);
+        writer.writeAttribute("id", containerClientId, null);
+        writer.writeAttribute("name", clientId, null);
+        writer.writeAttribute("type", "radio", null);
+        writer.writeAttribute("value", formattedValue, null);
+        if(componentValue != null && componentValue.equals(value)) {
+            writer.writeAttribute("checked", "checked", null);
+        }
+        writer.endElement("input");
+
+        writer.startElement("label", null);
+        writer.writeAttribute("for", containerClientId, null);
+        writer.write(label);
+        writer.endElement("label");
+    }
+}
--- primefaces/trunk/src/main/resources/META-INF/resources/primefaces/wijmo/wijmo.js
+++ primefaces/trunk/src/main/resources/META-INF/resources/primefaces/wijmo/wijmo.js
@@ -4525,3 +4525,168 @@ __wijReadOptionEvents = function (eventsArr, widgetInstance) {
 		}
 	});
 } (jQuery));
+
+/*globals jQuery*/
+/*
+ *
+ * Wijmo Library 1.1.2
+ * http://wijmo.com/
+ *
+ * Copyright(c) ComponentOne, LLC.  All rights reserved.
+ *
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ * licensing@wijmo.com
+ * http://www.wijmo.com/license
+ *
+ * * Wijmo RadioButtonDecorator widget.
+ *
+ * Depends:
+ *   jquery-1.4.2.js
+ *	jquery.ui.core.js
+ *	jquery.ui.widget.js
+ *
+ */
+(function ($) {
+	"use strict";
+	var radiobuttonId = 0;
+	$.widget("wijmo.wijradio", {
+		_radiobuttonPre: "wijmo-wijradio",
+		_create: function () {
+			var self = this,
+				ele = self.element,
+				radiobuttonElement, label, targetLabel, boxElement, iconElement;
+
+			if (ele.is(":radio")) {
+				if (!ele.attr("id")) {
+					ele.attr("id", "wijmo-radio-" + radiobuttonId);
+					radiobuttonId += 1;
+				}
+				if (ele.parent().is("label")) {
+					radiobuttonElement = ele.parent().wrap("<div class='" +
+					self._radiobuttonPre + "-inputwrapper'></div>").parent()
+					.wrap("<div></div>").parent()
+					.addClass(self._radiobuttonPre + " ui-widget");
+					label = ele.parent();
+					label.attr("for", ele.attr("id"));
+					radiobuttonElement.find("." + self._radiobuttonPre + "-inputwrapper")
+					.append(ele);
+					radiobuttonElement.append(label);
+
+				}
+				else {
+					radiobuttonElement = ele
+					.wrap("<div class='" + self._radiobuttonPre + "-inputwrapper'></div>")
+					.parent().wrap("<div></div>").parent()
+					.addClass(self._radiobuttonPre + " ui-widget");
+				}
+				targetLabel = $("label[for='" + ele.attr("id") + "']");
+				if (targetLabel.length > 0) {
+					radiobuttonElement.append(targetLabel);
+					targetLabel.attr("labelsign", "wij");
+					//targetLabel.attr("tabindex", 0);
+				}
+				boxElement = $("<div class='" + self._radiobuttonPre +
+				"-box ui-widget ui-state-default ui-corner-all'><span class='" +
+				self._radiobuttonPre + "-icon'></span></div>");
+				iconElement = boxElement.children("." + self._radiobuttonPre + "-icon");
+				radiobuttonElement.append(boxElement);
+				iconElement.addClass("ui-icon ui-icon-radio-on");
+				ele.data("iconElement", iconElement);
+				ele.data("boxElement", boxElement);
+				if (ele.is(":disabled")) {
+					self._setOption("disabled", true);
+				}
+
+				boxElement.removeClass(self._radiobuttonPre + "-relative")
+				.attr("role", "radio")
+				.bind("mouseover", function () {
+					ele.mouseover();
+				}).bind("mouseout", function () {
+					ele.mouseout();
+				});
+				if (targetLabel.length === 0 || targetLabel.html() === "") {
+					boxElement.addClass(self._radiobuttonPre + "-relative");
+				}
+				self._setDefaul();
+				//			boxElement.css("margin-top","9px");
+
+				ele.bind("click.checkbox", function () {
+					ele.focus();
+					self._refresh();
+				}).bind("focus.checkbox", function () {
+					if (self.options.disabled) {
+						return;
+					}
+					boxElement.removeClass("ui-state-default").addClass("ui-state-focus");
+				}).bind("blur.checkbox", function () {
+					if (self.options.disabled) {
+						return;
+					}
+					boxElement.removeClass("ui-state-focus").not(".ui-state-hover")
+					.addClass("ui-state-default");
+				});
+
+				radiobuttonElement.click(function () {
+					if (targetLabel.length === 0 || targetLabel.html() === "") {
+						ele.attr("checked", true).focus();
+						self._refresh();
+						ele.change();
+					}
+
+				});
+
+				radiobuttonElement.bind("mouseover.checkbox", function () {
+					if (self.options.disabled) {
+						return;
+					}
+					boxElement.removeClass("ui-state-default").addClass("ui-state-hover");
+				}).bind("mouseout.checkbox", function () {
+					if (self.options.disabled) {
+						return;
+					}
+					boxElement.removeClass("ui-state-hover").not(".ui-state-focus")
+					.addClass("ui-state-default");
+				});
+
+			}
+		},
+
+		_setDefaul: function () {
+			if (this.element.attr("checked")) {
+				this.element.parents(".wijmo-wijradio")
+				.find("." + this._radiobuttonPre + "-box").children()
+				.removeClass("ui-icon-radio-on ui-icon-radio-off")
+				.addClass("ui-icon-radio-off");
+				this.element.data("boxElement").removeClass("ui-state-default")
+				.addClass("ui-state-active").attr("aria-checked", true);
+			}
+		},
+
+		_refresh: function () {
+			var name = this.element.attr("name"), self = this;
+			$("[name=" + name + "]").each(function (i, n) {
+				$(n).parents(".wijmo-wijradio")
+				.find("." + self._radiobuttonPre + "-box").children()
+				.removeClass("ui-icon-radio-on ui-icon-radio-off")
+				.addClass("ui-icon-radio-on");
+				$(n).parents(".wijmo-wijradio")
+				.find("." + self._radiobuttonPre + "-box")
+				.removeClass("ui-state-active").addClass("ui-state-default")
+				.attr("aria-checked", false);
+			});
+			if (self.element.is(":checked")) {
+				self.element.data("iconElement")
+				.removeClass("ui-icon-radio-on").addClass("ui-icon-radio-off");
+				self.element.data("boxElement").removeClass("ui-state-default")
+				.addClass("ui-state-active").attr("aria-checked", true);
+			}
+		},
+		destroy: function () {
+			var self = this, boxelement = self.element.parent().parent();
+			boxelement.children("div." + self._radiobuttonPre + "-box").remove();
+			self.element.unwrap();
+			self.element.unwrap();
+			$.Widget.prototype.destroy.apply(self);
+		}
+	});
+} (jQuery));
