0b72eceb5cad95829e4e6be0887304f38d1a493a
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2011-02-09 14:44:38 +0000

    Implemented new selectOneListbox component.

---
+++ src/main/java-templates/org/primefaces/component/selectonelistbox/SelectOneListboxTemplate.java
@@ -0,0 +1,4 @@
+import javax.faces.component.UINamingContainer;
+
+    public static final String CONTAINER_CLASS = "ui-selectonelistbox ui-widget ui-widget-content ui-corner-all";
+    public static final String ITEM_CLASS = "ui-selectonelistbox-item ui-corner-all";
