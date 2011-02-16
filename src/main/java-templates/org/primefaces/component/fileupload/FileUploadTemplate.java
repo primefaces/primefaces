9776f16944ec93d498e6c56168f9af3a8f4fd736
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2011-02-16 16:34:10 +0000

    Added default controls (upload-cancel) for fileUpload.

--- src/main/java-templates/org/primefaces/component/fileupload/FileUploadTemplate.java
+++ src/main/java-templates/org/primefaces/component/fileupload/FileUploadTemplate.java
@@ -1,24 +1,10 @@
-import javax.faces.component.UINamingContainer;
-
-import org.primefaces.event.FileUploadEvent;
-
-	private String inputFileId;
-
-	public void broadcast(javax.faces.event.FacesEvent event) throws javax.faces.event.AbortProcessingException {
+    public void broadcast(javax.faces.event.FacesEvent event) throws javax.faces.event.AbortProcessingException {
 		super.broadcast(event);
 		
 		FacesContext facesContext = FacesContext.getCurrentInstance();
 		MethodExpression me = getFileUploadListener();
 		
-		if (me != null) {
+		if (me != null && event instanceof org.primefaces.event.FileUploadEvent) {
 			me.invoke(facesContext.getELContext(), new Object[] {event});
 		}
-	}
-	
-	String getInputFileId(FacesContext facesContext) {
-		if(inputFileId == null) {
-			inputFileId = this.getClientId(facesContext).replaceAll(String.valueOf(UINamingContainer.getSeparatorChar(facesContext)), "_") + "_file";
-		}
-		
-		return inputFileId;
 	}
