fcd81de35e5750ac999b2ff06852dcd1e94667fe
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2011-02-19 19:26:48 +0000

    Implemented tagCloud component

---
+++ primefaces/trunk/src/main/resources/META-INF/resources/primefaces/tagcloud/tagcloud.js
@@ -0,0 +1,25 @@
+/**
+ * PrimeFaces TagCloud Widget
+ */
+PrimeFaces.widget.TagCloud = function(id, cfg) {
+    this.id = id;
+    this.cfg = cfg;
+    this.jq = jQuery(PrimeFaces.escapeClientId(this.id));
+
+    this.jq.find('li').mouseover(function() {
+        jQuery(this).addClass('ui-state-hover');
+    }).mouseout(function() {
+        jQuery(this).removeClass('ui-state-hover');
+    })
+
+    if(this.cfg.sortable) {
+        this.jq.children('ul').sortable({
+            placeholder: 'ui-state-highlight'
+            ,forcePlaceholderSize: true
+            ,containment: 'parent'
+            ,stop: function(event, ui) {
+                ui.item.removeClass('ui-state-hover');
+            }
+        });
+    }
+}
