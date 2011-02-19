cfb26bc8efa222aec5ba174f12065aa666d0fa51
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2011-02-19 19:26:48 +0000

    Implemented tagCloud component

---
+++ src/main/java/org/primefaces/component/tagcloud/TagCloudRenderer.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2009-2011 Prime Technology.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.primefaces.component.tagcloud;
+
+import java.io.IOException;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+import org.primefaces.model.tagcloud.TagCloudItem;
+import org.primefaces.model.tagcloud.TagCloudModel;
+import org.primefaces.renderkit.CoreRenderer;
+
+public class TagCloudRenderer extends CoreRenderer {
+
+    @Override
+    public void encodeEnd(FacesContext context, UIComponent component) throws IOException {
+        TagCloud tagCloud = (TagCloud) component;
+
+        encodeMarkup(context, tagCloud);
+		encodeScript(context, tagCloud);
+    }
+
+    protected void encodeMarkup(FacesContext context, TagCloud tagCloud) throws IOException {
+        ResponseWriter writer = context.getResponseWriter();
+        TagCloudModel model = tagCloud.getModel();
+        String styleClass = tagCloud.getStyleClass();
+        String style = tagCloud.getStyle();
+        styleClass = styleClass == null ? TagCloud.STYLE_CLASS : TagCloud.STYLE_CLASS + " " + styleClass;
+
+        writer.startElement("div", tagCloud);
+        writer.writeAttribute("id", tagCloud.getClientId(context), "id");
+        writer.writeAttribute("class", styleClass, "styleClass");
+        if(style != null) writer.writeAttribute("style", style, "style");
+
+
+        writer.startElement("ul", null);
+
+        for(TagCloudItem item : model.getTags()) {
+            writer.startElement("li", null);
+            writer.writeAttribute("class", "ui-tagcloud-strength-" + item.getStrength(), null);
+
+            writer.startElement("a", null);
+            writer.writeAttribute("href", getResourceURL(context, item.getUrl()), null);
+            writer.writeText(item.getLabel(), null);
+            writer.endElement("a");
+
+            writer.endElement("li");
+        }
+
+        writer.endElement("ul");
+
+        writer.endElement("div");
+    }
+
+    protected void encodeScript(FacesContext context, TagCloud tagCloud) throws IOException {
+        ResponseWriter writer = context.getResponseWriter();
+        String clientId = tagCloud.getClientId(context);
+        
+        writer.startElement("script", null);
+		writer.writeAttribute("type", "text/javascript", null);
+
+		writer.write(tagCloud.resolveWidgetVar() + " = new PrimeFaces.widget.TagCloud('" + clientId + "', {");
+        writer.write("sortable:" + tagCloud.isSortable());
+        writer.write("});");
+
+        writer.endElement("script");
+    }
+}
