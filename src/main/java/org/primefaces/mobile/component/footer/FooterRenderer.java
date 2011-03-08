95ab20595599ad2dd022551e2af2f619576d886a
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2011-03-08 10:14:30 +0000

    Initial dropin of mobile layout components, removed old touchfaces

---
+++ src/main/java/org/primefaces/mobile/component/footer/FooterRenderer.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2009-2011 Prime Technology.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.primefaces.mobile.component.footer;
+
+import java.io.IOException;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+import org.primefaces.mobile.component.content.Footer;
+import org.primefaces.renderkit.CoreRenderer;
+
+public class FooterRenderer extends CoreRenderer {
+
+    @Override
+    public void encodeBegin(FacesContext context, UIComponent component) throws IOException {
+        ResponseWriter writer = context.getResponseWriter();
+        Footer footer = (Footer) component;
+
+        writer.startElement("div", footer);
+        writer.writeAttribute("id", footer.getClientId(context), "id");
+        writer.writeAttribute("data-role", "footer", null);
+    }
+
+    @Override
+    public void encodeEnd(FacesContext context, UIComponent component) throws IOException {
+        ResponseWriter writer = context.getResponseWriter();
+
+        writer.endElement("div");
+    }
+}
