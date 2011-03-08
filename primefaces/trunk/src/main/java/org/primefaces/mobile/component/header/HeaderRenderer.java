0be2517f6830df6dad61265b1ecc11cdf4d75c0f
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2011-03-08 10:14:30 +0000

    Initial dropin of mobile layout components, removed old touchfaces

---
+++ primefaces/trunk/src/main/java/org/primefaces/mobile/component/header/HeaderRenderer.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2009-2011 Prime Technology.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.primefaces.mobile.component.header;
+
+import java.io.IOException;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+import org.primefaces.renderkit.CoreRenderer;
+
+public class HeaderRenderer extends CoreRenderer {
+
+    @Override
+    public void encodeBegin(FacesContext context, UIComponent component) throws IOException {
+        ResponseWriter writer = context.getResponseWriter();
+        Header header = (Header) component;
+        String title = header.getTitle();
+
+        writer.startElement("div", header);
+        writer.writeAttribute("id", header.getClientId(context), "id");
+        writer.writeAttribute("data-role", "header", null);
+
+        if(title != null) {
+             writer.startElement("h1", header);
+             writer.writeText(title, null);
+             writer.endElement("h1");
+        }
+    }
+
+    @Override
+    public void encodeEnd(FacesContext context, UIComponent component) throws IOException {
+        ResponseWriter writer = context.getResponseWriter();
+
+        writer.endElement("div");
+    }
+}
---
+++ primefaces/trunk/src/main/java/org/primefaces/mobile/component/page/PageRenderer.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2009-2011 Prime Technology.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.primefaces.mobile.component.page;
+
+import java.io.IOException;
+import java.util.Map;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+import org.primefaces.renderkit.CoreRenderer;
+
+public class PageRenderer extends CoreRenderer {
+
+    @Override
+    public void encodeBegin(FacesContext context, UIComponent component) throws IOException {
+        ResponseWriter writer = context.getResponseWriter();
+        Page page = (Page) component;
+
+        writer.write("<!DOCTYPE html>\n");
+        writer.startElement("html", page);
+        writer.startElement("head", page);
+
+        writer.startElement("title", page);
+        writer.write(page.getTitle());
+        writer.endElement("title");
+
+        renderResource(context, "mobile/mobile.css", "javax.faces.resource.Stylesheet");
+        renderResource(context, "jquery/jquery.js", "javax.faces.resource.Script");
+        renderResource(context, "mobile/mobile.js", "javax.faces.resource.Script");
+
+        writer.endElement("head");
+
+        writer.startElement("body", page);
+    }
+
+    @Override
+    public void encodeEnd(FacesContext context, UIComponent component) throws IOException {
+        ResponseWriter writer = context.getResponseWriter();
+
+        writer.endElement("body");
+        writer.endElement("html");
+
+    }
+
+    protected void renderResource(FacesContext context, String resourceName, String renderer) throws IOException {
+        UIComponent resource = context.getApplication().createComponent("javax.faces.Output");
+        resource.setRendererType(renderer);
+
+        Map<String, Object> attrs = resource.getAttributes();
+        attrs.put("name", resourceName);
+        attrs.put("library", "primefaces");
+        attrs.put("target", "head");
+
+        resource.encodeAll(context);
+    }
+}
