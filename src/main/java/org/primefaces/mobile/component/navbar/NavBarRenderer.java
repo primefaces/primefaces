f45c55220b5cf9ea0a037688bf273899a1ab37f5
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2011-03-11 15:29:46 +0000

    Added navbar component

--- src/main/java/org/primefaces/mobile/component/navbar/NavBarRenderer.java
+++ src/main/java/org/primefaces/mobile/component/navbar/NavBarRenderer.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.primefaces.mobile.component.footer;
+package org.primefaces.mobile.component.navbar;
 
 import java.io.IOException;
 import javax.faces.component.UIComponent;
@@ -21,22 +21,39 @@ import javax.faces.context.FacesContext;
 import javax.faces.context.ResponseWriter;
 import org.primefaces.renderkit.CoreRenderer;
 
-public class FooterRenderer extends CoreRenderer {
-
-    @Override
-    public void encodeBegin(FacesContext context, UIComponent component) throws IOException {
-        ResponseWriter writer = context.getResponseWriter();
-        Footer footer = (Footer) component;
-
-        writer.startElement("div", footer);
-        writer.writeAttribute("id", footer.getClientId(context), "id");
-        writer.writeAttribute("data-role", "footer", null);
-    }
+public class NavBarRenderer extends CoreRenderer {
 
     @Override
     public void encodeEnd(FacesContext context, UIComponent component) throws IOException {
         ResponseWriter writer = context.getResponseWriter();
+        NavBar navBar = (NavBar) component;
+
+        writer.startElement("div", navBar);
+        writer.writeAttribute("id", navBar.getClientId(context), "id");
+        writer.writeAttribute("data-role", "navbar", null);
+        if(navBar.getSwatch() != null)
+            writer.writeAttribute("data-theme", navBar.getSwatch(), null);
+
+        writer.startElement("ul", navBar);
+        for(UIComponent child : navBar.getChildren()) {
+            writer.startElement("li", navBar);
+            if(child.isRendered()) {
+                child.encodeAll(context);
+            }
+            writer.endElement("li");
+        }
+        writer.endElement("ul");
 
         writer.endElement("div");
     }
+
+    @Override
+	public void encodeChildren(FacesContext context, UIComponent component) throws IOException {
+		//Rendering happens on encodeEnd
+	}
+
+    @Override
+	public boolean getRendersChildren() {
+		return true;
+	}
 }
