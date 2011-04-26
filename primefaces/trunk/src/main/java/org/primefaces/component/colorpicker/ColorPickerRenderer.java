9e74f8e24d3d6259de7a8f67cdb553f16ae3b0f3
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2011-04-26 14:02:00 +0000

    Dropped in new color picker

--- primefaces/trunk/src/main/java/org/primefaces/component/colorpicker/ColorPickerRenderer.java
+++ primefaces/trunk/src/main/java/org/primefaces/component/colorpicker/ColorPickerRenderer.java
@@ -15,7 +15,6 @@
  */
 package org.primefaces.component.colorpicker;
 
-import java.awt.Color;
 import java.io.IOException;
 import java.util.Map;
 
@@ -23,162 +22,94 @@ import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.context.ResponseWriter;
 import javax.faces.convert.ConverterException;
+import org.primefaces.model.Color;
 
 import org.primefaces.renderkit.CoreRenderer;
 
 public class ColorPickerRenderer extends CoreRenderer {
 
-	private final static String DEFAULT_PICKER_THUMB = "yui/colorpicker/assets/picker_thumb.png";
-	private final static String DEFAULT_HUE_THUMB = "yui/colorpicker/assets/hue_thumb.png";
-
     @Override
-	public void decode(FacesContext facesContext, UIComponent component) {
+	public void decode(FacesContext context, UIComponent component) {
 		ColorPicker colorPicker = (ColorPicker) component;
-		String paramName = colorPicker.getClientId(facesContext) + "_input";
-		Map<String, String> params = facesContext.getExternalContext().getRequestParameterMap();
-		
+		String paramName = colorPicker.getClientId(context) + "_input";
+		Map<String, String> params = context.getExternalContext().getRequestParameterMap();
+
 		if(params.containsKey(paramName)) {
-			String submittedValue = facesContext.getExternalContext().getRequestParameterMap().get(paramName);
+			String submittedValue = params.get(paramName);
 
 			colorPicker.setSubmittedValue(submittedValue);
 		}
 	}
 
     @Override
-	public void encodeEnd(FacesContext facesContext, UIComponent component) throws IOException {
+	public void encodeEnd(FacesContext context, UIComponent component) throws IOException {
 		ColorPicker colorPicker = (ColorPicker) component;
 
-		encodeMarkup(facesContext, colorPicker);
-		encodeScript(facesContext, colorPicker);
+		encodeMarkup(context, colorPicker);
+		encodeScript(context, colorPicker);
 	}
 
-	protected void encodeMarkup(FacesContext facesContext, ColorPicker colorPicker) throws IOException {
-		ResponseWriter writer = facesContext.getResponseWriter();
-		String clientId = colorPicker.getClientId(facesContext);
-		String value = getValueAsString(facesContext,colorPicker);
-		String buttonId = clientId + "_button";
-		String inputId = clientId + "_input";
+	protected void encodeMarkup(FacesContext context, ColorPicker colorPicker) throws IOException {
+        ResponseWriter writer = context.getResponseWriter();
+        String clientId = colorPicker.getClientId(context);
+        String inputId = clientId + "_input";
+        Color color = (Color) colorPicker.getValue();
 
-		renderIE6Fix(writer);
-		
-		writer.startElement("span", null);
-		writer.writeAttribute("id", clientId, null);
+        writer.startElement("div", null);
+        writer.writeAttribute("id", colorPicker.getClientId(context), "id");
 
-		//Button
-		writer.startElement("button", null);
-		writer.writeAttribute("id", buttonId, null);
-		writer.writeAttribute("name", buttonId, null);
-		writer.writeAttribute("type", "button", null);
-		writer.write("<em id=\""+ clientId + "_livePreview\" style=\"overflow:hidden;width:1em;height:1em;display:block;border:solid 1px #000;text-indent:1em;white-space:nowrap;");
-		if(value != null) {
-			writer.write("background-color:rgb(" + value + ");");
-		}
-		writer.write("\">Live Preview</em>");
-
-		writer.endElement("button");
-		
-		//Input
+        //Input
 		writer.startElement("input", null);
 		writer.writeAttribute("id", inputId, null);
 		writer.writeAttribute("name", inputId, null);
 		writer.writeAttribute("type", "hidden", null);
-		if(value != null) {
-			writer.writeAttribute("value", value, null);
+		if(color != null) {
+			writer.writeAttribute("value", color.toString(), null);
 		}
 		writer.endElement("input");
-		
-		//Dialog
-		writer.startElement("div", null);
-		writer.writeAttribute("id", clientId + "_dialog", null);
-		if(colorPicker.getHeader() != null) {
-			writer.writeAttribute("title", colorPicker.getHeader(), null);
-		}
-		
-		writer.startElement("div", null);
-		writer.writeAttribute("id", clientId + "_cpContainer", null);
-		writer.writeAttribute("class", "yui-picker", null);
-		writer.endElement("div");
-		
-		writer.endElement("div");
-		
-		writer.endElement("span");
-	}
 
-	private void renderIE6Fix(ResponseWriter writer) throws IOException {
-		writer.write("<!--[if lt IE 7]>\n");
-		writer.startElement("style", null);
-		writer.writeAttribute("type", "text/css", null);
-		writer.write("\n* html .yui-picker-bg {\n");
-		writer.write("background-image: none;\n");
-		writer.write("}\n");
-		writer.endElement("style");
-		writer.write("<![endif]-->");
-	}
-	
-	protected void encodeScript(FacesContext facesContext, ColorPicker colorPicker) throws IOException {
-		ResponseWriter writer = facesContext.getResponseWriter();
-		
-		String clientId = colorPicker.getClientId(facesContext);
-		String value = getValueAsString(facesContext,colorPicker);
+        writer.endElement("div");
+    }
+
+    protected void encodeScript(FacesContext context, ColorPicker colorPicker) throws IOException {
+		ResponseWriter writer = context.getResponseWriter();
+		String clientId = colorPicker.getClientId(context);
+        Color color = (Color) colorPicker.getValue();
 
 		writer.startElement("script", null);
 		writer.writeAttribute("type", "text/javascript", null);
-		
-		writer.write(colorPicker.resolveWidgetVar() + " = new PrimeFaces.widget.ColorPicker('" + clientId + "', {");
-		writer.write("images: {");
-		writer.write("PICKER_THUMB:'" + getResourceRequestPath(facesContext, DEFAULT_PICKER_THUMB) + "'");
-		writer.write(",HUE_THUMB:'" + getResourceRequestPath(facesContext, DEFAULT_HUE_THUMB) + "'");
-		writer.write("}");
-		
-		if(value != null) writer.write(",initialValue:[" + value + "]");
-		if(!colorPicker.isShowControls()) writer.write(",showcontrols: false");
-		if(!colorPicker.isShowHexControls()) writer.write(",showhexcontrols: false");
-		if(!colorPicker.isShowHexSummary()) writer.write(",showhexsummary: false");
-		if(colorPicker.isShowHsvControls()) writer.write(",showhsvcontrols: true");
-		if(!colorPicker.isShowRGBControls()) writer.write(",showrgbcontrols: false");
-		if(!colorPicker.isShowWebSafe()) writer.write(",showwebsafe: false");
-		
-		writer.write("});");
 
-		writer.endElement("script");
-	}
+        writer.write("$(function() {");
+        
+		writer.write(colorPicker.resolveWidgetVar() + " = new PrimeFaces.widget.ColorPicker('" + clientId + "', {");
+        writer.write("mode:'" + colorPicker.getMode() + "'");
+
+        if(color != null) writer.write(",color:'" + color.getHex() + "'");
+
+        writer.write("});});");
+
+        writer.endElement("script");
+    }
 
     @Override
 	public Object getConvertedValue(FacesContext context, UIComponent component, Object value) throws ConverterException {
 		try {
 			String submittedValue = (String) value;
-	
-			if(isValueBlank(submittedValue))
+                    
+			if(isValueBlank(submittedValue)) {
 				return null;
+            }
 
-			ColorPicker colorPicker = (ColorPicker) component;
+            String[] values = submittedValue.split(",");
+            String hex = values[0];
+            String[] rgb = values[1].split("_");
+            String[] hsb = values[2].split("_");
+
+            return new Color(hex, Integer.valueOf(rgb[0]), Integer.valueOf(rgb[1]), Integer.valueOf(rgb[2]),
+                                    Integer.valueOf(hsb[0]), Integer.valueOf(hsb[1]), Integer.valueOf(hsb[2]));
 			
-			// Delegate to user supplied converter if defined
-			if(colorPicker.getConverter() != null) {
-				return colorPicker.getConverter().getAsObject(context, colorPicker, submittedValue);
-			} else {
-				String[] rgb = submittedValue.split(",");
-
-				return new Color(Integer.parseInt(rgb[0]), Integer.parseInt(rgb[1]), Integer.parseInt(rgb[2]));
-			}
 		} catch (Exception e) {
 			throw new ConverterException(e);
 		}
-	}
-	
-	protected String getValueAsString(FacesContext facesContext, ColorPicker colorPicker) {
-		Object value = colorPicker.getValue();
-		
-		if(value == null)
-			return null;
-		else {
-			if(colorPicker.getConverter() != null)
-				return colorPicker.getConverter().getAsString(facesContext, colorPicker, value);
-			else {
-				Color color = (Color) value;
-
-				return color.getRed() + "," + color.getGreen() + "," + color.getBlue();
-			}		
-		}
-	}
+    }
 }
