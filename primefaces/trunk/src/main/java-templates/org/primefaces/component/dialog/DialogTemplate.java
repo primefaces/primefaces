4c602dbbf7c1c68b11cafa2d472030e5cb4adf38
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2011-05-14 18:24:04 +0000

    Fixed #2044

--- primefaces/trunk/src/main/java-templates/org/primefaces/component/dialog/DialogTemplate.java
+++ primefaces/trunk/src/main/java-templates/org/primefaces/component/dialog/DialogTemplate.java
@@ -5,6 +5,7 @@ import org.primefaces.event.CloseEvent;
 import org.primefaces.util.Constants;
 import javax.faces.event.BehaviorEvent;
 import javax.faces.event.FacesEvent;
+import javax.faces.event.PhaseId;
 
     private final static String DEFAULT_EVENT = "close";
 
@@ -28,8 +29,38 @@ import javax.faces.event.FacesEvent;
         if(eventName != null && eventName.equals("close") && event instanceof BehaviorEvent) {
             setVisible(false);
             CloseEvent closeEvent = new CloseEvent(this, ((BehaviorEvent) event).getBehavior());
+            closeEvent.setPhaseId(PhaseId.APPLY_REQUEST_VALUES);
             super.queueEvent(closeEvent);
+            context.renderResponse();       //just process the close event and skip to response
         } else {
             super.queueEvent(event);
         }
+    }
+
+    @Override
+    public void processDecodes(FacesContext context) {
+        if(isSelfRequest(context)) {
+            this.decode(context);
+        }
+        else {
+            super.processDecodes(context);
+        }
+    }
+
+    @Override
+    public void processValidators(FacesContext context) {
+        if(!isSelfRequest(context)) {
+            super.processValidators(context);
+        }
+    }
+
+    @Override
+    public void processUpdates(FacesContext context) {
+        if(!isSelfRequest(context)) {
+            super.processUpdates(context);
+        }
+    }
+
+    private boolean isSelfRequest(FacesContext context) {
+        return this.getClientId(context).equals(context.getExternalContext().getRequestParameterMap().get(Constants.PARTIAL_SOURCE_PARAM));
     }
