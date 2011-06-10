3849fbd187f8c2ffbf66e202a75f9047fa7dc7d1
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2011-06-10 10:25:30 +0000

    Implemented complete listener behavior for progressbar

--- primefaces/trunk/src/main/resources/META-INF/resources/primefaces/progressbar/progressbar.js
+++ primefaces/trunk/src/main/resources/META-INF/resources/primefaces/progressbar/progressbar.js
@@ -2,20 +2,21 @@ PrimeFaces.widget.ProgressBar = function(id, cfg) {
     this.id = id;
     this.cfg = cfg;
     this.jqId = PrimeFaces.escapeClientId(id);
+    this.jq = $(this.jqId);
 
     if(this.cfg.ajax) {
-        this.cfg.formId = $(this.jqId).parents('form:first').attr('id');
+        this.cfg.formId = this.jq.parents('form:first').attr('id');
     }
 	
-    jQuery(this.jqId).progressbar(this.cfg);
+    this.jq.progressbar(this.cfg);
 }
 
 PrimeFaces.widget.ProgressBar.prototype.setValue = function(value) {
-    jQuery(this.jqId).progressbar('value', value);
+    this.jq.progressbar('value', value);
 }
 
 PrimeFaces.widget.ProgressBar.prototype.getValue  = function() {
-    return jQuery(this.jqId).progressbar('value');
+    return this.jq.progressbar('value');
 }
 
 PrimeFaces.widget.ProgressBar.prototype.start = function() {
@@ -49,48 +50,24 @@ PrimeFaces.widget.ProgressBar.prototype.start = function() {
 PrimeFaces.widget.ProgressBar.prototype.fireCompleteEvent = function() {
     clearInterval(this.progressPoll);
 
-    var options = {
-        source: this.id,
-        process: this.id,
-        formId: this.cfg.formId,
-        async: true
-    };
-
-    if(this.cfg.onCompleteUpdate)
-        options.update = this.cfg.onCompleteUpdate;
-    if(this.cfg.oncomplete)
-        options.oncomplete = this.cfg.oncomplete;
-
-    var params = {};
-    params[this.id + '_complete'] = true;
-
-    options.params = params;
-	
-    PrimeFaces.ajax.AjaxRequest(options);
+    if(this.cfg.behaviors) {
+        var completeBehavior = this.cfg.behaviors['complete'];
+        
+        if(completeBehavior) {
+            completeBehavior.call(this);
+        }
+    }
 }
 
 PrimeFaces.widget.ProgressBar.prototype.cancel = function() {
     clearInterval(this.progressPoll);
-    var _self = this;
-
-    var options = {
-        source: this.id,
-        process: this.id,
-        formId: this.cfg.formId,
-        async: true,
-        oncomplete:function(xhr, status, args) {
-            _self.setValue(0);
+    this.setValue(0);
+    
+    if(this.cfg.behaviors) {
+        var cancelBehavior = this.cfg.behaviors['cancel'];
+        
+        if(cancelBehavior) {
+            cancelBehavior.call(this);
         }
-    };
-
-    if(this.cfg.onCancelUpdate) {
-        options.update = this.cfg.onCancelUpdate;
     }
-
-    var params = {};
-    params[this.id + '_cancel'] = true;
-
-    options.params = params;
-
-    PrimeFaces.ajax.AjaxRequest(options);
 }
