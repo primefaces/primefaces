a7dac12a09bec6e4bbefc23e44b6ae1cab798042
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2011-06-10 10:07:15 +0000

    Added progressbar template and removed listeners, component handler

--- primefaces/trunk/src/main/java/org/primefaces/component/progressbar/ProgressBarRenderer.java
+++ primefaces/trunk/src/main/java/org/primefaces/component/progressbar/ProgressBarRenderer.java
@@ -18,7 +18,6 @@ package org.primefaces.component.progressbar;
 import java.io.IOException;
 import java.util.Map;
 
-import javax.faces.FacesException;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.context.ResponseWriter;
@@ -30,60 +29,41 @@ import org.primefaces.util.ComponentUtils;
 public class ProgressBarRenderer extends CoreRenderer {
 
     @Override
-    public void decode(FacesContext facesContext, UIComponent component) {
+    public void decode(FacesContext context, UIComponent component) {
         ProgressBar progressBar = (ProgressBar) component;
-        String clientId = progressBar.getClientId(facesContext);
-        Map<String, String> params = facesContext.getExternalContext().getRequestParameterMap();
+        String clientId = progressBar.getClientId(context);
+        Map<String, String> params = context.getExternalContext().getRequestParameterMap();
 
         if(params.containsKey(clientId)) {
-            if (params.containsKey(clientId + "_cancel")) {
-                progressBar.getValueExpression("value").setValue(facesContext.getELContext(), 0);
-
-                if (progressBar.getCancelListener() != null) {
-                    progressBar.getCancelListener().invoke(facesContext.getELContext(), null);
-                }
-
-            } else if (params.containsKey(clientId + "_complete")) {
-                if (progressBar.getCompleteListener() != null) {
-                    progressBar.getCompleteListener().invoke(facesContext.getELContext(), null);
-                }
-
-            } else {
-                RequestContext.getCurrentInstance().addCallbackParam(progressBar.getClientId(facesContext) + "_value", progressBar.getValue());
-
-            }
-
-            FacesContext.getCurrentInstance().renderResponse();
+            RequestContext.getCurrentInstance().addCallbackParam(progressBar.getClientId(context) + "_value", progressBar.getValue());
         }
+
+        FacesContext.getCurrentInstance().renderResponse();
     }
 
     @Override
-    public void encodeEnd(FacesContext facesContext, UIComponent component) throws IOException {
+    public void encodeEnd(FacesContext context, UIComponent component) throws IOException {
         ProgressBar progressBar = (ProgressBar) component;
 
-        encodeMarkup(facesContext, progressBar);
-        encodeScript(facesContext, progressBar);
+        encodeMarkup(context, progressBar);
+        encodeScript(context, progressBar);
     }
 
-    protected void encodeMarkup(FacesContext facesContext, ProgressBar progressBar) throws IOException {
-        ResponseWriter writer = facesContext.getResponseWriter();
+    protected void encodeMarkup(FacesContext context, ProgressBar progressBar) throws IOException {
+        ResponseWriter writer = context.getResponseWriter();
 
         writer.startElement("div", progressBar);
-        writer.writeAttribute("id", progressBar.getClientId(facesContext), "id");
-
-        if (progressBar.getStyle() != null) {
-            writer.writeAttribute("style", progressBar.getStyle(), "style");
-        }
-        if (progressBar.getStyleClass() != null) {
-            writer.writeAttribute("class", progressBar.getStyleClass(), "styleClass");
-        }
-
+        writer.writeAttribute("id", progressBar.getClientId(context), "id");
+        if(progressBar.getStyle() != null) writer.writeAttribute("style", progressBar.getStyle(), "style");
+        if(progressBar.getStyleClass() != null) writer.writeAttribute("class", progressBar.getStyleClass(), "styleClass");
+        
         writer.endElement("div");
     }
 
-    protected void encodeScript(FacesContext facesContext, ProgressBar progressBar) throws IOException {
-        ResponseWriter writer = facesContext.getResponseWriter();
-        String clientId = progressBar.getClientId(facesContext);
+    protected void encodeScript(FacesContext context, ProgressBar progressBar) throws IOException {
+        ResponseWriter writer = context.getResponseWriter();
+        String clientId = progressBar.getClientId(context);
+        boolean isAjax = progressBar.isAjax();
 
         writer.startElement("script", progressBar);
         writer.writeAttribute("type", "text/javascript", null);
@@ -92,17 +72,14 @@ public class ProgressBarRenderer extends CoreRenderer {
 
         writer.write(progressBar.resolveWidgetVar() + " = new PrimeFaces.widget.ProgressBar('" + clientId + "', {");
         writer.write("value:" + progressBar.getValue());
+        writer.write(",ajax:" + isAjax);
 
-        if(progressBar.isAjax()) {
-            writer.write(",ajax:true");
+        if(isAjax) {
             writer.write(",interval:" + progressBar.getInterval());
 
-            if(progressBar.getOnCompleteUpdate() != null) writer.write(",onCompleteUpdate:'" + ComponentUtils.findClientIds(facesContext, progressBar, progressBar.getOnCompleteUpdate()) + "'");
-            if(progressBar.getOnCancelUpdate() != null) writer.write(",onCancelUpdate:'" + ComponentUtils.findClientIds(facesContext, progressBar, progressBar.getOnCancelUpdate()) + "'");
-            if(progressBar.getOncomplete() != null) writer.write(",oncomplete:function(xhr, status, args) {" + progressBar.getOncomplete() + "}");
+            if(progressBar.getOncomplete() != null) 
+                writer.write(",oncomplete:function(xhr, status, args) {" + progressBar.getOncomplete() + "}");
 
-        } else {
-            writer.write(",ajax:false");
         }
 
         if(progressBar.isDisabled()) {
