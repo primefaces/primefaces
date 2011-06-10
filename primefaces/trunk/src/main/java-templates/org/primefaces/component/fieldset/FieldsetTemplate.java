c834f5b610fca935c5428e80ab0bf6e64b448937
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2011-06-10 08:12:09 +0000

    Implemented #2123

--- primefaces/trunk/src/main/java-templates/org/primefaces/component/fieldset/FieldsetTemplate.java
+++ primefaces/trunk/src/main/java-templates/org/primefaces/component/fieldset/FieldsetTemplate.java
@@ -1,3 +1,16 @@
+import javax.faces.component.UIComponent;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import org.primefaces.model.Visibility;
+import org.primefaces.event.ToggleEvent;
+import org.primefaces.util.Constants;
+import javax.faces.event.AjaxBehaviorEvent;
+import javax.faces.event.FacesEvent;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.HashMap;
+import javax.faces.event.PhaseId;
 
     public static final String FIELDSET_CLASS = "ui-fieldset ui-widget ui-widget-content ui-corner-all";
     public static final String TOGGLEABLE_FIELDSET_CLASS = FIELDSET_CLASS + " ui-fieldset-toggleable";
@@ -7,17 +20,58 @@
     public static final String TOGGLER_MINUS_CLASS = "ui-icon-minusthick";
     public static final String TOGGLER_PLUS_CLASS = "ui-icon-plusthick";
 
+    private static final Collection<String> EVENT_NAMES = Collections.unmodifiableCollection(Arrays.asList("toggle"));
 
-	public void broadcast(javax.faces.event.FacesEvent event) throws javax.faces.event.AbortProcessingException {
-		super.broadcast(event);
+    @Override
+    public void queueEvent(FacesEvent event) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        
+        if(isRequestSource(context)) {
+            Map<String,String> params = context.getExternalContext().getRequestParameterMap();
+            String eventName = params.get(Constants.PARTIAL_BEHAVIOR_EVENT_PARAM);
+            String clientId = this.getClientId(context);
+            AjaxBehaviorEvent behaviorEvent = (AjaxBehaviorEvent) event;
+            
+            if(eventName.equals("toggle")) {
+                Visibility visibility = this.isCollapsed() ? Visibility.HIDDEN : Visibility.VISIBLE;
 
-		FacesContext facesContext = FacesContext.getCurrentInstance();
+                super.queueEvent(new ToggleEvent(this, behaviorEvent.getBehavior(), visibility));
+            }
+        }
+        else {
+            super.queueEvent(event);
+        }
+    }
 
-		if(event instanceof org.primefaces.event.ToggleEvent) {
-			MethodExpression toggleMe = getToggleListener();
+    @Override
+    public Collection<String> getEventNames() {
+        return EVENT_NAMES;
+    }
 
-			if(toggleMe != null) {
-				toggleMe.invoke(facesContext.getELContext(), new Object[] {event});
-			}
-		}
-	}
+    @Override
+    public void processDecodes(FacesContext context) {
+        if(isRequestSource(context)) {
+            this.decode(context);
+        }
+        else {
+            super.processDecodes(context);
+        }
+    }
+
+    @Override
+    public void processValidators(FacesContext context) {
+        if(!isRequestSource(context)) {
+            super.processValidators(context);
+        }
+    }
+
+    @Override
+    public void processUpdates(FacesContext context) {
+        if(!isRequestSource(context)) {
+            super.processUpdates(context);
+        }
+    }
+
+    private boolean isRequestSource(FacesContext context) {
+        return this.getClientId(context).equals(context.getExternalContext().getRequestParameterMap().get(Constants.PARTIAL_SOURCE_PARAM));
+    }
--- primefaces/trunk/src/main/resources/META-INF/resources/primefaces/fieldset/fieldset.js
+++ primefaces/trunk/src/main/resources/META-INF/resources/primefaces/fieldset/fieldset.js
@@ -5,19 +5,20 @@ PrimeFaces.widget.Fieldset = function(id, cfg) {
     this.id = id;
     this.cfg = cfg;
     this.jqId = PrimeFaces.escapeClientId(id);
-    this.jq = jQuery(this.jqId);
+    this.jq = $(this.jqId);
     this.legend = this.jq.children('.ui-fieldset-legend');
 
     var _self = this;
+    
     if(this.cfg.toggleable) {
         
         this.content = this.jq.children('.ui-fieldset-content');
         this.toggler = this.legend.children('.ui-fieldset-toggler');
-        this.stateHolder = jQuery(this.jqId + '_collapsed');
+        this.stateHolder = $(this.jqId + '_collapsed');
 
         //Add clickable legend state behavior
         var _legend = this.legend;
-        this.legend.click(function() {_self.toggle();})
+        this.legend.click(function(e) {_self.toggle(e);})
                            .mouseover(function() {_legend.toggleClass('ui-state-hover');})
                            .mouseout(function() {_legend.toggleClass('ui-state-hover');})
                            .mousedown(function() {_legend.toggleClass('ui-state-active');})
@@ -28,39 +29,20 @@ PrimeFaces.widget.Fieldset = function(id, cfg) {
 /**
  * Toggles the content
  */
-PrimeFaces.widget.Fieldset.prototype.toggle = function() {
-
+PrimeFaces.widget.Fieldset.prototype.toggle = function(e) {
     this.updateToggleState(this.cfg.collapsed);
-
+    
     var _self = this;
-    this.content.slideToggle(this.cfg.toggleSpeed,
-        function() {
-            if(_self.cfg.ajaxToggle) {
-                _self.fireToggleEvent();
+
+    this.content.slideToggle(this.cfg.toggleSpeed, function() {
+        if(_self.cfg.behaviors) {
+            var toggleBehavior = _self.cfg.behaviors['toggle'];
+
+            if(toggleBehavior) {
+                toggleBehavior.call(_self);
             }
-        });
-}
-
-/**
- * Fires an ajax toggle event and notify any server side toggleListeners
- */
-PrimeFaces.widget.Fieldset.prototype.fireToggleEvent = function() {
-    var options = {
-        source: this.id,
-        process: this.id
-    };
-
-    if(this.cfg.onToggleUpdate) {
-       options.update = this.cfg.onToggleUpdate;
-    }
-
-    var params = {};
-    params[this.id + "_ajaxToggle"] = true;
-    params[this.id + "_collapsed"] = this.cfg.collapsed;
-
-    options.params = params;
-
-    PrimeFaces.ajax.AjaxRequest(options);
+        }
+    });
 }
 
 /**
