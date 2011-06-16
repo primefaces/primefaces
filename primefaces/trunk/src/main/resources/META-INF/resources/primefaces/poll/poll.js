118b6f3d7e09ea9c81f40bf54bbcf7ed2f69dc51
leventgny@gmail.com <leventgny@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2011-06-16 13:32:59 +0000

    Fixed #777 Added active field to the poll widget on javascript. poll could be checked by  "poll.isActive()"

--- primefaces/trunk/src/main/resources/META-INF/resources/primefaces/poll/poll.js
+++ primefaces/trunk/src/main/resources/META-INF/resources/primefaces/poll/poll.js
@@ -1,6 +1,7 @@
 PrimeFaces.widget.Poll = function(id, cfg) {
     this.id = id;
     this.cfg = cfg;
+    this.active = false;
 
     if(this.cfg.autoStart) {
         this.start();
@@ -9,14 +10,20 @@ PrimeFaces.widget.Poll = function(id, cfg) {
 
 PrimeFaces.widget.Poll.prototype.start = function() {
     this.timer = setInterval(this.cfg.fn, (this.cfg.frequency * 1000));
+    this.active = true;
 }
 
 PrimeFaces.widget.Poll.prototype.stop = function() {
     clearInterval(this.timer);
+    this.active = false;
 }
 
 PrimeFaces.widget.Poll.prototype.handleComplete = function(xhr, status, args) {
     if(args.stop) {
         this.stop();
     }
+}
+
+PrimeFaces.widget.Poll.prototype.isActive = function() {
+    return this.active;
 }
