fe97fbd82f7d8806b201105cfe81aede4d3fac17
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2011-07-14 11:56:39 +0000

    Initial drop-in of new tooltip, does not support global tooltips and some more options yet.

--- primefaces/trunk/src/main/java/org/primefaces/component/tooltip/TooltipRenderer.java
+++ primefaces/trunk/src/main/java/org/primefaces/component/tooltip/TooltipRenderer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010 Prime Technology.
+ * Copyright 2009-2011 Prime Technology.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,82 +28,80 @@ import org.primefaces.util.ComponentUtils;
 public class TooltipRenderer extends CoreRenderer {
 
     @Override
-	public void encodeEnd(FacesContext facesContext, UIComponent component) throws IOException {
+	public void encodeEnd(FacesContext context, UIComponent component) throws IOException {
 		Tooltip tooltip = (Tooltip) component;
 		
-		encodeScript(facesContext, tooltip);
+        encodeMarkup(context, tooltip);
+		encodeScript(context, tooltip);
 	}
+    
+    protected void encodeMarkup(FacesContext context, Tooltip tooltip) throws IOException {
+        ResponseWriter writer = context.getResponseWriter();
+        String value = ComponentUtils.getStringValueToRender(context, tooltip);
+        String styleClass = tooltip.getStyleClass();
+        styleClass = styleClass == null ? Tooltip.CONTAINER_CLASS : Tooltip.CONTAINER_CLASS + " " + styleClass;
+        
+        writer.startElement("div", tooltip);
+        writer.writeAttribute("id", tooltip.getClientId(context), null);
+        writer.writeAttribute("class", styleClass, "styleClass");
+        
+        if(tooltip.getStyle() != null) 
+            writer.writeAttribute("style", tooltip.getStyle(), "style");
+        
+        writer.writeText(value, "value");
+        
+        writer.endElement("div");
+    }
 
-	protected void encodeScript(FacesContext facesContext, Tooltip tooltip) throws IOException {
-		ResponseWriter writer = facesContext.getResponseWriter();
-		boolean global = tooltip.isGlobal();
-		String owner = getTarget(facesContext, tooltip);
+	protected void encodeScript(FacesContext context, Tooltip tooltip) throws IOException {
+		ResponseWriter writer = context.getResponseWriter();
+        String clientId = tooltip.getClientId(context);
+		String target = getTarget(context, tooltip);
 		
 		writer.startElement("script", null);
 		writer.writeAttribute("type", "text/javascript", null);
 		
-        writer.write("jQuery(function() {");
+        writer.write("$(function() {");
 			
-		writer.write(tooltip.resolveWidgetVar() + " = new PrimeFaces.widget.Tooltip({");
-		writer.write("global:" + global);
+		writer.write(tooltip.resolveWidgetVar() + " = new PrimeFaces.widget.Tooltip('" + clientId + "', {");
+        writer.write("target:'" + target + "'");
         
-		if(!global) {
-			writer.write(",forComponent:'" + owner + "'");
-			writer.write(",content:'");
-			if(tooltip.getValue() == null)
-				renderChildren(facesContext, tooltip);
-			else
-				writer.write(ComponentUtils.getStringValueToRender(facesContext, tooltip).replaceAll("'", "\\\\'"));
-			
-			writer.write("'");
-		}
-		
-		//Events
-		writer.write(",show:{when:{event:'" + tooltip.getShowEvent()+"'}, delay:" + tooltip.getShowDelay() + ", effect:{length:" + tooltip.getShowEffectLength() + ", type: '"+tooltip.getShowEffect() + "'}}");
-		writer.write(",hide:{when:{event:'" + tooltip.getHideEvent()+"'}, delay:" + tooltip.getHideDelay() + ", effect:{length:" + tooltip.getHideEffectLength() + ", type: '"+tooltip.getHideEffect() + "'}}");
-	
-		//Position
-		writer.write(",position: {");
-        String container = owner == null ? "document.body" : "jQuery(PrimeFaces.escapeClientId('" + owner +"')).parent()";
-
-        writer.write("container:" + container);
-        writer.write(",corner:{");
-		writer.write("target:'" + tooltip.getTargetPosition() + "'");
-		writer.write(",tooltip:'" + tooltip.getPosition() + "'");
-		writer.write("}}");
-		
-		writer.write("});");	
-		
-		writer.write("});");	
+        if(tooltip.getShowEvent() != null) writer.write(",showEvent:'" + tooltip.getShowEvent() + "'");
+        if(tooltip.getHideEvent() != null) writer.write(",hideEvent:'" + tooltip.getHideEvent() + "'");
+        		
+		writer.write("});});");
 		
 		writer.endElement("script");
 	}
 	
-	protected String getTarget(FacesContext facesContext, Tooltip tooltip) {
+	protected String getTarget(FacesContext context, Tooltip tooltip) {
 		if(tooltip.isGlobal())
-			return null;
+			return "@all";
 		else {
 			String _for = tooltip.getFor();
 			
 			if(_for != null) {
 				UIComponent forComponent = tooltip.findComponent(_for);
+                
 				if(forComponent == null)
 					throw new FacesException("Cannot find component \"" + _for + "\" in view.");
 				else
-					return forComponent.getClientId(facesContext);
+					return forComponent.getClientId(context);
 				
 			} else if(tooltip.getForElement() != null) {
 				return tooltip.getForElement();
 			} else {
-				return tooltip.getParent().getClientId(facesContext);
+				throw new FacesException("No target is defined for tooltip '" + tooltip.getClientId(context) + "'");
 			}
 		}
 	}
 
+    @Override
 	public void encodeChildren(FacesContext context, UIComponent component) throws IOException {
 		//Rendering happens on encodeEnd
 	}
 
+    @Override
 	public boolean getRendersChildren() {
 		return true;
 	}
