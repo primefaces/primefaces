c10a8078d1b68ef68f237d99f92c5abd3bbaaaf5
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2011-07-17 18:16:29 +0000

    Fixed hardcoded value

--- primefaces/trunk/src/main/resources/META-INF/resources/primefaces/timeline/timeline.js
+++ primefaces/trunk/src/main/resources/META-INF/resources/primefaces/timeline/timeline.js
@@ -384,7 +384,7 @@ PrimeFaces.widget.Timeline = function(id, cfg) {
     this.cfg = cfg;
     
     this.cfg.show_footer = false;
-    this.cfg.icon_folder = '/prime-showcase/';
+    this.cfg.icon_folder = '';
     
     this.jq.timeline(this.cfg);
 }
