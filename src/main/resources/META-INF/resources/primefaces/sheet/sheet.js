77f3dc067027bcbc4deff928304549a7c674c929
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2011-07-19 09:25:17 +0000

    Implemented column resizing for sheet

--- src/main/resources/META-INF/resources/primefaces/sheet/sheet.js
+++ src/main/resources/META-INF/resources/primefaces/sheet/sheet.js
@@ -16,6 +16,8 @@ PrimeFaces.widget.Sheet = function(id, cfg) {
     });
     
     this.bindEvents();
+    
+    this.setupResizableColumns();
 }
 
 PrimeFaces.widget.Sheet.prototype.bindEvents = function() {
@@ -66,4 +68,73 @@ PrimeFaces.widget.Sheet.prototype.bindEvents = function() {
             input.blur();
         }
     });
-} 
+}
+
+PrimeFaces.widget.Sheet.prototype.setupResizableColumns = function() {
+    //Add resizers and resizer helper
+    this.header.find('th div.ui-sh-c').prepend('<div class="ui-column-resizer"></div>');
+    this.jq.append('<div class="ui-column-resizer-helper ui-state-highlight"></div>');
+
+    //Variables
+    var resizerHelper = $(this.jqId + ' .ui-column-resizer-helper'),
+    resizers = $(this.jqId + ' thead th div.ui-column-resizer'),
+    columnHeaders = $(this.jqId + ' thead th'),
+    thead = $(this.jqId + ' thead'),  
+    _self = this;
+    
+    //State cookie
+    this.columnWidthsCookie = this.id + '_columnWidths';
+    
+    //Main resize events
+    resizers.draggable({
+        axis: 'x',
+        start: function(event, ui) {
+            //Set height of resizer helper
+            resizerHelper.height(_self.body.height());
+            resizerHelper.show();
+        },
+        drag: function(event, ui) {
+            resizerHelper.offset(
+                {
+                    left: ui.helper.offset().left + ui.helper.width() / 2, 
+                    top: thead.offset().top + thead.height()
+                });  
+        },
+        stop: function(event, ui) {
+            var columnHeaderWrapper = ui.helper.parent(),
+            columnHeader = columnHeaderWrapper.parent(),
+            oldPos = ui.originalPosition.left,
+            newPos = ui.position.left,
+            change = (newPos - oldPos),
+            newWidth = columnHeaderWrapper.width() + change;
+            
+            ui.helper.css('left','');
+            resizerHelper.hide();
+            
+            columnHeaderWrapper.width(newWidth);
+                        
+            _self.body.find('tr td:nth-child(' + (columnHeader.index() + 1) + ')').width('').children('div').width(newWidth);
+            
+            //Save state
+            var columnWidths = [];
+            columnHeaders.each(function(i, item) {
+                columnWidths.push($(item).children('div').width());
+            });
+            PrimeFaces.setCookie(_self.columnWidthsCookie, columnWidths.join(','));
+        },
+        containment: this.jq
+    });
+    
+    //Restore widths on postback
+    var widths = PrimeFaces.getCookie(this.columnWidthsCookie);
+    if(widths) {
+        widths = widths.split(',');
+        for(var i = 0; i < widths.length; i++) {
+            var width = widths[i];
+            
+            columnHeaders.eq(i).children('div').width(width);
+            _self.body.find('tr td:nth-child(' + (i + 1) + ')').children('div').width(width);
+        }
+    }
+
+}
