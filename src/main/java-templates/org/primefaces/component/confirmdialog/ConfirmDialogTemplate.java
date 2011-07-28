c6e2cb3463721332693958498ab4bc8b1915848b
leventgny@gmail.com <leventgny@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2011-07-28 07:12:48 +0000

    Native p:confirmDialog implementation extends p:dialog.

---
+++ src/main/java-templates/org/primefaces/component/confirmdialog/ConfirmDialogTemplate.java
@@ -0,0 +1,3 @@
+    
+    public static final String CONTAINER_CLASS = "ui-confirmdialog ui-dialog ui-widget ui-widget-content ui-corner-all";
+    public static final String BUTTONPANE_CLASS = "ui-dialog-buttonpane ui-widget-content ui-helper-clearfix";
