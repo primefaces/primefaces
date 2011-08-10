fba1c34eeb1cff31a9fab70aa533b3f3b787d392
leventgny@gmail.com <leventgny@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2011-08-10 14:33:30 +0000

    Implemented new charts. BubbleChart and OHLC Chart.

---
+++ src/main/java/org/primefaces/component/chart/ohlc/OhlcChartRenderer.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2009-2011 Prime Technology.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.primefaces.component.chart.ohlc;
+
+import java.io.IOException;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+import org.primefaces.component.chart.BaseChartRenderer;
+import org.primefaces.component.chart.UIChart;
+import org.primefaces.model.chart.OhlcChartModel;
+
+public class OhlcChartRenderer extends BaseChartRenderer {
+
+       @Override
+    public void encodeEnd(FacesContext context, UIComponent component) throws IOException {
+        OhlcChart chart = (OhlcChart) component;
+
+        encodeMarkup(context, chart);
+        encodeScript(context, chart);
+    }
+
+    protected void encodeScript(FacesContext context, UIChart uichart) throws IOException{
+        ResponseWriter writer = context.getResponseWriter();
+        OhlcChart chart = (OhlcChart) uichart;
+        String clientId = chart.getClientId(context);
+
+        writer.startElement("script", null);
+        writer.writeAttribute("type", "text/javascript", null);
+
+        writer.write("$(function(){");
+
+        writer.write(chart.resolveWidgetVar() + " = new PrimeFaces.widget.OhlcChart('" + clientId + "', { ");
+
+        encodeData(context, chart);
+        
+        encodeOptions(context, chart);
+
+        encodeClientBehaviors(context, chart);
+
+        writer.write("});});");
+
+        writer.endElement("script");
+    }
+
+     
+    protected void encodeOptions(FacesContext context, OhlcChart chart) throws IOException {
+        
+        encodeCommonConfig(context, chart);
+        
+        encodeSeriesDefaults(context, chart);
+    }
+    
+    protected void encodeSeriesDefaults(FacesContext context, OhlcChart chart) throws IOException {
+        ResponseWriter writer = context.getResponseWriter();
+        writer.write(", seriesDefaults : { renderer: $.jqplot.OHLCRenderer, rendererOptions : {");
+        
+        if(chart.isCandleStick())
+            writer.write("candleStick:true");
+ 
+        writer.write("}}");
+    }
+
+    private void encodeData(FacesContext context, OhlcChart chart) throws IOException {
+        context.getResponseWriter().write("data:[" + ((OhlcChartModel)chart.getValue()).toString() + "]");
+    }
+}
---
+++ src/main/java/org/primefaces/model/chart/BubbleChartModel.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2009-2011 Prime Technology.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.primefaces.model.chart;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BubbleChartModel extends ChartModel implements Serializable {
+
+    private List<BubbleChartSeries> data;
+
+    public BubbleChartModel() {
+        data = new ArrayList<BubbleChartSeries>();
+    }
+
+    public BubbleChartModel(List<BubbleChartSeries> data) {
+        this.data = data;
+    }
+    
+    public List<BubbleChartSeries> getData() {
+        return data;
+    }
+
+    public void setData(List<BubbleChartSeries> data) {
+        this.data = data;
+    }
+    
+    public void addBubble(BubbleChartSeries bubble){
+        this.data.add(bubble);
+    }
+    
+    @Override
+    public String toString(){
+        String out = "[";
+        for( int i = 0; i < data.size() ; i++){
+            if(i != 0)
+                out += ",";
+            out += data.get(i).toString();
+        }
+        
+        return out + "]";
+    }
+
+    public void clear() {
+        this.data.clear();
+    }
+}
