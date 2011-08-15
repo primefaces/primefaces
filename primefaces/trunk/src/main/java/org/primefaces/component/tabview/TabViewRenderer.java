391448bbf60cf00cac5841e728d84076b16bac05
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2011-08-15 09:03:32 +0000

    Implemented dynamic tabs and user callbacks for tabview

--- primefaces/trunk/src/main/java/org/primefaces/component/tabview/TabViewRenderer.java
+++ primefaces/trunk/src/main/java/org/primefaces/component/tabview/TabViewRenderer.java
@@ -85,8 +85,8 @@ public class TabViewRenderer extends CoreRenderer {
 
         if(tabView.isCollapsible()) writer.write(",collapsible:true");
         if(tabView.getEvent() != null) writer.write(",event:'" + tabView.getEvent() + "'");
-        if(tabView.getOnTabChange() != null) writer.write(",onTabChange: function(event, ui) {" + tabView.getOnTabChange() + "}");
-        if(tabView.getOnTabShow() != null) writer.write(",onTabShow:function(event, ui) {" + tabView.getOnTabShow() + "}");
+        if(tabView.getOnTabChange() != null) writer.write(",onTabChange: function(index) {" + tabView.getOnTabChange() + "}");
+        if(tabView.getOnTabShow() != null) writer.write(",onTabShow:function(index) {" + tabView.getOnTabShow() + "}");
 
         if(tabView.getEffect() != null) {
             writer.write(",fx: {");
