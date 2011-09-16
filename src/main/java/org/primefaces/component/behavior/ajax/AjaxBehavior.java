8b897e6361e86bb9738ee9ea578b5d1560c2418f
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2011-09-16 13:42:05 +0000

    Stateful ajaxbehavior impl

--- src/main/java/org/primefaces/component/behavior/ajax/AjaxBehavior.java
+++ src/main/java/org/primefaces/component/behavior/ajax/AjaxBehavior.java
@@ -17,15 +17,19 @@ package org.primefaces.component.behavior.ajax;
 
 import java.util.Collections;
 import java.util.EnumSet;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
 import javax.el.ELContext;
+import javax.el.ELException;
 import javax.el.MethodExpression;
 import javax.el.MethodNotFoundException;
 import javax.el.ValueExpression;
+import javax.faces.FacesException;
 import javax.faces.application.ResourceDependencies;
 import javax.faces.application.ResourceDependency;
+import javax.faces.component.UIComponentBase;
 import javax.faces.component.behavior.ClientBehaviorBase;
 import javax.faces.component.behavior.ClientBehaviorHint;
 import javax.faces.context.FacesContext;
@@ -40,16 +44,15 @@ public class AjaxBehavior extends ClientBehaviorBase {
 
     private String update;
     private String process;
-    private boolean global = true;
-    private boolean async = false;
+    private Boolean global;
+    private Boolean async;
     private String oncomplete;
     private String onerror;
     private String onsuccess;
     private String onstart;
     private MethodExpression listener;
-    private boolean immediate = false;
-    private boolean disabled = false;
-    private boolean immediateSet = false;
+    private Boolean immediate;
+    private Boolean disabled;
 
     private static final Set<ClientBehaviorHint> HINTS = Collections.unmodifiableSet(EnumSet.of(ClientBehaviorHint.SUBMITTING));
 
@@ -66,99 +69,115 @@ public class AjaxBehavior extends ClientBehaviorBase {
     }
     
     public boolean isAsync() {
-        return async;
+        Boolean result = (Boolean) eval("async", async);
+        
+        return ((result != null) ? result : false);
     }
-
     public void setAsync(boolean async) {
         this.async = async;
+        
+        clearInitialState();
     }
 
     public boolean isGlobal() {
-        return global;
+        Boolean result = (Boolean) eval("global", global);
+        
+        return ((result != null) ? result : true);
     }
-
     public void setGlobal(boolean global) {
         this.global = global;
+        
+        clearInitialState();
     }
 
     public String getOncomplete() {
-        return oncomplete;
+        return (String) eval("oncomplete", oncomplete);
     }
-
     public void setOncomplete(String oncomplete) {
         this.oncomplete = oncomplete;
+
+        clearInitialState();
     }
 
     public String getOnstart() {
-        return onstart;
+        return (String) eval("onstart", onstart);
     }
 
     public void setOnstart(String onstart) {
         this.onstart = onstart;
+
+        clearInitialState();
     }
 
     public String getOnsuccess() {
-        return onsuccess;
+        return (String) eval("onsuccess", onsuccess);
     }
-
     public void setOnsuccess(String onsuccess) {
         this.onsuccess = onsuccess;
+        
+        clearInitialState();
     }
 
     public String getOnerror() {
-        return onerror;
+        return (String) eval("onerror", onerror);
     }
-
     public void setOnerror(String onerror) {
         this.onerror = onerror;
+        
+        clearInitialState();
     }
 
     public String getProcess() {
-        return process;
+        return (String) eval("process", process);
     }
-
     public void setProcess(String process) {
         this.process = process;
+        
+        clearInitialState();
     }
 
     public String getUpdate() {
-        return update;
+        return (String) eval("update", update);
     }
-
     public void setUpdate(String update) {
         this.update = update;
+
+        clearInitialState();
     }
 
     public MethodExpression getListener() {
         return listener;
     }
-
     public void setListener(MethodExpression listener) {
         this.listener = listener;
     }
 
     public boolean isDisabled() {
-        return disabled;
+        Boolean result = (Boolean) eval("disabled", disabled);
+        
+        return ((result != null) ? result : false);
     }
-
     public void setDisabled(boolean disabled) {
         this.disabled = disabled;
+        
+        clearInitialState();
     }
 
     public boolean isImmediate() {
-        return immediate;
+        Boolean result = (Boolean) eval("immediate", immediate);
+        
+        return ((result != null) ? result : false);
     }
-
-    public void setImmediate(boolean immediate) {
+    public void setImmediate(Boolean immediate) {
         this.immediate = immediate;
-
-        this.immediateSet = true;
+        
+        clearInitialState();
     }
-
+    
     public boolean isImmediateSet() {
-        return immediateSet;
+        return ((immediate != null) || (getValueExpression("immediate") != null));
     }
-
+    
     @Override
     public void broadcast(BehaviorEvent event) throws AbortProcessingException {
         FacesContext context = FacesContext.getCurrentInstance();
@@ -176,11 +195,94 @@ public class AjaxBehavior extends ClientBehaviorBase {
         }
     }
     
+    protected Object eval(String propertyName, Object value) {
+        if(value != null) {
+            return value;
+        }
+
+        ValueExpression expression = getValueExpression(propertyName);
+        if(expression != null) {
+            return expression.getValue(FacesContext.getCurrentInstance().getELContext());
+        }
+
+        return null;
+    }
+    
+    public ValueExpression getValueExpression(String name) {
+        if(name == null) {
+            throw new IllegalArgumentException();
+        }
+
+        return ((bindings == null) ? null : bindings.get(name));
+    }
+    
+    public void setValueExpression(String name, ValueExpression expr) {
+        if(name == null) {
+            throw new IllegalArgumentException();
+        }
+
+        if(expr != null) {
+            if(expr.isLiteralText()) {
+                setLiteralValue(name, expr);
+            } else {
+                if(bindings == null) {
+                    bindings = new HashMap<String, ValueExpression>(6,1.0f);
+                }
+
+                bindings.put(name, expr);
+            }
+        } 
+        else {
+            if(bindings != null) {
+                bindings.remove(name);
+                if(bindings.isEmpty()) {
+                    bindings = null;
+                }
+            }
+        }
+
+        clearInitialState();
+    }
+    
+    private void setLiteralValue(String propertyName, ValueExpression expression) {
+        Object value;
+        ELContext context = FacesContext.getCurrentInstance().getELContext();
+
+        try {
+            value = expression.getValue(context);
+        } 
+        catch (ELException eLException) {
+            throw new FacesException(eLException);
+        }
+        
+        if("update".equals(propertyName)) {
+            update = (String)value;
+        } else if ("process".equals(propertyName)) {
+            process = (String)value;
+        } else if ("oncomplete".equals(propertyName)) {
+            oncomplete = (String)value;
+        } else if ("onerror".equals(propertyName)) {
+            onerror = (String)value;
+        } else if ("onsuccess".equals(propertyName)) {
+            onsuccess = (String)value;
+        } else if ("onstart".equals(propertyName)) {
+            onstart = (String)value;
+        }else if ("immediate".equals(propertyName)) {
+            immediate = (Boolean)value;
+        } else if ("disabled".equals(propertyName)) {
+            disabled = (Boolean)value;
+        } else if ("async".equals(propertyName)) {
+            async = (Boolean)value;
+        } else if ("global".equals(propertyName)) {
+            global = (Boolean)value;
+        }
+    }
+    
     @Override
     public Object saveState(FacesContext context) {
-        Object superState = super.saveState(context);
-        
         Object[] values;
+
+        Object superState = super.saveState(context);
 
         if(initialStateMarked()) {
             if(superState == null)
@@ -189,10 +291,21 @@ public class AjaxBehavior extends ClientBehaviorBase {
                 values = new Object[] {superState};
         } 
         else {
-            values = new Object[2];
+            values = new Object[13];
       
             values[0] = superState;
-            values[1] = listener;
+            values[1] = onstart;
+            values[2] = onerror;
+            values[3] = onsuccess;
+            values[4] = oncomplete;
+            values[5] = disabled;
+            values[6] = immediate;
+            values[7] = process;
+            values[8] = update;
+            values[9] = async;
+            values[10] = global;
+            values[11] = listener;
+            values[12] = saveBindings(context, bindings);
         }
 
         return values;
@@ -201,15 +314,60 @@ public class AjaxBehavior extends ClientBehaviorBase {
     @Override
     public void restoreState(FacesContext context, Object state) {
         if(state != null) {
-
             Object[] values = (Object[]) state;
             super.restoreState(context, values[0]);
 
             if(values.length != 1) {
-                listener = (MethodExpression) values[1];
+                onstart = (String) values[1];
+                onerror = (String) values[2];
+                onsuccess = (String) values[3];
+                oncomplete = (String) values[4];
+                disabled = (Boolean) values[5];
+                immediate = (Boolean) values[6];
+                process = (String) values[7];
+                update = (String) values[8];
+                async = (Boolean) values[9];
+                global = (Boolean) values[10];
+                listener = (MethodExpression) values[11];
+                bindings = restoreBindings(context, values[12]);
 
                 clearInitialState();
             }
         }
+    }
+
+    
+    private Object saveBindings(FacesContext context, Map<String, ValueExpression> bindings) {
+        if(bindings == null) {
+            return null;
+        }
+
+        Object values[] = new Object[2];
+        values[0] = bindings.keySet().toArray(new String[bindings.size()]);
+
+        Object[] bindingValues = bindings.values().toArray();
+        for (int i = 0; i < bindingValues.length; i++) {
+            bindingValues[i] = UIComponentBase.saveAttachedState(context, bindingValues[i]);
+        }
+
+        values[1] = bindingValues;
+
+        return values;
+    }
+
+    private Map<String, ValueExpression> restoreBindings(FacesContext context, Object state) {
+        if(state == null) {
+            return null;
+        }
+        
+        Object values[] = (Object[]) state;
+        String names[] = (String[]) values[0];
+        Object states[] = (Object[]) values[1];
+        Map<String, ValueExpression> bindings = new HashMap<String, ValueExpression>(names.length);
+        
+        for (int i = 0; i < names.length; i++) {
+            bindings.put(names[i], (ValueExpression) UIComponentBase.restoreAttachedState(context, states[i]));
+        }
+        return bindings;
     }
 }
