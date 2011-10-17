3c8aadb64048c217c26cb27a77084423c7f224ab
leventgny@gmail.com <leventgny@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2011-10-17 08:34:05 +0000

    Fixed #803 Added redisplay behavior on password component.

--- primefaces/trunk/src/main/java/org/primefaces/component/password/PasswordRenderer.java
+++ primefaces/trunk/src/main/java/org/primefaces/component/password/PasswordRenderer.java
@@ -106,7 +106,7 @@ public class PasswordRenderer extends InputRenderer {
 		writer.writeAttribute("type", "password", null);
 		
 		String valueToRender = ComponentUtils.getStringValueToRender(context, password);
-		if(valueToRender != null) {
+		if(valueToRender != null && password.isRedisplay()) {
 			writer.writeAttribute("value", valueToRender , null);
 		}
 		
