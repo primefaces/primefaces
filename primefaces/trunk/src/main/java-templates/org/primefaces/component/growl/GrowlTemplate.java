ab1ca38c9df021e143db5af4372827b11c4c9cd6
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2011-10-19 08:20:58 +0000

    Resource convention for growl

--- primefaces/trunk/src/main/java-templates/org/primefaces/component/growl/GrowlTemplate.java
+++ primefaces/trunk/src/main/java-templates/org/primefaces/component/growl/GrowlTemplate.java
@@ -1,4 +1,4 @@
-	static final String WARN_ICON = "growl/assets/warn.png";
-	static final String ERROR_ICON = "growl/assets/error.png";
-	static final String INFO_ICON = "growl/assets/info.png";
-	static final String FATAL_ICON = "growl/assets/fatal.png";
+	static final String WARN_ICON = "growl/images/warn.png";
+	static final String ERROR_ICON = "growl/images/error.png";
+	static final String INFO_ICON = "growl/images/info.png";
+	static final String FATAL_ICON = "growl/images/fatal.png";
