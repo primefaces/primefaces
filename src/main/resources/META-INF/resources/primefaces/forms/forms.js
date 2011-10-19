96d5f21dc1c96642352df003a4b0d90cb306e5de
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2011-10-19 07:33:19 +0000

    Merge continued

--- src/main/resources/META-INF/resources/primefaces/forms/forms.js
+++ src/main/resources/META-INF/resources/primefaces/forms/forms.js
@@ -919,3 +919,51 @@ PrimeFaces.widget.SelectListbox.prototype.disable = function() {
     this.jq.addClass('ui-state-disabled');
     this.cfg.disabled = true;
 }
+
+/* 
+ * PrimeFaces CommandButton Widget
+ */
+PrimeFaces.widget.CommandButton = function(id, cfg) {
+	this.id = id;
+	this.cfg = cfg;
+	this.jqId = PrimeFaces.escapeClientId(id);
+    this.jq = $(this.jqId);
+	
+	this.jq.button(this.cfg);
+	
+	if(this.jq.attr('title') === 'ui-button') {
+        this.jq.removeAttr('title');
+    }
+    
+    this.postConstruct();
+}
+
+PrimeFaces.extend(PrimeFaces.widget.CommandButton, PrimeFaces.widget.BaseWidget);
+
+PrimeFaces.widget.CommandButton.prototype.disable = function() {
+    this.jq.button('disable');
+}
+
+PrimeFaces.widget.CommandButton.prototype.enable = function() {
+    this.jq.button('enable');
+}
+
+/*
+ * PrimeFaces Button Widget
+ */
+PrimeFaces.widget.Button = function(id, cfg) {
+	this.id = id;
+	this.cfg = cfg;
+	this.jqId = PrimeFaces.escapeClientId(id);
+    this.jq = $(this.jqId);
+
+	$(this.jqId).button(this.cfg);
+    
+    if(this.jq.attr('title') === 'ui-button') {
+        this.jq.removeAttr('title');
+    }
+    
+    this.postConstruct();
+}
+
+PrimeFaces.extend(PrimeFaces.widget.Button, PrimeFaces.widget.BaseWidget);
