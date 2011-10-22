f2ad1f10c52e71778e52d847e3e76d58044902c1
leventgny@gmail.com <leventgny@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2011-10-22 00:03:30 +0000

    Refactor

--- src/main/resources/META-INF/resources/primefaces/autocomplete/autocomplete.js
+++ src/main/resources/META-INF/resources/primefaces/autocomplete/autocomplete.js
@@ -254,20 +254,20 @@ PrimeFaces.widget.AutoComplete.prototype.search = function(value) {
                     
                     var items = _self.panel.find('.ui-autocomplete-item');
                     
-                    //highlight first item
-                    items.eq(0).addClass('ui-state-highlight');
-                    
-                    //highlight query string
-                    items.each(function() {
-                      var text = '<span class="ui-autocomplete-query">';
-                          + value 
-                          + '</span>'
-                          + this.textContent.substring(value.length);
-                      
-                      this.innerHTML = text;
-                    });
-                    
                     if(items.length > 0) {
+                        //highlight first item
+                        items.eq(0).addClass('ui-state-highlight');
+
+                        //highlight query string
+                        items.each(function() {
+                          var text = '<span class="ui-autocomplete-query">';
+                              + value 
+                              + '</span>'
+                              + this.textContent.substring(value.length);
+
+                          this.innerHTML = text;
+                        });
+                        
                         if(_self.cfg.forceSelection) {
                             _self.cachedResults = [];
                             items.each(function(i, item) {
