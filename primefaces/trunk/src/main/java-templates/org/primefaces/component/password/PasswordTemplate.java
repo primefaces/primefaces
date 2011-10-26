f6fe5be0e5b8aec4f951fc07a000f1cc7d0aa6a7
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2011-10-26 11:49:03 +0000

    Added match mode for password

--- primefaces/trunk/src/main/java-templates/org/primefaces/component/password/PasswordTemplate.java
+++ primefaces/trunk/src/main/java-templates/org/primefaces/component/password/PasswordTemplate.java
@@ -1,3 +1,45 @@
+import javax.faces.component.UIComponent;
+import javax.faces.FacesException;
+import javax.faces.application.FacesMessage;
+import org.primefaces.util.MessageFactory;
 
     public final static String STYLE_CLASS = "ui-inputfield ui-password ui-widget ui-state-default ui-corner-all";
     public final static String PLAIN_STYLE_CLASS = "ui-password";
+
+    public final static String INVALID_MATCH_KEY = "primefaces.password.INVALID_MATCH";
+
+    @Override
+	protected void validateValue(FacesContext context, Object value) {
+		super.validateValue(context, value);
+        String match = this.getMatch();
+
+        if(isValid() && match != null) {
+            Password matchWith = (Password) this.findComponent(match);
+            if(matchWith == null) {
+                throw new FacesException("Cannot find component " + match + " in view.");
+            }
+
+            Object matchValue = matchWith.getValue();
+
+            if(value != null && !value.equals(matchValue)) {
+                this.setValid(false);
+                matchWith.setValid(false);
+
+                String validatorMessage = getValidatorMessage();
+                FacesMessage msg = null;
+
+                if(validatorMessage != null) {
+                    msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, validatorMessage, validatorMessage);
+                }
+                else {
+                    Object[] params = new Object[2];
+                    params[0] = MessageFactory.getLabel(context, this);
+                    params[1] = MessageFactory.getLabel(context, matchWith);
+
+                    msg = MessageFactory.getMessage(Password.INVALID_MATCH_KEY, FacesMessage.SEVERITY_ERROR, params);
+                }
+
+                context.addMessage(getClientId(context), msg);
+            }
+        }
+	}
