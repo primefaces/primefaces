d035ebfa311ca88187a5fe51d4244b69ec6c8126
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2011-11-15 15:42:19 +0000

    Added iconPos to commandButton to place icon left or right.

--- src/main/java-templates/org/primefaces/component/commandbutton/CommandButtonTemplate.java
+++ src/main/java-templates/org/primefaces/component/commandbutton/CommandButtonTemplate.java
@@ -1,8 +1,10 @@
 
     public final static String TEXT_ONLY_BUTTON_CLASS = "ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only";
     public final static String ICON_ONLY_BUTTON_CLASS = "ui-button ui-widget ui-state-default ui-corner-all ui-button-icon-only";
-    public final static String TEXT_ICON_BUTTON_CLASS = "ui-button ui-widget ui-state-default ui-corner-all ui-button-text-icon-primary";
-    public final static String ICON_CLASS = "ui-button-icon-primary ui-icon";
+    public final static String TEXT_ICON_LEFT_BUTTON_CLASS = "ui-button ui-widget ui-state-default ui-corner-all ui-button-text-icon-left";
+    public final static String TEXT_ICON_RIGHT_BUTTON_CLASS = "ui-button ui-widget ui-state-default ui-corner-all ui-button-text-icon-right";
+    public final static String LEFT_ICON_CLASS = "ui-button-icon-left";
+    public final static String RIGHT_ICON_CLASS = "ui-button-icon-right";
     public final static String TEXT_CLASS = "ui-button-text";
 
     public String resolveIcon() {
@@ -20,12 +22,15 @@
         Object value = getValue();
         String styleClass = ""; 
     
-        if(value != null && icon == null)
+        if(value != null && icon == null) {
             styleClass = TEXT_ONLY_BUTTON_CLASS;
-        else if(value != null && icon != null)
-            styleClass = TEXT_ICON_BUTTON_CLASS;
-        else if(value == null && icon != null)
+        }
+        else if(value != null && icon != null) {
+            styleClass = getIconPos().equals("left") ? TEXT_ICON_LEFT_BUTTON_CLASS : TEXT_ICON_RIGHT_BUTTON_CLASS;
+        }
+        else if(value == null && icon != null) {
             styleClass = ICON_ONLY_BUTTON_CLASS;
+        }
     
         if(isDisabled()) {
             styleClass = styleClass + " ui-state-disabled";
