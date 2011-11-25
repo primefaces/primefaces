2e10cebb4726c73133ae44ab9872b21f8bd7f59c
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2011-11-25 08:09:42 +0000

    No corner

--- primefaces/trunk/src/main/java-templates/org/primefaces/component/treetable/TreeTableTemplate.java
+++ primefaces/trunk/src/main/java-templates/org/primefaces/component/treetable/TreeTableTemplate.java
@@ -19,7 +19,7 @@ import org.primefaces.component.column.Column;
 import java.lang.StringBuilder;
 
 	public final static String CONTAINER_CLASS = "ui-treetable ui-widget";
-    public final static String HEADER_CLASS = "ui-treetable-header ui-widget-header ui-corner-top";
+    public final static String HEADER_CLASS = "ui-treetable-header ui-widget-header";
 	public final static String DATA_CLASS = "ui-treetable-data";
     public final static String FOOTER_CLASS = "ui-treetable-footer ui-widget-header";
     public final static String COLUMN_HEADER_CLASS = "ui-state-default";
