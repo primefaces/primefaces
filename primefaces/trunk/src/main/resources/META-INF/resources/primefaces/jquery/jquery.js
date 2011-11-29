288759edec372487ddcd50e20931a5d418e252ec
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2011-11-29 08:24:06 +0000

    Fixed #2925

--- primefaces/trunk/src/main/resources/META-INF/resources/primefaces/jquery/jquery.js
+++ primefaces/trunk/src/main/resources/META-INF/resources/primefaces/jquery/jquery.js
@@ -18734,3 +18734,108 @@ jQuery.extend( jQuery.easing,
 		return jQuery.easing.easeOutBounce (x, t*2-d, 0, c, d) * .5 + c*.5 + b;
 	}
 });
+
+/* General Utilities */
+
+/**
+ * jQuery Cookie plugin
+ *
+ * Copyright (c) 2010 Klaus Hartl (stilbuero.de)
+ * Dual licensed under the MIT and GPL licenses:
+ * http://www.opensource.org/licenses/mit-license.php
+ * http://www.gnu.org/licenses/gpl.html
+ *
+ */
+jQuery.cookie = function (key, value, options) {
+
+    // key and value given, set cookie...
+    if (arguments.length > 1 && (value === null || typeof value !== "object")) {
+        options = jQuery.extend({}, options);
+
+        if (value === null) {
+            options.expires = -1;
+        }
+
+        if (typeof options.expires === 'number') {
+            var days = options.expires, t = options.expires = new Date();
+            t.setDate(t.getDate() + days);
+        }
+
+        return (document.cookie = [
+            encodeURIComponent(key), '=',
+            options.raw ? String(value) : encodeURIComponent(String(value)),
+            options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
+            options.path ? '; path=' + options.path : '',
+            options.domain ? '; domain=' + options.domain : '',
+            options.secure ? '; secure' : ''
+        ].join(''));
+    }
+
+    // key and possibly options given, get cookie...
+    options = value || {};
+    var result, decode = options.raw ? function (s) {return s;} : decodeURIComponent;
+    return (result = new RegExp('(?:^|; )' + encodeURIComponent(key) + '=([^;]*)').exec(document.cookie)) ? decode(result[1]) : null;
+};
+
+/*jQuery MouseWheel Plugin
+ *
+ *! Copyright (c) 2009 Brandon Aaron (http://brandonaaron.net)
+ * Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php)
+ * and GPL (http://www.opensource.org/licenses/gpl-license.php) licenses.
+ * Thanks to: http://adomas.org/javascript-mouse-wheel/ for some pointers.
+ * Thanks to: Mathias Bank(http://www.mathias-bank.de) for a scope bug fix.
+ *
+ * Version: 3.0.2
+ * 
+ * Requires: 1.2.2+
+ */
+
+(function($) {
+
+    var types = ['DOMMouseScroll', 'mousewheel'];
+
+    $.event.special.mousewheel = {
+        setup: function() {
+            if ( this.addEventListener )
+                for ( var i=types.length; i; )
+                    this.addEventListener( types[--i], handler, false );
+            else
+                this.onmousewheel = handler;
+        },
+
+        teardown: function() {
+            if ( this.removeEventListener )
+                for ( var i=types.length; i; )
+                    this.removeEventListener( types[--i], handler, false );
+            else
+                this.onmousewheel = null;
+        }
+    };
+
+    $.fn.extend({
+        mousewheel: function(fn) {
+            return fn ? this.bind("mousewheel", fn) : this.trigger("mousewheel");
+        },
+
+        unmousewheel: function(fn) {
+            return this.unbind("mousewheel", fn);
+        }
+    });
+
+
+    function handler(event) {
+        var args = [].slice.call( arguments, 1 ), delta = 0, returnValue = true;
+
+        event = $.event.fix(event || window.event);
+        event.type = "mousewheel";
+
+        if ( event.wheelDelta ) delta = event.wheelDelta/120;
+        if ( event.detail     ) delta = -event.detail/3;
+
+        // Add events and delta to the front of the arguments
+        args.unshift(event, delta);
+
+        return $.event.handle.apply(this, args);
+    }
+
+})(jQuery);
