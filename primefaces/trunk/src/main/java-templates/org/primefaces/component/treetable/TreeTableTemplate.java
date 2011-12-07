135f671d59a178a0475e361b8d2b49013b58cd5b
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2011-12-07 15:31:00 +0000

    Fixed treetable issues #2998, #2999, #3000

--- primefaces/trunk/src/main/java-templates/org/primefaces/component/treetable/TreeTableTemplate.java
+++ primefaces/trunk/src/main/java-templates/org/primefaces/component/treetable/TreeTableTemplate.java
@@ -20,7 +20,7 @@ import java.lang.StringBuilder;
 
 	public final static String CONTAINER_CLASS = "ui-treetable ui-widget";
     public final static String HEADER_CLASS = "ui-treetable-header ui-widget-header";
-	public final static String DATA_CLASS = "ui-treetable-data";
+	public final static String DATA_CLASS = "ui-treetable-data ui-widget-content";
     public final static String FOOTER_CLASS = "ui-treetable-footer ui-widget-header";
     public final static String COLUMN_HEADER_CLASS = "ui-state-default";
     public final static String ROW_CLASS = "ui-widget-content";
@@ -106,7 +106,6 @@ import java.lang.StringBuilder;
     public void processDecodes(FacesContext context) {
         if(isToggleRequest(context)) {
             this.decode(context);
-            context.renderResponse();
         } else {
             super.processDecodes(context);
         }
