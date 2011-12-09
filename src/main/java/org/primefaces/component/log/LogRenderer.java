4c1d49ba41a64fbdcac194c30c4dcac2749568b5
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2011-12-09 09:38:09 +0000

    More log component work

--- src/main/java/org/primefaces/component/log/LogRenderer.java
+++ src/main/java/org/primefaces/component/log/LogRenderer.java
@@ -34,15 +34,46 @@ public class LogRenderer extends CoreRenderer {
     protected void encodeMarkup(FacesContext context, Log log) throws IOException {
         ResponseWriter writer = context.getResponseWriter();
         
+        //container
         writer.startElement("div", log);
         writer.writeAttribute("id", log.getClientId(context), "id");
         writer.writeAttribute("class", Log.CONTAINER_CLASS, null);
         
+        //header
+        writer.startElement("div", null);
+        writer.writeAttribute("class", Log.HEADER_CLASS, null);
+        encodeIcon(context, Log.CLEAR_BUTTON_CLASS, "ui-icon ui-icon-trash", "Clear");
+        encodeIcon(context, Log.ALL_BUTTON_CLASS, "ui-icon ui-icon-note", "All");
+        encodeIcon(context, Log.INFO_BUTTON_CLASS, "ui-icon ui-icon-info", "Info");
+        encodeIcon(context, Log.WARN_BUTTON_CLASS, "ui-icon ui-icon-notice", "Warn");
+        encodeIcon(context, Log.DEBUG_BUTTON_CLASS, "ui-icon ui-icon-search", "Debug");
+        encodeIcon(context, Log.ERROR_BUTTON_CLASS, "ui-icon ui-icon-alert", "Error");
+        writer.endElement("div");
+        
+        //content
+        writer.startElement("div", log);
+        writer.writeAttribute("class", Log.CONTENT_CLASS, null);
         writer.startElement("ul", null);
         writer.writeAttribute("class", Log.ITEMS_CLASS, null);
         writer.endElement("ul");
+        writer.endElement("div");
         
         writer.endElement("div");
+    }
+    
+    protected void encodeIcon(FacesContext context, String anchorClass, String iconClass, String title) throws IOException {
+        ResponseWriter writer = context.getResponseWriter();
+        
+        writer.startElement("a", null);
+        writer.writeAttribute("href", "#", null);
+        writer.writeAttribute("class", anchorClass, null);
+        writer.writeAttribute("title", title, null);
+
+        writer.startElement("span", null);
+        writer.writeAttribute("class", iconClass, null);
+        writer.endElement("span");
+
+        writer.endElement("a");
     }
     
     protected void encodeScript(FacesContext context, Log log) throws IOException {
--- src/main/resources/META-INF/resources/primefaces/log/log.js
+++ src/main/resources/META-INF/resources/primefaces/log/log.js
@@ -6,23 +6,101 @@ PrimeFaces.widget.Log = function(cfg) {
     this.id = this.cfg.id;
     this.jqId = PrimeFaces.escapeClientId(this.id);
     this.jq = $(this.jqId);
-    this.itemsContainer = this.jq.children('.ui-log-items');
+    this.header = this.jq.children('.ui-log-header');
+    this.content = this.jq.children('.ui-log-content');
+    this.itemsContainer = this.content.find('.ui-log-items');
+    this.filters = this.header.children('.ui-log-button');
+    var _self = this;
+    
+    //make draggable
+    this.jq.draggable({handle:this.header});
+    this.header.mousedown(function(e) {
+        _self.jq.zIndex(++PrimeFaces.zindex);
+    });
+    
+    //attach events
+    this.bindEvents();
 
+    //attach
     PrimeFaces.logger = this;
 }
 
+PrimeFaces.widget.Log.prototype.bindEvents = function() {
+    var _self = this;
+    
+    //visuals
+    this.header.children('.ui-log-button').mouseover(function() {
+        var el = $(this);
+        if(!el.hasClass('ui-state-active'))
+            $(this).addClass('ui-state-hover');
+    }).mouseout(function() {
+        $(this).removeClass('ui-state-hover');
+    });
+    
+    //clear
+    this.header.children('.ui-log-clear').click(function(e) {
+        _self.itemsContainer.html('');
+        _self.filters.filter('.ui-state-active').removeClass('ui-state-active');
+        _self.filters.filter('.ui-log-all').addClass('ui-state-active');
+        e.preventDefault();
+    });
+    
+    //all
+    this.header.children('.ui-log-all').click(function(e) {
+        _self.itemsContainer.children().show();
+        _self.filters.filter('.ui-state-active').removeClass('ui-state-active');
+        $(this).addClass('ui-state-active').removeClass('ui-state-hover');
+        e.preventDefault();
+    });
+    
+    //info
+    this.header.children('.ui-log-info').click(function(e) {
+        _self.handleFilterClick(e, '.ui-log-item-info', $(this));
+    });
+    
+    //warn
+    this.header.children('.ui-log-warn').click(function(e) {
+        _self.handleFilterClick(e, '.ui-log-item-warn', $(this));
+    });
+    
+    //debug
+    this.header.children('.ui-log-debug').click(function(e) {
+        _self.handleFilterClick(e, '.ui-log-item-debug', $(this));
+    });
+    
+    //error
+    this.header.children('.ui-log-error').click(function(e) {
+        _self.handleFilterClick(e, '.ui-log-item-error', $(this));
+    });
+}
+
 PrimeFaces.widget.Log.prototype.info = function(msg) {
-    this.itemsContainer.append('<li class="ui-log-item">' + msg + '</li>');
+    this.add(msg, 'info', 'ui-icon-info');
 }
 
 PrimeFaces.widget.Log.prototype.warn = function(msg) {
-    this.itemsContainer.append('<li class="ui-log-item">' + msg + '</li>');
+    this.add(msg, 'warn', 'ui-icon-notice');
 }
 
 PrimeFaces.widget.Log.prototype.debug = function(msg) {
-    this.itemsContainer.append('<li class="ui-log-item">' + msg + '</li>');
+    this.add(msg, 'debug', 'ui-icon-search');
 }
 
 PrimeFaces.widget.Log.prototype.error = function(msg) {
-    this.itemsContainer.append('<li class="ui-log-item">' + msg + '</li>');
+    this.add(msg, 'error', 'ui-icon-alert');
+}
+
+PrimeFaces.widget.Log.prototype.add = function(msg, severity, icon) {
+    this.itemsContainer.append('<li class="ui-log-item ui-log-item-' + severity + ' ui-helper-clearfix"><span class="ui-icon ' + icon + '"></span>' + msg + '</li>');
+}
+
+PrimeFaces.widget.Log.prototype.filter = function(severity) {
+    this.itemsContainer.children().hide().filter(severity).show();
+}
+
+PrimeFaces.widget.Log.prototype.handleFilterClick = function(event, severity, button) {
+    this.filter(severity);
+    this.filters.filter('.ui-state-active').removeClass('ui-state-active');
+    button.addClass('ui-state-active').removeClass('ui-state-hover');
+    event.preventDefault();
 }
