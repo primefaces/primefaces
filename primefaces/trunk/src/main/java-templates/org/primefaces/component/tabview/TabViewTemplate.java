0a408540a0bd17ef5ade9b56e06bc8ad70992245
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2012-01-05 10:41:15 +0000

    Bottom orientation mode for tabview

--- primefaces/trunk/src/main/java-templates/org/primefaces/component/tabview/TabViewTemplate.java
+++ primefaces/trunk/src/main/java-templates/org/primefaces/component/tabview/TabViewTemplate.java
@@ -19,8 +19,8 @@ import javax.faces.component.visit.VisitResult;
 
     public static final String CONTAINER_CLASS = "ui-tabs ui-widget ui-widget-content ui-corner-all ui-hidden-container";
     public static final String NAVIGATOR_CLASS = "ui-tabs-nav ui-helper-reset ui-helper-clearfix ui-widget-header ui-corner-all";
-    public static final String INACTIVE_TAB_HEADER_CLASS = "ui-state-default ui-corner-top";
-    public static final String ACTIVE_TAB_HEADER_CLASS = "ui-state-default ui-corner-top ui-tabs-selected ui-state-active";
+    public static final String INACTIVE_TAB_HEADER_CLASS = "ui-state-default";
+    public static final String ACTIVE_TAB_HEADER_CLASS = "ui-state-default ui-tabs-selected ui-state-active";
     public static final String PANELS_CLASS = "ui-tabs-panels";
     public static final String ACTIVE_TAB_CONTENT_CLASS = "ui-tabs-panel ui-widget-content ui-corner-bottom";
     public static final String INACTIVE_TAB_CONTENT_CLASS = "ui-tabs-panel ui-widget-content ui-corner-bottom ui-helper-hidden";
