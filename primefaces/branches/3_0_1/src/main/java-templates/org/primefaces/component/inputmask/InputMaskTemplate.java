1175c45a3b935a229523a1ec54b860b4640f32c3
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2012-01-17 12:49:13 +0000

    remove 3.0.1 branch

--- primefaces/branches/3_0_1/src/main/java-templates/org/primefaces/component/inputmask/InputMaskTemplate.java
+++
@@ -1,3 +0,0 @@
-
-    public final static String THEME_INPUT_CLASS = "ui-inputfield ui-inputmask ui-widget ui-state-default ui-corner-all";
-    public final static String PLAIN_INPUT_CLASS = "ui-inputmask";
--- primefaces/branches/3_0_1/src/main/java-templates/org/primefaces/component/keyboard/KeyboardTemplate.java
+++
@@ -1,3 +0,0 @@
-
-    public final static String THEME_INPUT_CLASS = "ui-inputfield ui-keyboard-input ui-widget ui-state-default ui-corner-all";
-    public final static String PLAIN_INPUT_CLASS = "ui-keyboard-input";
