c77e8c29613b539fe40a2123b287287f37217deb
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2012-01-21 09:19:00 +0000

    Pom update

--- src/main/java/org/primefaces/component/lightbox/LightBoxRenderer.java
+++ src/main/java/org/primefaces/component/lightbox/LightBoxRenderer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2011 Prime Teknoloji.
+ * Copyright 2009-2012 Prime Teknoloji.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
