b2111923687415def67074329b910c7a675842b9
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2012-01-29 10:17:05 +0000

    UI flicker fix selectonemenu on page load

--- src/main/java/org/primefaces/component/selectonemenu/SelectOneMenuRenderer.java
+++ src/main/java/org/primefaces/component/selectonemenu/SelectOneMenuRenderer.java
@@ -245,8 +245,7 @@ public class SelectOneMenuRenderer extends SelectOneRenderer {
         String clientId = menu.getClientId(context);
 
         startScript(writer, clientId);
-        
-        writer.write("$(function(){");
+
         writer.write("PrimeFaces.cw('SelectOneMenu','" + menu.resolveWidgetVar() + "',{");
         writer.write("id:'" + clientId + "'");
         writer.write(",effect:'" + menu.getEffect() + "'");
@@ -257,7 +256,7 @@ public class SelectOneMenuRenderer extends SelectOneRenderer {
 
         encodeClientBehaviors(context, menu);
 
-        writer.write("});});");
+        writer.write("});");
 
         endScript(writer);
     }
