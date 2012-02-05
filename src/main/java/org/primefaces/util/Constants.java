b1f2359fa93a64a7712d5c344eba595f66f427af
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2012-02-05 22:39:48 +0000

    New dev iter

--- src/main/java/org/primefaces/util/Constants.java
+++ src/main/java/org/primefaces/util/Constants.java
@@ -29,7 +29,7 @@ public class Constants {
     
     public static final String DOWNLOAD_COOKIE = "primefaces.download";
 
-    public final static String VERSION = "3.1-SNAPSHOT";
+    public final static String VERSION = "3.2-SNAPSHOT";
     public final static String LIBRARY = "primefaces";
 
     public final static String CUSTOM_EVENT = "CUSTOM_EVENT";
