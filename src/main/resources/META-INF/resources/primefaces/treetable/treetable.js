8db69c1f0cab47e8dccb093a1f66bc82da617797
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2012-02-22 10:42:58 +0000

    Migrated treetable to new widget architecture

--- src/main/resources/META-INF/resources/primefaces/treetable/treetable.js
+++ src/main/resources/META-INF/resources/primefaces/treetable/treetable.js
@@ -1,330 +1,327 @@
 /**
  * PrimeFaces TreeTable Widget
  */
-PrimeFaces.widget.TreeTable = function(cfg) {
-    this.cfg = cfg;
-    this.id = this.cfg.id;
-	this.jqId = PrimeFaces.escapeClientId(this.id);
-    this.jq = $(this.jqId);
-	this.cfg = cfg;
-    this.cfg.scrollable = this.jq.hasClass('ui-treetable-scrollable');
-    this.cfg.resizable = this.jq.hasClass('ui-treetable-resizable');
+PrimeFaces.widget.TreeTable = PrimeFaces.widget.BaseWidget.extend({
     
-    this.bindToggleEvents();
+    init: function(cfg) {
+        this._super(cfg);
         
-    //scrolling
-    if(this.cfg.scrollable) {
-        this.alignColumnWidths();
-        this.setupScrolling();
-    }
+        this.cfg.scrollable = this.jq.hasClass('ui-treetable-scrollable');
+        this.cfg.resizable = this.jq.hasClass('ui-treetable-resizable');
 
-    //selection
-    if(this.cfg.selectionMode) {
-        this.jqSelection = $(this.jqId + '_selection');
-        var selectionValue = this.jqSelection.val();
-        
-        this.selection = selectionValue === "" ? [] : selectionValue.split(',');
-        
-        this.bindSelectionEvents();
-    }
-    
-    this.postConstruct();
-}
+        this.bindToggleEvents();
 
-PrimeFaces.extend(PrimeFaces.widget.TreeTable, PrimeFaces.widget.BaseWidget);
-
-PrimeFaces.widget.TreeTable.prototype.bindToggleEvents = function() {
-    var _self = this;
-    
-    //expand and collapse
-    $(this.jqId + ' .ui-treetable-toggler').die('click.treetable')
-        .live('click.treetable', function(e) {
-            var toggler = $(this),
-            node = toggler.parents('tr:first');
-            
-            if(toggler.hasClass('ui-icon-triangle-1-e'))
-                _self.expandNode(e, node);
-            else {
-                _self.collapseNode(e, node);
-            }
-        });
-}
-
-PrimeFaces.widget.TreeTable.prototype.bindSelectionEvents = function() {
-    var _self = this;
-    
-    $(this.jqId + ' .ui-treetable-data tr.ui-treetable-selectable-node').die('mouseover.treetable mouseout.treetable click.treetable contextmenu.treetable')
-            .live('mouseover.treetable', function(e) {
-                var element = $(this);
-
-                if(!element.hasClass('ui-state-highlight')) {
-                    element.addClass('ui-state-hover');
-                }
-            })
-            .live('mouseout.treetable', function(e) {
-                var element = $(this);
-
-                if(!element.hasClass('ui-state-highlight')) {
-                    element.removeClass('ui-state-hover');
-                }
-            })
-            .live('click.treetable', function(e) {
-                _self.onRowClick(e, $(this));
-                e.preventDefault();
-            })           
-            .live('contextmenu.treetable', function(event) {
-               _self.onRowClick(event, $(this));
-               event.preventDefault();
-            });
-}
-
-PrimeFaces.widget.TreeTable.prototype.expandNode = function(e, node) {
-    var options = {
-        source: this.id,
-        process: this.id,
-        update: this.id
-    },
-    _self = this,
-    nodeKey = node.attr('id').split('_node_')[1];
-    
-    options.onsuccess = function(responseXML) {
-        var xmlDoc = $(responseXML.documentElement),
-        updates = xmlDoc.find("update");
-
-        for(var i=0; i < updates.length; i++) {
-            var update = updates.eq(i),
-            id = update.attr('id'),
-            content = update.text();
-
-            if(id == _self.id){
-                node.replaceWith(content);
-                node.find('.ui-treetable-toggler:first').addClass('ui-icon-triangle-1-s').removeClass('ui-icon-triangle-1-e');
-                node.attr('aria-expanded', true);
-            }
-            else {
-                PrimeFaces.ajax.AjaxUtils.updateElement.call(this, id, content);
-            }
+        //scrolling
+        if(this.cfg.scrollable) {
+            this.alignColumnWidths();
+            this.setupScrolling();
         }
 
-        PrimeFaces.ajax.AjaxUtils.handleResponse.call(this, xmlDoc);
-        
-        return true;
-    };
-    
-    var params = {};
-    params[this.id + '_expand'] = nodeKey;
-    
-    options.params = params;
-    
-    if(this.hasBehavior('expand')) {
-        var expandBehavior = this.cfg.behaviors['expand'];
-        
-        expandBehavior.call(this, e, options);
-    }
-    else {
-        PrimeFaces.ajax.AjaxRequest(options);
-    }
-}
+        //selection
+        if(this.cfg.selectionMode) {
+            this.jqSelection = $(this.jqId + '_selection');
+            var selectionValue = this.jqSelection.val();
 
-PrimeFaces.widget.TreeTable.prototype.collapseNode = function(e, node) {
-    node.siblings('[id^="' + node.attr('id') + '_"]').remove();
+            this.selection = selectionValue === "" ? [] : selectionValue.split(',');
 
-    node.find('.ui-treetable-toggler:first').addClass('ui-icon-triangle-1-e').removeClass('ui-icon-triangle-1-s');
+            this.bindSelectionEvents();
+        }
+    },
     
-    node.attr('aria-expanded', false);
+    bindToggleEvents: function() {
+        var _self = this;
+
+        //expand and collapse
+        $(this.jqId + ' .ui-treetable-toggler').die('click.treetable')
+            .live('click.treetable', function(e) {
+                var toggler = $(this),
+                node = toggler.parents('tr:first');
+
+                if(toggler.hasClass('ui-icon-triangle-1-e'))
+                    _self.expandNode(e, node);
+                else {
+                    _self.collapseNode(e, node);
+                }
+            });
+    },
     
-    if(this.hasBehavior('collapse')) {
-        var collapseBehavior = this.cfg.behaviors['collapse'],
-        nodeKey = node.attr('id').split('_node_')[1];
-        
+    bindSelectionEvents: function() {
+        var _self = this;
+
+        $(this.jqId + ' .ui-treetable-data tr.ui-treetable-selectable-node').die('mouseover.treetable mouseout.treetable click.treetable contextmenu.treetable')
+                .live('mouseover.treetable', function(e) {
+                    var element = $(this);
+
+                    if(!element.hasClass('ui-state-highlight')) {
+                        element.addClass('ui-state-hover');
+                    }
+                })
+                .live('mouseout.treetable', function(e) {
+                    var element = $(this);
+
+                    if(!element.hasClass('ui-state-highlight')) {
+                        element.removeClass('ui-state-hover');
+                    }
+                })
+                .live('click.treetable', function(e) {
+                    _self.onRowClick(e, $(this));
+                    e.preventDefault();
+                })           
+                .live('contextmenu.treetable', function(event) {
+                _self.onRowClick(event, $(this));
+                event.preventDefault();
+                });
+    },
+    
+    expandNode: function(e, node) {
         var options = {
-            params : {}
+            source: this.id,
+            process: this.id,
+            update: this.id
+        },
+        _self = this,
+        nodeKey = node.attr('id').split('_node_')[1];
+
+        options.onsuccess = function(responseXML) {
+            var xmlDoc = $(responseXML.documentElement),
+            updates = xmlDoc.find("update");
+
+            for(var i=0; i < updates.length; i++) {
+                var update = updates.eq(i),
+                id = update.attr('id'),
+                content = update.text();
+
+                if(id == _self.id){
+                    node.replaceWith(content);
+                    node.find('.ui-treetable-toggler:first').addClass('ui-icon-triangle-1-s').removeClass('ui-icon-triangle-1-e');
+                    node.attr('aria-expanded', true);
+                }
+                else {
+                    PrimeFaces.ajax.AjaxUtils.updateElement.call(this, id, content);
+                }
+            }
+
+            PrimeFaces.ajax.AjaxUtils.handleResponse.call(this, xmlDoc);
+
+            return true;
         };
-        options.params[this.id + '_collapse'] = nodeKey;
-        
-        collapseBehavior.call(this, e, options);
-    }
-}
 
-PrimeFaces.widget.TreeTable.prototype.onRowClick = function(e, node) {
+        var params = {};
+        params[this.id + '_expand'] = nodeKey;
+
+        options.params = params;
+
+        if(this.hasBehavior('expand')) {
+            var expandBehavior = this.cfg.behaviors['expand'];
+
+            expandBehavior.call(this, e, options);
+        }
+        else {
+            PrimeFaces.ajax.AjaxRequest(options);
+        }
+    },
     
-    //Check if rowclick triggered this event not an element in row content
-    if($(e.target).is('div.ui-tt-c,td')) {
-        var selected = node.hasClass('ui-state-highlight');
+    collapseNode: function(e, node) {
+        node.siblings('[id^="' + node.attr('id') + '_"]').remove();
 
-        if(selected)
-            this.unselectNode(e, node);
-        else
-            this.selectNode(e, node);
-        
-        PrimeFaces.clearSelection();
-    }
-}
+        node.find('.ui-treetable-toggler:first').addClass('ui-icon-triangle-1-e').removeClass('ui-icon-triangle-1-s');
 
-PrimeFaces.widget.TreeTable.prototype.selectNode = function(e, node) {
-    var nodeKey = node.attr('id').split('_node_')[1],
-    metaKey = (e.metaKey||e.ctrlKey);
+        node.attr('aria-expanded', false);
+
+        if(this.hasBehavior('collapse')) {
+            var collapseBehavior = this.cfg.behaviors['collapse'],
+            nodeKey = node.attr('id').split('_node_')[1];
+
+            var options = {
+                params : {}
+            };
+            options.params[this.id + '_collapse'] = nodeKey;
+
+            collapseBehavior.call(this, e, options);
+        }
+    },
     
-    //unselect previous selection
-    if(this.isSingleSelection() || (this.isMultipleSelection() && !metaKey)) {
-        node.siblings('.ui-state-highlight').removeClass('ui-state-highlight').attr('aria-selected', false);
-        this.selection = [];
-    }
-
-    //add to selection
-    node.removeClass('ui-state-hover').addClass('ui-state-highlight').attr('aria-selected', true);
-    this.addSelection(nodeKey);
-
-    //save state
-    this.writeSelections();
+    onRowClick: function(e, node) {
     
-    this.fireSelectNodeEvent(e, nodeKey);
-}
+        //Check if rowclick triggered this event not an element in row content
+        if($(e.target).is('div.ui-tt-c,td')) {
+            var selected = node.hasClass('ui-state-highlight');
 
-PrimeFaces.widget.TreeTable.prototype.unselectNode = function(e, node) {
-    var nodeKey = node.attr('id').split('_node_')[1],
-    metaKey = metaKey = (e.metaKey||e.ctrlKey);
+            if(selected)
+                this.unselectNode(e, node);
+            else
+                this.selectNode(e, node);
 
-    if(metaKey) {
-        //remove visual style
-        node.removeClass('ui-state-highlight');
-        
-        //aria
-        node.attr('aria-selected', false);
+            PrimeFaces.clearSelection();
+        }
+    },
+    
+    selectNode: function(e, node) {
+        var nodeKey = node.attr('id').split('_node_')[1],
+        metaKey = (e.metaKey||e.ctrlKey);
 
-        //remove from selection
-        this.removeSelection(nodeKey);
+        //unselect previous selection
+        if(this.isSingleSelection() || (this.isMultipleSelection() && !metaKey)) {
+            node.siblings('.ui-state-highlight').removeClass('ui-state-highlight').attr('aria-selected', false);
+            this.selection = [];
+        }
+
+        //add to selection
+        node.removeClass('ui-state-hover').addClass('ui-state-highlight').attr('aria-selected', true);
+        this.addSelection(nodeKey);
 
         //save state
         this.writeSelections();
-        
-        this.fireUnselectNodeEvent(e, nodeKey);
-    }
-    else if(this.isMultipleSelection()){
-        this.selectNode(e, node);
-    }
-}
 
-PrimeFaces.widget.TreeTable.prototype.hasBehavior = function(event) {
-    if(this.cfg.behaviors) {
-        return this.cfg.behaviors[event] != undefined;
-    }
+        this.fireSelectNodeEvent(e, nodeKey);
+    },
     
-    return false;
-}
+    unselectNode: function(e, node) {
+        var nodeKey = node.attr('id').split('_node_')[1],
+        metaKey = metaKey = (e.metaKey||e.ctrlKey);
 
-/**
- * Remove given rowIndex from selection
- */
-PrimeFaces.widget.TreeTable.prototype.removeSelection = function(nodeKey) {
-    var selection = this.selection;
-    
-    $.each(selection, function(index, value) {
-        if(value === nodeKey) {
-            selection.remove(index);
-            
-            return false;       //break
-        } 
-        else {
-            return true;        //continue
+        if(metaKey) {
+            //remove visual style
+            node.removeClass('ui-state-highlight');
+
+            //aria
+            node.attr('aria-selected', false);
+
+            //remove from selection
+            this.removeSelection(nodeKey);
+
+            //save state
+            this.writeSelections();
+
+            this.fireUnselectNodeEvent(e, nodeKey);
         }
-    });
-}
-
-/**
- * Adds given rowIndex to selection if it doesn't exist already
- */
-PrimeFaces.widget.TreeTable.prototype.addSelection = function(nodeKey) {
-    if(!this.isSelected(nodeKey)) {
-        this.selection.push(nodeKey);
-    }
-}
-
-PrimeFaces.widget.TreeTable.prototype.isSelected = function(nodeKey) {
-    var selection = this.selection,
-    selected = false;
-    
-    $.each(selection, function(index, value) {
-        if(value === nodeKey) {
-            selected = true;
-            
-            return false;       //break
-        } 
-        else {
-            return true;        //continue
+        else if(this.isMultipleSelection()){
+            this.selectNode(e, node);
         }
-    });
+    },
     
-    return selected;
-}
+    hasBehavior: function(event) {
+        if(this.cfg.behaviors) {
+            return this.cfg.behaviors[event] != undefined;
+        }
 
-PrimeFaces.widget.TreeTable.prototype.isSingleSelection = function() {
-    return this.cfg.selectionMode == 'single';
-}
-
-PrimeFaces.widget.TreeTable.prototype.isMultipleSelection = function() {
-    return this.cfg.selectionMode == 'multiple';
-}
-
-/**
- * Writes selected row ids to state holder
- */
-PrimeFaces.widget.TreeTable.prototype.writeSelections = function() {
-    this.jqSelection.val(this.selection.join(','));
-}
-
-PrimeFaces.widget.TreeTable.prototype.fireSelectNodeEvent = function(e, nodeKey) {
-    if(this.hasBehavior('select')) {
-        var selectBehavior = this.cfg.behaviors['select'],
-        options = {
-            params : {}
-        };
-        
-        options.params[this.id + '_instantSelect'] = nodeKey;
-        
-        selectBehavior.call(this, e, options);
-    }
-}
-
-PrimeFaces.widget.TreeTable.prototype.fireUnselectNodeEvent = function(e, nodeKey) {
-    if(this.hasBehavior('unselect')) {
-        var unselectBehavior = this.cfg.behaviors['unselect'],
-        options = {
-            params : {}
-        };
-        
-        options.params[this.id + '_instantUnselect'] = nodeKey;
-        
-        unselectBehavior.call(this, e, options);
-    }
-}
-
-PrimeFaces.widget.TreeTable.prototype.setupScrolling = function() {
-    var scrollHeader = $(this.jqId + ' .ui-treetable-scrollable-header'),
-    scrollBody = $(this.jqId + ' .ui-treetable-scrollable-body'),
-    scrollFooter = $(this.jqId + ' .ui-treetable-scrollable-footer');
+        return false;
+    },
     
-    if(this.cfg.scrollWidth) {
-        scrollHeader.width(this.cfg.scrollWidth);
-        scrollBody.width(this.cfg.scrollWidth);
-        scrollFooter.width(this.cfg.scrollWidth);
-    }
-        
-    scrollBody.scroll(function() {
-        scrollHeader.scrollLeft(scrollBody.scrollLeft());
-        scrollFooter.scrollLeft(scrollBody.scrollLeft());
-    });
-}
+    /**
+     * Remove given rowIndex from selection
+     */
+    removeSelection: function(nodeKey) {
+        var selection = this.selection;
 
-/**
- * Moves widths of columns to column wrappers
- */
-PrimeFaces.widget.TreeTable.prototype.alignColumnWidths = function() {
-    this.jq.find('div.ui-tt-c').each(function() {
-        var wrapper = $(this),
-        column = wrapper.parent();
-        
-        wrapper.width(column.width());
-        column.width('');
-    });
-}
+        $.each(selection, function(index, value) {
+            if(value === nodeKey) {
+                selection.remove(index);
+
+                return false;       //break
+            } 
+            else {
+                return true;        //continue
+            }
+        });
+    },
+    
+    /**
+     * Adds given rowIndex to selection if it doesn't exist already
+     */
+    addSelection: function(nodeKey) {
+        if(!this.isSelected(nodeKey)) {
+            this.selection.push(nodeKey);
+        }
+    },
+    
+    isSelected: function(nodeKey) {
+        var selection = this.selection,
+        selected = false;
+
+        $.each(selection, function(index, value) {
+            if(value === nodeKey) {
+                selected = true;
+
+                return false;       //break
+            } 
+            else {
+                return true;        //continue
+            }
+        });
+
+        return selected;
+    },
+    
+    isSingleSelection: function() {
+        return this.cfg.selectionMode == 'single';
+    },
+    
+    isMultipleSelection: function() {
+        return this.cfg.selectionMode == 'multiple';
+    },
+    
+    /**
+     * Writes selected row ids to state holder
+     */
+    writeSelections: function() {
+        this.jqSelection.val(this.selection.join(','));
+    },
+    
+    fireSelectNodeEvent: function(e, nodeKey) {
+        if(this.hasBehavior('select')) {
+            var selectBehavior = this.cfg.behaviors['select'],
+            options = {
+                params : {}
+            };
+
+            options.params[this.id + '_instantSelect'] = nodeKey;
+
+            selectBehavior.call(this, e, options);
+        }
+    },
+    
+    fireUnselectNodeEvent: function(e, nodeKey) {
+        if(this.hasBehavior('unselect')) {
+            var unselectBehavior = this.cfg.behaviors['unselect'],
+            options = {
+                params : {}
+            };
+
+            options.params[this.id + '_instantUnselect'] = nodeKey;
+
+            unselectBehavior.call(this, e, options);
+        }
+    },
+    
+    setupScrolling: function() {
+        var scrollHeader = $(this.jqId + ' .ui-treetable-scrollable-header'),
+        scrollBody = $(this.jqId + ' .ui-treetable-scrollable-body'),
+        scrollFooter = $(this.jqId + ' .ui-treetable-scrollable-footer');
+
+        if(this.cfg.scrollWidth) {
+            scrollHeader.width(this.cfg.scrollWidth);
+            scrollBody.width(this.cfg.scrollWidth);
+            scrollFooter.width(this.cfg.scrollWidth);
+        }
+
+        scrollBody.scroll(function() {
+            scrollHeader.scrollLeft(scrollBody.scrollLeft());
+            scrollFooter.scrollLeft(scrollBody.scrollLeft());
+        });
+    },
+    
+    /**
+     * Moves widths of columns to column wrappers
+     */
+    alignColumnWidths: function() {
+        this.jq.find('div.ui-tt-c').each(function() {
+            var wrapper = $(this),
+            column = wrapper.parent();
+
+            wrapper.width(column.width());
+            column.width('');
+        });
+    }
+
+});
