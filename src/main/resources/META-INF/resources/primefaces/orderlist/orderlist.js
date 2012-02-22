58ab91683e0052e2360e075c2283406136f38c3d
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2012-02-22 09:46:40 +0000

    Migrated orderlist to new widget architecture

--- src/main/resources/META-INF/resources/primefaces/orderlist/orderlist.js
+++ src/main/resources/META-INF/resources/primefaces/orderlist/orderlist.js
@@ -1,172 +1,164 @@
 /**
  * PrimeFaces OrderList Widget
  */
-PrimeFaces.widget.OrderList = function(cfg) {
-    this.cfg = cfg;
-    this.id = this.cfg.id;
-    this.jqId = PrimeFaces.escapeClientId(this.id);
-    this.jq = $(this.jqId);
-    this.list = this.jq.find('.ui-orderlist-list'),
-    this.items = this.list.children('.ui-orderlist-item');
-    this.input = $(this.jqId + '_values');
-    this.cfg.effect = this.cfg.effect||'fade';
-    var _self = this;
+PrimeFaces.widget.OrderList = PrimeFaces.widget.BaseWidget.extend({
     
-    this.generateItems();
-
-    this.setupButtons();
-    
-    this.bindEvents();
-
-    //Enable dnd
-    this.list.sortable({
-        revert: true,
-        start: function(event, ui) {
-            PrimeFaces.clearSelection();
-        } 
-        ,update: function(event, ui) {
-            _self.onDragDrop(event, ui);
-        }
-    });
-    
-    this.postConstruct();
-}
-
-PrimeFaces.extend(PrimeFaces.widget.OrderList, PrimeFaces.widget.BaseWidget);
-
-PrimeFaces.widget.OrderList.prototype.generateItems = function() {
-    var _self = this;
-    
-    this.list.children('.ui-orderlist-item').each(function(i, item) {
-        var item = $(this),
-        itemValue = item.data('item-value');
+    init: function(cfg) {
+        this._super(cfg);
         
-        _self.input.append('<option value="' + itemValue + '" selected="selected">' + itemValue + '</option>');
-    });
-}
+        this.list = this.jq.find('.ui-orderlist-list'),
+        this.items = this.list.children('.ui-orderlist-item');
+        this.input = $(this.jqId + '_values');
+        this.cfg.effect = this.cfg.effect||'fade';
+        var _self = this;
 
-/**
- * Visuals
- */
-PrimeFaces.widget.OrderList.prototype.bindEvents = function() {
+        this.generateItems();
 
-    this.items.mouseover(function(e) {
-        var element = $(this);
+        this.setupButtons();
 
-        if(!element.hasClass('ui-state-highlight'))
-            $(this).addClass('ui-state-hover');
-    })
-    .mouseout(function(e) {
-        var element = $(this);
+        this.bindEvents();
 
-        if(!element.hasClass('ui-state-highlight'))
-            $(this).removeClass('ui-state-hover');
-    })
-    .mousedown(function(e) {
-        var element = $(this),
-        metaKey = (e.metaKey||e.ctrlKey);
-
-        if(!metaKey) {
-            element.removeClass('ui-state-hover').addClass('ui-state-highlight')
-            .siblings('.ui-state-highlight').removeClass('ui-state-highlight');
-        }
-        else {
-            if(element.hasClass('ui-state-highlight'))
-                element.removeClass('ui-state-highlight');
-            else
-                element.removeClass('ui-state-hover').addClass('ui-state-highlight');
-        }
-    });
-}
-
-/**
- * Creates button controls using progressive enhancement
- */
-PrimeFaces.widget.OrderList.prototype.setupButtons = function() {
-    var _self = this;
+        //Enable dnd
+        this.list.sortable({
+            revert: true,
+            start: function(event, ui) {
+                PrimeFaces.clearSelection();
+            } 
+            ,update: function(event, ui) {
+                _self.onDragDrop(event, ui);
+            }
+        });
+    },
     
-    PrimeFaces.skinButton(this.jq.find('.ui-button'));
+    generateItems: function() {
+        var _self = this;
+
+        this.list.children('.ui-orderlist-item').each(function(i, item) {
+            var item = $(this),
+            itemValue = item.data('item-value');
+
+            _self.input.append('<option value="' + itemValue + '" selected="selected">' + itemValue + '</option>');
+        });
+    },
     
-    this.jq.find(' .ui-orderlist-controls .ui-orderlist-button-move-up').click(function() {_self.moveUp(_self.sourceList);});
-    this.jq.find(' .ui-orderlist-controls .ui-orderlist-button-move-top').click(function() {_self.moveTop(_self.sourceList);});
-    this.jq.find(' .ui-orderlist-controls .ui-orderlist-button-move-down').click(function() {_self.moveDown(_self.sourceList);});
-    this.jq.find(' .ui-orderlist-controls .ui-orderlist-button-move-bottom').click(function() {_self.moveBottom(_self.sourceList);});
-}
+    bindEvents: function() {
 
-PrimeFaces.widget.OrderList.prototype.onDragDrop = function(event, ui) {
-    ui.item.removeClass('ui-state-highlight');
+        this.items.mouseover(function(e) {
+            var element = $(this);
 
-    this.saveState();
-}
+            if(!element.hasClass('ui-state-highlight'))
+                $(this).addClass('ui-state-hover');
+        })
+        .mouseout(function(e) {
+            var element = $(this);
 
-PrimeFaces.widget.OrderList.prototype.saveState = function() {
-    this.input.children().remove();
+            if(!element.hasClass('ui-state-highlight'))
+                $(this).removeClass('ui-state-hover');
+        })
+        .mousedown(function(e) {
+            var element = $(this),
+            metaKey = (e.metaKey||e.ctrlKey);
+
+            if(!metaKey) {
+                element.removeClass('ui-state-hover').addClass('ui-state-highlight')
+                .siblings('.ui-state-highlight').removeClass('ui-state-highlight');
+            }
+            else {
+                if(element.hasClass('ui-state-highlight'))
+                    element.removeClass('ui-state-highlight');
+                else
+                    element.removeClass('ui-state-hover').addClass('ui-state-highlight');
+            }
+        });
+    },
     
-    this.generateItems();
-}
+    setupButtons: function() {
+        var _self = this;
 
-PrimeFaces.widget.OrderList.prototype.moveUp = function(list) {
-    var _self = this;
+        PrimeFaces.skinButton(this.jq.find('.ui-button'));
 
-    this.items.filter('.ui-state-highlight').each(function() {
-        var item = $(this);
+        this.jq.find(' .ui-orderlist-controls .ui-orderlist-button-move-up').click(function() {_self.moveUp(_self.sourceList);});
+        this.jq.find(' .ui-orderlist-controls .ui-orderlist-button-move-top').click(function() {_self.moveTop(_self.sourceList);});
+        this.jq.find(' .ui-orderlist-controls .ui-orderlist-button-move-down').click(function() {_self.moveDown(_self.sourceList);});
+        this.jq.find(' .ui-orderlist-controls .ui-orderlist-button-move-bottom').click(function() {_self.moveBottom(_self.sourceList);});
+    },
+    
+    onDragDrop: function(event, ui) {
+        ui.item.removeClass('ui-state-highlight');
 
-        if(!item.is(':first-child')) {
-            item.hide(_self.cfg.effect, {}, 'fast', function() {
-                item.insertBefore(item.prev()).show(_self.cfg.effect, {}, 'fast', function() {
-                    _self.saveState();
+        this.saveState();
+    },
+    
+    saveState: function() {
+        this.input.children().remove();
+
+        this.generateItems();
+    },
+    
+    moveUp: function(list) {
+        var _self = this;
+
+        this.items.filter('.ui-state-highlight').each(function() {
+            var item = $(this);
+
+            if(!item.is(':first-child')) {
+                item.hide(_self.cfg.effect, {}, 'fast', function() {
+                    item.insertBefore(item.prev()).show(_self.cfg.effect, {}, 'fast', function() {
+                        _self.saveState();
+                    });
                 });
-            });
-        }
-    });
-}
+            }
+        });
+    },
+    
+    moveTop: function(list) {
+        var _self = this;
 
-PrimeFaces.widget.OrderList.prototype.moveTop = function(list) {
-    var _self = this;
+        this.items.filter('.ui-state-highlight').each(function() {
+            var item = $(this);
 
-    this.items.filter('.ui-state-highlight').each(function() {
-        var item = $(this);
-
-        if(!item.is(':first-child')) {
-            item.hide(_self.cfg.effect, {}, 'fast', function() {
-                item.prependTo(item.parent()).show(_self.cfg.effect, {}, 'fast', function(){
-                    _self.saveState();
+            if(!item.is(':first-child')) {
+                item.hide(_self.cfg.effect, {}, 'fast', function() {
+                    item.prependTo(item.parent()).show(_self.cfg.effect, {}, 'fast', function(){
+                        _self.saveState();
+                    });
                 });
-            });
-        }
+            }
 
-    });
-}
+        });
+    },
+    
+    moveDown: function(list) {
+        var _self = this;
 
-PrimeFaces.widget.OrderList.prototype.moveDown = function(list) {
-    var _self = this;
+        this.items.filter('.ui-state-highlight').each(function() {
+            var item = $(this);
 
-    this.items.filter('.ui-state-highlight').each(function() {
-        var item = $(this);
-
-        if(!item.is(':last-child')) {
-            item.hide(_self.cfg.effect, {}, 'fast', function() {
-                item.insertAfter(item.next()).show(_self.cfg.effect, {}, 'fast', function() {
-                    _self.saveState();
+            if(!item.is(':last-child')) {
+                item.hide(_self.cfg.effect, {}, 'fast', function() {
+                    item.insertAfter(item.next()).show(_self.cfg.effect, {}, 'fast', function() {
+                        _self.saveState();
+                    });
                 });
-            });
-        }
+            }
 
-    });
-}
+        });
+    },
+    
+    moveBottom: function(list) {
+        var _self = this;
 
-PrimeFaces.widget.OrderList.prototype.moveBottom = function(list) {
-    var _self = this;
+        this.items.filter('.ui-state-highlight').each(function() {
+            var item = $(this);
 
-    this.items.filter('.ui-state-highlight').each(function() {
-        var item = $(this);
-
-        if(!item.is(':last-child')) {
-            item.hide(_self.cfg.effect, {}, 'fast', function() {
-                item.appendTo(item.parent()).show(_self.cfg.effect, {}, 'fast', function() {
-                    _self.saveState();
+            if(!item.is(':last-child')) {
+                item.hide(_self.cfg.effect, {}, 'fast', function() {
+                    item.appendTo(item.parent()).show(_self.cfg.effect, {}, 'fast', function() {
+                        _self.saveState();
+                    });
                 });
-            });
-        }
-    });
-}
+            }
+        });
+    }
+
+});
