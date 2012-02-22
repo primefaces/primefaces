fc48ef2332f4fcaf2dcd07b96c6eb770b46ebad4
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2012-02-22 10:25:02 +0000

    Migrated tagcloud to new widget architecture

--- src/main/resources/META-INF/resources/primefaces/tagcloud/tagcloud.js
+++ src/main/resources/META-INF/resources/primefaces/tagcloud/tagcloud.js
@@ -1,18 +1,16 @@
 /**
  * PrimeFaces TagCloud Widget
  */
-PrimeFaces.widget.TagCloud = function(cfg) {
-    this.cfg = cfg;
-    this.id = this.cfg.id;
-    this.jq = $(PrimeFaces.escapeClientId(this.id));
-
-    this.jq.find('li').mouseover(function() {
-        $(this).addClass('ui-state-hover');
-    }).mouseout(function() {
-        $(this).removeClass('ui-state-hover');
-    });
+PrimeFaces.widget.TagCloud = PrimeFaces.widget.BaseWidget.extend({
     
-    this.postConstruct();
-}
-
-PrimeFaces.extend(PrimeFaces.widget.TagCloud, PrimeFaces.widget.BaseWidget);
+    init: function(cfg) {
+        this._super(cfg);
+        
+        this.jq.find('li').mouseover(function() {
+            $(this).addClass('ui-state-hover');
+        }).mouseout(function() {
+            $(this).removeClass('ui-state-hover');
+        });
+    }
+    
+});
