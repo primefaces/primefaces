e509bb6cf3a287b545cff5f62eb98473894b3b71
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2012-02-22 09:41:57 +0000

    Migrated notificationbar to new widget architecture

--- src/main/resources/META-INF/resources/primefaces/notificationbar/notificationbar.js
+++ src/main/resources/META-INF/resources/primefaces/notificationbar/notificationbar.js
@@ -1,42 +1,43 @@
-PrimeFaces.widget.NotificationBar = function(cfg) {
-	this.cfg = cfg;
-    this.id = this.cfg.id;
-    this.jqId = PrimeFaces.escapeClientId(this.id);
-	this.jq = $(this.jqId);
-    var _self = this;
+/**
+ * PrimeFaces NotificationBar Widget
+ */
+PrimeFaces.widget.NotificationBar = PrimeFaces.widget.BaseWidget.extend({
+    
+    init: function(cfg) {
+        this._super(cfg);
+        
+        var _self = this;
 	
-    //relocate
-	this.jq.css(this.cfg.position, '0').appendTo($('body'));
+        //relocate
+        this.jq.css(this.cfg.position, '0').appendTo($('body'));
 
-    //display initially
-	if(this.cfg.autoDisplay) {
-		$(this.jq).css('display','block')
+        //display initially
+        if(this.cfg.autoDisplay) {
+            $(this.jq).css('display','block')
+        }
+
+        //bind events
+        this.jq.children('.ui-notificationbar-close').click(function() {
+            _self.hide();
+        });
+    },
+    
+    show: function() {
+        if(this.cfg.effect === 'slide')
+            $(this.jq).slideDown(this.cfg.effect);
+        else if(this.cfg.effect === 'fade')
+            $(this.jq).fadeIn(this.cfg.effect);
+        else if(this.cfg.effect === 'none')
+            $(this.jq).show();
+    },
+    
+    hide: function() {
+        if(this.cfg.effect === 'slide')
+            $(this.jq).slideUp(this.cfg.effect);
+        else if(this.cfg.effect === 'fade')
+            $(this.jq).fadeOut(this.cfg.effect);
+        else if(this.cfg.effect === 'none')
+            $(this.jq).hide();
     }
     
-    //bind events
-    this.jq.children('.ui-notificationbar-close').click(function() {
-        _self.hide();
-    });
-    
-    this.postConstruct();
-}
-
-PrimeFaces.extend(PrimeFaces.widget.NotificationBar, PrimeFaces.widget.BaseWidget);
-
-PrimeFaces.widget.NotificationBar.prototype.show = function() {
-	if(this.cfg.effect === 'slide')
-		$(this.jq).slideDown(this.cfg.effect);
-	else if(this.cfg.effect === 'fade')
-		$(this.jq).fadeIn(this.cfg.effect);
-	else if(this.cfg.effect === 'none')
-		$(this.jq).show();
-}
-
-PrimeFaces.widget.NotificationBar.prototype.hide = function() {
-	if(this.cfg.effect === 'slide')
-		$(this.jq).slideUp(this.cfg.effect);
-	else if(this.cfg.effect === 'fade')
-		$(this.jq).fadeOut(this.cfg.effect);
-	else if(this.cfg.effect === 'none')
-		$(this.jq).hide();
-}
+});
