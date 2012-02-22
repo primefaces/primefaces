cb100f265ce9220c7c7ffb18e7238b566170cf1c
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2012-02-22 09:51:04 +0000

    Migrated panel to new widget architecture

--- primefaces/trunk/src/main/resources/META-INF/resources/primefaces/panel/panel.js
+++ primefaces/trunk/src/main/resources/META-INF/resources/primefaces/panel/panel.js
@@ -1,109 +1,107 @@
 /**
  * PrimeFaces Panel Widget
  */
-PrimeFaces.widget.Panel = function(cfg) {
-    this.cfg = cfg;
-    this.id = this.cfg.id;
-    this.jqId = PrimeFaces.escapeClientId(this.id);
-
-    if(this.cfg.toggleable) {
-        this.toggler = $(this.jqId + '_toggler');
-        this.toggleStateHolder = $(this.jqId + '_collapsed');
-        this.content = $(this.jqId + '_content');
-
-        this.setupToggleTrigger();
-    }
-
-    if(this.cfg.closable) {
-        this.visibleStateHolder = $(this.jqId + "_visible");
-
-        this.setupCloseTrigger();
-    }
-
-    if(this.cfg.hasMenu) {
-        this.visibleStateHolder = $(this.jqId + "_visible");
-
-        this.setupMenuTrigger();
-    }
+PrimeFaces.widget.Panel = PrimeFaces.widget.BaseWidget.extend({
     
-    this.postConstruct();
-}
+    init: function(cfg) {
+        this._super(cfg);
+        
+        if(this.cfg.toggleable) {
+            this.toggler = $(this.jqId + '_toggler');
+            this.toggleStateHolder = $(this.jqId + '_collapsed');
+            this.content = $(this.jqId + '_content');
 
-PrimeFaces.extend(PrimeFaces.widget.Panel, PrimeFaces.widget.BaseWidget);
-
-PrimeFaces.widget.Panel.prototype.toggle = function() {
-    if(this.cfg.collapsed) {
-        this.toggler.removeClass('ui-icon-plusthick').addClass('ui-icon-minusthick');
-        this.cfg.collapsed = false;
-        this.toggleStateHolder.val(false);
-    }
-    else {
-        this.toggler.removeClass('ui-icon-minusthick').addClass('ui-icon-plusthick');
-        this.cfg.collapsed = true;
-        this.toggleStateHolder.val(true);
-    }
-	
-    var _self = this;
-
-    this.content.slideToggle(this.cfg.toggleSpeed,
-        function(e) {
-            if(_self.cfg.behaviors) {
-                var toggleBehavior = _self.cfg.behaviors['toggle'];
-                if(toggleBehavior) {
-                    toggleBehavior.call(_self, e);
-                }
-            }
-        });
-}
-
-PrimeFaces.widget.Panel.prototype.close = function() {
-    this.visibleStateHolder.val(false);
-
-    var _self = this;
-
-    $(this.jqId).fadeOut(this.cfg.closeSpeed,
-        function(e) {
-            if(_self.cfg.behaviors) {
-                var closeBehavior = _self.cfg.behaviors['close'];
-                if(closeBehavior) {
-                    closeBehavior.call(_self, e);
-                }
-            }
+            this.setupToggleTrigger();
         }
-    );
-}
 
-PrimeFaces.widget.Panel.prototype.show = function() {
-    $(this.jqId).fadeIn(this.cfg.closeSpeed);
-	
-    this.visibleStateHolder.val(true);
-}
+        if(this.cfg.closable) {
+            this.visibleStateHolder = $(this.jqId + "_visible");
 
-PrimeFaces.widget.Panel.prototype.setupToggleTrigger = function() {
-    var _self = this,
-    trigger = this.toggler.parent();
+            this.setupCloseTrigger();
+        }
 
-    this.setupTriggerVisuals(trigger);
+        if(this.cfg.hasMenu) {
+            this.visibleStateHolder = $(this.jqId + "_visible");
+
+            this.setupMenuTrigger();
+        }
+    },
     
-    trigger.click(function() {_self.toggle();});
-}
+    toggle: function() {
+        if(this.cfg.collapsed) {
+            this.toggler.removeClass('ui-icon-plusthick').addClass('ui-icon-minusthick');
+            this.cfg.collapsed = false;
+            this.toggleStateHolder.val(false);
+        }
+        else {
+            this.toggler.removeClass('ui-icon-minusthick').addClass('ui-icon-plusthick');
+            this.cfg.collapsed = true;
+            this.toggleStateHolder.val(true);
+        }
 
-PrimeFaces.widget.Panel.prototype.setupCloseTrigger = function() {
-    var _self = this,
-    trigger = $(this.jqId + '_closer').parent();
+        var _self = this;
 
-    this.setupTriggerVisuals(trigger);
+        this.content.slideToggle(this.cfg.toggleSpeed,
+            function(e) {
+                if(_self.cfg.behaviors) {
+                    var toggleBehavior = _self.cfg.behaviors['toggle'];
+                    if(toggleBehavior) {
+                        toggleBehavior.call(_self, e);
+                    }
+                }
+            });
+    },
     
-    trigger.click(function() {_self.close();});
-}
+    close: function() {
+        this.visibleStateHolder.val(false);
 
-PrimeFaces.widget.Panel.prototype.setupMenuTrigger = function() {
-    var trigger = $(this.jqId + '_menu').parent();
+        var _self = this;
 
-    this.setupTriggerVisuals(trigger);
-}
+        $(this.jqId).fadeOut(this.cfg.closeSpeed,
+            function(e) {
+                if(_self.cfg.behaviors) {
+                    var closeBehavior = _self.cfg.behaviors['close'];
+                    if(closeBehavior) {
+                        closeBehavior.call(_self, e);
+                    }
+                }
+            }
+        );
+    },
+    
+    show: function() {
+        $(this.jqId).fadeIn(this.cfg.closeSpeed);
 
-PrimeFaces.widget.Panel.prototype.setupTriggerVisuals = function(trigger) {
-    trigger.mouseover(function() {$(this).addClass('ui-state-hover');})
-            .mouseout(function() {$(this).removeClass('ui-state-hover');});
-}
+        this.visibleStateHolder.val(true);
+    },
+    
+    setupToggleTrigger: function() {
+        var _self = this,
+        trigger = this.toggler.parent();
+
+        this.setupTriggerVisuals(trigger);
+
+        trigger.click(function() {_self.toggle();});
+    },
+    
+    setupCloseTrigger: function() {
+        var _self = this,
+        trigger = $(this.jqId + '_closer').parent();
+
+        this.setupTriggerVisuals(trigger);
+
+        trigger.click(function() {_self.close();});
+    },
+    
+    setupMenuTrigger: function() {
+        var trigger = $(this.jqId + '_menu').parent();
+
+        this.setupTriggerVisuals(trigger);
+    },
+    
+    setupTriggerVisuals: function(trigger) {
+        trigger.mouseover(function() {$(this).addClass('ui-state-hover');})
+                .mouseout(function() {$(this).removeClass('ui-state-hover');});
+    }
+
+});
