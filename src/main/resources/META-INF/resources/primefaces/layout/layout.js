26c2501c23fffad14811962af56ac94d2241f533
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2012-02-22 09:30:23 +0000

    Migrated layout to new widget architecture

--- src/main/resources/META-INF/resources/primefaces/layout/layout.js
+++ src/main/resources/META-INF/resources/primefaces/layout/layout.js
@@ -4355,178 +4355,180 @@ debugData( test, pane );
 /**
  * PrimeFaces Layout Widget
  */
-PrimeFaces.widget.Layout = function(cfg) {
-    this.cfg = cfg;
-    this.id = this.cfg.id;
-    this.jqId = PrimeFaces.escapeClientId(this.id);
-
-    if(this.cfg.full) {                                                 //full
-        this.jq = $('body');
-    } else if(this.cfg.parent) {                                        //nested
-        this.jq = $(PrimeFaces.escapeClientId(this.cfg.parent));
-    } else {                                                            //element
-        this.jq = $(this.jqId);
-    }
+ PrimeFaces.widget.Layout = PrimeFaces.widget.BaseWidget.extend({
     
-    var _self = this;
-    
-    if(this.jq.is(':visible')) {
-        this.init();
-    } 
-    else {
-        var hiddenParent = this.jq.parents('.ui-hidden-container:first'),
-        hiddenParentWidget = hiddenParent.data('widget');
+    init: function(cfg) {
+        this._super(cfg);
         
-        if(hiddenParentWidget) {
-            hiddenParentWidget.addOnshowHandler(function() {
-                return _self.init();
-            });
+        this.cfg = cfg;
+        this.id = this.cfg.id;
+        this.jqId = PrimeFaces.escapeClientId(this.id);
+
+        if(this.cfg.full) {                                                 //full
+            this.jq = $('body');
+        } else if(this.cfg.parent) {                                        //nested
+            this.jq = $(PrimeFaces.escapeClientId(this.cfg.parent));
+        } else {                                                            //element
+            this.jq = $(this.jqId);
         }
-    }
-    
-    this.postConstruct();
-}
 
-PrimeFaces.extend(PrimeFaces.widget.Layout, PrimeFaces.widget.BaseWidget);
+        var _self = this;
 
-PrimeFaces.widget.Layout.prototype.init = function() {
-    var _self = this;
+        if(this.jq.is(':visible')) {
+            this.render();
+        } 
+        else {
+            var hiddenParent = this.jq.parents('.ui-hidden-container:first'),
+            hiddenParentWidget = hiddenParent.data('widget');
 
-    //defaults
-    this.cfg.defaults = {
-        onshow: function(location,pane,state,options) { _self.onshow(location,pane,state); },
-        onhide: function(location,pane,state,options) { _self.onhide(location,pane,state); },
-        onopen: function(location,pane,state,options) { _self.onopen(location,pane,state); },
-        onclose: function(location,pane,state,options) { _self.onclose(location,pane,state); },
-        onresize: function(location,pane,state,options) { _self.onresize(location,pane,state); },
-        contentSelector: '.ui-layout-unit-content',
-        slidable: false,
-        togglerLength_open: 0,
-        togglerLength_closed: 23,
-        togglerAlign_closed: 'top',
-        togglerContent_closed: '<a href="javascript:void(0)" class="ui-layout-unit-expand-icon ui-state-default ui-corner-all"><span class="ui-icon ui-icon-arrow-4-diag"></span></a>'
-    };
-
-    this.layout = this.jq.layout(this.cfg);
-
-    if(!this.cfg.full) {
-        this.jq.css('overflow','visible');
-    }
-
-    this.bindEvents();
-}
-
-PrimeFaces.widget.Layout.prototype.bindEvents = function() {
-    var _self = this,
-    units = this.jq.children('.ui-layout-unit'),
-    resizers = this.jq.children('.ui-layout-resizer');
-
-    units.children('.ui-layout-unit-header').children('a.ui-layout-unit-header-icon').mouseover(function() {
-        $(this).addClass('ui-state-hover');
-    }).mouseout(function() {
-        $(this).removeClass('ui-state-hover');
-    }).click(function() {
-        var element = $(this),
-        unit = element.parents('.ui-layout-unit:first'),
-        pane = unit.data('layoutEdge');
-
-        if(element.children('span').hasClass('ui-icon-close')) {
-            _self.hide(pane);
-        } else {
-            _self.toggle(pane);
+            if(hiddenParentWidget) {
+                hiddenParentWidget.addOnshowHandler(function() {
+                    return _self.render();
+                });
+            }
         }
-    });
+    },
     
-    resizers.find('a.ui-layout-unit-expand-icon').mouseover(function() {
-        $(this).addClass('ui-state-hover');
-    }).mouseout(function() {
-        $(this).removeClass('ui-state-hover');
-    });
-}
+    render: function() {
+        var _self = this;
 
-PrimeFaces.widget.Layout.prototype.toggle = function(location) {
-    this.layout.toggle(location);
-}
-
-PrimeFaces.widget.Layout.prototype.show = function(location) {
-    this.layout.show(location);
-}
-
-PrimeFaces.widget.Layout.prototype.hide = function(location) {
-    this.layout.hide(location);
-}
-
-PrimeFaces.widget.Layout.prototype.onhide = function(location, pane, state) {
-    if(this.cfg.onClose) {
-        this.cfg.onClose.call(this, state);
-    }
-
-    var closeBehavior = this.cfg.behaviors['close'];
-    if(closeBehavior) {
-        var ext = {
-            params : {}
+        //defaults
+        this.cfg.defaults = {
+            onshow: function(location,pane,state,options) { _self.onshow(location,pane,state); },
+            onhide: function(location,pane,state,options) { _self.onhide(location,pane,state); },
+            onopen: function(location,pane,state,options) { _self.onopen(location,pane,state); },
+            onclose: function(location,pane,state,options) { _self.onclose(location,pane,state); },
+            onresize: function(location,pane,state,options) { _self.onresize(location,pane,state); },
+            contentSelector: '.ui-layout-unit-content',
+            slidable: false,
+            togglerLength_open: 0,
+            togglerLength_closed: 23,
+            togglerAlign_closed: 'top',
+            togglerContent_closed: '<a href="javascript:void(0)" class="ui-layout-unit-expand-icon ui-state-default ui-corner-all"><span class="ui-icon ui-icon-arrow-4-diag"></span></a>'
         };
-        ext.params[this.id + '_unit'] = location;
 
-        closeBehavior.call(this, location, ext);
-    }
-}
+        this.layout = this.jq.layout(this.cfg);
 
-PrimeFaces.widget.Layout.prototype.onshow = function(location, pane, state) {
-    
-}
-
-PrimeFaces.widget.Layout.prototype.onopen = function(location, pane, state) {
-    pane.siblings('.ui-layout-resizer-' + location).removeClass('ui-widget-content ui-corner-all');
-
-    if(this.cfg.onToggle) {
-        this.cfg.onToggle.call(this, state);
-    }
-
-    this.fireToggleEvent(location, false);
-}
-
-PrimeFaces.widget.Layout.prototype.onclose = function(location, pane, state) {
-    pane.siblings('.ui-layout-resizer-' + location).addClass('ui-widget-content ui-corner-all');
-
-    if(!state.isHidden) {
-        if(this.cfg.onToggle) {
-            this.cfg.onToggle.call(this, state);
+        if(!this.cfg.full) {
+            this.jq.css('overflow','visible');
         }
 
-        this.fireToggleEvent(location, true);
-    }
-}
+        this.bindEvents();
+    },
+    
+    bindEvents: function() {
+        var _self = this,
+        units = this.jq.children('.ui-layout-unit'),
+        resizers = this.jq.children('.ui-layout-resizer');
 
-PrimeFaces.widget.Layout.prototype.onresize = function(location, pane, state) {
-    if(this.cfg.onResize) {
-        this.cfg.onResize.call(this, state);
-    }
+        units.children('.ui-layout-unit-header').children('a.ui-layout-unit-header-icon').mouseover(function() {
+            $(this).addClass('ui-state-hover');
+        }).mouseout(function() {
+            $(this).removeClass('ui-state-hover');
+        }).click(function() {
+            var element = $(this),
+            unit = element.parents('.ui-layout-unit:first'),
+            pane = unit.data('layoutEdge');
 
-    if(!state.isClosed && !state.isHidden) {
-        var resizeBehavior = this.cfg.behaviors['resize'];
-        if(resizeBehavior) {
+            if(element.children('span').hasClass('ui-icon-close')) {
+                _self.hide(pane);
+            } else {
+                _self.toggle(pane);
+            }
+        });
+
+        resizers.find('a.ui-layout-unit-expand-icon').mouseover(function() {
+            $(this).addClass('ui-state-hover');
+        }).mouseout(function() {
+            $(this).removeClass('ui-state-hover');
+        });
+    },
+    
+    toggle: function(location) {
+        this.layout.toggle(location);
+    },
+    
+    show: function(location) {
+        this.layout.show(location);
+    },
+    
+    hide: function(location) {
+        this.layout.hide(location);
+    },
+    
+    onhide: function(location, pane, state) {
+        if(this.cfg.onClose) {
+            this.cfg.onClose.call(this, state);
+        }
+
+        var closeBehavior = this.cfg.behaviors['close'];
+        if(closeBehavior) {
             var ext = {
                 params : {}
             };
             ext.params[this.id + '_unit'] = location;
-            ext.params[this.id + '_width'] = state.innerWidth;
-            ext.params[this.id + '_height'] = state.innerHeight;
 
-            resizeBehavior.call(this, location, ext);
+            closeBehavior.call(this, location, ext);
+        }
+    },
+    
+    onshow: function(location, pane, state) {
+    
+    },
+    
+    onopen: function(location, pane, state) {
+        pane.siblings('.ui-layout-resizer-' + location).removeClass('ui-widget-content ui-corner-all');
+
+        if(this.cfg.onToggle) {
+            this.cfg.onToggle.call(this, state);
+        }
+
+        this.fireToggleEvent(location, false);
+    },
+    
+    onclose: function(location, pane, state) {
+        pane.siblings('.ui-layout-resizer-' + location).addClass('ui-widget-content ui-corner-all');
+
+        if(!state.isHidden) {
+            if(this.cfg.onToggle) {
+                this.cfg.onToggle.call(this, state);
+            }
+
+            this.fireToggleEvent(location, true);
+        }
+    },
+    
+    onresize: function(location, pane, state) {
+        if(this.cfg.onResize) {
+            this.cfg.onResize.call(this, state);
+        }
+
+        if(!state.isClosed && !state.isHidden) {
+            var resizeBehavior = this.cfg.behaviors['resize'];
+            if(resizeBehavior) {
+                var ext = {
+                    params : {}
+                };
+                ext.params[this.id + '_unit'] = location;
+                ext.params[this.id + '_width'] = state.innerWidth;
+                ext.params[this.id + '_height'] = state.innerHeight;
+
+                resizeBehavior.call(this, location, ext);
+            }
+        }
+    },
+    
+    fireToggleEvent: function(location, collapsed) {
+        var toggleBehavior = this.cfg.behaviors['toggle'];
+        if(toggleBehavior) {
+            var ext = {
+                params : {}
+            };
+            ext.params[this.id + '_unit'] = location;
+            ext.params[this.id + '_collapsed'] = collapsed;
+
+            toggleBehavior.call(this, location, ext);
         }
     }
-}
-
-PrimeFaces.widget.Layout.prototype.fireToggleEvent = function(location, collapsed) {
-    var toggleBehavior = this.cfg.behaviors['toggle'];
-    if(toggleBehavior) {
-        var ext = {
-            params : {}
-        };
-        ext.params[this.id + '_unit'] = location;
-        ext.params[this.id + '_collapsed'] = collapsed;
-
-        toggleBehavior.call(this, location, ext);
-    }
-}
+    
+});
