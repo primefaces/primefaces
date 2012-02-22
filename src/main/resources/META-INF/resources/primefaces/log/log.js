45fae4f7f77f800d28abf2d78a042f655c9c86c4
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2012-02-22 09:34:44 +0000

    Migrated log to new widget architecture

--- src/main/resources/META-INF/resources/primefaces/log/log.js
+++ src/main/resources/META-INF/resources/primefaces/log/log.js
@@ -1,120 +1,121 @@
-PrimeFaces.widget.Log = function(cfg) {
-    this.cfg = cfg;
-    this.id = this.cfg.id;
-    this.jqId = PrimeFaces.escapeClientId(this.id);
-    this.jq = $(this.jqId);
-    this.header = this.jq.children('.ui-log-header');
-    this.content = this.jq.children('.ui-log-content');
-    this.itemsContainer = this.content.find('.ui-log-items');
-    this.filters = this.header.children('.ui-log-button');
-    this.severity = 'all';
-    var _self = this;
+/**
+ * PrimeFaces Log Widget
+ */
+PrimeFaces.widget.Log = PrimeFaces.widget.BaseWidget.extend({
     
-    //make draggable
-    this.jq.draggable({handle:this.header});
-    this.header.mousedown(function(e) {
-        _self.jq.zIndex(++PrimeFaces.zindex);
-    });
-    
-    //attach events
-    this.bindEvents();
-    
-    //append to body
-    this.jq.appendTo('body');
+    init: function(cfg) {
+        this._super(cfg);
+        
+        this.header = this.jq.children('.ui-log-header');
+        this.content = this.jq.children('.ui-log-content');
+        this.itemsContainer = this.content.find('.ui-log-items');
+        this.filters = this.header.children('.ui-log-button');
+        this.severity = 'all';
+        var _self = this;
 
-    //attach
-    PrimeFaces.logger = this;
-    
-    this.postConstruct();
-}
+        //make draggable
+        this.jq.draggable({handle:this.header});
+        this.header.mousedown(function(e) {
+            _self.jq.zIndex(++PrimeFaces.zindex);
+        });
 
-PrimeFaces.extend(PrimeFaces.widget.Log, PrimeFaces.widget.BaseWidget);
+        //attach events
+        this.bindEvents();
 
-PrimeFaces.widget.Log.prototype.bindEvents = function() {
-    var _self = this;
-    
-    //visuals
-    this.header.children('.ui-log-button').mouseover(function() {
-        var el = $(this);
-        if(!el.hasClass('ui-state-active'))
-            $(this).addClass('ui-state-hover');
-    }).mouseout(function() {
-        $(this).removeClass('ui-state-hover');
-    });
-    
-    //clear
-    this.header.children('.ui-log-clear').click(function(e) {
-        _self.itemsContainer.html('');
-        _self.filters.filter('.ui-state-active').removeClass('ui-state-active');
-        _self.filters.filter('.ui-log-all').addClass('ui-state-active');
-        _self.severity = 'all';
-        e.preventDefault();
-    });
-    
-    //all
-    this.header.children('.ui-log-all').click(function(e) {
-        _self.itemsContainer.children().show();
-        _self.filters.filter('.ui-state-active').removeClass('ui-state-active');
-        $(this).addClass('ui-state-active').removeClass('ui-state-hover');
-        _self.severity = 'all';
-        e.preventDefault();
-    });
-    
-    //info
-    this.header.children('.ui-log-info').click(function(e) {
-        _self.handleFilterClick(e, '.ui-log-item-info', 'info', $(this));
-    });
-    
-    //warn
-    this.header.children('.ui-log-warn').click(function(e) {
-        _self.handleFilterClick(e, '.ui-log-item-warn', 'warn', $(this));
-    });
-    
-    //debug
-    this.header.children('.ui-log-debug').click(function(e) {
-        _self.handleFilterClick(e, '.ui-log-item-debug', 'debug', $(this));
-    });
-    
-    //error
-    this.header.children('.ui-log-error').click(function(e) {
-        _self.handleFilterClick(e, '.ui-log-item-error', 'error', $(this));
-    });
-}
+        //append to body
+        this.jq.appendTo('body');
 
-PrimeFaces.widget.Log.prototype.info = function(msg) {
-    this.add(msg, 'info', 'ui-icon-info');
-}
-
-PrimeFaces.widget.Log.prototype.warn = function(msg) {
-    this.add(msg, 'warn', 'ui-icon-notice');
-}
-
-PrimeFaces.widget.Log.prototype.debug = function(msg) {
-    this.add(msg, 'debug', 'ui-icon-search');
-}
-
-PrimeFaces.widget.Log.prototype.error = function(msg) {
-    this.add(msg, 'error', 'ui-icon-alert');
-}
-
-PrimeFaces.widget.Log.prototype.add = function(msg, severity, icon) {
-    var visible = this.severity == severity || this.severity == 'all',
-    style = visible ? 'display:block' : 'display:none';
+        //attach
+        PrimeFaces.logger = this;
+    },
     
-    var item = '<li class="ui-log-item ui-log-item-' + severity + ' ui-helper-clearfix" style="' + style + 
-        '"><span class="ui-icon ' + icon + '"></span>' + new Date().toLocaleString() + ' : '  + msg + '</li>';
+    bindEvents: function() {
+        var _self = this;
+
+        //visuals
+        this.header.children('.ui-log-button').mouseover(function() {
+            var el = $(this);
+            if(!el.hasClass('ui-state-active'))
+                $(this).addClass('ui-state-hover');
+        }).mouseout(function() {
+            $(this).removeClass('ui-state-hover');
+        });
+
+        //clear
+        this.header.children('.ui-log-clear').click(function(e) {
+            _self.itemsContainer.html('');
+            _self.filters.filter('.ui-state-active').removeClass('ui-state-active');
+            _self.filters.filter('.ui-log-all').addClass('ui-state-active');
+            _self.severity = 'all';
+            e.preventDefault();
+        });
+
+        //all
+        this.header.children('.ui-log-all').click(function(e) {
+            _self.itemsContainer.children().show();
+            _self.filters.filter('.ui-state-active').removeClass('ui-state-active');
+            $(this).addClass('ui-state-active').removeClass('ui-state-hover');
+            _self.severity = 'all';
+            e.preventDefault();
+        });
+
+        //info
+        this.header.children('.ui-log-info').click(function(e) {
+            _self.handleFilterClick(e, '.ui-log-item-info', 'info', $(this));
+        });
+
+        //warn
+        this.header.children('.ui-log-warn').click(function(e) {
+            _self.handleFilterClick(e, '.ui-log-item-warn', 'warn', $(this));
+        });
+
+        //debug
+        this.header.children('.ui-log-debug').click(function(e) {
+            _self.handleFilterClick(e, '.ui-log-item-debug', 'debug', $(this));
+        });
+
+        //error
+        this.header.children('.ui-log-error').click(function(e) {
+            _self.handleFilterClick(e, '.ui-log-item-error', 'error', $(this));
+        });
+    },
     
-    this.itemsContainer.append(item);
-}
+    info: function(msg) {
+        this.add(msg, 'info', 'ui-icon-info');
+    },
+    
+    warn: function(msg) {
+        this.add(msg, 'warn', 'ui-icon-notice');
+    },
+    
+    debug: function(msg) {
+        this.add(msg, 'debug', 'ui-icon-search');
+    },
+    
+    error: function(msg) {
+        this.add(msg, 'error', 'ui-icon-alert');
+    },
+    
+    add: function(msg, severity, icon) {
+        var visible = this.severity == severity || this.severity == 'all',
+        style = visible ? 'display:block' : 'display:none';
 
-PrimeFaces.widget.Log.prototype.filter = function(severity) {
-    this.itemsContainer.children().hide().filter(severity).show();
-}
+        var item = '<li class="ui-log-item ui-log-item-' + severity + ' ui-helper-clearfix" style="' + style + 
+            '"><span class="ui-icon ' + icon + '"></span>' + new Date().toLocaleString() + ' : '  + msg + '</li>';
 
-PrimeFaces.widget.Log.prototype.handleFilterClick = function(event, severityClass, severity, button) {
-    this.filter(severityClass);
-    this.filters.filter('.ui-state-active').removeClass('ui-state-active');
-    button.addClass('ui-state-active').removeClass('ui-state-hover');
-    this.severity = severity;
-    event.preventDefault();
-}
+        this.itemsContainer.append(item);
+    },
+    
+    filter: function(severity) {
+        this.itemsContainer.children().hide().filter(severity).show();
+    },
+    
+    handleFilterClick: function(event, severityClass, severity, button) {
+        this.filter(severityClass);
+        this.filters.filter('.ui-state-active').removeClass('ui-state-active');
+        button.addClass('ui-state-active').removeClass('ui-state-hover');
+        this.severity = severity;
+        event.preventDefault();
+    }
+    
+});
