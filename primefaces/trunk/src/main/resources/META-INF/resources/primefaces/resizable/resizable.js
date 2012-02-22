8299eb2264e45cb81f91a8aad8ab55ad9620ea49
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2012-02-22 10:03:54 +0000

    Migrated resizable to new widget architecture

--- primefaces/trunk/src/main/resources/META-INF/resources/primefaces/resizable/resizable.js
+++ primefaces/trunk/src/main/resources/META-INF/resources/primefaces/resizable/resizable.js
@@ -1,53 +1,58 @@
-PrimeFaces.widget.Resizable = function(cfg) {
-    this.cfg = cfg;
-    this.id = this.cfg.id;
-    this.jqId = PrimeFaces.escapeClientId(this.id);
-    this.jqTarget = $(PrimeFaces.escapeClientId(this.cfg.target));
+/** 
+ * PrimeFaces Resizable Widget
+ */
+PrimeFaces.widget.XXX = PrimeFaces.widget.BaseWidget.extend({
+    
+    init: function(cfg) {
+        this.cfg = cfg;
+        this.id = this.cfg.id;
+        this.jqId = PrimeFaces.escapeClientId(this.id);
+        this.jqTarget = $(PrimeFaces.escapeClientId(this.cfg.target));
 
-    if(this.cfg.ajaxResize) {
-        this.cfg.formId = $(this.target).parents('form:first').attr('id');
-    }
-
-    var _self = this;
-
-    this.cfg.stop = function(event, ui) {
-        if(_self.cfg.onStop) {
-            _self.cfg.onStop.call(_self, event, ui);
+        if(this.cfg.ajaxResize) {
+            this.cfg.formId = $(this.target).parents('form:first').attr('id');
         }
 
-        _self.fireAjaxResizeEvent(event, ui);
-    }
+        var _self = this;
 
-    this.cfg.start = function(event, ui) {
-        if(_self.cfg.onStart) {
-            _self.cfg.onStart.call(_self, event, ui);
+        this.cfg.stop = function(event, ui) {
+            if(_self.cfg.onStop) {
+                _self.cfg.onStop.call(_self, event, ui);
+            }
+
+            _self.fireAjaxResizeEvent(event, ui);
+        }
+
+        this.cfg.start = function(event, ui) {
+            if(_self.cfg.onStart) {
+                _self.cfg.onStart.call(_self, event, ui);
+            }
+        }
+
+        this.cfg.resize = function(event, ui) {
+            if(_self.cfg.onResize) {
+                _self.cfg.onResize.call(_self, event, ui);
+            }
+        }
+
+        this.jqTarget.resizable(this.cfg);
+        
+        $(this.jqId + '_script').remove();
+    },
+    
+    fireAjaxResizeEvent: function(event, ui) {
+        if(this.cfg.behaviors) {
+            var resizeBehavior = this.cfg.behaviors['resize'];
+            if(resizeBehavior) {
+                var ext = {
+                    params:{}
+                };
+                ext.params[this.id + '_width'] = ui.helper.width();
+                ext.params[this.id + '_height'] = ui.helper.height();
+
+                resizeBehavior.call(this, event, ext);
+            }
         }
     }
     
-    this.cfg.resize = function(event, ui) {
-        if(_self.cfg.onResize) {
-            _self.cfg.onResize.call(_self, event, ui);
-        }
-    }
-
-    this.jqTarget.resizable(this.cfg);
-    
-    this.postConstruct();
-}
-
-PrimeFaces.extend(PrimeFaces.widget.Resizable, PrimeFaces.widget.BaseWidget);
-
-PrimeFaces.widget.Resizable.prototype.fireAjaxResizeEvent = function(event, ui) {
-    if(this.cfg.behaviors) {
-        var resizeBehavior = this.cfg.behaviors['resize'];
-        if(resizeBehavior) {
-            var ext = {
-                params:{}
-            };
-            ext.params[this.id + '_width'] = ui.helper.width();
-            ext.params[this.id + '_height'] = ui.helper.height();
-
-            resizeBehavior.call(this, event, ext);
-        }
-    }
-}
+});
