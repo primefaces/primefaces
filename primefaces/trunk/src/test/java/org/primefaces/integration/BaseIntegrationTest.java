8759bf4b0ace16137537f4a23e38c2c91aee7f71
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2012-03-06 14:18:55 +0000

    Test refactor

---
+++ primefaces/trunk/src/test/java/org/primefaces/integration/BaseIntegrationTest.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2009-2012 Prime Teknoloji.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.primefaces.integration;
+
+import java.io.File;
+import java.io.IOException;
+import junit.framework.Assert;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.jsfunit.jsfsession.JSFSession;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.primefaces.integration.bean.SpinnerView;
+import org.primefaces.jsfunit.PrimeClientFactory;
+import org.primefaces.jsfunit.SpinnerClient;
+
+@RunWith(Arquillian.class)
+public class BaseIntegrationTest {
+    
+    @Deployment
+    public static WebArchive createDeployment() {
+            return ShrinkWrap.create(WebArchive.class, "primefaces-test.war")
+                .setWebXML(new File("src/test/resources/test-web.xml"))
+                .addClass(SpinnerView.class)
+                .addAsWebResource("view/spinner.xhtml", "spinner.xhtml")
+                .addAsWebResource("faces-config.xml");
+    }
+    
+    @Test
+    public void spinner() throws IOException {
+        JSFSession session = new JSFSession("/spinner.jsf");
+        SpinnerClient spinner = PrimeClientFactory.spinnerClient(session.getJSFClientSession()).setComponentID("spinnerBasic");       
+        
+        Assert.assertEquals("Initial spinner value.", spinner.getValue(), "1");
+        
+        spinner.spinUp();
+        
+        Assert.assertEquals("After spin-up value.", spinner.getValue(), "2");
+        
+        spinner.spinDown();
+        
+        Assert.assertEquals("After spin-down value.", spinner.getValue(), "1");
+        
+        spinner.spinDown();
+
+        Assert.assertEquals("After second spin-down value.", spinner.getValue(), "0");
+    }
+}
