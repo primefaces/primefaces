9abca9652cfaec38925bd173c1e3ed9cff93ec6b
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2012-06-14 13:57:49 +0000

    Initial drop-in of autocomplete for inputtextarea feature

--- primefaces/trunk/src/main/java-templates/org/primefaces/component/inputtextarea/InputTextareaTemplate.java
+++ primefaces/trunk/src/main/java-templates/org/primefaces/component/inputtextarea/InputTextareaTemplate.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+import javax.el.MethodExpression;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+
 
     public final static String STYLE_CLASS = "ui-inputfield ui-inputtextarea ui-widget ui-state-default ui-corner-all";
 
@@ -13,4 +19,27 @@
         int rows = super.getRows();
     
         return rows > 0 ? rows : 3;
+    }
+
+    private List suggestions = null;
+
+    public void broadcast(javax.faces.event.FacesEvent event) throws javax.faces.event.AbortProcessingException {
+		super.broadcast(event);
+		
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		MethodExpression me = getCompleteMethod();
+		
+		if(me != null && event instanceof org.primefaces.event.AutoCompleteEvent) {
+			suggestions = (List) me.invoke(facesContext.getELContext(), new Object[] {((org.primefaces.event.AutoCompleteEvent) event).getQuery()});
+            
+            if(suggestions == null) {
+                suggestions = new ArrayList();
+            }
+
+            facesContext.renderResponse();
+		}
+	}
+
+    public List getSuggestions() {
+        return this.suggestions;
     }
