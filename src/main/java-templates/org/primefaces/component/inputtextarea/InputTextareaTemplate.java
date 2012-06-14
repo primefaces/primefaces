9e0b7814e36818b723f312cd83533aab21ef6aa5
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2012-06-14 15:02:14 +0000

    Added itemSelect event to inputtextarea autocomplete

--- src/main/java-templates/org/primefaces/component/inputtextarea/InputTextareaTemplate.java
+++ src/main/java-templates/org/primefaces/component/inputtextarea/InputTextareaTemplate.java
@@ -1,11 +1,32 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Date;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import javax.faces.event.AjaxBehaviorEvent;
+import javax.faces.event.FacesEvent;
+import javax.faces.event.PhaseId;
+import javax.el.ValueExpression;
+import javax.faces.convert.Converter;
+import javax.faces.component.behavior.Behavior;
 import javax.el.MethodExpression;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
+import org.primefaces.event.SelectEvent;
+import org.primefaces.util.Constants;
 
+    private static final Collection<String> EVENT_NAMES = Collections.unmodifiableCollection(Arrays.asList("blur","change","valueChange","click","dblclick","focus","keydown","keypress","keyup","mousedown","mousemove","mouseout","mouseover","mouseup","select", "itemSelect"));
 
     public final static String STYLE_CLASS = "ui-inputfield ui-inputtextarea ui-widget ui-state-default ui-corner-all";
+
+    @Override
+    public Collection<String> getEventNames() {
+        return EVENT_NAMES;
+    }
 
     @Override
     public int getCols() {
@@ -19,6 +40,32 @@ import javax.faces.context.FacesContext;
         int rows = super.getRows();
     
         return rows > 0 ? rows : 3;
+    }
+
+    @Override
+    public void queueEvent(FacesEvent event) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        Map<String,String> params = context.getExternalContext().getRequestParameterMap();
+        String eventName = params.get(Constants.PARTIAL_BEHAVIOR_EVENT_PARAM);
+
+        if(eventName != null && event instanceof AjaxBehaviorEvent) {
+            AjaxBehaviorEvent ajaxBehaviorEvent = (AjaxBehaviorEvent) event;
+
+            if(eventName.equals("itemSelect")) {
+                String selectedItemValue = params.get(this.getClientId(context) + "_itemSelect");
+                SelectEvent selectEvent = new SelectEvent(this, (Behavior) ajaxBehaviorEvent.getBehavior(), selectedItemValue);
+                selectEvent.setPhaseId(ajaxBehaviorEvent.getPhaseId());
+                super.queueEvent(selectEvent);
+            }
+            else {
+                //e.g. blur, focus, change
+                super.queueEvent(event);
+            }
+        }
+        else {
+            //e.g. valueChange, autoCompleteEvent
+            super.queueEvent(event);
+        }
     }
 
     private List suggestions = null;
