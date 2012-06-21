7127dfd814225b3da2a04c033201d0b1fc762a9c
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2012-06-21 12:32:51 +0000

    Improved poll ajax update handling

--- src/main/resources/META-INF/resources/primefaces/poll/poll.js
+++ src/main/resources/META-INF/resources/primefaces/poll/poll.js
@@ -1,29 +1,43 @@
-PrimeFaces.widget.Poll = function(cfg) {
-    this.cfg = cfg;
-    this.id = this.cfg.id;
-    this.active = false;
+/**
+ * PrimeFaces Poll Widget
+ */
+PrimeFaces.widget.Poll = PrimeFaces.widget.BaseWidget.extend({
 
-    if(this.cfg.autoStart) {
-        this.start();
+    init: function(cfg) {
+        this.cfg = cfg;
+        this.id = this.cfg.id;
+        this.active = false;
+
+        if(this.cfg.autoStart) {
+            this.start();
+        }
+    },
+    
+    refresh: function(cfg) {
+        if(this.isActive()) {
+            this.stop();
+        }
+        
+        this.init(cfg);
+    },
+
+    start: function() {
+        this.timer = setInterval(this.cfg.fn, (this.cfg.frequency * 1000));
+        this.active = true;
+    },
+
+    stop: function() {
+        clearInterval(this.timer);
+        this.active = false;
+    },
+
+    handleComplete: function(xhr, status, args) {
+        if(args.stop) {
+            this.stop();
+        }
+    },
+
+    isActive: function() {
+        return this.active;
     }
-}
-
-PrimeFaces.widget.Poll.prototype.start = function() {
-    this.timer = setInterval(this.cfg.fn, (this.cfg.frequency * 1000));
-    this.active = true;
-}
-
-PrimeFaces.widget.Poll.prototype.stop = function() {
-    clearInterval(this.timer);
-    this.active = false;
-}
-
-PrimeFaces.widget.Poll.prototype.handleComplete = function(xhr, status, args) {
-    if(args.stop) {
-        this.stop();
-    }
-}
-
-PrimeFaces.widget.Poll.prototype.isActive = function() {
-    return this.active;
-}
+});
