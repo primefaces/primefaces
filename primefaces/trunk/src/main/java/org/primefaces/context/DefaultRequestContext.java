f5387c5a089f962b6ef29a06f872338f58346362
cagatay.civici <cagatay.civici@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2012-07-06 09:19:30 +0000

    Removed old push bits and pieces

--- primefaces/trunk/src/main/java/org/primefaces/context/DefaultRequestContext.java
+++ primefaces/trunk/src/main/java/org/primefaces/context/DefaultRequestContext.java
@@ -27,7 +27,6 @@ public class DefaultRequestContext extends RequestContext {
 
     private final static String CALLBACK_PARAMS_KEY = "CALLBACK_PARAMS";
     private final static String EXECUTE_SCRIPT_KEY = "EXECUTE_SCRIPT";
-    private final static String PUSH_DATA_KEY = "PUSH_DATA";
     private Map<String, Object> attributes;
 
     public DefaultRequestContext() {
@@ -67,16 +66,6 @@ public class DefaultRequestContext extends RequestContext {
     public void execute(String script) {
         getScriptsToExecute().add(script);
     }
-
-    @Override
-    public void push(String channel, Object data) {
-        Map<String,List<Object>> pushData = getPushData(); 
-        if(!pushData.containsKey(channel)) {
-            pushData.put(channel, new ArrayList<Object>());
-        }
-        
-        pushData.get(channel).add(data);
-    }
     
     @Override
     @SuppressWarnings("unchecked")
@@ -95,14 +84,6 @@ public class DefaultRequestContext extends RequestContext {
         return (List<String>) attributes.get(EXECUTE_SCRIPT_KEY);
     }
     
-    @SuppressWarnings("unchecked")
-    public Map<String, List<Object>> getPushData() {
-        if(attributes.get(PUSH_DATA_KEY) == null) {
-            attributes.put(PUSH_DATA_KEY, new HashMap<String, List<Object>>());
-        }
-        return (Map<String, List<Object>>) attributes.get(PUSH_DATA_KEY);
-    }
-
     @Override
     public void scrollTo(String clientId) {
         this.execute("PrimeFaces.scrollTo('" + clientId +  "');");
