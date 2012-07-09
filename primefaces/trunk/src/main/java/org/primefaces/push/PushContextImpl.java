a6c33fae354c018acdac275869cd054bbfe8ead7
jfarcand.oss@gmail.com <jfarcand.oss@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2012-07-09 07:50:34 +0000

    Add support for PushContextListener

--- primefaces/trunk/src/main/java/org/primefaces/push/PushContextImpl.java
+++ primefaces/trunk/src/main/java/org/primefaces/push/PushContextImpl.java
@@ -1,7 +1,10 @@
 package org.primefaces.push;
 
+import org.atmosphere.cpr.Broadcaster;
+import org.atmosphere.cpr.BroadcasterListener;
 import org.atmosphere.cpr.MetaBroadcaster;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
@@ -11,64 +14,45 @@ import org.primefaces.json.JSONObject;
 
 public class PushContextImpl implements PushContext {
 
-    public <T> Future<T> push(String channel, final T t) {
+    private final ConcurrentLinkedQueue<PushContextListener> listeners = new ConcurrentLinkedQueue<PushContextListener>();
+    private final MetaBroadcaster broadcaster = MetaBroadcaster.getDefault();
+
+    public <T> Future<T> push(final String channel, final T t) {
         String data = toJSON(t);
-        final Future<?> f = MetaBroadcaster.getDefault().broadcastTo(channel, data);
-
-        return new Future<T>() {
-
-            public boolean cancel(boolean b) {
-                return f.cancel(b);
+        final Future<?> f = broadcaster.addBroadcasterListener(new BroadcasterListener() {
+            public void onComplete(Broadcaster b) {
+                for (PushContextListener p: listeners) {
+                    p.onComplete(channel, t);
+                    broadcaster.removeBroadcasterListener(this);
+                }
             }
+        }).broadcastTo(channel, data);
 
-            public boolean isCancelled() {
-                return f.isCancelled();
-            }
-
-            public boolean isDone() {
-                return f.isDone();
-            }
-
-            public T get() throws InterruptedException, ExecutionException {
-                f.get();
-                return t;
-            }
-
-            public T get(long l, TimeUnit timeUnit) throws InterruptedException, ExecutionException, TimeoutException {
-                f.get(l, timeUnit);
-                return t;
-            }
-        };
+        return new WrappedFuture(f, t);
     }
 
-    public <T> Future<T> schedule(String channel, final T t, int time, TimeUnit unit) {
+    public <T> Future<T> schedule(final String channel, final T t, int time, TimeUnit unit) {
         String data = toJSON(t);
-        final Future<?> f = MetaBroadcaster.getDefault().scheduleTo(channel, data, time, unit);
-
-        return new Future<T>() {
-
-            public boolean cancel(boolean b) {
-                return f.cancel(b);
+        final Future<?> f = broadcaster.addBroadcasterListener(new BroadcasterListener() {
+            public void onComplete(Broadcaster b) {
+                for (PushContextListener p: listeners) {
+                    p.onComplete(channel, t);
+                    broadcaster.removeBroadcasterListener(this);
+                }
             }
+        }).scheduleTo(channel, data, time, unit);
 
-            public boolean isCancelled() {
-                return f.isCancelled();
-            }
+        return new WrappedFuture(f, t);
+    }
 
-            public boolean isDone() {
-                return f.isDone();
-            }
+    public PushContext addListener(PushContextListener p) {
+        listeners.add(p);
+        return this;
+    }
 
-            public T get() throws InterruptedException, ExecutionException {
-                f.get();
-                return t;
-            }
-
-            public T get(long l, TimeUnit timeUnit) throws InterruptedException, ExecutionException, TimeoutException {
-                f.get(l, timeUnit);
-                return t;
-            }
-        };
+    public PushContext removeListener(PushContextListener p) {
+        listeners.remove(p);
+        return this;
     }
 
     private String toJSON(Object data) {
@@ -108,4 +92,38 @@ public class PushContextImpl implements PushContext {
 
         return true;
     }
+
+    private final static class WrappedFuture<T> implements Future {
+
+        private final Future<?> f;
+        private final T t;
+
+        private WrappedFuture(Future<?> f, T t) {
+            this.f = f;
+            this.t = t;
+        }
+
+        public boolean cancel(boolean b) {
+            return f.cancel(b);
+        }
+
+        public boolean isCancelled() {
+            return f.isCancelled();
+        }
+
+        public boolean isDone() {
+            return f.isDone();
+        }
+
+        public T get() throws InterruptedException, ExecutionException {
+            f.get();
+            return t;
+        }
+
+        public T get(long l, TimeUnit timeUnit) throws InterruptedException, ExecutionException, TimeoutException {
+            f.get(l, timeUnit);
+            return t;
+        }
+    }
+
 }
