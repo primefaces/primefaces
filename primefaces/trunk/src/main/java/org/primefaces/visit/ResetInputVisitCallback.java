31cc9c06b9285d3ff7b3fd7d76ce5e19ae801f9e
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2012-08-12 15:03:37 +0000

    package refactor

--- primefaces/trunk/src/main/java/org/primefaces/visit/ResetInputVisitCallback.java
+++ primefaces/trunk/src/main/java/org/primefaces/visit/ResetInputVisitCallback.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.primefaces.component.resetinput;
+package org.primefaces.visit;
 
 import javax.faces.component.EditableValueHolder;
 import javax.faces.component.UIComponent;
