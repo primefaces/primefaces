1875f679d59063535c316b28b5529fc311412479
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2012-09-01 12:42:27 +0000

    Fixed #4449

--- primefaces/trunk/src/main/java-templates/org/primefaces/component/columns/ColumnsTemplate.java
+++ primefaces/trunk/src/main/java-templates/org/primefaces/component/columns/ColumnsTemplate.java
@@ -1,6 +1,7 @@
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Map;
+import org.primefaces.component.api.UIData;
 import org.primefaces.component.celleditor.CellEditor;
 
     public String getSelectionMode() {
@@ -18,4 +19,11 @@ import org.primefaces.component.celleditor.CellEditor;
     public boolean isDynamic() {
         return true;
     }
+
+    public java.lang.String getColumnIndexVar() {
+		return super.getRowIndexVar();
+	}
+	public void setColumnIndexVar(String _columnIndexVar) {
+		super.setRowIndexVar(_columnIndexVar);
+	}
     
