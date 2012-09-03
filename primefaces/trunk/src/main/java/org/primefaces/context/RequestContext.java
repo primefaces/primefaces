2bdeed36907121dd6e4afc4e3cff77c113cf0d52
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2012-09-03 07:36:18 +0000

    Add javadoc

--- primefaces/trunk/src/main/java/org/primefaces/context/RequestContext.java
+++ primefaces/trunk/src/main/java/org/primefaces/context/RequestContext.java
@@ -21,29 +21,78 @@ import java.util.Map;
 import javax.faces.context.FacesContext;
 import org.primefaces.util.Constants;
 
+/**
+ * A RequestContext is a helper class consisting of several utilities.
+ * RequestContext is thread-safe and scope is same as FacesContext.
+ * Current instance can be retrieved as;
+ * <blockquote>
+ *  RequestContext.getCurrentInstance();
+ * </blockquote>
+ */
 public abstract class RequestContext {
 
+    /**
+     * @return Current RequestRequest instance in thread-local.
+     */
     public static RequestContext getCurrentInstance() {
         return (RequestContext) FacesContext.getCurrentInstance().getAttributes().get(Constants.REQUEST_CONTEXT_ATTR);
     }
 
+    /**
+     * @return true if request is an ajax request, otherwise return false.
+     */
     public abstract boolean isAjaxRequest();
 
+    /**
+     * Add a parameter for ajax oncomplete client side callbacks. Value would be serialized to json.
+     * @param name name of the parameter.
+     * @param object value of the parameter.
+     */
     public abstract void addCallbackParam(String name, Object value);
 
+    /**
+     * @return all callback parameters added in the current request.
+     */
     public abstract Map<String, Object> getCallbackParams();
 
+    /**
+     * @return all scripts added in the current request.
+     */
     public abstract List<String> getScriptsToExecute();
 
+    /**
+     * Execute a javascript after current ajax request is completed.
+     * @param script Javascript statement to execute.
+     */
     public abstract void execute(String script);
     
+    /**
+     * Scroll to a component after ajax request is completed.
+     * @param clientId Client side identifier of the component.
+     */
     public abstract void scrollTo(String clientId);
     
+    /**
+     * Update a component with ajax.
+     * @param name Client side identifier of the component.
+     */
     public abstract void update(String name);
 
+    /**
+     * Update components with ajax.
+     * @param collection Client side identifiers of the components.
+     */
     public abstract void update(Collection<String> collection);
     
+    /**
+     * Reset an editableValueHolder.
+     * @param id Client side identifier of the component.
+     */
     public abstract void reset(String id);
     
+    /**
+     * Reset a collection of editableValueHolders.
+     * @param ids Client side identifiers of the components.
+     */
     public abstract void reset(Collection<String> ids);
 }
