04e5f68f409c71196c55908f53d21b2d654d277f
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2012-09-19 14:57:15 +0000

    Fixed #4651

--- src/main/resources/META-INF/resources/primefaces/menu/menu.js
+++ src/main/resources/META-INF/resources/primefaces/menu/menu.js
@@ -930,7 +930,7 @@ PrimeFaces.widget.PanelMenu = PrimeFaces.widget.BaseWidget.extend({
 
     init: function(cfg) {
         this._super(cfg);
-        this.headers = this.jq.children('h3.ui-panelmenu-header:not(.ui-state-disabled)');
+        this.headers = this.jq.find('> .ui-panelmenu-panel > h3.ui-panelmenu-header:not(.ui-state-disabled)');
         this.menuitemLinks = this.jq.find('.ui-menuitem-link:not(.ui-state-disabled)');
         this.treeLinks = this.jq.find('.ui-menu-parent > .ui-menuitem-link:not(.ui-state-disabled)');
         this.bindEvents();
