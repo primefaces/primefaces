ffcbf450a1b2a7da466beb2e79bbc34c12fcd72a
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2012-10-17 13:44:25 +0000

    Fixed #4113, #4211

--- primefaces/trunk/src/main/java-templates/org/primefaces/component/outputlabel/OutputLabelTemplate.java
+++ primefaces/trunk/src/main/java-templates/org/primefaces/component/outputlabel/OutputLabelTemplate.java
@@ -1,4 +1,3 @@
 
-    public final static String VALID_CLASS = "ui-outputlabel";
-    public final static String INVALID_CLASS = "ui-outputlabel ui-state-error";
+    public final static String STYLE_CLASS = "ui-outputlabel";
     public final static String REQUIRED_FIELD_INDICATOR_CLASS = "ui-outputlabel-rfi";
