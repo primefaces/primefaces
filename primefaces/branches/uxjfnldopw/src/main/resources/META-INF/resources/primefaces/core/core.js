c8e01ca3e2f644458c60b8e83a70b511058ade57
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2012-10-17 07:40:16 +0000

    Fixed #8157

--- primefaces/branches/uxjfnldopw/src/main/resources/META-INF/resources/primefaces/core/core.js
+++ primefaces/branches/uxjfnldopw/src/main/resources/META-INF/resources/primefaces/core/core.js
@@ -289,6 +289,25 @@ PrimeFaces = {
             },1000);
             
     },
+    
+    /**
+     *  Aligns container scrollbar to keep item in container viewport, algorithm copied from jquery-ui menu widget
+     */
+    scrollInView: function(container, item) {        
+        var borderTop = parseFloat(container.css('borderTopWidth')) || 0,
+        paddingTop = parseFloat(container.css('paddingTop')) || 0,
+        offset = item.offset().top - container.offset().top - borderTop - paddingTop,
+        scroll = container.scrollTop(),
+        elementHeight = container.height(),
+        itemHeight = item.outerHeight(true);
+
+        if(offset < 0) {
+            container.scrollTop(scroll + offset);
+        }
+        else if((offset + itemHeight) > elementHeight) {
+            container.scrollTop(scroll + offset - elementHeight + itemHeight);
+        }
+    },
 
     locales : {},
     
