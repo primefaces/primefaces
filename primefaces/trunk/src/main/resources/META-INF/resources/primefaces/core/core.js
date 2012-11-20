115f27333e4e8cec98b3fb4ae845ce363722cdd1
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2012-11-20 13:04:11 +0000

    Fixed #4898

--- primefaces/trunk/src/main/resources/META-INF/resources/primefaces/core/core.js
+++ primefaces/trunk/src/main/resources/META-INF/resources/primefaces/core/core.js
@@ -294,7 +294,11 @@ PrimeFaces = {
     /**
      *  Aligns container scrollbar to keep item in container viewport, algorithm copied from jquery-ui menu widget
      */
-    scrollInView: function(container, item) {        
+    scrollInView: function(container, item) { 
+        if(item.length == 0) {
+            return;
+        }
+        
         var borderTop = parseFloat(container.css('borderTopWidth')) || 0,
         paddingTop = parseFloat(container.css('paddingTop')) || 0,
         offset = item.offset().top - container.offset().top - borderTop - paddingTop,
