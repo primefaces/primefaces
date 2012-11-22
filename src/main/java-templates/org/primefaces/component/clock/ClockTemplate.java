05358c025806e2f7642db277351a2e48acf3979f
umuttirasoglu@gmail.com <umuttirasoglu@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2012-11-22 15:19:37 +0000

    Drop in of new clock component.

---
+++ src/main/java-templates/org/primefaces/component/clock/ClockTemplate.java
@@ -0,0 +1,29 @@
+import org.primefaces.component.calendar.Calendar;
+import org.primefaces.event.DateSelectEvent;
+import org.primefaces.util.HTML;
+import org.primefaces.util.ArrayUtils;
+import org.primefaces.util.Constants;
+import org.primefaces.util.ComponentUtils;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Date;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import javax.faces.event.AjaxBehaviorEvent;
+import javax.faces.event.FacesEvent;
+import javax.faces.event.PhaseId;
+
+    public final static String INPUT_STYLE_CLASS = "ui-inputfield ui-widget ui-state-default ui-corner-all";
+
+	private java.util.Locale calculatedLocale;
+	private java.util.TimeZone appropriateTimeZone;
+	
+
+
+    
