1bc1229a1855435a4e09b1eeab83a7b0a0a7eacb
umuttirasoglu@gmail.com <umuttirasoglu@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2012-11-22 15:19:37 +0000

    Drop in of new clock component.

---
+++ primefaces/trunk/src/main/java/org/primefaces/component/clock/ClockRenderer.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2009-2012 Prime Teknoloji.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.primefaces.component.clock;
+
+import java.io.IOException;
+import java.util.Date;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+import org.primefaces.component.calendar.CalendarUtils;
+import org.primefaces.renderkit.CoreRenderer;
+import org.primefaces.util.WidgetBuilder;
+
+public class ClockRenderer extends CoreRenderer {
+
+    @Override
+    public void encodeEnd(FacesContext context, UIComponent component) throws IOException {
+
+        ResponseWriter writer = context.getResponseWriter();
+        Clock clock = (Clock) component;
+        WidgetBuilder wb = getWidgetBuilder(context);
+        //wb.widget("Clock", null, clock.getClientId(context), true);
+
+        writer.startElement("span", clock);
+        writer.writeAttribute("id", clock.getClientId(), null);
+        writer.writeAttribute("class", "ui-clock", null);
+        writer.endElement("span");
+
+        encodeScript(context, clock);
+
+    }
+
+    @Override
+    public boolean getRendersChildren() {
+        return true;
+    }
+
+    protected void encodeScript(FacesContext context, Clock clock) throws IOException {
+        ResponseWriter writer = context.getResponseWriter();
+        String clientId = clock.getClientId(context);
+        WidgetBuilder wb = getWidgetBuilder(context);
+        Date newDate = new Date();
+
+        wb.widget("Clock", clock.resolveWidgetVar(), clientId, false);
+        wb.attr("dateFormat", clock.getPattern());
+        wb.attr("serverDate", newDate.toString());
+        wb.attr("serverMode", clock.isServerMode());
+
+        encodeClientBehaviors(context, clock, wb);
+
+        startScript(writer, clientId);
+        writer.write(wb.build());
+        endScript(writer);
+    }
+}
