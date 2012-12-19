a5233c83b3cfdd10f7890774d45c12ac63f0e9bc
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2012-12-19 13:31:38 +0000

    Initial drop in of custom content impl for selectOneListbox

--- src/main/java-templates/org/primefaces/component/selectonelistbox/SelectOneListboxTemplate.java
+++ src/main/java-templates/org/primefaces/component/selectonelistbox/SelectOneListboxTemplate.java
@@ -1,4 +1,5 @@
 import javax.faces.component.UINamingContainer;
 
     public static final String CONTAINER_CLASS = "ui-selectonelistbox ui-inputfield ui-widget ui-widget-content ui-corner-all";
+    public static final String LIST_CLASS = "ui-selectlistbox-list";
     public static final String ITEM_CLASS = "ui-selectlistbox-item ui-corner-all";
