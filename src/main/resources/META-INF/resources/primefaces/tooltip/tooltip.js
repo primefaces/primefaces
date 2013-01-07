be1ab1092f7add4f635f59aa58c9100ca11b9fd5
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2013-01-07 19:05:59 +0000

    Added global tooltip feature.

--- src/main/resources/META-INF/resources/primefaces/tooltip/tooltip.js
+++ src/main/resources/META-INF/resources/primefaces/tooltip/tooltip.js
@@ -5,60 +5,87 @@ PrimeFaces.widget.Tooltip = PrimeFaces.widget.BaseWidget.extend({
     
     init: function(cfg) {
         this.cfg = cfg;
-        this.id = this.cfg.id;
-        this.jqId = PrimeFaces.escapeClientId(this.id);
-        var _self = this;
-
-        //remove previous element to support ajax updates
-        $(document.body).children(this.jqId).remove();
-
-        this.jq = $(this.jqId);
-        this.cfg = cfg;
-        this.target = $(PrimeFaces.escapeClientId(this.cfg.target));
-
-        //options
-        this.cfg.showEvent = this.cfg.showEvent ? this.cfg.showEvent : 'mouseover';
-        this.cfg.hideEvent = this.cfg.hideEvent ? this.cfg.hideEvent : 'mouseout';
+        this.cfg.showEvent = this.cfg.showEvent ? this.cfg.showEvent + '.tooltip' : 'mouseover.tooltip';
+        this.cfg.hideEvent = this.cfg.hideEvent ? this.cfg.hideEvent + '.tooltip' : 'mouseout.tooltip';
         this.cfg.showEffect = this.cfg.showEffect ? this.cfg.showEffect : 'fade';
         this.cfg.hideEffect = this.cfg.hideEffect ? this.cfg.hideEffect : 'fade';
-
-        //bind tooltip to the target
-        this.bindEvents();
-
-        //append to body
-        this.jq.appendTo(document.body);
-
-        //use target title if value is blank
-        if($.trim(this.jq.html()) == '') {
-            this.jq.html(this.target.attr('title'));
-        }
-
-        //remove target's title
-        this.target.removeAttr('title');
+        
+        if(this.cfg.target)
+            this.bindTarget();
+        else
+            this.bindGlobal();
 
         //Hide overlay on resize
         var resizeNS = 'resize.' + this.id;
         $(window).unbind(resizeNS).bind(resizeNS, function() {
-            if(_self.jq.is(':visible')) {
-                _self.align();
+            if($this.jq.is(':visible')) {
+                $this.align();
             }
         });
         
         $(this.jqId + '_s').remove();
     },
     
-    bindEvents: function() {
-        var _self = this;
-
-        this.target.bind(this.cfg.showEvent, function() {
-            _self.show();
-        })
-        .bind(this.cfg.hideEvent, function() {
-            _self.hide();
-        });
+    refresh: function(cfg) {
+        if(cfg.target) {
+            $(document.body).children(PrimeFaces.escapeClientId(cfg.id)).remove();
+        } 
     },
     
-    align: function() {
+    bindGlobal: function() {
+        this.jq = $('<div class="ui-tooltip ui-tooltip-global ui-widget ui-widget-content ui-corner-all ui-shadow" />').appendTo('body');
+        this.globalSelector = 'a,:input,:button';
+        var $this = this;
+        
+        $(document).off(this.cfg.showEvent + ' ' + this.cfg.hideEvent, this.globalSelector)
+                    .on(this.cfg.showEvent, this.globalSelector, function() {
+                        var element = $(this),
+                        title = element.attr('title');
+
+                        if(title) {
+                            $this.jq.text(title);
+                            $this.globalTitle = title;
+                            element.removeAttr('title');
+                            $this.show(element);
+                        }
+                    })
+                    .on(this.cfg.hideEvent + '.tooltip', this.globalSelector, function() {
+                        var element = $(this);
+
+                        if($this.globalTitle) {
+                            $this.hide();
+                            element.attr('title', $this.globalTitle);
+                            $this.globalTitle = null;
+                        }
+                    });
+    },
+    
+    bindTarget: function() {
+        this.id = this.cfg.id;
+        this.jqId = PrimeFaces.escapeClientId(this.id);
+        this.jq = $(this.jqId);
+        this.target = $(PrimeFaces.escapeClientId(this.cfg.target));
+        
+        var $this = this;
+        this.target.on(this.cfg.showEvent + '.tooltip', function() {
+            $this.show();
+        })
+        .on(this.cfg.hideEvent + '.tooltip', function() {
+            $this.hide();
+        });
+
+        this.jq.appendTo(document.body);
+
+        if($.trim(this.jq.html()) == '') {
+            this.jq.html(this.target.attr('title'));
+        }
+
+        this.target.removeAttr('title');
+    },
+
+    align: function(element) {
+        var el = element ? element : this.target;
+        
         this.jq.css({
             left:'', 
             top:'',
@@ -67,15 +94,15 @@ PrimeFaces.widget.Tooltip = PrimeFaces.widget.BaseWidget.extend({
         .position({
             my: 'left top',
             at: 'right bottom',
-            of: this.target
+            of: el
         });
     },
     
-    show: function() {
+    show: function(element) {
         var _self = this;
 
         this.timeout = setTimeout(function() {
-            _self.align();
+            _self.align(element);
             _self.jq.show(_self.cfg.showEffect, {}, 400);
         }, 150);
     },
