a34371a79e6cb1ddaa91f639abc0346b6eeed8dd
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2013-03-28 13:16:27 +0000

    Set to 4.0

--- src/main/java/org/primefaces/util/Constants.java
+++ src/main/java/org/primefaces/util/Constants.java
@@ -32,7 +32,7 @@ public class Constants {
     
     public static final String DOWNLOAD_COOKIE = "primefaces.download";
 
-    public final static String VERSION = "3.6-SNAPSHOT";
+    public final static String VERSION = "4.0-SNAPSHOT";
     public final static String LIBRARY = "primefaces";
 
     public final static String CUSTOM_EVENT = "CUSTOM_EVENT";
