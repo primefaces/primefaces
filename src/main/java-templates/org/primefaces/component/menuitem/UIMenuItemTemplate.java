dad2f83fe0da5e196f434d61ac62cfe67f89f609
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2013-04-09 08:02:23 +0000

    Fixed template name

---
+++ src/main/java-templates/org/primefaces/component/menuitem/UIMenuItemTemplate.java
@@ -0,0 +1,50 @@
+import java.util.List;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIParameter;
+import java.util.Map;
+import javax.faces.event.ActionEvent;
+import javax.el.MethodExpression;
+
+	public void decode(FacesContext facesContext) {
+		Map<String,String> params = facesContext.getExternalContext().getRequestParameterMap();
+		String clientId = getClientId(facesContext);
+		
+		if(params.containsKey(clientId)) {
+			this.queueEvent(new ActionEvent(this));
+		}
+	}
+	
+	public boolean shouldRenderChildren() {
+		if(getChildCount() == 0)
+			return false;
+		else {
+			for(UIComponent child : getChildren()) {
+				if(! (child instanceof UIParameter) ) {
+					return true;
+				}
+			}
+		}
+		
+		return false;
+	}
+
+    public boolean isPartialSubmitSet() {
+        return (getStateHelper().get(PropertyKeys.partialSubmit) != null) || (this.getValueExpression("partialSubmit") != null); 
+    }
+
+    public String getHref() {
+        return this.getUrl();
+    }
+
+    public boolean isDynamic() {
+        return false;
+    }
+
+    public Map<String, List<String>> getParams() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public String getCommand() {
+        MethodExpression expr = super.getActionExpression();
+        return expr != null ? expr.getExpressionString() : null;
+    }
