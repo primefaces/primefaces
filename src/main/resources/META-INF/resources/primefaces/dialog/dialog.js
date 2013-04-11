ce3f747869bff25c91a0dc2eb52d67af0e194d62
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2013-04-11 19:24:17 +0000

    Add options feature to Dialog Framework

--- src/main/resources/META-INF/resources/primefaces/dialog/dialog.js
+++ src/main/resources/META-INF/resources/primefaces/dialog/dialog.js
@@ -339,10 +339,6 @@ PrimeFaces.widget.Dialog = PrimeFaces.widget.BaseWidget.extend({
             ,'visibility':'hidden'
         });
         
-        if(this.cfg.onHide) {
-            this.cfg.onHide.call(this, event, ui);
-        }
-
         if(this.cfg.behaviors) {
             var closeBehavior = this.cfg.behaviors['close'];
 
@@ -355,6 +351,10 @@ PrimeFaces.widget.Dialog = PrimeFaces.widget.BaseWidget.extend({
             'aria-hidden': true
             ,'aria-live': 'off'
         });
+        
+        if(this.cfg.onHide) {
+            this.cfg.onHide.call(this, event, ui);
+        }
     },
     
     moveToTop: function() {
