34e306b9a33c7fe27a5dce4886966025e9df7691
andraschko.thomas@gmail.com <andraschko.thomas@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2013-04-19 20:15:07 +0000

    #5497

--- primefaces/trunk/src/main/java/org/primefaces/component/fileupload/FileUploadHandler.java
+++ primefaces/trunk/src/main/java/org/primefaces/component/fileupload/FileUploadHandler.java
@@ -28,7 +28,7 @@ import org.primefaces.facelets.MethodRule;
 public class FileUploadHandler extends ComponentHandler {
 
 	private static final MetaRule FILE_UPLOAD_LISTENER =
-			new MethodRule("fileUploadListener", List.class, new Class[]{FileUploadEvent.class});
+			new MethodRule("fileUploadListener", null, new Class[]{FileUploadEvent.class});
 	
 	public FileUploadHandler(ComponentConfig config) {
 		super(config);
