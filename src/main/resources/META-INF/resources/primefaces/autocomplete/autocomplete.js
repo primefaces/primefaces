f14eb1fc3c65b316d6fbf47e77a50342f3d5c4a6
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2013-05-09 21:39:54 +0000

    Refactor

--- src/main/resources/META-INF/resources/primefaces/autocomplete/autocomplete.js
+++ src/main/resources/META-INF/resources/primefaces/autocomplete/autocomplete.js
@@ -647,15 +647,14 @@ PrimeFaces.widget.AutoComplete = PrimeFaces.widget.BaseWidget.extend({
                 panelWidth = inputWidth;
             }
         }
-            var hidden = this.panel.is(':hidden');
-            //adjust height
-            if(this.cfg.scrollHeight) {
-                var heightConstraint = hidden ? this.panel.height() : this.panel.children().height();
-                if(heightConstraint > this.cfg.scrollHeight)
-                    this.panel.height(this.cfg.scrollHeight);
-                else
-                    this.panel.css('height', 'auto');                                               
-            }
+        
+        if(this.cfg.scrollHeight) {
+            var heightConstraint = this.panel.is(':hidden') ? this.panel.height() : this.panel.children().height();
+            if(heightConstraint > this.cfg.scrollHeight)
+                this.panel.height(this.cfg.scrollHeight);
+            else
+                this.panel.css('height', 'auto');                                               
+        }
 
         this.panel.css({
                         'left':'',
