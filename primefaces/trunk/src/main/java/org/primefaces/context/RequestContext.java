13d81e40bc94f7295a12730504c4762ae90b312e
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2013-06-07 20:38:36 +0000

    Add javadoc

--- primefaces/trunk/src/main/java/org/primefaces/context/RequestContext.java
+++ primefaces/trunk/src/main/java/org/primefaces/context/RequestContext.java
@@ -115,17 +115,43 @@ public abstract class RequestContext {
      */
     public abstract AjaxRequestBuilder getAjaxRequestBuilder();
     
+    /**
+     * @return Attributes map in RequestContext scope
+     */
     public abstract Map<Object,Object> getAttributes();
 
+    /**
+     * Open a view in dialog.
+     * @param outcome The logical outcome used to resolve a navigation case.
+     */
     public abstract void openDialog(String outcome);
     
+    /**
+     * Open a view in dialog.
+     * @param outcome The logical outcome used to resolve a navigation case.
+     * @param options Configuration options for the dialog.
+     * @param params Parameters to send to the view displayed in a dialog.
+     */
     public abstract void openDialog(String outcome, Map<String,Object> options, Map<String,List<String>> params);
     
+    /**
+     * Close a dialog.
+     * @param data Optional data to pass back to a dialogReturn event.
+     */
     public abstract void closeDialog(Object data);
 
+    /**
+     * @return ApplicationContext instance.
+     */
     public abstract ApplicationContext getApplicationContext();
 
+    /**
+     * @return StringEncrypter used to encode and decode a string.
+     */
 	public abstract StringEncrypter getEncrypter();
     
+    /**
+     * Clear resources.
+     */
     public abstract void release();
 }
