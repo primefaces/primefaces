5bda7fef2091a69bf08929532cd5a330db387af2
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2013-07-13 11:23:03 +0000

    Added fileupload support for IE 8-9 (graceful degredation) and IE10 (fixed choose button css issue).

--- primefaces/trunk/src/main/resources/META-INF/resources/primefaces/fileupload/fileupload.js
+++ primefaces/trunk/src/main/resources/META-INF/resources/primefaces/fileupload/fileupload.js
@@ -1538,7 +1538,6 @@
  * PrimeFaces FileUpload Widget
  */
 PrimeFaces.widget.FileUpload = PrimeFaces.widget.BaseWidget.extend({
-    
     init: function(cfg) {
         this._super(cfg);
         this.ucfg = {};
@@ -1552,26 +1551,26 @@ PrimeFaces.widget.FileUpload = PrimeFaces.widget.BaseWidget.extend({
         this.filesTbody = this.content.find('> table.ui-fileupload-files > tbody');
         this.sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
         this.files = [];
-        this.cfg.invalidFileMessage = this.cfg.invalidFileMessage||'Invalid file type';
-        this.cfg.invalidSizeMessage = this.cfg.invalidSizeMessage||'Invalid file size';
-        this.cfg.fileLimitMessage = this.cfg.fileLimitMessage||'Maximum number of files exceeded';
-        this.cfg.messageTemplate = this.cfg.messageTemplate||'{name} {size}';
-        this.cfg.previewWidth = this.cfg.previewWidth||48;
+        this.cfg.invalidFileMessage = this.cfg.invalidFileMessage || 'Invalid file type';
+        this.cfg.invalidSizeMessage = this.cfg.invalidSizeMessage || 'Invalid file size';
+        this.cfg.fileLimitMessage = this.cfg.fileLimitMessage || 'Maximum number of files exceeded';
+        this.cfg.messageTemplate = this.cfg.messageTemplate || '{name} {size}';
+        this.cfg.previewWidth = this.cfg.previewWidth || 48;
         this.uploadedFileCount = 0;
-        
+
         this.renderMessages();
-        
+
         this.bindEvents();
-        
+
         var $this = this,
-        encodedURLfield = this.form.children("input[name='javax.faces.encodedURL']");
+                encodedURLfield = this.form.children("input[name='javax.faces.encodedURL']");
 
         this.ucfg = {
             url: (encodedURLfield.length) ? encodedURLfield.val() : this.form.attr('action'),
             fileInput: this.input,
             paramName: this.id,
             dataType: 'xml',
-            dropZone: (this.cfg.dnd == false) ? null : this.jq, 
+            dropZone: (this.cfg.dnd == false) ? null : this.jq,
             formData: function() {
                 return $this.createPostData();
             },
@@ -1579,21 +1578,23 @@ PrimeFaces.widget.FileUpload = PrimeFaces.widget.BaseWidget.extend({
                 xhr.setRequestHeader('Faces-Request', 'partial/ajax');
             },
             add: function(e, data) {
+                $this.chooseButton.removeClass('ui-state-hover ui-state-focus');
+
                 var files = data.files;
-                
-                if($this.cfg.fileLimit && ($this.uploadedFileCount + files.length) > $this.cfg.fileLimit) {
+
+                if ($this.cfg.fileLimit && ($this.uploadedFileCount + files.length) > $this.cfg.fileLimit) {
                     $this.showMessage({
                         summary: $this.cfg.fileLimitMessage
                     });
-                    
+
                     return;
                 }
 
-                if(files && files.length) {
-                    for(var i = 0; i < files.length; i++) {
+                if (files && files.length) {
+                    for (var i = 0; i < files.length; i++) {
                         var file = files[i],
-                        validMsg = $this.validate(file);
-                
+                                validMsg = $this.validate(file);
+
                         if(validMsg) {
                             $this.showMessage({
                                 summary: validMsg,
@@ -1603,25 +1604,28 @@ PrimeFaces.widget.FileUpload = PrimeFaces.widget.BaseWidget.extend({
                         }
                         else {
                             $this.clearMessages();
-                            
+
                             var row = $('<tr></tr>').append('<td class="ui-fileupload-preview"></td>')
-                                        .append('<td>' + file.name + '</td>')
-                                        .append('<td>' + $this.formatSize(file.size) + '</td>')
-                                        .append('<td class="ui-fileupload-progress"></td>')
-                                        .append('<td><button class="ui-fileupload-cancel ui-button ui-widget ui-state-default ui-corner-all ui-button-icon-only"><span class="ui-button-icon-left ui-icon ui-icon ui-icon-close"></span><span class="ui-button-text">ui-button</span></button></td>')
-                                        .appendTo($this.filesTbody)
-                                
+                                    .append('<td>' + file.name + '</td>')
+                                    .append('<td>' + $this.formatSize(file.size) + '</td>')
+                                    .append('<td class="ui-fileupload-progress"></td>')
+                                    .append('<td><button class="ui-fileupload-cancel ui-button ui-widget ui-state-default ui-corner-all ui-button-icon-only"><span class="ui-button-icon-left ui-icon ui-icon ui-icon-close"></span><span class="ui-button-text">ui-button</span></button></td>')
+                                    .appendTo($this.filesTbody);
+
                             //preview
-                            var reader = new FileReader();
-                            reader.onload = function (e) {
-                                var img = $('<img alt="' + file.name + '" src="' + e.target.result + '" width="' + $this.cfg.previewWidth + '"></img>');
-                                if($this.cfg.previewHeight) {
-                                    img.height($this.cfg.previewHeight);
+                            if(window.FileReader) {
+                                var reader = new FileReader();
+                                reader.onload = function(e) {
+                                    var img = $('<img alt="' + file.name + '" src="' + e.target.result + '" width="' + $this.cfg.previewWidth + '"></img>');
+                                    if ($this.cfg.previewHeight) {
+                                        img.height($this.cfg.previewHeight);
+                                    }
+
+                                    row.children('td.ui-fileupload-preview').append(img);
                                 }
-                                
-                                row.children('td.ui-fileupload-preview').append(img);
+                                reader.readAsDataURL(file);
                             }
-                            reader.readAsDataURL(file);
+
 
                             //progress
                             row.children('td.ui-fileupload-progress').append('<div class="ui-progressbar ui-widget ui-widget-content ui-corner-all" role="progressbar" aria-valuemin="0" aria-valuemax="100" aria-valuenow="0"><div class="ui-progressbar-value ui-widget-header ui-corner-left" style="display: none; width: 0%;"></div></div>');
@@ -1629,37 +1633,53 @@ PrimeFaces.widget.FileUpload = PrimeFaces.widget.BaseWidget.extend({
                             file.row = row;
 
                             $this.files.push(file);
-                            
-                            if($this.cfg.auto) {
+
+                            if ($this.cfg.auto) {
                                 $this.upload();
                             }
                         }
                     }
                 }
             },
-            progress: function(e, data) {
-                var progress = parseInt(data.loaded / data.total * 100, 10);
+            send: function(e, data) {
+                if(!window.FormData) {
+                    for(var i = 0; i < data.files.length; i++) {
+                        var file = data.files[i];
 
-                for(var i = 0; i < data.files.length; i++) {
-                    var file = data.files[i];
-                    
-                    file.row.children('.ui-fileupload-progress').find('> .ui-progressbar > .ui-progressbar-value').css({
-                        width: progress + '%',
-                        display: 'block'
-                    });
+                        file.row.children('.ui-fileupload-progress').find('> .ui-progressbar > .ui-progressbar-value')
+                                .addClass('ui-progressbar-value-legacy')
+                                .css({
+                                    width: '100%',
+                                    display: 'block'
+                                });
+                    }
+                }
+            },
+            progress: function(e, data) {
+                if(window.FormData) {
+                    var progress = parseInt(data.loaded / data.total * 100, 10);
+
+                    for(var i = 0; i < data.files.length; i++) {
+                        var file = data.files[i];
+
+                        file.row.children('.ui-fileupload-progress').find('> .ui-progressbar > .ui-progressbar-value').css({
+                            width: progress + '%',
+                            display: 'block'
+                        });
+                    }
                 }
             },
             done: function(e, data) {
                 $this.uploadedFileCount += data.files.length;
                 $this.removeFiles(data.files);
-                        
-                var xmlDoc = $(data.result.documentElement),
-                updates = xmlDoc.find('update');
 
-                for(var i=0; i < updates.length; i++) {
+                var xmlDoc = $(data.result.documentElement),
+                        updates = xmlDoc.find('update');
+
+                for (var i = 0; i < updates.length; i++) {
                     var update = updates.eq(i),
-                    id = update.attr('id'),
-                    content = update.text();
+                            id = update.attr('id'),
+                            content = update.text();
 
                     PrimeFaces.ajax.AjaxUtils.updateElement.call(this, id, content);
                 }
@@ -1667,177 +1687,167 @@ PrimeFaces.widget.FileUpload = PrimeFaces.widget.BaseWidget.extend({
                 PrimeFaces.ajax.AjaxUtils.handleResponse.call(this, xmlDoc);
             }
         };
-        
+
         this.jq.fileupload(this.ucfg);
     },
-        
     bindEvents: function() {
         var $this = this;
-        
+
         PrimeFaces.skinButton(this.buttonBar.children('.ui-button'));
-        
+
         this.uploadButton.on('click.fileupload', function(e) {
             $this.upload();
-            
+
             e.preventDefault();
         });
-        
+
         this.cancelButton.on('click.fileupload', function(e) {
             $this.clear();
-            
+
             e.preventDefault();
         });
-        
+
         this.clearMessageLink.on('click.fileupload', function(e) {
-            $this.messageContainer.slideUp(function() {
+            $this.messageContainer.fadeOut(function() {
                 $this.messageList.children().remove();
             });
-            
+
             e.preventDefault();
         });
-        
+
         this.rowActionSelector = this.jqId + " .ui-fileupload-files button";
         this.rowCancelActionSelector = this.jqId + " .ui-fileupload-files .ui-fileupload-cancel";
         this.clearMessagesSelector = this.jqId + " .ui-messages .ui-messages-close";
-        
+
         $(document).off('mouseover.fileupload mouseout.fileupload mousedown.fileupload mouseup.fileupload focus.fileupload blur.fileupload click.fileupload ', this.rowCancelActionSelector)
-                    .on('mouseover.fileupload', this.rowCancelActionSelector, null, function(e) {
-                        $(this).addClass('ui-state-hover');
-                    })
-                    .on('mouseout.fileupload', this.rowCancelActionSelector, null, function(e) {
-                        $(this).removeClass('ui-state-hover ui-state-active');
-                    })
-                    .on('mousedown.fileupload', this.rowCancelActionSelector, null, function(e) {
-                        $(this).addClass('ui-state-active').removeClass('ui-state-hover');
-                    })
-                    .on('mouseup.fileupload', this.rowCancelActionSelector, null, function(e) {
-                        $(this).addClass('ui-state-hover').removeClass('ui-state-active');
-                    })
-                    .on('focus.fileupload', this.rowCancelActionSelector, null, function(e) {
-                        $(this).addclass('ui-state-focus');
-                    })
-                    .on('blur.fileupload', this.rowCancelActionSelector, null, function(e) {
-                        $(this).removeClass('ui-state-focus');
-                    })
-                    .on('click.fileupload', this.rowCancelActionSelector, null, function(e) {
-                        var row = $(this).closest('tr'),
-                        removedFile = $this.files.splice(row.index(), 1);
-                        removedFile[0].row = null;
-                                                
-                        $this.removeFileRow(row);
-                        
-                        e.preventDefault();
-                    });
+                .on('mouseover.fileupload', this.rowCancelActionSelector, null, function(e) {
+            $(this).addClass('ui-state-hover');
+        })
+                .on('mouseout.fileupload', this.rowCancelActionSelector, null, function(e) {
+            $(this).removeClass('ui-state-hover ui-state-active');
+        })
+                .on('mousedown.fileupload', this.rowCancelActionSelector, null, function(e) {
+            $(this).addClass('ui-state-active').removeClass('ui-state-hover');
+        })
+                .on('mouseup.fileupload', this.rowCancelActionSelector, null, function(e) {
+            $(this).addClass('ui-state-hover').removeClass('ui-state-active');
+        })
+                .on('focus.fileupload', this.rowCancelActionSelector, null, function(e) {
+            $(this).addclass('ui-state-focus');
+        })
+                .on('blur.fileupload', this.rowCancelActionSelector, null, function(e) {
+            $(this).removeClass('ui-state-focus');
+        })
+                .on('click.fileupload', this.rowCancelActionSelector, null, function(e) {
+            var row = $(this).closest('tr'),
+                    removedFile = $this.files.splice(row.index(), 1);
+            removedFile[0].row = null;
+
+            $this.removeFileRow(row);
+
+            e.preventDefault();
+        });
     },
-            
     upload: function() {
-        if(this.cfg.merge) {
+        if (this.cfg.merge) {
             this.jq.fileupload('send', {files: $this.files});
         }
         else {
-            for(var i = 0; i < this.files.length; i++) {
+            for (var i = 0; i < this.files.length; i++) {
                 this.jq.fileupload('send', {files: this.files[i]});
             }
         }
     },
-            
     createPostData: function() {
         var process = this.cfg.process ? this.id + ' ' + this.cfg.process : this.id,
-        params = this.form.serializeArray();
+                params = this.form.serializeArray();
 
         params.push({name: PrimeFaces.PARTIAL_REQUEST_PARAM, value: 'true'});
         params.push({name: PrimeFaces.PARTIAL_PROCESS_PARAM, value: process});
         params.push({name: PrimeFaces.PARTIAL_SOURCE_PARAM, value: this.id});
 
-        if(this.cfg.update) {
+        if (this.cfg.update) {
             params.push({name: PrimeFaces.PARTIAL_UPDATE_PARAM, value: this.cfg.update});
         }
 
         return params;
     },
-            
     formatSize: function(bytes) {
-        if(bytes === 0) 
+        if(bytes === undefined)
+            return '';
+
+        if (bytes === 0)
             return 'N/A';
-        
+
         var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
-        if(i === 0) 
-            return bytes + ' ' + this.sizes[i]; 
+        if (i === 0)
+            return bytes + ' ' + this.sizes[i];
         else
             return (bytes / Math.pow(1024, i)).toFixed(1) + ' ' + this.sizes[i];
     },
-            
     removeFiles: function(files) {
-        for(var i = 0; i < files.length; i++) {
+        for (var i = 0; i < files.length; i++) {
             this.removeFile(files[i]);
-        }
-    },      
-            
+        }           },
     removeFile: function(file) {
         var $this = this;
-        
+
         this.files = $.grep(this.files, function(value) {
             return (value.name === file.name && value.size === file.size);
         }, true);
-        
+
         $this.removeFileRow(file.row);
         file.row = null;
     },
-            
     removeFileRow: function(row) {
         row.fadeOut(function() {
             $(this).remove();
         });
     },
-            
     clear: function() {
-        for(var i = 0; i < this.files.length; i++) {
+        for (var i = 0; i < this.files.length; i++) {
             this.removeFileRow(this.files[i].row);
             this.files[i].row = null;
         }
-        
+
         this.clearMessages();
-        
+
         this.files = [];
     },
-            
     validate: function(file) {
-        if(this.cfg.allowTypes && !(this.cfg.allowTypes.test(file.type) || this.cfg.allowTypes.test(file.name))) {
+        if (this.cfg.allowTypes && !(this.cfg.allowTypes.test(file.type) || this.cfg.allowTypes.test(file.name))) {
             return this.cfg.invalidFileMessage;
         }
-        
-        if(this.cfg.maxFileSize && file.size > this.cfg.maxFileSize) {
+
+        if (this.cfg.maxFileSize && file.size > this.cfg.maxFileSize) {
             return this.cfg.invalidSizeMessage;
         }
-        
+
         return null;
     },
-            
     renderMessages: function() {
         var markup = '<div class="ui-messages ui-widget ui-helper-hidden"><div class="ui-messages-error ui-corner-all">' +
-                     '<a class="ui-messages-close" href="#"><span class="ui-icon ui-icon-close"></span></a>' +
-                     '<span class="ui-messages-error-icon"></span>' +
-                     '<ul></ul>' +
-                     '</div></div>';
-             
+                '<a class="ui-messages-close" href="#"><span class="ui-icon ui-icon-close"></span></a>' +
+                '<span class="ui-messages-error-icon"></span>' +
+                '<ul></ul>' +
+                '</div></div>';
+
         this.messageContainer = $(markup).prependTo(this.content);
         this.messageList = this.messageContainer.find('> .ui-messages-error > ul');
         this.clearMessageLink = this.messageContainer.find('> .ui-messages-error > a.ui-messages-close');
-    },  
-            
+  },
     clearMessages: function() {
         this.messageContainer.hide();
         this.messageList.children().remove();
     },
-            
     showMessage: function(msg) {
         var summary = msg.summary,
-        detail = '';
+                detail = '';
 
-        if(msg.filename && msg.filesize) {
+        if (msg.filename && msg.filesize) {
             detail = this.cfg.messageTemplate.replace('{name}', msg.filename).replace('{size}', this.formatSize(msg.filesize));
         }
-        
+
         this.messageList.append('<li><span class="ui-messages-error-summary">' + summary + '</span><span class="ui-messages-error-detail">' + detail + '</span></li>');
         this.messageContainer.show();
     }
