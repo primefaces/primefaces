aa41a0a94497ebf68c0b1f217a98b2526d10f73e
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2013-07-15 06:37:34 +0000

    Base artifacts for new multiselectlistbox component.

---
+++ primefaces/trunk/src/main/java-templates/org/primefaces/component/multiselectlistbox/MultiSelectListboxTemplate.java
@@ -0,0 +1,3 @@
+    public static String CONTAINER_CLASS = "ui-multiselectlistbox ui-widget";
+    public static String LIST_CONTAINER_CLASS = "ui-multiselectlistbox-listcontainer ui-widget";
+    public static String LIST_CLASS = "ui-multiselectlistbox-list ui-widget";
