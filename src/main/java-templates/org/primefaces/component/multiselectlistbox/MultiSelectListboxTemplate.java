03ba1741e8f65fa690bb8906ab85eab722f4d04a
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2013-07-16 03:55:49 +0000

    Markup encode for multiselectlistbox component plus css, next is the widget.

--- src/main/java-templates/org/primefaces/component/multiselectlistbox/MultiSelectListboxTemplate.java
+++ src/main/java-templates/org/primefaces/component/multiselectlistbox/MultiSelectListboxTemplate.java
@@ -1,3 +1,4 @@
-    public static String CONTAINER_CLASS = "ui-multiselectlistbox ui-widget";
-    public static String LIST_CONTAINER_CLASS = "ui-multiselectlistbox-listcontainer ui-widget";
-    public static String LIST_CLASS = "ui-multiselectlistbox-list ui-widget";
+    public static String CONTAINER_CLASS = "ui-multiselectlistbox ui-inputfield ui-widget ui-widget-content ui-corner-all";
+    public static String LIST_CONTAINER_CLASS = "ui-multiselectlistbox-listcontainer";
+    public static String LIST_CLASS = "ui-multiselectlistbox-list";
+    public static String ITEM_CLASS = "ui-multiselectlistbox-item";
