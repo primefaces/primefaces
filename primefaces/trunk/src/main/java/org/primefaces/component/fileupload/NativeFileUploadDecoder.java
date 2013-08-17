f29afce03f5624e177df107dc1a16920af84c777
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2013-08-17 12:02:47 +0000

    Removed unused method.

--- primefaces/trunk/src/main/java/org/primefaces/component/fileupload/NativeFileUploadDecoder.java
+++ primefaces/trunk/src/main/java/org/primefaces/component/fileupload/NativeFileUploadDecoder.java
@@ -16,9 +16,6 @@
 package org.primefaces.component.fileupload;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 import javax.faces.FacesException;
 import javax.faces.context.FacesContext;
 import javax.servlet.ServletException;
@@ -26,7 +23,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.Part;
 import org.primefaces.event.FileUploadEvent;
 import org.primefaces.model.NativeUploadedFile;
-import org.primefaces.model.UploadedFile;
 
 public class NativeFileUploadDecoder {
 
