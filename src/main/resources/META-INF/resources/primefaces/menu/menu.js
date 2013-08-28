4e3f5b1a3fc9bc8e9fd9423419ff592a2526db62
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2013-08-28 22:38:33 +0000

    Bug fix

--- src/main/resources/META-INF/resources/primefaces/menu/menu.js
+++ src/main/resources/META-INF/resources/primefaces/menu/menu.js
@@ -151,6 +151,9 @@ PrimeFaces.widget.TieredMenu = PrimeFaces.widget.Menu.extend({
             
             var activeSibling = menuitem.siblings('.ui-menuitem-active');
             if(activeSibling.length === 1) {
+                activeSibling.find('li.ui-menuitem-active').each(function() {
+                    $this.deactivate($(this));
+                });
                 $this.deactivate(activeSibling);
             }
             
