de7bf9877e7925e4d658ba740303e0ee08ac35b0
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2013-09-28 10:27:45 +0000

    Bug fix

--- primefaces/trunk/src/main/resources/META-INF/resources/primefaces/tree/tree.js
+++ primefaces/trunk/src/main/resources/META-INF/resources/primefaces/tree/tree.js
@@ -926,6 +926,14 @@ PrimeFaces.widget.VerticalTree = PrimeFaces.widget.BaseTree.extend({
         if(dragNode.has(dropNode.get(0)).length) {
             return false;
         }
+        
+        //drop restriction
+        if(this.cfg.dropRestrict) {
+            if(this.cfg.dropRestrict === 'sibling' && dragNode.parent().get(0) === dropNode.parent().get(0)) {
+                console.log('same parent')
+                return false;
+            }
+        }
        
         return true;
     },
