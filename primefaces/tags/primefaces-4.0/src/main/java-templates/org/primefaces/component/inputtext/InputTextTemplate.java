af0a3e500df3212fbc9b6df628d12bfb1da39bb9
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2013-10-02 17:57:47 +0000

    Enough with maven release plugin

--- primefaces/tags/primefaces-4.0/src/main/java-templates/org/primefaces/component/inputtext/InputTextTemplate.java
+++
@@ -1,2 +0,0 @@
-
-    public final static String STYLE_CLASS = "ui-inputfield ui-inputtext ui-widget ui-state-default ui-corner-all";
--- primefaces/tags/primefaces-4.0/src/main/java-templates/org/primefaces/component/outputlabel/OutputLabelTemplate.java
+++
@@ -1,3 +0,0 @@
-
-    public final static String STYLE_CLASS = "ui-outputlabel ui-widget";
-    public final static String REQUIRED_FIELD_INDICATOR_CLASS = "ui-outputlabel-rfi";
