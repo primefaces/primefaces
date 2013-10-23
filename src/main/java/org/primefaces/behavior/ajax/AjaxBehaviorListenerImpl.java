8284dbbb34c41bf2272a05d4506e40205f16344f
Thomas Andraschko <tandraschko@apache.org>
2013-10-23 19:38:38 +0000

    refactored behaviors

--- src/main/java/org/primefaces/behavior/ajax/AjaxBehaviorListenerImpl.java
+++ src/main/java/org/primefaces/behavior/ajax/AjaxBehaviorListenerImpl.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.primefaces.component.behavior.ajax;
+package org.primefaces.behavior.ajax;
 
 import java.io.Serializable;
 import java.util.logging.Level;
