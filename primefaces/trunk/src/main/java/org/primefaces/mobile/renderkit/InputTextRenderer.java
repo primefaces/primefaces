14068a4fccbfe13093a06010a08b4a8dd5bb7c7b
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2013-11-10 01:16:17 +0000

    Add mobile renderer for inputtextarea component.

--- primefaces/trunk/src/main/java/org/primefaces/mobile/renderkit/InputTextRenderer.java
+++ primefaces/trunk/src/main/java/org/primefaces/mobile/renderkit/InputTextRenderer.java
@@ -56,17 +56,14 @@ public class InputTextRenderer extends InputRenderer {
 		writer.writeAttribute("name", clientId, null);
 		writer.writeAttribute("type", inputText.getType(), null);           
       
-		renderPassThruAttributes(context, inputText, HTML.INPUT_TEXT_ATTRS_WITHOUT_EVENTS);
-        renderEvents(context, inputText, HTML.INPUT_TEXT_EVENTS, inputText.getClientBehaviors());
-
         if(inputText.isDisabled()) writer.writeAttribute("disabled", "disabled", null);
         if(inputText.isReadonly()) writer.writeAttribute("readonly", "readonly", null);
         if(style != null) writer.writeAttribute("style", style, null);  
         if(styleClass != null) writer.writeAttribute("class", styleClass, null);  
+        if(valueToRender != null) writer.writeAttribute("value", valueToRender , null);
         
-        if(valueToRender != null) {
-			writer.writeAttribute("value", valueToRender , null);
-		}
+        renderPassThruAttributes(context, inputText, HTML.INPUT_TEXT_ATTRS_WITHOUT_EVENTS);
+        renderEvents(context, inputText, HTML.INPUT_TEXT_EVENTS, inputText.getClientBehaviors());
         
         writer.endElement("input");
 	}
---
+++ primefaces/trunk/src/main/java/org/primefaces/mobile/renderkit/InputTextareaRenderer.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2009-2013 PrimeTek.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.primefaces.mobile.renderkit;
+
+import java.io.IOException;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+import org.primefaces.component.inputtextarea.InputTextarea;
+import org.primefaces.renderkit.InputRenderer;
+import org.primefaces.util.ComponentUtils;
+import org.primefaces.util.HTML;
+
+public class InputTextareaRenderer extends InputRenderer {
+
+    @Override
+	public void decode(FacesContext context, UIComponent component) {
+		InputTextarea inputTextarea = (InputTextarea) component;
+        if(inputTextarea.isDisabled() || inputTextarea.isReadonly()) {
+            return;
+        }
+
+        decodeBehaviors(context, inputTextarea);
+        
+        String clientId = inputTextarea.getClientId(context);
+		String submittedValue = (String) context.getExternalContext().getRequestParameterMap().get(clientId);
+        if(submittedValue != null) {
+            inputTextarea.setSubmittedValue(submittedValue);
+        }
+	}
+
+	@Override
+	public void encodeEnd(FacesContext context, UIComponent component) throws IOException {
+		ResponseWriter writer = context.getResponseWriter();
+        InputTextarea inputTextarea = (InputTextarea) component;
+        String clientId = inputTextarea.getClientId(context);
+        String valueToRender = ComponentUtils.getValueToRender(context, inputTextarea);
+        String style = inputTextarea.getStyle();
+        String styleClass = inputTextarea.getStyle();
+
+		writer.startElement("textarea", null);
+		writer.writeAttribute("id", clientId, null);
+		writer.writeAttribute("name", clientId, null);
+
+		renderPassThruAttributes(context, inputTextarea, HTML.INPUT_TEXTAREA_ATTRS);
+
+        if(inputTextarea.isDisabled()) writer.writeAttribute("disabled", "disabled", "disabled");
+        if(inputTextarea.isReadonly()) writer.writeAttribute("readonly", "readonly", "readonly");
+        if(style != null) writer.writeAttribute("style", style, null);  
+        if(styleClass != null) writer.writeAttribute("class", styleClass, null); 
+		if(valueToRender != null) writer.writeText(valueToRender, "value");  
+        
+        renderPassThruAttributes(context, inputTextarea, HTML.TEXTAREA_ATTRS);
+        renderEvents(context, inputTextarea, HTML.INPUT_TEXT_EVENTS, inputTextarea.getClientBehaviors());
+
+        writer.endElement("textarea");
+	}
+}
