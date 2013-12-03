c7833fa423c8acde4808b65e9caabf96b4c27891
Thomas Andraschko <tandraschko@apache.org>
2013-12-03 20:42:40 +0000

    moved widget and dialog

--- src/main/resources/META-INF/resources/primefaces/core/core.js
+++ src/main/resources/META-INF/resources/primefaces/core/core.js
@@ -389,138 +389,7 @@
 
             return this.scrollbarWidth;
         },
-                
-        openDialog: function(cfg) {
-            var dialogId = cfg.sourceComponentId + '_dlg';
-            if(document.getElementById(dialogId)) {
-                return;
-            }
 
-            var dialogWidgetVar = cfg.sourceComponentId.replace(/:/g, '_') + '_dlgwidget',
-            dialogDOM = $('<div id="' + dialogId + '" class="ui-dialog ui-widget ui-widget-content ui-corner-all ui-shadow ui-overlay-hidden"' + 
-                    ' data-pfdlgcid="' + cfg.pfdlgcid + '" data-widgetvar="' + dialogWidgetVar + '"/>')
-                    .append('<div class="ui-dialog-titlebar ui-widget-header ui-helper-clearfix ui-corner-top"><span class="ui-dialog-title"></span>' +
-                    '<a class="ui-dialog-titlebar-icon ui-dialog-titlebar-close ui-corner-all" href="#" role="button"><span class="ui-icon ui-icon-closethick"></span></a></div>' + 
-                    '<div class="ui-dialog-content ui-widget-content" style="height: auto;">' +
-                    '<iframe style="border:0 none" frameborder="0"/>' + 
-                    '</div>')
-                    .appendTo(document.body),
-            dialogFrame = dialogDOM.find('iframe'),
-            symbol = cfg.url.indexOf('?') === -1 ? '?' : '&',
-            frameURL = cfg.url + symbol + 'pfdlgcid=' + cfg.pfdlgcid,
-            frameWidth = cfg.options.contentWidth||640;
-
-            dialogFrame.width(frameWidth);
-    
-            dialogFrame.on('load', function() {
-                var $frame = $(this),
-                titleElement = $frame.contents().find('title');
-                
-                if(!$frame.data('initialized')) {
-                    PrimeFaces.cw('Dialog', dialogWidgetVar, {
-                        id: dialogId,
-                        position: 'center',
-                        sourceComponentId: cfg.sourceComponentId,
-                        sourceWidget: cfg.sourceWidget,
-                        onHide: function() {
-                            this.jq.remove();
-                            PF[dialogWidgetVar] = undefined;
-                        },
-                        modal: cfg.options.modal,
-                        resizable: cfg.options.resizable,
-                        draggable: cfg.options.draggable,
-                        width: cfg.options.width,
-                        height: cfg.options.height
-                    });
-                }
-                
-                if(titleElement.length > 0) {
-                    PF(dialogWidgetVar).titlebar.children('span.ui-dialog-title').html(titleElement.text());
-                }
-                
-                //adjust height
-                var frameHeight = cfg.options.contentHeight||$frame.get(0).contentWindow.document.body.scrollHeight + 5;
-                $frame.height(frameHeight);
-                
-                PF(dialogWidgetVar).show();
-                
-                dialogFrame.data('initialized', true);
-            })
-            .attr('src', frameURL);
-        },
-
-        closeDialog: function(cfg) {
-            var dlg = $(document.body).children('div.ui-dialog').filter(function() {
-                return $(this).data('pfdlgcid') === cfg.pfdlgcid;
-            }),
-            dlgWidget = PF(dlg.data('widgetvar')),
-            sourceWidget = dlgWidget.cfg.sourceWidget,
-            sourceComponentId = dlgWidget.cfg.sourceComponentId,
-            dialogReturnBehavior = null;
-    
-            if(sourceWidget && sourceWidget.cfg.behaviors) {
-                dialogReturnBehavior = sourceWidget.cfg.behaviors['dialogReturn'];
-            }
-            else if(sourceComponentId) {
-                var dialogReturnBehaviorStr = $(document.getElementById(sourceComponentId)).data('dialogreturn');
-                if(dialogReturnBehaviorStr) {
-                    dialogReturnBehavior = eval('(function(){' + dialogReturnBehaviorStr + '})');
-                }
-
-            }
-                        
-            if(dialogReturnBehavior) {
-                var ext = {
-                        params: [
-                            {name: sourceComponentId + '_pfdlgcid', value: cfg.pfdlgcid}
-                        ]
-                    };
-                
-                dialogReturnBehavior.call(this, null, ext);
-            }
-            
-            dlgWidget.hide();
-        },
-                
-        showMessageInDialog: function(msg) {
-            if(!this.messageDialog) {
-                var messageDialogDOM = $('<div id="primefacesmessagedlg" class="ui-message-dialog ui-dialog ui-widget ui-widget-content ui-corner-all ui-shadow ui-overlay-hidden"/>')
-                            .append('<div class="ui-dialog-titlebar ui-widget-header ui-helper-clearfix ui-corner-top"><span class="ui-dialog-title"></span>' +
-                            '<a class="ui-dialog-titlebar-icon ui-dialog-titlebar-close ui-corner-all" href="#" role="button"><span class="ui-icon ui-icon-closethick"></span></a></div>' + 
-                            '<div class="ui-dialog-content ui-widget-content" style="height: auto;"></div>')
-                            .appendTo(document.body);
-
-                PrimeFaces.cw('Dialog', 'primefacesmessagedialog', {
-                    id: 'primefacesmessagedlg', 
-                    modal:true,
-                    draggable: false,
-                    resizable: false,
-                    showEffect: 'fade',
-                    hideEffect: 'fade'
-                });
-                this.messageDialog = PF('primefacesmessagedialog');
-                this.messageDialog.titleContainer = this.messageDialog.titlebar.children('span.ui-dialog-title');
-            }
-
-            this.messageDialog.titleContainer.text(msg.summary);
-            this.messageDialog.content.html('').append('<span class="ui-dialog-message ui-messages-' + msg.severity.split(' ')[0].toLowerCase() + '-icon" />').append(msg.detail);
-            this.messageDialog.show();
-        },
-                
-        confirm: function(msg) {
-            if(PrimeFaces.confirmDialog) {
-                PrimeFaces.confirmSource = $(PrimeFaces.escapeClientId(msg.source));
-                PrimeFaces.confirmDialog.showMessage(msg);
-            }
-            else {
-                PrimeFaces.warn('No global confirmation dialog available.');
-            }
-        },
-        
-        /*bc: function(source, event, ext, behaviorsArray) {
-            this.chainBehaviors(source, event, ext, behaviorsArray);
-        },*/
-    
         bcn: function(element, event, functions) {
             if(functions) {
                 for(var i = 0; i < functions.length; i++) {
@@ -557,6 +426,23 @@
     		return true;
     	},
 
+    	/**
+    	 * moved to core.dialog.js
+    	 */
+        openDialog: function(cfg) {
+        	PrimeFaces.dialog.DialogHandler.openDialog(cfg);
+        },
+        closeDialog: function(cfg) {
+        	PrimeFaces.dialog.DialogHandler.closeDialog(cfg);
+        },      
+        showMessageInDialog: function(msg) {
+        	PrimeFaces.dialog.DialogHandler.showMessageInDialog(msg);
+        },        
+        confirm: function(msg) {
+        	PrimeFaces.dialog.DialogHandler.confirm(msg);
+        },
+    	
+    	
         locales : {},
 
         zindex : 1000,
@@ -596,7 +482,7 @@
      * PrimeFaces Namespaces
      */
     
-    PrimeFaces.widget = {};
+   
     PrimeFaces.settings = {};
     PrimeFaces.util = {};
     PrimeFaces.widgets = {};
@@ -618,166 +504,7 @@
 
  
 
-    /* Simple JavaScript Inheritance
-     * By John Resig http://ejohn.org/
-     * MIT Licensed.
-     */
-    // Inspired by base2 and Prototype
-    (function(){
-      var initializing = false, fnTest = /xyz/.test(function(){xyz;}) ? /\b_super\b/ : /.*/;
-      // The base Class implementation (does nothing)
-      this.Class = function(){};
-
-      // Create a new Class that inherits from this class
-      Class.extend = function(prop) {
-        var _super = this.prototype;
-
-        // Instantiate a base class (but only create the instance,
-        // don't run the init constructor)
-        initializing = true;
-        var prototype = new this();
-        initializing = false;
-
-        // Copy the properties over onto the new prototype
-        for (var name in prop) {
-          // Check if we're overwriting an existing function
-          prototype[name] = typeof prop[name] == "function" && 
-            typeof _super[name] == "function" && fnTest.test(prop[name]) ?
-            (function(name, fn){
-              return function() {
-                var tmp = this._super;
-
-                // Add a new ._super() method that is the same method
-                // but on the super-class
-                this._super = _super[name];
-
-                // The method only need to be bound temporarily, so we
-                // remove it when we're done executing
-                var ret = fn.apply(this, arguments);        
-                this._super = tmp;
-
-                return ret;
-              };
-            })(name, prop[name]) :
-            prop[name];
-        }
-
-        // The dummy class constructor
-        function Class() {
-          // All construction is actually done in the init method
-          if ( !initializing && this.init )
-            this.init.apply(this, arguments);
-        }
-
-        // Populate our constructed prototype object
-        Class.prototype = prototype;
-
-        // Enforce the constructor to be what we expect
-        Class.prototype.constructor = Class;
-
-        // And make this class extendable
-        Class.extend = arguments.callee;
-
-        return Class;
-      };
-    })();
-
-    /**
-     * BaseWidget for PrimeFaces Widgets
-     */
-    PrimeFaces.widget.BaseWidget = Class.extend({
-
-        init: function(cfg) {
-            this.cfg = cfg;
-            this.id = cfg.id;
-            this.jqId = PrimeFaces.escapeClientId(this.id);
-            this.jq = $(this.jqId);
-            this.widgetVar = cfg.widgetVar;
-            
-            //remove script tag
-            $(this.jqId + '_s').remove();
-            
-            if (this.widgetVar) {
-	            var $this = this;
-	            this.jq.on("remove", function() {
-	            	PrimeFaces.updatedWidgets.push($this.widgetVar);
-	            });
-            }
-        },
-
-        //used in ajax updates, reloads the widget configuration
-        refresh: function(cfg) {
-            return this.init(cfg);
-        },
-        
-        //will be called when the widget is detached
-        destroy: function() {
-        	console.log('widget destroyed: ' + this.id + " ; " + this.widgetVar);
-        },
-        
-        //checks if the given widget is detached
-        isDetached: function() {
-        	return document.getElementById(this.id) === null;
-        },
-
-        //returns jquery object representing the main dom element related to the widget
-        getJQ: function(){
-            return this.jq;
-        },
-
-    	/**
-    	 * Removes the widget's script block from the DOM.
-    	 *
-    	 * @param {string} clientId The id of the widget.
-    	 */
-        removeScriptElement: function(clientId) {
-        	$(PrimeFaces.escapeClientId(clientId) + '_s').remove();
-        }
-    });
-    
-    /**
-     * Widgets that require to be visible to initialize properly for hidden container support
-     */
-    PrimeFaces.widget.DeferredWidget = PrimeFaces.widget.BaseWidget.extend({
-
-        renderDeferred: function() {     
-            if(this.jq.is(':visible')) {
-                this._render();
-            }
-            else {
-                var hiddenParent = this.jq.closest('.ui-hidden-container'),
-                hiddenParentWidgetVar = hiddenParent.data('widget'),
-                $this = this;
-
-                if(hiddenParentWidgetVar) {
-                    var hiddenParentWidget = PF(hiddenParentWidgetVar);
-                    
-                    if(hiddenParentWidget) {
-                        hiddenParentWidget.addOnshowHandler(this.id, function() {
-                            return $this.render();
-                        });
-                    }
-                }
-            }
-        },
-        
-        render: function() {
-            if(this.jq.is(':visible')) {
-                this._render();
-                return true;
-            }
-            else {
-                return false;
-            }
-        },
-        
-        /**
-         * Must be overriden
-         */
-        _render: function() {
-            throw 'Unsupported Operation';
-        }
-    });
+ 
         
     //expose globally
     window.PrimeFaces = PrimeFaces;
