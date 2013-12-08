806caf0d64cb11a15b6d22a0871cc131a49d7338
andraschko.thomas@gmail.com <andraschko.thomas@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2013-12-08 17:52:05 +0000

    cosmetics

--- primefaces/trunk/src/main/resources/META-INF/resources/primefaces/core/core.ajax.response.js
+++ primefaces/trunk/src/main/resources/META-INF/resources/primefaces/core/core.ajax.response.js
@@ -1,135 +1,135 @@
 PrimeFaces.ajax.Response = {
 		
-	handleResponse: function(xml, status, xhr, updateHandler) {
+    handleResponse: function(xml, status, xhr, updateHandler) {
         var partialResponseNode = xml.getElementsByTagName("partial-response")[0];
         var responseTypeNode = partialResponseNode.firstChild;
-        
+
         if (responseTypeNode.nodeName === "redirect") {
-        	PrimeFaces.ajax.ResponseProcessor.doRedirect(responseTypeNode);
+            PrimeFaces.ajax.ResponseProcessor.doRedirect(responseTypeNode);
         }
 
         if (responseTypeNode.nodeName === "changes") {
-    		var changesNode = responseTypeNode.childNodes;
+            var changesNode = responseTypeNode.childNodes;
 
             for (var i = 0; i < changesNode.length; i++) {
-            	var currentChangeNode = changesNode[i];
+                var currentChangeNode = changesNode[i];
                 switch (currentChangeNode.nodeName) {
                     case "update":
-                		PrimeFaces.ajax.ResponseProcessor.doUpdate(currentChangeNode, xhr, updateHandler);
+                                PrimeFaces.ajax.ResponseProcessor.doUpdate(currentChangeNode, xhr, updateHandler);
                         break;
                     case "delete":
-                    	PrimeFaces.ajax.ResponseProcessor.doDelete(currentChangeNode);
+                        PrimeFaces.ajax.ResponseProcessor.doDelete(currentChangeNode);
                         break;
                     case "insert":
-                    	PrimeFaces.ajax.ResponseProcessor.doInsert(currentChangeNode);
+                        PrimeFaces.ajax.ResponseProcessor.doInsert(currentChangeNode);
                         break;
                     case "attributes":
-                    	PrimeFaces.ajax.ResponseProcessor.doAttributes(currentChangeNode);
+                        PrimeFaces.ajax.ResponseProcessor.doAttributes(currentChangeNode);
                         break;
                     case "eval":
-                    	PrimeFaces.ajax.ResponseProcessor.doEval(currentChangeNode);
+                        PrimeFaces.ajax.ResponseProcessor.doEval(currentChangeNode);
                         break;
                     case "extension":
-                    	PrimeFaces.ajax.ResponseProcessor.doExtension(currentChangeNode, xhr);
+                        PrimeFaces.ajax.ResponseProcessor.doExtension(currentChangeNode, xhr);
                         break;
                 }
             }
 
-    	    PrimeFaces.ajax.Response.handleReFocus();
-    	    PrimeFaces.ajax.Response.destroyDetachedWidgets();
+            PrimeFaces.ajax.Response.handleReFocus();
+            PrimeFaces.ajax.Response.destroyDetachedWidgets();
         }
-	},
+    },
 
-	handleReFocus : function() {
-		var activeElementId = $(document.activeElement).attr('id');
-		
-	    // re-focus element
-	    if (PrimeFaces.customFocus == false
-	            && activeElementId
-	            // do we really need to refocus? we just check the current activeElement here
-	            && activeElementId != $(document.activeElement).attr('id')) {
-	
-	    	var elementToFocus = $(PrimeFaces.escapeClientId(activeElementId));
-	    	elementToFocus.focus();
-	
-	    	// double check it - required for IE
-	    	setTimeout(function() {
-	    		if (!elementToFocus.is(":focus")) {
-	    			elementToFocus.focus();
-	    		}
-	    	}, 150);
-	    }
+    handleReFocus : function() {
+        var activeElementId = $(document.activeElement).attr('id');
 
-	    PrimeFaces.customFocus = false;
-	},
+        // re-focus element
+        if (PrimeFaces.customFocus == false
+                && activeElementId
+                // do we really need to refocus? we just check the current activeElement here
+                && activeElementId != $(document.activeElement).attr('id')) {
 
-	destroyDetachedWidgets : function() {
-	    // destroy detached widgets
-	    for (var i = 0; i < PrimeFaces.updatedWidgets.length; i++) {
-	    	var widgetVar = PrimeFaces.updatedWidgets[i];
-	
-	    	var widget = PF(widgetVar);
-	    	if (widget) {
-				if (widget.isDetached()) {
-					widget.destroy();
-					PrimeFaces.widgets[widgetVar] = null;
-	
-					try {
-						delete widget;
-					} catch (e) {}
-				}
-	    	}
-	    }
+            var elementToFocus = $(PrimeFaces.escapeClientId(activeElementId));
+            elementToFocus.focus();
 
-	    PrimeFaces.updatedWidgets = [];
-	}
+            // double check it - required for IE
+            setTimeout(function() {
+                if (!elementToFocus.is(":focus")) {
+                    elementToFocus.focus();
+                }
+            }, 150);
+        }
+
+        PrimeFaces.customFocus = false;
+    },
+
+    destroyDetachedWidgets : function() {
+        // destroy detached widgets
+        for (var i = 0; i < PrimeFaces.updatedWidgets.length; i++) {
+            var widgetVar = PrimeFaces.updatedWidgets[i];
+
+            var widget = PF(widgetVar);
+            if (widget) {
+                if (widget.isDetached()) {
+                    PrimeFaces.widgets[widgetVar] = null;
+                    widget.destroy();
+                    
+                    try {
+                        delete widget;
+                    } catch (e) {}
+                }
+            }
+        }
+
+        PrimeFaces.updatedWidgets = [];
+    }
 };
 
 
 PrimeFaces.ajax.ResponseProcessor = {
 
     doRedirect : function(node) {
-		window.location = node.getAttribute('url');
-	},
-	
-	doUpdate : function(node, xhr, handler) {
-		var id = node.getAttribute('id');
-		var content = PrimeFaces.ajax.Utils.getContent(node);
+        window.location = node.getAttribute('url');
+    },
 
-		if (handler) {
-			// if the handler returns "true", it handles the current id
-			if (handler.call(this, id, content) === false) {
-				PrimeFaces.ajax.Utils.updateElement(id, content, xhr);
-			}
-		} else {
-			PrimeFaces.ajax.Utils.updateElement(id, content, xhr);
-		}
-	},
-	
-	doEval : function(node) {
-		var textContent = node.textContent || node.innerText || node.text;
-		$.globalEval(textContent);
-	},
-	
-	doExtension : function(node, xhr) {
-		if (xhr) {
-			if (node.getAttribute("ln") === "primefaces" && node.getAttribute("type") === "args") {
-				var textContent = node.textContent || node.innerText || node.text;
-				xhr.pfArgs = $.parseJSON(textContent);
-			}
-		}
-	},
-	
-	doDelete : function(node) {
-		var id = node.getAttribute('id');
-		$(PrimeFaces.escapeClientId(id)).remove();
-	},
-	
-	doInsert : function(node) {
+    doUpdate : function(node, xhr, handler) {
+        var id = node.getAttribute('id');
+        var content = PrimeFaces.ajax.Utils.getContent(node);
+
+        if (handler) {
+            // if the handler returns "true", it handles the current id
+            if (handler.call(this, id, content) === false) {
+                    PrimeFaces.ajax.Utils.updateElement(id, content, xhr);
+            }
+        } else {
+            PrimeFaces.ajax.Utils.updateElement(id, content, xhr);
+        }
+    },
+
+    doEval : function(node) {
+        var textContent = node.textContent || node.innerText || node.text;
+        $.globalEval(textContent);
+    },
+
+    doExtension : function(node, xhr) {
+        if (xhr) {
+            if (node.getAttribute("ln") === "primefaces" && node.getAttribute("type") === "args") {
+                var textContent = node.textContent || node.innerText || node.text;
+                xhr.pfArgs = $.parseJSON(textContent);
+            }
+        }
+    },
+
+    doDelete : function(node) {
+        var id = node.getAttribute('id');
+        $(PrimeFaces.escapeClientId(id)).remove();
+    },
+
+    doInsert : function(node) {
         if (!node.childNodes) {
             return false;
         }
-        
+
         for (var i = 0; i < node.childNodes.length; i++) {
             var childNode = node.childNodes[i];
             var id = childNode.getAttribute('id');
@@ -137,29 +137,29 @@ PrimeFaces.ajax.ResponseProcessor = {
             var content = PrimeFaces.ajax.Utils.getContent(childNode);
 
             if (childNode.nodeName === "after") {
-            	$(content).insertAfter(jq);
+                $(content).insertAfter(jq);
             }
             else if (childNode.nodeName === "before") {
-            	$(content).insertBefore(jq);
+                $(content).insertBefore(jq);
             }
         }
-	},
-	
-	doAttributes : function(node) {
-		var id = node.getAttribute('id');
-		var jq = $(PrimeFaces.escapeClientId(id));
+    },
+
+    doAttributes : function(node) {
+        var id = node.getAttribute('id');
+        var jq = $(PrimeFaces.escapeClientId(id));
 
         if (!node.childNodes) {
             return false;
         }
-        
+
         for (var i = 0; i < node.childNodes.length; i++) {
             var attrNode = node.childNodes[i];
             var attrName = attrNode.getAttribute("name");
             var attrValue = attrNode.getAttribute("value");
 
             if (!attrName) {
-            	return;
+                return;
             }
 
             if (!attrValue || attrValue === null) {
@@ -168,6 +168,6 @@ PrimeFaces.ajax.ResponseProcessor = {
 
             jq.attr(attrName, attrValue);
         }
-	}
+    }
 };
 
