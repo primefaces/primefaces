d38636d2541a580f88446a4d42fc6ab786ca9e15
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2013-12-13 21:34:27 +0000

    Bug fix

--- src/main/java/org/primefaces/component/paginator/RowsPerPageDropdownRenderer.java
+++ src/main/java/org/primefaces/component/paginator/RowsPerPageDropdownRenderer.java
@@ -52,6 +52,7 @@ public class RowsPerPageDropdownRenderer implements PaginatorElementRenderer {
             }
             writer.writeAttribute("class", UIData.PAGINATOR_RPP_OPTIONS_CLASS, null);
             writer.writeAttribute("value", uidata.getRows(), null);
+            writer.writeAttribute("autocomplete", "off", null);
 
             for( String option : options){
                 int rows = Integer.parseInt(option);
