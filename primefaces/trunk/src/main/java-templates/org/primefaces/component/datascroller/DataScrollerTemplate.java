b0a3eb8658e675583a904eb866f8ef3c56323ba4
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2013-12-14 22:50:06 +0000

    Initial implementation of infinite scrolling with datascroller component using window scroll.

--- primefaces/trunk/src/main/java-templates/org/primefaces/component/datascroller/DataScrollerTemplate.java
+++ primefaces/trunk/src/main/java-templates/org/primefaces/component/datascroller/DataScrollerTemplate.java
@@ -1,2 +1,9 @@
 
     public final static String CONTAINER_CLASS = "ui-datascroller ui-widget ui-widget-content ui-corner-all";
+    public final static String LIST_CLASS = "ui-datascroller-list";
+    public final static String ITEM_CLASS = "ui-datascroller-item";
+
+    public boolean isLoadRequest() {
+        FacesContext context = getFacesContext();
+        return context.getExternalContext().getRequestParameterMap().containsKey(this.getClientId(context) + "_load");
+    }
--- primefaces/trunk/src/main/resources/META-INF/resources/primefaces/datascroller/datascroller.js
+++ primefaces/trunk/src/main/resources/META-INF/resources/primefaces/datascroller/datascroller.js
@@ -5,8 +5,63 @@ PrimeFaces.widget.DataScroller = PrimeFaces.widget.BaseWidget.extend({
     
     init: function(cfg) {
         this._super(cfg);
+        this.list = this.jq.children('ul');
+        this.loading = false;
+        this.cfg.offset = this.cfg.chunkSize;
         
-        
-    }
+        var win = $(window),
+        doc = $(document),
+        $this = this;
+
+        this.shouldLoad();
+
+        win.scroll(function () {
+            if((win.scrollTop() === (doc.height() - win.height()) && $this.shouldLoad())) {
+                $this.load();
+            }
+        });
+    },
     
+    load: function() {
+        this.loading = true;
+        
+        var $this = this,
+        options = {
+            source: this.id,
+            process: this.id,
+            update: this.id,
+            params: [{name: this.id + '_load', value: true},{name: this.id + '_offset', value: this.cfg.offset}],
+            onsuccess: function(responseXML) {
+                var xmlDoc = $(responseXML.documentElement),
+                updates = xmlDoc.find("update");
+
+                for(var i=0; i < updates.length; i++) {
+                    var update = updates.eq(i),
+                    id = update.attr('id'),
+                    content = PrimeFaces.ajax.AjaxUtils.getContent(update);
+
+                    if(id === $this.id) {
+                        $this.list.append(content);
+                    }
+                    else {
+                        PrimeFaces.ajax.AjaxUtils.updateElement.call(this, id, content);
+                    }
+                }
+
+                PrimeFaces.ajax.AjaxUtils.handleResponse.call(this, responseXML);
+
+                return true;
+            },
+            oncomplete: function() {
+                $this.cfg.offset += $this.cfg.chunkSize;
+                $this.loading = false;
+            }
+        };
+        
+        PrimeFaces.ajax.AjaxRequest(options);
+    },
+    
+    shouldLoad: function() {
+        return (!this.loading && ((this.cfg.offset + this.cfg.chunkSize) <= this.cfg.totalSize));
+    }
 });
