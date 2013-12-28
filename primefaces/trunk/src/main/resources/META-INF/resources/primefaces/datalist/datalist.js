677055926e1f739bc74fab927311280a13ffed66
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2013-12-28 22:39:15 +0000

    Migrated DataList to new ajax apis plus refactor

--- primefaces/trunk/src/main/resources/META-INF/resources/primefaces/datalist/datalist.js
+++ primefaces/trunk/src/main/resources/META-INF/resources/primefaces/datalist/datalist.js
@@ -8,7 +8,6 @@ PrimeFaces.widget.DataList = PrimeFaces.widget.BaseWidget.extend({
         
         this.cfg.formId = $(this.jqId).parents('form:first').attr('id');
         this.content = $(this.jqId + '_content');
-        this.list = this.content.children('.ui-datalist-data');
 
         if(this.cfg.paginator) {
             this.setupPaginator();
@@ -16,56 +15,41 @@ PrimeFaces.widget.DataList = PrimeFaces.widget.BaseWidget.extend({
     },
     
     setupPaginator: function() {
-        var _self = this;
+        var $this = this;
         this.cfg.paginator.paginate = function(newState) {
-            _self.handlePagination(newState);
+            $this.handlePagination(newState);
         };
 
         this.paginator = new PrimeFaces.widget.Paginator(this.cfg.paginator);
     },
     
     handlePagination: function(newState) {
-        var _self = this,
+        var $this = this,
         options = {
             source: this.id,
             update: this.id,
             process: this.id,
             formId: this.cfg.formId,
-            onsuccess: function(responseXML) {
-                var xmlDoc = $(responseXML.documentElement),
-                updates = xmlDoc.find("update");
+            params: [
+                {name: this.id + '_pagination', value: true},
+                {name: this.id + '_first', value: newState.first},
+                {name: this.id + '_rows', value: newState.rows}
+            ],
+            onsuccess: function(responseXML, status, xhr) {
+                PrimeFaces.ajax.Response.handle(responseXML, status, xhr, {
+                        widget: $this,
+                        handle: function(content) {
+                            this.content.html(content);
+                        }
+                    });
 
-                for(var i=0; i < updates.length; i++) {
-                    var update = updates.eq(i),
-                    id = update.attr('id'),
-                    content = PrimeFaces.ajax.AjaxUtils.getContent(update);
-
-                    if(id == _self.id){
-                        _self.content.html(content);
-                    }
-                    else {
-                        PrimeFaces.ajax.AjaxUtils.updateElement.call(this, id, content);
-                    }
-                }
-
-                PrimeFaces.ajax.AjaxUtils.handleResponse.call(this, responseXML);
-               
                 return true;
+            },
+            oncomplete: function() {
+                $this.paginator.cfg.page = newState.page;
+                $this.paginator.updateUI();
             }
         };
-        
-        options.oncomplete = function() {
-            //update paginator state
-            _self.paginator.cfg.page = newState.page;
-                
-            _self.paginator.updateUI();
-        };
-        
-        options.params = [
-            {name: this.id + '_pagination', value: true},
-            {name: this.id + '_first', value: newState.first},
-            {name: this.id + '_rows', value: newState.rows}
-        ];
 
         PrimeFaces.ajax.AjaxRequest(options);
     },
@@ -73,4 +57,5 @@ PrimeFaces.widget.DataList = PrimeFaces.widget.BaseWidget.extend({
     getPaginator: function() {
         return this.paginator;
     }
+    
 });
