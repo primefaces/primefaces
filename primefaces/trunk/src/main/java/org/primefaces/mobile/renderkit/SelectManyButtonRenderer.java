2a12137fc974665962f4da1b80118d73327c47c4
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2014-01-14 09:05:07 +0000

    Mobile renderer for SelectManyCheckbox and SelectManyButton

--- primefaces/trunk/src/main/java/org/primefaces/mobile/renderkit/SelectManyButtonRenderer.java
+++ primefaces/trunk/src/main/java/org/primefaces/mobile/renderkit/SelectManyButtonRenderer.java
@@ -18,25 +18,25 @@ package org.primefaces.mobile.renderkit;
 import java.io.IOException;
 import java.util.List;
 import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
 import javax.faces.component.UINamingContainer;
 import javax.faces.context.FacesContext;
 import javax.faces.context.ResponseWriter;
 import javax.faces.convert.Converter;
 import javax.faces.model.SelectItem;
-import org.primefaces.component.selectonebutton.SelectOneButton;
+import org.primefaces.component.selectmanybutton.SelectManyButton;
 
-public class SelectOneButtonRenderer extends org.primefaces.component.selectonebutton.SelectOneButtonRenderer {
+public class SelectManyButtonRenderer extends org.primefaces.component.selectmanybutton.SelectManyButtonRenderer {
     
     @Override
     public void encodeEnd(FacesContext context, UIComponent component) throws IOException {
+        SelectManyButton button = (SelectManyButton) component;
         ResponseWriter writer = context.getResponseWriter();
-        SelectOneButton button = (SelectOneButton) component;
         String clientId = button.getClientId(context);
         List<SelectItem> selectItems = getSelectItems(context, button);
         String style = button.getStyle();
         String styleClass = button.getStyleClass();
-        Converter converter = button.getConverter();
-        
+     
         writer.startElement("div", button);
         writer.writeAttribute("data-role", "controlgroup", null);
         writer.writeAttribute("data-type", "horizontal", null);
@@ -45,40 +45,51 @@ public class SelectOneButtonRenderer extends org.primefaces.component.selectoneb
         if (styleClass != null) writer.writeAttribute("class", style, "styleClass");
         
         if (selectItems != null && !selectItems.isEmpty()) {
-            Object value = button.getSubmittedValue();
-            if(value == null) {
-                value = button.getValue();
-            }
-            Class type = value == null ? String.class : value.getClass();
-        
+            Converter converter = button.getConverter();
+            Object values = getValues(button);
+            Object submittedValues = getSubmittedValues(button);
+
             int idx = 0;
             for (SelectItem selectItem : selectItems) {
-                boolean disabled = selectItem.isDisabled() || button.isDisabled();
-                String id = clientId + UINamingContainer.getSeparatorChar(context) + idx;
-                Object coercedItemValue = coerceToModelType(context, selectItem.getValue(), type);
-                boolean selected = (coercedItemValue != null) && coercedItemValue.equals(value);
- 
-                encodeOption(context, button, selectItem, id, clientId, converter, selected, disabled); 
-                idx++;
-            }
+                encodeOption(context, button, values, submittedValues, converter, selectItem, idx);
+                idx++;        
+            }           
         }
         
         writer.endElement("div");
     }
     
-    protected void encodeOption(FacesContext context, SelectOneButton button, SelectItem option, String id, String name, Converter converter, boolean selected, boolean disabled) throws IOException {
+    protected void encodeOption(FacesContext context, UIInput component, Object values, Object submittedValues, Converter converter, SelectItem option, int idx) throws IOException {
         ResponseWriter writer = context.getResponseWriter();
-        String itemValueAsString = getOptionAsString(context, button, converter, option.getValue());
+        SelectManyButton button = (SelectManyButton) component;
+        String itemValueAsString = getOptionAsString(context, component, converter, option.getValue());
+        String name = button.getClientId(context);
+        String id = name + UINamingContainer.getSeparatorChar(context) + idx;
+        boolean disabled = option.isDisabled() || button.isDisabled();
 
-        //input
+        Object valuesArray;
+        Object itemValue;
+        if(submittedValues != null) {
+            valuesArray = submittedValues;
+            itemValue = itemValueAsString;
+        } else {
+            valuesArray = values;
+            itemValue = option.getValue();
+        }
+        
+        boolean selected = isSelected(context, component, itemValue, valuesArray, converter);
+        if(option.isNoSelectionOption() && values != null && !selected) {
+            return;
+        }
+        
         writer.startElement("input", null);
         writer.writeAttribute("id", id, null);
         writer.writeAttribute("name", name, null);
-        writer.writeAttribute("type", "radio", null);
+        writer.writeAttribute("type", "checkbox", null);
         writer.writeAttribute("value", itemValueAsString, null);
 
         renderOnchange(context, button);
-
+        
         if (selected) writer.writeAttribute("checked", "checked", null);
         if (disabled) writer.writeAttribute("disabled", "disabled", null);
         
