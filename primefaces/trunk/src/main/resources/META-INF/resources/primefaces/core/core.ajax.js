683aa923d22ff0b87073b8153dadf615ee20100a
andraschko.thomas@gmail.com <andraschko.thomas@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2014-01-19 16:06:51 +0000

    #6524

--- primefaces/trunk/src/main/resources/META-INF/resources/primefaces/core/core.ajax.js
+++ primefaces/trunk/src/main/resources/META-INF/resources/primefaces/core/core.ajax.js
@@ -70,10 +70,6 @@ PrimeFaces.ajax = {
                 if(input.length > 0) {
                     input.val(trimmedValue);
                 }
-                else
-                {
-                    form.append('<input type="hidden" name="' + name + '" value="' + trimmedValue + '" autocomplete="off" />');
-                }
             });
         },
 
