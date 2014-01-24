60697700920a276220c86804f592689a53e1b88d
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2014-01-24 14:44:24 +0000

    Bug fix

--- src/main/java-templates/org/primefaces/component/spinner/SpinnerTemplate.java
+++ src/main/java-templates/org/primefaces/component/spinner/SpinnerTemplate.java
@@ -3,8 +3,8 @@
     public final static String INPUT_CLASS = "ui-spinner-input ui-inputfield ui-state-default ui-corner-all";
     public final static String UP_BUTTON_CLASS = "ui-spinner-button ui-spinner-up ui-corner-tr ui-button ui-widget ui-state-default ui-button-text-only";
     public final static String DOWN_BUTTON_CLASS = "ui-spinner-button ui-spinner-down ui-corner-br ui-button ui-widget ui-state-default ui-button-text-only";
-    public final static String UP_ICON_CLASS = "ui-icon ui-icon-triangle-1-n";
-    public final static String DOWN_ICON_CLASS = "ui-icon ui-icon-triangle-1-s";
+    public final static String UP_ICON_CLASS = "ui-icon ui-icon-triangle-1-n ui-c";
+    public final static String DOWN_ICON_CLASS = "ui-icon ui-icon-triangle-1-s ui-c";
 
     public String getInputClientId() {
         return this.getClientId(getFacesContext()) + "_input";
