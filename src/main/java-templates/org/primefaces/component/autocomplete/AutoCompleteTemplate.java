bf393c8404b6dd3187233818418b918314f3e118
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2014-02-04 18:18:33 +0000

    cosmetics

--- src/main/java-templates/org/primefaces/component/autocomplete/AutoCompleteTemplate.java
+++ src/main/java-templates/org/primefaces/component/autocomplete/AutoCompleteTemplate.java
@@ -28,7 +28,7 @@ import javax.faces.component.behavior.Behavior;
     public final static String MULTIPLE_STYLE_CLASS = "ui-autocomplete-multiple";
     public final static String INPUT_CLASS = "ui-autocomplete-input ui-inputfield ui-widget ui-state-default ui-corner-all";
     public final static String INPUT_WITH_DROPDOWN_CLASS = "ui-autocomplete-input ui-inputfield ui-widget ui-state-default ui-corner-left";
-    public final static String DROPDOWN_CLASS= "ui-autocomplete-dropdown ui-button ui-widget ui-state-default ui-corner-right ui-button-icon-only";
+    public final static String DROPDOWN_CLASS = "ui-autocomplete-dropdown ui-button ui-widget ui-state-default ui-corner-right ui-button-icon-only";
     public final static String PANEL_CLASS = "ui-autocomplete-panel ui-widget-content ui-corner-all ui-helper-hidden ui-shadow";
     public final static String LIST_CLASS = "ui-autocomplete-items ui-autocomplete-list ui-widget-content ui-widget ui-corner-all ui-helper-reset";
     public final static String TABLE_CLASS = "ui-autocomplete-items ui-autocomplete-table ui-widget-content ui-widget ui-corner-all ui-helper-reset";
