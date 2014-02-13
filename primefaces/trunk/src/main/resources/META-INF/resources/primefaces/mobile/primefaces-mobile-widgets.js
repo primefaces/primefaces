12a9578a88f3a2866142efcc578fbac1d30c7dee
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2014-02-13 19:37:11 +0000

    Initiated autocomplete widget for PrimeFaces Mobile

--- primefaces/trunk/src/main/resources/META-INF/resources/primefaces/mobile/primefaces-mobile-widgets.js
+++ primefaces/trunk/src/main/resources/META-INF/resources/primefaces/mobile/primefaces-mobile-widgets.js
@@ -879,3 +879,120 @@ PrimeFaces.widget.Calendar = PrimeFaces.widget.BaseWidget.extend({
     }
     
 });
+
+/**
+ * PrimeFaces Mobile AutoComplete Widget
+ */
+PrimeFaces.widget.AutoComplete = PrimeFaces.widget.BaseWidget.extend({
+    
+    init: function(cfg) {
+        this._super(cfg);
+        this.cfg.minLength = (this.cfg.minLength !== undefined) ? this.cfg.minLength : 1;
+        this.cfg.delay = (this.cfg.delay !== undefined) ? this.cfg.delay : 300;
+        this.input = $(this.jqId + '_input');
+        this.panel = this.jq.children('.ui-controlgroup');
+        this.itemContainer = this.panel.children('.ui-controlgroup-controls');
+        
+        this.bindEvents();
+    },
+    
+    bindEvents: function() {
+        var $this = this;
+
+        this.input.on('keyup.autoComplete', function(e) {
+            var value = $this.input.val();
+
+            if(!value.length) {
+                $this.hide();
+            }
+
+            if(value.length >= $this.cfg.minLength) {
+                //Cancel the search request if user types within the timeout
+                if($this.timeout) {
+                    clearTimeout($this.timeout);
+                }
+
+                $this.timeout = setTimeout(function() {
+                    $this.search(value);
+                }, $this.cfg.delay);
+            }
+        });
+        this.input.trigger('click');
+    },
+    
+    search: function(query) {
+        //allow empty string but not undefined or null
+        if(query === undefined || query === null) {
+            return;
+        }
+
+        var $this = this,
+        options = {
+            source: this.id,
+            process: this.id,
+            update: this.id,
+            formId: this.cfg.formId,
+            onsuccess: function(responseXML, status, xhr) {
+                PrimeFaces.ajax.Response.handle(responseXML, status, xhr, {
+                    widget: $this,
+                    handle: function(content) {
+                        this.itemContainer.html(content);
+
+                        this.showSuggestions();
+                    }
+                });
+                
+                return true;
+            }
+        };
+
+        options.params = [
+            {name: this.id + '_query', value: query}  
+        ];
+        
+        if(this.hasBehavior('query')) {
+            var queryBehavior = this.cfg.behaviors['query'];
+            queryBehavior.call(this, options);
+        } 
+        else {
+            PrimeFaces.ajax.Request.handle(options); 
+        }
+    },
+    
+    show: function() {
+        this.panel.removeClass('ui-screen-hidden');
+    },
+    
+    hide: function() {        
+        this.panel.addClass('ui-screen-hidden');
+    },
+    
+    showSuggestions: function() {
+        this.items = this.itemContainer.find('.ui-autocomplete-item');                   
+        //this.bindDynamicEvents();
+                
+        if(this.items.length) {
+            if(this.panel.is(':hidden')) {
+                this.show();
+            }
+        }
+        else {
+            if(this.cfg.emptyMessage) { 
+                var emptyText = '<div class="ui-autocomplete-emptyMessage ui-widget">'+this.cfg.emptyMessage+'</div>';
+                this.itemContainer.html(emptyText);
+            }
+            else {
+                this.hide();
+            }
+        }
+    },
+    
+    hasBehavior: function(event) {
+        if(this.cfg.behaviors) {
+            return this.cfg.behaviors[event] !== undefined;
+        }
+    
+        return false;
+    }
+    
+});
