43dac657021d3faa1c35a5b778b1f52bf2bea38e
jfarcand.oss <jfarcand.oss@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2014-02-14 17:53:17 +0000

    Rename fire for publish, remove build in JSON

--- primefaces/trunk/src/main/java/org/primefaces/push/impl/EventBusImpl.java
+++ primefaces/trunk/src/main/java/org/primefaces/push/impl/EventBusImpl.java
@@ -16,74 +16,21 @@
 package org.primefaces.push.impl;
 
 import org.atmosphere.cpr.MetaBroadcaster;
-import org.primefaces.json.JSONException;
-import org.primefaces.json.JSONObject;
 import org.primefaces.push.EventBus;
 
 public class EventBusImpl implements EventBus {
 
     // TODO: Add caching support here.
-
     //@Override
-    public EventBus encodeToJsonAndFire(Object o) {
-        MetaBroadcaster.getDefault().broadcastTo("/*", toJSON(o));
-        return this;
-    }
-
-    //@Override
-    public EventBus encodeToJsonAndFire(String path, Object o) {
-        MetaBroadcaster.getDefault().broadcastTo(path, toJSON(o));
-        return this;
-    }
-
-    //@Override
-    public EventBus fire(Object o) {
+    public EventBus publish(Object o) {
         MetaBroadcaster.getDefault().broadcastTo("/*", o);
         return this;
     }
 
     //@Override
-    public EventBus fire(String path, Object o) {
+    public EventBus publish(String path, Object o) {
         MetaBroadcaster.getDefault().broadcastTo(path, o);
         return this;
     }
 
-    private String toJSON(Object data) {
-        try {
-            StringBuilder jsonBuilder = new StringBuilder();
-            jsonBuilder.append("{");
-
-            if(isBean(data)) {
-                jsonBuilder.append("\"").append("data").append("\":").append(new JSONObject(data).toString());
-            }
-            else {
-                String json = new JSONObject().put("data", data).toString();
-
-                jsonBuilder.append(json.substring(1, json.length() - 1));
-            }
-
-            jsonBuilder.append("}");
-
-            return jsonBuilder.toString();
-        }
-
-        catch(JSONException e) {
-            System.out.println(e.getMessage());
-
-            throw new RuntimeException(e);
-        }
-
-    }
-
-    private boolean isBean(Object value) {
-        if(value == null) {
-            return false;
-        }
-
-        if(value instanceof Boolean || value instanceof String || value instanceof Number) {
-            return false;
-        }
-
-        return true;
-    }
 }
