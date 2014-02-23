681c2496e3ab7b6521a99fdf904be16bcadc2172
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2014-02-23 17:26:21 +0000

    Improved mobile selectManyButton with prerendered markup + ajax update support.

--- src/main/resources/META-INF/resources/primefaces/mobile/widgets/forms.js
+++ src/main/resources/META-INF/resources/primefaces/mobile/widgets/forms.js
@@ -37,7 +37,7 @@ PrimeFaces.widget.SelectOneButton = PrimeFaces.widget.BaseWidget.extend({
     init: function(cfg) {
         this._super(cfg);
         
-        this.controlGroup = this.jq.children('.ui-controlgroup-controls ');
+        this.controlGroup = this.jq.children('.ui-controlgroup-controls');
         this.buttons = this.controlGroup.find('> .ui-radio > label.ui-btn');
         this.bindEvents();
     },
@@ -58,6 +58,43 @@ PrimeFaces.widget.SelectOneButton = PrimeFaces.widget.BaseWidget.extend({
         this.buttons.filter('.ui-btn-active').removeClass('ui-btn-active').next().prop('checked', false);
 
         button.addClass('ui-btn-active').next().prop('checked', true).change();
+    }
+    
+});
+
+/**
+ * PrimeFaces SelecyManyButton Widget
+ */
+PrimeFaces.widget.SelectManyButton = PrimeFaces.widget.BaseWidget.extend({
+    
+    init: function(cfg) {
+        this._super(cfg);
+        
+        this.controlGroup = this.jq.children('.ui-controlgroup-controls ');
+        this.buttons = this.controlGroup.find('> .ui-checkbox > label.ui-btn');
+        
+        this.bindEvents();
+    },
+    
+    bindEvents: function() {
+        var $this = this;
+        this.buttons.on('click.selectManyButton', function() {
+            var button = $(this);
+
+            if(button.hasClass('ui-btn-active'))
+                $this.unselect(button);
+            else
+                $this.select(button);
+        });
+    },
+    
+    select: function(button) {
+        button.addClass('ui-btn-active').next().prop('checked', true).change();
+
+    },
+    
+    unselect: function(button) {
+        button.removeClass('ui-btn-active').next().prop('checked', false).change();
     }
     
 });
