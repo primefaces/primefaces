ec4dd34bcaaa884f3753805c087f2484fe8e9e97
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2014-02-23 17:15:45 +0000

    Separated mobile widgets

---
+++ primefaces/trunk/src/main/resources/META-INF/resources/primefaces/mobile/widgets/forms.js
@@ -0,0 +1,63 @@
+/**
+ * PrimeFaces Mobile InputText Widget
+ */
+PrimeFaces.widget.InputText = PrimeFaces.widget.BaseWidget.extend({
+    
+    init: function(cfg) {
+        this._super(cfg);
+        this.input = this.jq.children('input');
+        this.cfg.enhanced = true;
+        this.cfg.clearBtn = true;
+        
+        this.input.textinput(this.cfg);
+    }
+    
+});
+
+/**
+ * PrimeFaces Mobile InputTextarea Widget
+ */
+PrimeFaces.widget.InputTextarea = PrimeFaces.widget.BaseWidget.extend({
+    
+    init: function(cfg) {
+        this._super(cfg);
+        this.cfg.enhanced = true;
+        this.cfg.autogrow = false;
+        
+        this.jq.textinput(this.cfg);
+    }
+    
+});
+
+/**
+ * PrimeFaces SelectOneButton Widget
+ */
+PrimeFaces.widget.SelectOneButton = PrimeFaces.widget.BaseWidget.extend({
+    
+    init: function(cfg) {
+        this._super(cfg);
+        
+        this.controlGroup = this.jq.children('.ui-controlgroup-controls ');
+        this.buttons = this.controlGroup.find('> .ui-radio > label.ui-btn');
+        this.bindEvents();
+    },
+    
+    bindEvents: function() {
+        var $this = this;
+                
+        this.buttons.on('click.selectOneButton', function(e) {
+            var button = $(this);
+
+            if(!button.hasClass('ui-btn-active')) {
+                $this.select(button);
+            }
+        });
+    },
+    
+    select: function(button) {
+        this.buttons.filter('.ui-btn-active').removeClass('ui-btn-active').next().prop('checked', false);
+
+        button.addClass('ui-btn-active').next().prop('checked', true).change();
+    }
+    
+});
