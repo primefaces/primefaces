34ad2ab7e7d2f1959f1f3088b3f1d57da3e745cd
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2014-02-26 17:39:10 +0000

    Implemented lazyloading for multi page mobile views

--- src/main/resources/META-INF/resources/primefaces/core/core.mobile.js
+++ src/main/resources/META-INF/resources/primefaces/core/core.mobile.js
@@ -11,7 +11,27 @@ PrimeFaces.Mobile = {
     navigate: function(to, cfg) {
         cfg.changeHash = cfg.changeHash||false;
         
-        $('body').pagecontainer('change', to, cfg);
+        var targetPage = $(to);
+        if(targetPage.hasClass('ui-lazypage')) {
+            var pageId = to.substring(1),
+            options = {
+                source: pageId,
+                process: pageId,
+                update: pageId,
+                params: [
+                    {name: pageId + '_lazyload', value: true}
+                ],
+                oncomplete: function() {
+                    $(to).page();
+                    $('body').pagecontainer('change', to, cfg);
+                }
+            };
+
+            PrimeFaces.ajax.Request.handle(options);
+        }
+        else {
+            $('body').pagecontainer('change', to, cfg);
+        }
     }
     
 };
