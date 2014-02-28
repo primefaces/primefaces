0d230585625cb1407b6976d83a43b42bfb354c56
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2014-02-28 14:07:23 +0000

    style and styleClass for messages

---
+++ src/main/java-templates/org/primefaces/component/messages/MessagesTemplate.java
@@ -0,0 +1,5 @@
+
+    public static final String CONTAINER_CLASS = "ui-messages ui-widget";
+    public static final String CLOSE_LINK_CLASS = "ui-messages-close";
+    public static final String CLOSE_ICON_CLASS = "ui-icon ui-icon-close";
+    public static final String SEVERITY_PREFIX_CLASS = "ui-messages-";
