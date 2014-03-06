731a8805d60b7e2c919ada9fae55dc1bcd18dbbb
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2014-03-06 22:44:49 +0000

    Removed static mode of messages in favor of prerendered markup.

--- src/main/java/org/primefaces/component/message/MessageRenderer.java
+++ src/main/java/org/primefaces/component/message/MessageRenderer.java
@@ -35,53 +35,12 @@ public class MessageRenderer extends UINotificationRenderer {
 	public void encodeEnd(FacesContext context, UIComponent component) throws IOException {
         Message uiMessage = (Message) component;
 
-        if (uiMessage.isStaticMode()) {
-        	encodeStaticMarkup(context, uiMessage);
-        } else {
-            UIComponent target = SearchExpressionFacade.resolveComponent(context, uiMessage, uiMessage.getFor());
-            String targetClientId = target.getClientId(context);
-        	
-        	encodeMarkup(context, uiMessage, targetClientId);
-        	encodeScript(context, uiMessage, targetClientId);
-        }
+        UIComponent target = SearchExpressionFacade.resolveComponent(context, uiMessage, uiMessage.getFor());
+        String targetClientId = target.getClientId(context);
+
+        encodeMarkup(context, uiMessage, targetClientId);
+        encodeScript(context, uiMessage, targetClientId);
 	}
-	
-    protected void encodeStaticMarkup(FacesContext context, Message uiMessage) throws IOException {
-    	ResponseWriter writer = context.getResponseWriter();
-    	boolean escape = uiMessage.isEscape();
-    	String severity = uiMessage.getSeverity();
-    	String display = uiMessage.getDisplay();
-    	String message = uiMessage.getMessage();
-    	boolean iconOnly = display.equals("icon");
-        String userStyle = uiMessage.getStyle();
-        String userStyleClass = uiMessage.getStyleClass();
-
-    	String styleClass = "ui-message ui-message-" + severity + " ui-widget ui-corner-all";
-        if(iconOnly) {
-            styleClass +=  " ui-message-icon-only ui-helper-clearfix";
-        }
-        if(userStyleClass != null) {
-            styleClass +=  " " + userStyleClass;
-        }
-    	
-		writer.startElement("div", uiMessage);
-		writer.writeAttribute("id", uiMessage.getClientId(context), null);
-        writer.writeAttribute("aria-live", "polite", null);
-        writer.writeAttribute("class", styleClass , null);
-        if(userStyle != null) {
-            writer.writeAttribute("style", userStyle , null);
-        }
-    	
-        if(!display.equals("text")) {
-            encodeIcon(writer, severity, message, iconOnly);
-        }
-
-        if(!iconOnly) {
-        	encodeText(writer, message, severity + "-summary", escape);
-        }
-        
-        writer.endElement("div");
-    }
     
     protected void encodeMarkup(FacesContext context, Message uiMessage, String targetClientId) throws IOException {        
         ResponseWriter writer = context.getResponseWriter();
