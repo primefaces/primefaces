724e4f84f2ae4d884348c92c7d18801418bed983
jfarcand.oss <jfarcand.oss@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2014-04-11 14:46:43 +0000

    Extends Atmosphere Internals to reduce code duplication

--- primefaces/trunk/src/main/java/org/primefaces/push/impl/PushEndpointInterceptor.java
+++ primefaces/trunk/src/main/java/org/primefaces/push/impl/PushEndpointInterceptor.java
@@ -15,83 +15,22 @@
  */
 package org.primefaces.push.impl;
 
+import org.atmosphere.config.managed.ServiceInterceptor;
 import org.atmosphere.config.service.AtmosphereInterceptorService;
-import org.atmosphere.cpr.Action;
-import org.atmosphere.cpr.AtmosphereConfig;
-import org.atmosphere.cpr.AtmosphereFramework.AtmosphereHandlerWrapper;
-import org.atmosphere.cpr.AtmosphereInterceptorAdapter;
+import org.atmosphere.cpr.AtmosphereFramework;
 import org.atmosphere.cpr.AtmosphereRequest;
-import org.atmosphere.cpr.AtmosphereResource;
-import org.atmosphere.cpr.Broadcaster;
 import org.atmosphere.cpr.FrameworkConfig;
 import org.atmosphere.handler.AnnotatedProxy;
-import org.atmosphere.interceptor.InvokationOrder;
 import org.primefaces.push.annotation.PushEndpoint;
 import org.primefaces.push.annotation.Singleton;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @AtmosphereInterceptorService
-public class PushEndpointInterceptor extends AtmosphereInterceptorAdapter {
-
+public class PushEndpointInterceptor extends ServiceInterceptor {
     private final static Logger logger = LoggerFactory.getLogger(PushEndpointInterceptor.class);
-    private AtmosphereConfig config;
-    private boolean wildcardMapping = false;
 
-    public PushEndpointInterceptor() {
-    }
-
-    @Override
-    public void configure(AtmosphereConfig config) {
-        this.config = config;
-        optimizeMapping();
-    }
-
-    @Override
-    public Action inspect(AtmosphereResource r) {
-        if (!wildcardMapping) return Action.CONTINUE;
-
-        mapAnnotatedService(r.getRequest(), (AtmosphereHandlerWrapper)
-                r.getRequest().getAttribute(FrameworkConfig.ATMOSPHERE_HANDLER_WRAPPER));
-
-        return Action.CONTINUE;
-    }
-
-    protected void optimizeMapping() {
-        for (String w : config.handlers().keySet()) {
-            if (w.contains("{") && w.contains("}")) {
-                wildcardMapping = true;
-                break;
-            }
-        }
-    }
-
-    protected void mapAnnotatedService(AtmosphereRequest request, AtmosphereHandlerWrapper w) {
-        Broadcaster b = w.broadcaster;
-
-        String path;
-        String pathInfo = null;
-        try {
-            pathInfo = request.getPathInfo();
-        } catch (IllegalStateException ex) {
-            // http://java.net/jira/browse/GRIZZLY-1301
-        }
-
-        if (pathInfo != null) {
-            path = request.getServletPath() + pathInfo;
-        } else {
-            path = request.getServletPath();
-        }
-
-        if (path == null || path.isEmpty()) {
-            path = "/";
-        }
-
-        // Remove the Broadcaster with curly braces
-        if (b.getID().contains("{")) {
-            config.getBroadcasterFactory().remove(b.getID());
-        }
-
+    protected void mapAnnotatedService(boolean reMap, String path, AtmosphereRequest request, AtmosphereFramework.AtmosphereHandlerWrapper w) {
         synchronized (config.handlers()) {
             String servletPath = (String) config.properties().get("servletPath");
             if (path.startsWith(servletPath)) {
@@ -125,11 +64,6 @@ public class PushEndpointInterceptor extends AtmosphereInterceptorAdapter {
                 }
             }
         }
-    }
-
-    @Override
-    public PRIORITY priority() {
-        return InvokationOrder.AFTER_DEFAULT;
     }
 
     @Override
