a7929916afcdac45ce8ce673e72f62aeb5024e25
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2014-04-15 07:22:11 +0000

    Fixed #6751

--- primefaces/trunk/src/main/java/org/primefaces/component/inputtextarea/InputTextareaRenderer.java
+++ primefaces/trunk/src/main/java/org/primefaces/component/inputtextarea/InputTextareaRenderer.java
@@ -128,7 +128,7 @@ public class InputTextareaRenderer extends InputRenderer {
 		writer.writeAttribute("id", clientId, null);
 		writer.writeAttribute("name", clientId, null);
 
-		renderPassThruAttributes(context, inputTextarea, HTML.INPUT_TEXT_ATTRS_WITHOUT_EVENTS);
+		renderPassThruAttributes(context, inputTextarea, HTML.TEXTAREA_ATTRS_WITHOUT_EVENTS);
         renderDomEvents(context, inputTextarea, HTML.INPUT_TEXT_EVENTS);
         
         if(inputTextarea.isDisabled()) writer.writeAttribute("disabled", "disabled", null);
