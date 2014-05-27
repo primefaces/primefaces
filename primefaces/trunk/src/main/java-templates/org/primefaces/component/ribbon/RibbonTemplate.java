3ecfecb0c7b8547dd107926f5ebe33be567492cf
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2014-05-27 14:54:58 +0000

    Core for ribbon component

---
+++ primefaces/trunk/src/main/java-templates/org/primefaces/component/ribbon/RibbonTemplate.java
@@ -0,0 +1,8 @@
+
+    public static final String CONTAINER_CLASS = "ui-ribbon ui-widget";
+    public static final String NAVIGATOR_CLASS = "ui-ribbon-nav ui-helper-reset ui-helper-clearfix ui-widget-header ui-corner-all";
+    public static final String INACTIVE_TAB_HEADER_CLASS = "ui-ribbon-header ui-state-default ui-corner-top";
+    public static final String ACTIVE_TAB_HEADER_CLASS = "ui-ribbon-header ui-state-default ui-state-active ui-corner-top";
+    public static final String PANELS_CLASS = "ui-ribbon-panels";
+    public static final String ACTIVE_TAB_CONTENT_CLASS = "ui-ribbon-panel ui-widget-content ui-corner-bottom";
+    public static final String INACTIVE_TAB_CONTENT_CLASS = "ui-ribbon-panel ui-widget-content ui-corner-bottom ui-helper-hidden";
