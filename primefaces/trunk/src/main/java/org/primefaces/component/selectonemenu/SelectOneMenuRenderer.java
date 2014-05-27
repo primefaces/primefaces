c02e6b6393b4ba118ede4615b514fa19b0867a0f
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2014-05-27 18:56:44 +0000

    bugfix

--- primefaces/trunk/src/main/java/org/primefaces/component/selectonemenu/SelectOneMenuRenderer.java
+++ primefaces/trunk/src/main/java/org/primefaces/component/selectonemenu/SelectOneMenuRenderer.java
@@ -134,6 +134,7 @@ public class SelectOneMenuRenderer extends SelectOneRenderer {
         writer.writeAttribute("id", focusId, null);
         writer.writeAttribute("name", focusId, null);
         writer.writeAttribute("type", "text", null);
+        writer.writeAttribute("autocomplete", "off", null);
         if(menu.getTabindex() != null) writer.writeAttribute("tabindex", menu.getTabindex(), null);
         if(menu.isDisabled()) writer.writeAttribute("disabled", "disabled", null);
         
