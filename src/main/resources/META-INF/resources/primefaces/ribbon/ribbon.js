75bae3366c2b936399f517735713660d68c81172
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2014-05-27 14:54:58 +0000

    Core for ribbon component

---
+++ src/main/resources/META-INF/resources/primefaces/ribbon/ribbon.js
@@ -0,0 +1,10 @@
+/**
+ * PrimeFaces Ribbon Widget
+ */
+PrimeFaces.widget.Ribbon = PrimeFaces.widget.BaseWidget.extend({
+    
+    init: function(cfg) {
+        this._super(cfg);
+    }
+    
+});
