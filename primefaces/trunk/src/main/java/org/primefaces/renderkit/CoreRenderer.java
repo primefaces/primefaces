f111afda067ba984b0e8bf8292ba79de1e39d43a
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2014-06-05 07:39:49 +0000

    Removed unused import

--- primefaces/trunk/src/main/java/org/primefaces/renderkit/CoreRenderer.java
+++ primefaces/trunk/src/main/java/org/primefaces/renderkit/CoreRenderer.java
@@ -24,7 +24,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import javax.faces.application.Resource;
-import javax.faces.application.ResourceHandler;
 import javax.faces.component.EditableValueHolder;
 import javax.faces.component.UIComponent;
 import javax.faces.component.UIParameter;
