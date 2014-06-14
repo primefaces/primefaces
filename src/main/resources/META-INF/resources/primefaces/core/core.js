a15057aadd8f4495549eac4f287fbf8c8c514b62
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2014-06-14 08:19:42 +0000

    bugfix

--- src/main/resources/META-INF/resources/primefaces/core/core.js
+++ src/main/resources/META-INF/resources/primefaces/core/core.js
@@ -529,7 +529,7 @@
             monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ],
             monthNamesShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ],
             dayNames: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-            dayNamesShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Tue', 'Fri', 'Sat'],
+            dayNamesShort: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
             dayNamesMin: ['S', 'M', 'T', 'W ', 'T', 'F ', 'S'],
             weekHeader: 'Week',
             firstDay: 0,
