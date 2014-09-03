bbdacb90234b5c3d8f269403032ed19765dccbe4
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2014-09-03 22:05:05 +0000

    Configurable layout for DataGrid, defaults to tabular for backward compatibility

--- primefaces/trunk/src/main/java-templates/org/primefaces/component/datagrid/DataGridTemplate.java
+++ primefaces/trunk/src/main/java-templates/org/primefaces/component/datagrid/DataGridTemplate.java
@@ -15,12 +15,15 @@ import org.primefaces.model.LazyDataModel;
 import org.primefaces.util.Constants;
 
 	public static final String DATAGRID_CLASS = "ui-datagrid ui-widget";
-    public static final String CONTENT_CLASS = "ui-datagrid-content ui-widget-content ui-grid ui-grid-responsive";
-    public static final String EMPTY_CONTENT_CLASS = "ui-datagrid-content ui-datagrid-content-empty ui-widget-content ui-grid ui-grid-responsive";
-	public static final String COLUMN_CLASS = "ui-datagrid-column";
-    public static final String ROW_CLASS = "ui-grid-row";
     public static final String HEADER_CLASS = "ui-datagrid-header ui-widget-header ui-corner-top";
     public static final String FOOTER_CLASS = "ui-datagrid-footer ui-widget-header ui-corner-bottom";
+    public static final String TABLE_CONTENT_CLASS = "ui-datagrid-content ui-widget-content";
+    public static final String EMPTY_CONTENT_CLASS = "ui-datagrid-content ui-datagrid-content-empty ui-widget-content";
+	public static final String TABLE_CLASS = "ui-datagrid-data";
+	public static final String TABLE_ROW_CLASS = "ui-datagrid-row";
+    public static final String GRID_CONTENT_CLASS = "ui-datagrid-content ui-widget-content ui-grid ui-grid-responsive";
+    public static final String GRID_ROW_CLASS = "ui-grid-row";
+    public static final String COLUMN_CLASS = "ui-datagrid-column";
 
     private static final Collection<String> EVENT_NAMES = Collections.unmodifiableCollection(Arrays.asList("page"));
 
