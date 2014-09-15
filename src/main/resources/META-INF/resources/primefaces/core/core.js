c7e9b301a79b1e9f7cffdaf867dced030cf5901e
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2014-09-15 11:28:35 +0000

    Update browser resolution

--- src/main/resources/META-INF/resources/primefaces/core/core.js
+++ src/main/resources/META-INF/resources/primefaces/core/core.js
@@ -439,36 +439,104 @@
         },
         
         //adapted from jquery browser plugin
-        resolveUserAgent: function(ua) {
+        resolveUserAgent: function() {
             if($.browser) {
                 this.browser = $.browser;
             }
             else {
-                ua = ua.toLowerCase();
+                var matched, browser;
 
-                var match = /(chrome)[ \/]([\w.]+)/.exec(ua) ||
-                    /(webkit)[ \/]([\w.]+)/.exec(ua) ||
-                    /(opera)(?:.*version|)[ \/]([\w.]+)/.exec(ua) ||
-                    /(msie) ([\w.]+)/.exec(ua) ||
-                    ua.indexOf("compatible") < 0 && /(mozilla)(?:.*? rv:([\w.]+)|)/.exec(ua) || [],
-                userAgent =  {
-                    browser: match[ 1 ] || "",
-                    version: match[ 2 ] || "0"
-                },
+                jQuery.uaMatch = function( ua ) {
+                  ua = ua.toLowerCase();
+
+                  var match = /(opr)[\/]([\w.]+)/.exec( ua ) ||
+                      /(chrome)[ \/]([\w.]+)/.exec( ua ) ||
+                      /(version)[ \/]([\w.]+).*(safari)[ \/]([\w.]+)/.exec( ua ) ||
+                      /(webkit)[ \/]([\w.]+)/.exec( ua ) ||
+                      /(opera)(?:.*version|)[ \/]([\w.]+)/.exec( ua ) ||
+                      /(msie) ([\w.]+)/.exec( ua ) ||
+                      ua.indexOf("trident") >= 0 && /(rv)(?::| )([\w.]+)/.exec( ua ) ||
+                      ua.indexOf("compatible") < 0 && /(mozilla)(?:.*? rv:([\w.]+)|)/.exec( ua ) ||
+                      [];
+
+                  var platform_match = /(ipad)/.exec( ua ) ||
+                      /(iphone)/.exec( ua ) ||
+                      /(android)/.exec( ua ) ||
+                      /(windows phone)/.exec( ua ) ||
+                      /(win)/.exec( ua ) ||
+                      /(mac)/.exec( ua ) ||
+                      /(linux)/.exec( ua ) ||
+                      /(cros)/i.exec( ua ) ||
+                      [];
+
+                  return {
+                      browser: match[ 3 ] || match[ 1 ] || "",
+                      version: match[ 2 ] || "0",
+                      platform: platform_match[ 0 ] || ""
+                  };
+                };
+
+                matched = jQuery.uaMatch( window.navigator.userAgent );
                 browser = {};
 
-                if(userAgent.browser) {
-                    browser[userAgent.browser] = true;
-                    browser.version = userAgent.version;
+                if ( matched.browser ) {
+                  browser[ matched.browser ] = true;
+                  browser.version = matched.version;
+                  browser.versionNumber = parseInt(matched.version);
                 }
 
-                if (browser.chrome) {
-                    browser.webkit = true;
-                } else if (browser.webkit) {
-                    browser.safari = true;
+                if ( matched.platform ) {
+                  browser[ matched.platform ] = true;
                 }
+
+                // These are all considered mobile platforms, meaning they run a mobile browser
+                if ( browser.android || browser.ipad || browser.iphone || browser[ "windows phone" ] ) {
+                  browser.mobile = true;
+                }
+
+                // These are all considered desktop platforms, meaning they run a desktop browser
+                if ( browser.cros || browser.mac || browser.linux || browser.win ) {
+                  browser.desktop = true;
+                }
+
+                // Chrome, Opera 15+ and Safari are webkit based browsers
+                if ( browser.chrome || browser.opr || browser.safari ) {
+                  browser.webkit = true;
+                }
+
+                // IE11 has a new token so we will assign it msie to avoid breaking changes
+                if ( browser.rv )
+                {
+                  var ie = "msie";
+
+                  matched.browser = ie;
+                  browser[ie] = true;
+                }
+
+                // Opera 15+ are identified as opr
+                if ( browser.opr )
+                {
+                  var opera = "opera";
+
+                  matched.browser = opera;
+                  browser[opera] = true;
+                }
+
+                // Stock Android browsers are marked as Safari on Android.
+                if ( browser.safari && browser.android )
+                {
+                  var android = "android";
+
+                  matched.browser = android;
+                  browser[android] = true;
+                }
+
+                // Assign the name and platform variable
+                browser.name = matched.browser;
+                browser.platform = matched.platform;
 
                 this.browser = browser;
+                $.browser = browser;
             }
         },
 
@@ -588,7 +656,7 @@
         return widgetInstance;
     };
     
-    PrimeFaces.resolveUserAgent(navigator.userAgent);
+    PrimeFaces.resolveUserAgent();
    
     //expose globally
     window.PrimeFaces = PrimeFaces;
