66e8ff51bf1ca86929aaed5eaf7c9d8f029803be
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2014-10-02 08:18:48 +0000

    Deprecated pm:switch in favor of mobile renderer of p:inputSwitch

--- src/main/java/org/primefaces/mobile/renderkit/InputSwitchRenderer.java
+++ src/main/java/org/primefaces/mobile/renderkit/InputSwitchRenderer.java
@@ -13,80 +13,55 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.primefaces.mobile.component.uiswitch;
+package org.primefaces.mobile.renderkit;
 
 import java.io.IOException;
-import java.util.logging.Logger;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.context.ResponseWriter;
-import org.primefaces.renderkit.CoreRenderer;
+import org.primefaces.component.inputswitch.InputSwitch;
+import org.primefaces.mobile.component.uiswitch.UISwitch;
 import org.primefaces.util.ComponentUtils;
 import org.primefaces.util.WidgetBuilder;
 
-public class UISwitchRenderer extends CoreRenderer {
+public class InputSwitchRenderer extends org.primefaces.component.inputswitch.InputSwitchRenderer {
     
-    private final static Logger logger = Logger.getLogger(UISwitchRenderer.class.getName());
-    
-    @Override
-	public void decode(FacesContext context, UIComponent component) {
-		UISwitch uiswitch = (UISwitch) component;
-        if(uiswitch.isDisabled()) {
-            return;
-        }
-             
-        String clientId = uiswitch.getClientId(context);
-		String submittedValue = (String) context.getExternalContext().getRequestParameterMap().get(clientId + "_input");
-
-        if(submittedValue != null && isChecked(submittedValue)) {
-            uiswitch.setSubmittedValue(true);
-        }
-        else {
-            uiswitch.setSubmittedValue(false);
-        }
-	}
-    
-    protected boolean isChecked(String value) {
-        return value.equalsIgnoreCase("on")||value.equalsIgnoreCase("yes")||value.equalsIgnoreCase("true");
-    }
-
     @Override
     public void encodeEnd(FacesContext context, UIComponent component) throws IOException {
-        UISwitch uiswitch = (UISwitch) component;
-        
-        encodeMarkup(context, uiswitch);
-        encodeScript(context, uiswitch);
-        
-        logger.info("Mobile only switch component is deprecated, use p:inputSwitch instead.");
+        InputSwitch inputSwitch = (InputSwitch) component;
+
+        encodeMarkup(context, inputSwitch);
+        encodeScript(context, inputSwitch);
     }
     
-    public void encodeMarkup(FacesContext context, UISwitch uiswitch) throws IOException {
+    @Override
+    public void encodeMarkup(FacesContext context, InputSwitch inputSwitch) throws IOException {
         ResponseWriter writer = context.getResponseWriter();
-        String clientId = uiswitch.getClientId(context);
-        boolean checked = Boolean.valueOf(ComponentUtils.getValueToRender(context, uiswitch));
+        String clientId = inputSwitch.getClientId(context);
+        boolean checked = Boolean.valueOf(ComponentUtils.getValueToRender(context, inputSwitch));
         String inputId = clientId + "_input";
-        String onLabel = uiswitch.getOnLabel();
-        String offLabel = uiswitch.getOffLabel();
-        String style = uiswitch.getStyle();
-        String styleClass = uiswitch.getStyleClass();
+        String onLabel = inputSwitch.getOnLabel();
+        String offLabel = inputSwitch.getOffLabel();
+        String style = inputSwitch.getStyle();
+        String styleClass = inputSwitch.getStyleClass();
         styleClass = (styleClass == null) ? UISwitch.CONTAINER_CLASS: UISwitch.CONTAINER_CLASS + " " + styleClass;
         
-        writer.startElement("div", uiswitch);
+        writer.startElement("div", inputSwitch);
         writer.writeAttribute("id", clientId, "id");
         if(style != null) writer.writeAttribute("style", style, "style");
         if(styleClass != null) writer.writeAttribute("class", styleClass, "styleClass");
         
-        writer.startElement("span", uiswitch);
+        writer.startElement("span", inputSwitch);
         writer.writeAttribute("class", UISwitch.ON_CLASS, null);
         writer.writeText(onLabel, null);
         writer.endElement("span");
         
-        writer.startElement("span", uiswitch);
+        writer.startElement("span", inputSwitch);
         writer.writeAttribute("class", UISwitch.OFF_CLASS, null);
         writer.writeText(offLabel, null);
         writer.endElement("span");
         
-        writer.startElement("input", uiswitch);
+        writer.startElement("input", inputSwitch);
         writer.writeAttribute("id", inputId, "id");
         writer.writeAttribute("name", inputId, null);
         writer.writeAttribute("data-role", "none", null);
@@ -94,18 +69,19 @@ public class UISwitchRenderer extends CoreRenderer {
         writer.writeAttribute("class", UISwitch.INPUT_CLASS, null);
         
         if (checked) writer.writeAttribute("checked", "checked", null);
-        if (uiswitch.isDisabled()) writer.writeAttribute("disabled", "disabled", null);
+        if (inputSwitch.isDisabled()) writer.writeAttribute("disabled", "disabled", null);
         
-        renderOnchange(context, uiswitch);
+        renderOnchange(context, inputSwitch);
         
         writer.endElement("input");
         
         writer.endElement("div");
     }
     
-    public void encodeScript(FacesContext context, UISwitch uiswitch) throws IOException {
-        String clientId = uiswitch.getClientId(context);
+    @Override
+    public void encodeScript(FacesContext context, InputSwitch inputSwitch) throws IOException {
+        String clientId = inputSwitch.getClientId(context);
         WidgetBuilder wb = getWidgetBuilder(context);
-        wb.init("UISwitch", uiswitch.resolveWidgetVar(), clientId).finish();
+        wb.init("UISwitch", inputSwitch.resolveWidgetVar(), clientId).finish();
     }
 }
