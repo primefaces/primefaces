bcf08de775b9e8be93fdc670320984d8a86bbb9d
Çağatay Çivici <cagataycivici@users.noreply.github.com>
2014-10-21 09:56:20 +0000

    mobile datatable

---
+++ src/main/resources/META-INF/resources/primefaces/mobile/widgets/datatable.js
@@ -0,0 +1,9 @@
+/**
+ * PrimeFaces Mobile DataTable Widget
+ */
+PrimeFaces.widget.DataTable = PrimeFaces.widget.BaseWidget.extend({
+    
+    init: function(cfg) {
+        this._super(cfg);
+    }
+});
