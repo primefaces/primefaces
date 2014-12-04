cb4f6f9054eb971c3ca3d0192a4ef36d59cf7145
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2014-12-04 12:39:57 +0000

    Better compatibility with horizontal scrolling and keyboard support of sortable columns.

--- primefaces/trunk/src/main/java/org/primefaces/component/datatable/DataTableRenderer.java
+++ primefaces/trunk/src/main/java/org/primefaces/component/datatable/DataTableRenderer.java
@@ -375,6 +375,7 @@ public class DataTableRenderer extends DataRenderer {
 
         writer.startElement("div", null);
         writer.writeAttribute("class", DataTable.SCROLLABLE_BODY_CLASS, null);
+        writer.writeAttribute("tabindex", "-1", null);
         if(scrollHeight != null && scrollHeight.indexOf("%") == -1) {
             writer.writeAttribute("style", "height:" + scrollHeight + "px", null);
         }
