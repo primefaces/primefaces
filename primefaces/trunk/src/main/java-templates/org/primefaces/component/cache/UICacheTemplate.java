b9a66f1a3458f209c5acb1e6d61f6e5b8d82ac7b
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2014-12-09 19:59:14 +0000

    Fixed #7761

--- primefaces/trunk/src/main/java-templates/org/primefaces/component/cache/UICacheTemplate.java
+++ primefaces/trunk/src/main/java-templates/org/primefaces/component/cache/UICacheTemplate.java
@@ -14,3 +14,28 @@ import javax.faces.component.visit.VisitContext;
     protected boolean isVisitable(VisitContext visitContext) {
         return this.isDisabled() || this.isCacheSetInCurrentRequest();
     }
+
+    protected boolean shouldProcess() {
+        return this.isDisabled() || this.isCacheSetInCurrentRequest();
+    }
+
+    @Override
+    public void processDecodes(FacesContext context) {
+        if(shouldProcess()) {
+            super.processDecodes(context);
+        }
+    }
+
+    @Override
+    public void processValidators(FacesContext context) {
+        if(shouldProcess()) {
+            super.processValidators(context);
+        }
+    }
+
+    @Override
+    public void processUpdates(FacesContext context) {
+        if(shouldProcess()) {
+            super.processUpdates(context);
+        }
+    }
