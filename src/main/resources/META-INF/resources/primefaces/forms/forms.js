27a8e316e9cb84cf73a470415f00c95cc5b59a27
sincan.mert@gmail.com <mertsincan@users.noreply.github.com>
2015-02-10 08:20:14 +0000

    SelectOneListbox and SelectManyMenu disabled don't work with filter.

--- src/main/resources/META-INF/resources/primefaces/forms/forms.js
+++ src/main/resources/META-INF/resources/primefaces/forms/forms.js
@@ -1651,25 +1651,27 @@ PrimeFaces.widget.SelectOneListbox = PrimeFaces.widget.SelectListbox.extend({
     bindEvents: function() {
         this._super();
         var $this = this;
+        
+        if(!this.cfg.disabled) {
+            this.items.on('click.selectListbox', function(e) {
+                var item = $(this),
+                selectedItem = $this.items.filter('.ui-state-highlight');
 
-        this.items.on('click.selectListbox', function(e) {
-            var item = $(this),
-            selectedItem = $this.items.filter('.ui-state-highlight');
+                if(item.index() !== selectedItem.index()) {
+                    if(selectedItem.length) {
+                        $this.unselectItem(selectedItem);
+                    }
 
-            if(item.index() !== selectedItem.index()) {
-                if(selectedItem.length) {
-                    $this.unselectItem(selectedItem);
+                    $this.selectItem(item);
+                    $this.input.trigger('change');
                 }
 
-                $this.selectItem(item);
-                $this.input.trigger('change');
-            }
+                $this.input.trigger('click');
 
-            $this.input.trigger('click');
-
-            PrimeFaces.clearSelection();
-            e.preventDefault();
-        });
+                PrimeFaces.clearSelection();
+                e.preventDefault();
+            });
+        }
     }
 });
 
