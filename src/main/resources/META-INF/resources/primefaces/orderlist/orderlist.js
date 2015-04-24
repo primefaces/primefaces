6117039107604e91d3c16dc795f50ca172861e46
Tobias Mayer <github@tobiasm.de>
2015-04-24 15:19:47 +0200

    improved events to represent what really happend on the ui

--- src/main/resources/META-INF/resources/primefaces/orderlist/orderlist.js
+++ src/main/resources/META-INF/resources/primefaces/orderlist/orderlist.js
@@ -65,7 +65,10 @@ PrimeFaces.widget.OrderList = PrimeFaces.widget.BaseWidget.extend({
 
             if(!metaKey) {
                 element.removeClass('ui-state-hover').addClass('ui-state-highlight')
-                .siblings('.ui-state-highlight').removeClass('ui-state-highlight');
+                .siblings('.ui-state-highlight').each(function() {
+                	$(this).removeClass('ui-state-highlight');
+                	$this.fireItemUnselectEvent($(this));
+                });
         
                 $this.fireItemSelectEvent(element);
             }
@@ -97,6 +100,7 @@ PrimeFaces.widget.OrderList = PrimeFaces.widget.BaseWidget.extend({
         ui.item.removeClass('ui-state-highlight');
         this.saveState();
         this.fireReorderEvent();
+        this.fireItemUnselectEvent(ui.item);
     },
     
     saveState: function() {
