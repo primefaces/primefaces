2f0a5c1454b8875d9700d912b9b41e9d61b16e4c
Çağatay Çivici <cagatay.civici@github.com>
2015-06-20 00:04:58 +0300

    Added swipe support to carousel

--- src/main/resources/META-INF/resources/primefaces/carousel/carousel.js
+++ src/main/resources/META-INF/resources/primefaces/carousel/carousel.js
@@ -62,6 +62,29 @@ PrimeFaces.widget.Carousel = PrimeFaces.widget.DeferredWidget.extend({
             }
         });
         
+        this.itemsContainer.swipe({
+            swipe:function(event, direction) {
+                if(direction === 'left') {
+                    if($this.page === ($this.totalPages - 1)) {
+                        if($this.cfg.circular)
+                            $this.setPage(0);
+                    }
+                    else {
+                        $this.setPage($this.page + 1);
+                    }
+                }
+                else if(direction === 'right') {
+                    if($this.page === 0) {
+                        if($this.cfg.circular)
+                            $this.setPage($this.totalPages - 1);
+                    }
+                    else {
+                        $this.setPage($this.page - 1);
+                    }
+                }
+            }
+        });
+        
         if(this.pageLinks.length) {
             this.pageLinks.on('click', function(e) {
                 $this.setPage($(this).index());
