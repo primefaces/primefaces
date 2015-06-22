4b28896fe2f8c5072e9f48c71e8ce77a922d6e75
Çağatay Çivici <cagatay.civici@github.com>
2015-06-22 18:46:20 +0300

    Fixed #285

--- src/main/resources/META-INF/resources/primefaces/spotlight/spotlight.js
+++ src/main/resources/META-INF/resources/primefaces/spotlight/spotlight.js
@@ -14,20 +14,25 @@ PrimeFaces.widget.Spotlight = PrimeFaces.widget.BaseWidget.extend({
     },
     
     createMasks: function() {
-        $(document.body).append('<div class="ui-widget-overlay ui-spotlight ui-spotlight-top ui-helper-hidden"></div><div class="ui-widget-overlay ui-spotlight ui-spotlight-bottom ui-helper-hidden"></div>' + 
+        var documentBody = $(document.body);
+        documentBody.append('<div class="ui-widget-overlay ui-spotlight ui-spotlight-top ui-helper-hidden"></div><div class="ui-widget-overlay ui-spotlight ui-spotlight-bottom ui-helper-hidden"></div>' + 
                         '<div class="ui-widget-overlay ui-spotlight ui-spotlight-left ui-helper-hidden"></div><div class="ui-widget-overlay ui-spotlight ui-spotlight-right ui-helper-hidden"></div>');
     },
     
     show: function() {
-        var doc = $(document),
-        body = $(document.body),
-        offset = this.target.offset(),
-        topMask = body.children('div.ui-spotlight-top'),
-        bottomMask = body.children('div.ui-spotlight-bottom'),
-        leftMask = body.children('div.ui-spotlight-left'),
-        rightMask = body.children('div.ui-spotlight-right');
+        this.calculatePositions();
 
-        topMask.css({
+        $(document.body).children('div.ui-spotlight').show();
+        
+        this.bindEvents();
+    },
+    
+    calculatePositions: function() {
+        var doc = $(document),
+        documentBody = $(document.body),
+        offset = this.target.offset();
+        
+        documentBody.children('div.ui-spotlight-top').css({
             'left': 0,
             'top': 0,
             'width': doc.width(),
@@ -35,14 +40,14 @@ PrimeFaces.widget.Spotlight = PrimeFaces.widget.BaseWidget.extend({
         });
         
         var bottomTop = offset.top + this.target.outerHeight();
-        bottomMask.css({
+        documentBody.children('div.ui-spotlight-bottom').css({
             'left': 0,
             'top': bottomTop,
             'width': doc.width(),
             'height': doc.height() - bottomTop
         });
         
-        leftMask.css({
+        documentBody.children('div.ui-spotlight-left').css({
             'left': 0,
             'top': offset.top,
             'width': offset.left,
@@ -50,22 +55,15 @@ PrimeFaces.widget.Spotlight = PrimeFaces.widget.BaseWidget.extend({
         });
         
         var rightLeft = offset.left + this.target.outerWidth();
-        rightMask.css({
+        documentBody.children('div.ui-spotlight-right').css({
             'left': rightLeft,
             'top': offset.top,
             'width': doc.width() - rightLeft,
             'height': this.target.outerHeight()
         });
-    
-        topMask.appendTo(document.body).show();
-        bottomMask.appendTo(document.body).show();
-        leftMask.appendTo(document.body).show();
-        rightMask.appendTo(document.body).show();
-        
-        this.blockEvents();
     },
     
-    blockEvents: function() {
+    bindEvents: function() {
         var $this = this;
         
         this.target.data('zindex',this.target.zIndex()).css('z-index', ++PrimeFaces.zindex);
@@ -103,15 +101,20 @@ PrimeFaces.widget.Spotlight = PrimeFaces.widget.BaseWidget.extend({
                         event.preventDefault();
                     }
                 });
+                
+        $(window).on('resize.spotlight', function() {
+            $this.calculatePositions();
+        });
     },
     
-    unblockEvents: function() {
+    unbindEvents: function() {
         $(document).off(this.eventsToBlock).off('keydown.' + this.id);
+        $(window).off('resize.spotlight');
     },
     
     hide: function() {
         $(document.body).children('.ui-spotlight').hide();
-        this.unblockEvents();
+        this.unbindEvents();
         this.target.css('z-index', this.target.zIndex());
     }
 
