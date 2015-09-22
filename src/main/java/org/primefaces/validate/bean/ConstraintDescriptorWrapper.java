6a5ed24fe67930c8d24375b1ca243e76f58b179f
lordeagle <r.wermelinger@gmail.com>
2015-09-22 14:46:12 +0200

    normalize to prime formatting rules
    set space intentions to 4 as described:
    https://github.com/primefaces/primefaces/blob/master/CONTRIBUTING.md

--- src/main/java/org/primefaces/validate/bean/ConstraintDescriptorWrapper.java
+++ src/main/java/org/primefaces/validate/bean/ConstraintDescriptorWrapper.java
@@ -1,17 +1,17 @@
 /*
  * Copyright 2009-2014 PrimeTek.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package org.primefaces.validate.bean;
 
@@ -25,39 +25,39 @@ import javax.validation.Payload;
 import javax.validation.metadata.ConstraintDescriptor;
 
 public class ConstraintDescriptorWrapper<T extends Annotation> implements ConstraintDescriptor<T>{
-  protected final ConstraintDescriptor<T> constraint;
+    protected final ConstraintDescriptor<T> constraint;
 
-  @SuppressWarnings("unchecked")
-  public ConstraintDescriptorWrapper(ConstraintDescriptor<?> constraint){
-    this.constraint = (ConstraintDescriptor<T>) constraint;
-  }
+    @SuppressWarnings("unchecked")
+    public ConstraintDescriptorWrapper(ConstraintDescriptor<?> constraint){
+        this.constraint = (ConstraintDescriptor<T>) constraint;
+    }
 
-  public T getAnnotation(){
-    return constraint.getAnnotation();
-  }
+    public T getAnnotation(){
+        return constraint.getAnnotation();
+    }
 
-  public Set<Class<?>> getGroups(){
-    return constraint.getGroups();
-  }
+    public Set<Class<?>> getGroups(){
+        return constraint.getGroups();
+    }
 
-  public Set<Class<? extends Payload>> getPayload(){
-    return constraint.getPayload();
-  }
+    public Set<Class<? extends Payload>> getPayload(){
+        return constraint.getPayload();
+    }
 
-  public List<Class<? extends ConstraintValidator<T, ?>>> getConstraintValidatorClasses(){
-    return constraint.getConstraintValidatorClasses();
-  }
+    public List<Class<? extends ConstraintValidator<T, ?>>> getConstraintValidatorClasses(){
+        return constraint.getConstraintValidatorClasses();
+    }
 
-  public Map<String, Object> getAttributes(){
-    return constraint.getAttributes();
-  }
+    public Map<String, Object> getAttributes(){
+        return constraint.getAttributes();
+    }
 
-  public Set<ConstraintDescriptor<?>> getComposingConstraints(){
-    return constraint.getComposingConstraints();
-  }
+    public Set<ConstraintDescriptor<?>> getComposingConstraints(){
+        return constraint.getComposingConstraints();
+    }
 
-  public boolean isReportAsSingleViolation(){
-    return constraint.isReportAsSingleViolation();
-  }
+    public boolean isReportAsSingleViolation(){
+        return constraint.isReportAsSingleViolation();
+    }
 
 }
--- src/main/java/org/primefaces/validate/bean/MessageInterpolatingConstraint.java
+++ src/main/java/org/primefaces/validate/bean/MessageInterpolatingConstraint.java
@@ -1,17 +1,17 @@
 /*
  * Copyright 2009-2014 PrimeTek.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package org.primefaces.validate.bean;
 
@@ -22,38 +22,45 @@ import javax.validation.MessageInterpolator;
 import javax.validation.metadata.ConstraintDescriptor;
 
 public class MessageInterpolatingConstraint extends ConstraintDescriptorWrapper<Annotation>{
-  private static final String MESSAGE = "message";
-  private MessageInterpolator interpolator;
+    private static final String MESSAGE = "message";
+    private MessageInterpolator interpolator;
 
-  public MessageInterpolatingConstraint(MessageInterpolator interpolator, ConstraintDescriptor<?> constraint){
-    super(constraint);
-    this.interpolator = interpolator;
-  }
-  
-  @Override
-  public Map<String, Object> getAttributes(){
-    Map<String, Object> attrs = super.getAttributes();
-    Object message = attrs.remove(MESSAGE);
-    if (message != null){
-      if (message instanceof String){
-        String template = (String) message;
-        message = interpolator.interpolate(template, getContext());
-      }
-      attrs.put(MESSAGE, message);
+    public MessageInterpolatingConstraint(MessageInterpolator interpolator, ConstraintDescriptor<?> constraint){
+        super(constraint);
+        this.interpolator = interpolator;
     }
-    return attrs;
-  }
-  
-  private MessageInterpolator.Context getContext(){
-    return new MessageInterpolator.Context(){
-      public ConstraintDescriptor<?> getConstraintDescriptor(){
-        return constraint;
-      }
 
-      public Object getValidatedValue(){
-        return null;
-      }
-    };
-  }
+    @Override
+    public Map<String, Object> getAttributes()
+    {
+        Map<String, Object> attrs = super.getAttributes();
+        Object message = attrs.remove(MESSAGE);
+        if (message != null)
+        {
+            if (message instanceof String)
+            {
+                String template = (String) message;
+                message = interpolator.interpolate(template, getContext());
+            }
+            attrs.put(MESSAGE, message);
+        }
+        return attrs;
+    }
+
+    private MessageInterpolator.Context getContext()
+    {
+        return new MessageInterpolator.Context()
+            {
+                public ConstraintDescriptor<?> getConstraintDescriptor()
+                {
+                    return constraint;
+                }
+
+                public Object getValidatedValue()
+                {
+                    return null;
+                }
+            };
+    }
 
 }
