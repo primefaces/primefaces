6ca2da41a0c95a619f4849da8b931ba827276b77
Thomas Andraschko <tandraschko@apache.org>
2015-10-29 16:34:20 +0100

    Implement InputHolder

--- src/main/java-templates/org/primefaces/component/inputnumber/InputNumberTemplate.java
+++ src/main/java-templates/org/primefaces/component/inputnumber/InputNumberTemplate.java
@@ -1,2 +1,22 @@
 
     public final static String STYLE_CLASS = "ui-inputnumber ui-widget";
+
+    @Override
+	public String getInputClientId() {
+		return getClientId() + "_input";
+	}
+
+    @Override
+    public String getValidatableInputClientId() {
+        return getClientId() + "_hinput";
+    }
+
+    @Override
+    public void setLabelledBy(String labelledBy) {
+        getStateHelper().put("labelledby", labelledBy);
+    }
+
+    @Override
+    public String getLabelledBy() {
+        return (String) getStateHelper().get("labelledby");
+    } 
