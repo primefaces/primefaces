5a9c594c63497ad68e96873ff8eec2801209ebe9
tandraschko <tandraschko@apache.org>
2015-12-18 13:30:20 +0100

    #965

--- src/main/resources/META-INF/resources/primefaces/inputnumber/2-inputnumber.js
+++ src/main/resources/META-INF/resources/primefaces/inputnumber/2-inputnumber.js
@@ -2,7 +2,7 @@ PrimeFaces.widget.InputNumber = PrimeFaces.widget.BaseWidget.extend({
 
     /**
 	 * Initializes the widget.
-	 * 
+	 *
 	 * @param {object} cfg The widget configuration.
 	 */
     init : function(cfg) {
@@ -11,9 +11,9 @@ PrimeFaces.widget.InputNumber = PrimeFaces.widget.BaseWidget.extend({
         this.inputExternal = $(this.jqId + '_input');
         this.plugOptArray =   cfg.pluginOptions;
         this.valueToRender =  cfg.valueToRender;
-        this.disabled = cfg.disabled;      
-                
-        var $this = this;             
+        this.disabled = cfg.disabled;
+
+        var $this = this;
 
         //bind events if not disabled
         if (this.disabled) {
@@ -21,12 +21,16 @@ PrimeFaces.widget.InputNumber = PrimeFaces.widget.BaseWidget.extend({
             this.inputExternal.addClass("ui-state-disabled");
             this.inputInternal.attr("disabled", "disabled");
         }
-        
+
+        if (this.plugOptArray.vMin) {
+            this.inputExternal.attr('value', this.plugOptArray.vMin);
+        }
+
         //Visual effects
         PrimeFaces.skinInput(this.inputExternal);
 
         // copy the value from the external input to the hidden input
-        this.inputExternal.on('keyup', function(event) { 
+        this.inputExternal.on('keyup', function(event) {
             //filter keys
             var keyCode = event.keyCode;
             if (keyCode === 8 || keyCode === 13 || keyCode === 32
@@ -50,18 +54,18 @@ PrimeFaces.widget.InputNumber = PrimeFaces.widget.BaseWidget.extend({
         });
 
         this.inputExternal.autoNumeric('init', this.plugOptArray);
-                       
+
         if (this.valueToRender !== ""){
-            //set the value to the external input the plugin will format it.                 
-            this.inputExternal.autoNumeric('set',this.valueToRender);                     
+            //set the value to the external input the plugin will format it.
+            this.inputExternal.autoNumeric('set',this.valueToRender);
             //then copie the value to the internal input
-            var cleanVal = $this.inputExternal.autoNumeric('get');              
+            var cleanVal = $this.inputExternal.autoNumeric('get');
             $this.inputInternal.attr('value', cleanVal);
         }else{
             $this.inputInternal.removeAttr('value');
         }
     },
-       
+
     copyValueToHiddenInput : function() {
         var cleanVal = this.inputExternal.autoNumeric('get');
         if (cleanVal !== ""){
@@ -70,7 +74,7 @@ PrimeFaces.widget.InputNumber = PrimeFaces.widget.BaseWidget.extend({
             this.inputInternal.removeAttr('value');
         }
     },
-       
+
     enable : function() {
         this.inputExternal.removeAttr("disabled");
         this.inputExternal.removeClass("ui-state-disabled");
@@ -84,14 +88,14 @@ PrimeFaces.widget.InputNumber = PrimeFaces.widget.BaseWidget.extend({
         this.inputInternal.attr("disabled", "disabled");
         this.disabled = true;
     },
-            
-    setValue : function(value){                
-        this.inputExternal.autoNumeric('set', value);                  
-        var cleanVal = this.inputExternal.autoNumeric('get');              
+
+    setValue : function(value){
+        this.inputExternal.autoNumeric('set', value);
+        var cleanVal = this.inputExternal.autoNumeric('get');
         this.inputInternal.attr('value', cleanVal);
     },
-    
+
     getValue : function(){
-         return this.inputExternal.autoNumeric('get'); 
-    }   
+         return this.inputExternal.autoNumeric('get');
+    }
 });
