bf94072c40f30e3b7d02b938c51976b611539c3d
Marcus Geißler <msgeissler@users.noreply.github.com>
2016-03-08 11:01:09 +0100

    Update core.ajax.js
    Changed console.warn to PrimeFaces.warn
    https://github.com/primefaces/primefaces/pull/1211#issuecomment-193697009

--- src/main/resources/META-INF/resources/primefaces/core/core.ajax.js
+++ src/main/resources/META-INF/resources/primefaces/core/core.ajax.js
@@ -705,7 +705,7 @@ PrimeFaces.ajax = {
             try {
         		window.location.assign(node.getAttribute('url'));
         	} catch (error) {
-        		console.warn('Error redirecting to URL');
+        		PrimeFaces.warn('Error redirecting to URL');
         	}
         },
 
