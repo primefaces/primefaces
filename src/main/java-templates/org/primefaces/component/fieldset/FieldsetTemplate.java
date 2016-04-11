8e232422d4df5a9130d42b15d4d1d192bd097370
Thomas Andraschko <tandraschko@apache.org>
2016-04-11 18:34:43 +0200

    #1098 for Fieldset

--- src/main/java-templates/org/primefaces/component/fieldset/FieldsetTemplate.java
+++ src/main/java-templates/org/primefaces/component/fieldset/FieldsetTemplate.java
@@ -11,6 +11,7 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.HashMap;
 import javax.faces.event.PhaseId;
+import javax.faces.event.BehaviorEvent;
 
     public static final String FIELDSET_CLASS = "ui-fieldset ui-widget ui-widget-content ui-corner-all ui-hidden-container";
     public static final String TOGGLEABLE_FIELDSET_CLASS = FIELDSET_CLASS + " ui-fieldset-toggleable";
@@ -19,7 +20,11 @@ import javax.faces.event.PhaseId;
     public static final String TOGGLER_MINUS_CLASS = "ui-fieldset-toggler ui-icon ui-icon-minusthick";
     public static final String TOGGLER_PLUS_CLASS = "ui-fieldset-toggler ui-icon ui-icon-plusthick";
 
-    private static final Collection<String> EVENT_NAMES = Collections.unmodifiableCollection(Arrays.asList("toggle"));
+    private static final Map<String, Class<? extends BehaviorEvent>> BEHAVIOR_EVENT_MAPPING = Collections.unmodifiableMap(new HashMap<String, Class<? extends BehaviorEvent>>() {{
+        put("toggle", ToggleEvent.class);
+    }});
+
+    private static final Collection<String> EVENT_NAMES = BEHAVIOR_EVENT_MAPPING.keySet();
 
     @Override
     public void queueEvent(FacesEvent event) {
@@ -44,6 +49,11 @@ import javax.faces.event.PhaseId;
     @Override
     public Collection<String> getEventNames() {
         return EVENT_NAMES;
+    }
+
+    @Override
+    public Map<String, Class<? extends BehaviorEvent>> getBehaviorEventMapping() {
+         return BEHAVIOR_EVENT_MAPPING;
     }
 
     @Override
