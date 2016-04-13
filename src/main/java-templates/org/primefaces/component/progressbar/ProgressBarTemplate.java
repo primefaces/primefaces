999ef8103eb86eeb752ddafc8f19e088eecc722d
tandraschko <tandraschko@apache.org>
2016-04-13 11:30:10 +0200

    #1098 for ProgressBar

--- src/main/java-templates/org/primefaces/component/progressbar/ProgressBarTemplate.java
+++ src/main/java-templates/org/primefaces/component/progressbar/ProgressBarTemplate.java
@@ -8,17 +8,34 @@ import javax.faces.event.FacesEvent;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.HashMap;
+import javax.faces.event.BehaviorEvent;
 import javax.faces.event.PhaseId;
 
     public final static String CONTAINER_CLASS = "ui-progressbar ui-widget ui-widget-content ui-corner-all";
     public final static String VALUE_CLASS = "ui-progressbar-value ui-widget-header ui-corner-all";
     public final static String LABEL_CLASS = "ui-progressbar-label";
 
-    private static final Collection<String> EVENT_NAMES = Collections.unmodifiableCollection(Arrays.asList("complete"));
+    private final static String DEFAULT_EVENT = "complete";
+
+    private static final Map<String, Class<? extends BehaviorEvent>> BEHAVIOR_EVENT_MAPPING = Collections.unmodifiableMap(new HashMap<String, Class<? extends BehaviorEvent>>() {{
+        put(DEFAULT_EVENT, null);
+    }});
+
+    private static final Collection<String> EVENT_NAMES = BEHAVIOR_EVENT_MAPPING.keySet();
+
+    @Override
+    public Map<String, Class<? extends BehaviorEvent>> getBehaviorEventMapping() {
+         return BEHAVIOR_EVENT_MAPPING;
+    }
 
     @Override
     public Collection<String> getEventNames() {
         return EVENT_NAMES;
+    }
+
+    @Override
+    public String getDefaultEventName() {
+        return DEFAULT_EVENT;
     }
 
     @Override
