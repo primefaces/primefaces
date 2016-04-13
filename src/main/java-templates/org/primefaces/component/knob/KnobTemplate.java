ad033e4832ba911964a7f10a05c4a758dfc21cf0
tandraschko <tandraschko@apache.org>
2016-04-13 13:31:59 +0200

    #1098 for Knob

--- src/main/java-templates/org/primefaces/component/knob/KnobTemplate.java
+++ src/main/java-templates/org/primefaces/component/knob/KnobTemplate.java
@@ -1,14 +1,30 @@
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Map;
+import java.util.HashMap;
+import javax.faces.event.BehaviorEvent;
 
-    private static final Collection<String> EVENT_NAMES = Collections.unmodifiableCollection(Arrays.asList("change"));
+    private final static String DEFAULT_EVENT = "change";
 
+    private static final Map<String, Class<? extends BehaviorEvent>> BEHAVIOR_EVENT_MAPPING = Collections.unmodifiableMap(new HashMap<String, Class<? extends BehaviorEvent>>() {{
+        put(DEFAULT_EVENT, null);
+    }});
+
+    private static final Collection<String> EVENT_NAMES = BEHAVIOR_EVENT_MAPPING.keySet();
+
+    @Override
+    public Map<String, Class<? extends BehaviorEvent>> getBehaviorEventMapping() {
+         return BEHAVIOR_EVENT_MAPPING;
+    }
+
+    @Override
     public Collection<String> getEventNames() {
-            return EVENT_NAMES;
+        return EVENT_NAMES;
     }
 
     @Override
     public String getDefaultEventName() {
-        return "change";
+        return DEFAULT_EVENT;
     }
+
