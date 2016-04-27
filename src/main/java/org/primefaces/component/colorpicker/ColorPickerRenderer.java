977a2965619a874368e31a660f4603d80550b3b6
tandraschko <tandraschko@apache.org>
2016-04-27 10:00:52 +0200

    #1352

--- src/main/java/org/primefaces/component/colorpicker/ColorPickerRenderer.java
+++ src/main/java/org/primefaces/component/colorpicker/ColorPickerRenderer.java
@@ -76,6 +76,9 @@ public class ColorPickerRenderer extends CoreRenderer {
 		writer.writeAttribute("id", inputId, null);
 		writer.writeAttribute("name", inputId, null);
 		writer.writeAttribute("type", "hidden", null);
+                
+                renderPassThruAttributes(context, colorPicker, null);
+                
 		if(value != null) {
 			writer.writeAttribute("value", value, null);
 		}
