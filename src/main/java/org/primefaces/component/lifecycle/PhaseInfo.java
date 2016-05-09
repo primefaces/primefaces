a1f501de7f7e286a527c2c5718fea3c4434d8351
Thomas Andraschko <tandraschko@apache.org>
2016-05-09 21:33:59 +0200

    Initial import of new lifecycle component

---
+++ src/main/java/org/primefaces/component/lifecycle/PhaseInfo.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2009-2016 PrimeTek.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.primefaces.component.lifecycle;
+
+import java.io.Serializable;
+import javax.faces.event.PhaseId;
+
+public class PhaseInfo implements Serializable {
+    
+    private PhaseId phaseId;
+    private long start;
+    private long end;
+    private long duration;
+
+    public PhaseId getPhaseId() {
+        return phaseId;
+    }
+
+    public void setPhaseId(PhaseId phaseId) {
+        this.phaseId = phaseId;
+    }
+
+    public long getStart() {
+        return start;
+    }
+
+    public void setStart(long start) {
+        this.start = start;
+    }
+
+    public long getEnd() {
+        return end;
+    }
+
+    public void setEnd(long end) {
+        this.end = end;
+    }
+
+    public long getDuration() {
+        return duration;
+    }
+
+    public void setDuration(long duration) {
+        this.duration = duration;
+    }
+}
---
+++ src/main/resources/META-INF/resources/primefaces/lifecycle/lifecycle.js
@@ -0,0 +1,43 @@
+PrimeFaces.widget.Lifecycle = PrimeFaces.widget.BaseWidget.extend({
+	
+	init : function(cfg) {
+		this._super(cfg);
+
+        if (!this.initialized) {
+            this.initialized = true;
+            this.updating = false;
+
+            this.update();
+        }
+
+        var $this = this;
+        $(document).on('pfAjaxSuccess', function() {
+            if (!$this.updating) {
+                setTimeout(function() {
+                    if (!$this.updating) {
+                        $this.update();
+                    }
+                }, 25);
+            }
+        });
+	},
+    
+    update: function() {
+        this.updating = true;
+
+        var $this = this;
+
+        var options = {
+            source: this.id,
+            process: this.id,
+            update: this.id,
+            formId: this.cfg.formId,
+            params: [{ name: this.id + '_getlifecycleinfo', value: true }],
+            oncomplete: function(xhr, status, args) {
+                $this.updating = false;
+            }
+        };
+
+        PrimeFaces.ajax.Request.handle(options);
+    }
+});
