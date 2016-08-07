0a15f21a69ad03165cec8b4a70fb4ba560327c23
mertsincan <sincan.mert@gmail.com>
2016-08-07 23:15:30 +0300

    Fixed #1679

--- src/main/java/org/primefaces/component/megamenu/MegaMenuRenderer.java
+++ src/main/java/org/primefaces/component/megamenu/MegaMenuRenderer.java
@@ -233,11 +233,11 @@ public class MegaMenuRenderer extends BaseMenuRenderer {
             writer.writeAttribute("style", style, null);
         }
         
-        writer.startElement("h3", null);
+        writer.startElement("span", null);
         if(label != null) {
             writer.writeText(label, "value");
         }
-        writer.endElement("h3");
+        writer.endElement("span");
         
         writer.endElement("li");
 
