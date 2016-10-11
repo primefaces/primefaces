2d90f2555603095eed9121049a7e807bc9d5cd64
mertsincan <sincan.mert@gmail.com>
2016-10-11 16:30:36 +0300

    Fixed #1770

--- src/main/resources/META-INF/resources/primefaces/menu/menu.js
+++ src/main/resources/META-INF/resources/primefaces/menu/menu.js
@@ -1230,6 +1230,12 @@ PrimeFaces.widget.MegaMenu = PrimeFaces.widget.BaseWidget.extend({
                         $this.activate(menuitem);
                     }
                 }
+                else {
+                    var href = link.attr('href');
+                    if(href && href !== '#') {
+                        window.location.href = href;
+                    }
+                }
                 
                 e.preventDefault();
             });
