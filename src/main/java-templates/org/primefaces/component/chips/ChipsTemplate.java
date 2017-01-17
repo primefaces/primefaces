da3a1cb5940fb4af9a2009a3290cfa574ce1ed58
mertsincan <sincan.mert@gmail.com>
2017-01-18 00:07:55 +0300

    Fixed #2024

---
+++ src/main/java-templates/org/primefaces/component/chips/ChipsTemplate.java
@@ -0,0 +1,7 @@
+    
+    public final static String STYLE_CLASS = "ui-chips ui-widget";
+    public final static String CONTAINER_CLASS = "ui-chips-container ui-inputfield ui-state-default ui-corner-all";
+    public final static String TOKEN_DISPLAY_CLASS = "ui-chips-token ui-state-active ui-corner-all";
+    public final static String TOKEN_LABEL_CLASS = "ui-chips-token-label";
+    public final static String TOKEN_CLOSE_ICON_CLASS = "ui-chips-token-icon ui-icon ui-icon-close";
+    public final static String TOKEN_INPUT_CLASS = "ui-chips-input-token";
