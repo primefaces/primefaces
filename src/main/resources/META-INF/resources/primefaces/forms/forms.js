84ff4d3fc3a02bfd64e245e265626c317de835c9
melloware <mellowaredev@gmail.com>
2017-04-05 09:01:44 -0400

    Fix #2191 and #1551 HTML5 placeholder attribute to show Watermark on SelectOneMenu.

--- src/main/resources/META-INF/resources/primefaces/forms/forms.js
+++ src/main/resources/META-INF/resources/primefaces/forms/forms.js
@@ -1162,10 +1162,19 @@ PrimeFaces.widget.SelectOneMenu = PrimeFaces.widget.DeferredWidget.extend({
                 this.label.val(displayedLabel);
         }
         else {
-            if(value === '&nbsp;')
-                this.label.html('&nbsp;');
-            else
+            var labelText = this.label.attr('placeholder');
+            if (labelText == null) {
+                labelText = '&nbsp;';
+            }
+            
+            if (value === '&nbsp;') {
+                this.label.addClass('ui-watermark');
+                this.label.html(labelText);
+            }
+            else {
+                this.label.removeClass('ui-watermark');
                 this.label.text(displayedLabel);
+            }
         }
     },
 
