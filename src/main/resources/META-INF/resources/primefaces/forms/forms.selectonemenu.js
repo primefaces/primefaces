b84ffd909aeb6ddafcb5e32df6c2a71f1f603eeb
Thomas Andraschko <tandraschko@apache.org>
2017-08-09 11:37:03 +0200

    #2618

--- src/main/resources/META-INF/resources/primefaces/forms/forms.selectonemenu.js
+++ src/main/resources/META-INF/resources/primefaces/forms/forms.selectonemenu.js
@@ -94,7 +94,7 @@ PrimeFaces.widget.SelectOneMenu = PrimeFaces.widget.DeferredWidget.extend({
         this.input.data(PrimeFaces.CLIENT_ID_DATA, this.id);
 
         //for Screen Readers
-        for(var i = 0; i < this.items.size(); i++) {
+        for(var i = 0; i < this.items.length; i++) {
             this.items.eq(i).attr('id', this.id + '_' + i);
         }
 
@@ -755,7 +755,7 @@ PrimeFaces.widget.SelectOneMenu = PrimeFaces.widget.DeferredWidget.extend({
 	        if(this.cfg.editable) {
 	            this.label.removeAttr('disabled');
 	        }
-	        
+
             this.bindEvents();
             this.bindItemEvents();
     	}
