c769aeaf9056d0ebdf96d28909c795dd2cfe3b99
mertsincan <sincan.mert@gmail.com>
2017-10-09 15:45:57 +0300

    Fixed #2790

---
+++ src/main/java-templates/org/primefaces/component/sidebar/SidebarTemplate.java
@@ -0,0 +1,5 @@
+  
+    public final static String STYLE_CLASS = "ui-sidebar ui-widget ui-widget-content ui-shadow";
+    public static final String TITLE_BAR_CLOSE_CLASS = "ui-sidebar-close ui-corner-all";
+    public static final String CLOSE_ICON_CLASS = "ui-icon ui-icon-closethick";
+    public static final String FULL_BAR_CLASS = "ui-sidebar-full";
