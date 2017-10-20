b796112268c41b21365d029ad45cf9b1dbc8fdc6
mertsincan <sincan.mert@gmail.com>
2017-10-20 09:32:37 +0300

    Fixed #2843

--- src/main/resources/META-INF/resources/primefaces/datatable/datatable.js
+++ src/main/resources/META-INF/resources/primefaces/datatable/datatable.js
@@ -939,8 +939,16 @@ PrimeFaces.widget.DataTable = PrimeFaces.widget.DeferredWidget.extend({
         if(this.cfg.virtualScroll) {
             var row = this.bodyTable.children('tbody').children('tr.ui-widget-content');
             if(row) {
+                var hasEmptyMessage = row.eq(0).hasClass('ui-datatable-empty-message'),
+                scrollLimit = $this.cfg.scrollLimit;
+
+                if(hasEmptyMessage) {
+                    scrollLimit = 1;
+                    $this.bodyTable.css('top', '0px');
+                }
+                
                 this.rowHeight = row.outerHeight();
-                this.scrollBody.children('div').css('height', parseFloat((this.cfg.scrollLimit * this.rowHeight) + 'px'));
+                this.scrollBody.children('div').css('height', parseFloat((scrollLimit * this.rowHeight + 1) + 'px'));
             }
         }
 
