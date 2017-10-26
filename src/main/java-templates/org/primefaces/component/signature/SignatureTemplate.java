e5396080fd3ecb6d28209a2ea108b6b49aeafc6c
melloware <mellowaredev@gmail.com>
2017-10-26 09:14:53 -0400

    Fix #2102: Signature add PF styles.

--- src/main/java-templates/org/primefaces/component/signature/SignatureTemplate.java
+++ src/main/java-templates/org/primefaces/component/signature/SignatureTemplate.java
@@ -2,6 +2,9 @@ import javax.el.ValueExpression;
 import javax.faces.context.FacesContext;
 import org.primefaces.component.api.UITree;
 
+public static final String STYLE_CLASS = "ui-inputfield ui-widget ui-state-default ui-corner-all";
+public static final String READONLY_STYLE_CLASS = "ui-widget ui-widget-content ui-corner-all";
+
     public void processUpdates(FacesContext context) {
         super.processUpdates(context);
         String base64Value = this.getBase64Value();
@@ -13,4 +16,14 @@ import org.primefaces.component.api.UITree;
                 getStateHelper().put(PropertyKeys.base64Value, null);
             }
         }
+    }
+    
+    public String resolveStyleClass() {
+        String styleClass = STYLE_CLASS; 
+        
+        if(isReadonly()) {
+            styleClass = READONLY_STYLE_CLASS;
+        } 
+        
+        return styleClass;
     }
