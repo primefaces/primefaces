b63daf19665a3eaf68dd420f6c60a6dada340d52
jxmai <maij@myumanitoba.ca>
2017-11-18 22:49:54 -0600

    cosmetic

--- src/main/resources/META-INF/resources/primefaces/texteditor/texteditor.js
+++ src/main/resources/META-INF/resources/primefaces/texteditor/texteditor.js
@@ -10,154 +10,154 @@
  * PrimeFaces TextEditor Widget
  */
 PrimeFaces.widget.TextEditor = PrimeFaces.widget.DeferredWidget.extend({
-    
-        toolbarTemplate: '<div class="ui-editor-toolbar">' +
-                    '<span class="ql-formats">' +
-                        '<select class="ql-font"></select>' +
-                        '<select class="ql-size"></select>' +
-                    '</span>' +
-                    '<span class="ql-formats">' +
-                        '<button class="ql-bold"></button>' +
-                        '<button class="ql-italic"></button>' +
-                        '<button class="ql-underline"></button>' +
-                        '<button class="ql-strike"></button>' +
-                    '</span>' +
-                    '<span class="ql-formats">' +
-                        '<select class="ql-color"></select>' +
-                        '<select class="ql-background"></select>' +
-                    '</span>' +
-                    '<span class="ql-formats">' +
-                        '<button class="ql-script" value="sub"></button>' +
-                        '<button class="ql-script" value="super"></button>' +
-                    '</span>' +
-                    '<span class="ql-formats">' +
-                        '<button class="ql-header" value="1"></button>' +
-                        '<button class="ql-header" value="2"></button>' +
-                        '<button class="ql-blockquote"></button>' +
-                        '<button class="ql-code-block"></button>' +
-                    '</span>' +
-                    '<span class="ql-formats">' +
-                        '<button class="ql-list" value="ordered"></button>' +
-                        '<button class="ql-list" value="bullet"></button>' +
-                        '<button class="ql-indent" value="-1"></button>' +
-                        '<button class="ql-indent" value="+1"></button>' +
-                    '</span>' +
-                    '<span class="ql-formats">' +
-                        '<button class="ql-direction" value="rtl"></button>' +
-                        '<select class="ql-align"></select>' +
-                    '</span>' +
-                    '<span class="ql-formats">' +
-                        '<button class="ql-link"></button>' +
-                        '<button class="ql-image"></button>' +
-                        '<button class="ql-video"></button>' +
-                    '</span>' +
-                    '<span class="ql-formats">' +
-                        '<button class="ql-clean"></button>' +
-                    '</span>' +
-                '</div>',
-    
-        init: function(cfg) {
-            this._super(cfg);
-    
-            this.editorContainer = $(this.jqId + '_editor');
-            this.input = this.jq.children('input');
-            this.renderDeferred();
-        },
-    
-        _render: function() {
-            var $this = this;
-    
-            //toolbar
-            this.toolbar = $(this.jqId + '_toolbar');
-            if(!this.toolbar.length && this.cfg.toolbarVisible) {
-                this.jq.prepend(this.toolbarTemplate);
-                this.toolbar = this.jq.children('.ui-editor-toolbar')
-                this.toolbar.attr('id', this.id + '_toolbar');
+
+    toolbarTemplate: '<div class="ui-editor-toolbar">' +
+                '<span class="ql-formats">' +
+                    '<select class="ql-font"></select>' +
+                    '<select class="ql-size"></select>' +
+                '</span>' +
+                '<span class="ql-formats">' +
+                    '<button class="ql-bold"></button>' +
+                    '<button class="ql-italic"></button>' +
+                    '<button class="ql-underline"></button>' +
+                    '<button class="ql-strike"></button>' +
+                '</span>' +
+                '<span class="ql-formats">' +
+                    '<select class="ql-color"></select>' +
+                    '<select class="ql-background"></select>' +
+                '</span>' +
+                '<span class="ql-formats">' +
+                    '<button class="ql-script" value="sub"></button>' +
+                    '<button class="ql-script" value="super"></button>' +
+                '</span>' +
+                '<span class="ql-formats">' +
+                    '<button class="ql-header" value="1"></button>' +
+                    '<button class="ql-header" value="2"></button>' +
+                    '<button class="ql-blockquote"></button>' +
+                    '<button class="ql-code-block"></button>' +
+                '</span>' +
+                '<span class="ql-formats">' +
+                    '<button class="ql-list" value="ordered"></button>' +
+                    '<button class="ql-list" value="bullet"></button>' +
+                    '<button class="ql-indent" value="-1"></button>' +
+                    '<button class="ql-indent" value="+1"></button>' +
+                '</span>' +
+                '<span class="ql-formats">' +
+                    '<button class="ql-direction" value="rtl"></button>' +
+                    '<select class="ql-align"></select>' +
+                '</span>' +
+                '<span class="ql-formats">' +
+                    '<button class="ql-link"></button>' +
+                    '<button class="ql-image"></button>' +
+                    '<button class="ql-video"></button>' +
+                '</span>' +
+                '<span class="ql-formats">' +
+                    '<button class="ql-clean"></button>' +
+                '</span>' +
+            '</div>',
+
+    init: function(cfg) {
+        this._super(cfg);
+
+        this.editorContainer = $(this.jqId + '_editor');
+        this.input = this.jq.children('input');
+        this.renderDeferred();
+    },
+
+    _render: function() {
+        var $this = this;
+
+        //toolbar
+        this.toolbar = $(this.jqId + '_toolbar');
+        if(!this.toolbar.length && this.cfg.toolbarVisible) {
+            this.jq.prepend(this.toolbarTemplate);
+            this.toolbar = this.jq.children('.ui-editor-toolbar')
+            this.toolbar.attr('id', this.id + '_toolbar');
+        }
+
+        //configuration
+        if(this.cfg.height) {
+            this.editorContainer.height(this.cfg.height);
+        }
+
+        this.cfg.theme = 'snow';
+        this.cfg.modules = {
+            toolbar: this.cfg.toolbarVisible ? PrimeFaces.escapeClientId(this.id + '_toolbar') : false
+        };
+
+        //initialize
+        this.editor = new Quill(PrimeFaces.escapeClientId(this.id) + '_editor', this.cfg);
+
+        var events = ["keyup", "keydown", "click", "dblclick", "keypress", "mousedown", "mousemove", "mouseout",
+                        "mouseover", "mouseup"];
+
+        $.each(events, function(index, value) {
+            $this.registerEvent(value);
+        });
+
+        //set initial value
+        this.input.val(this.getEditorValue());
+
+        //update input on change
+        this.editor.on('text-change', function(delta, oldDelta, source) {
+            $this.input.val($this.getEditorValue());
+            if($this.cfg.behaviors) {
+                var changeBehavior = $this.cfg.behaviors['change'];
+                if(changeBehavior) {
+                    changeBehavior.call($this);
+                }
             }
-    
-            //configuration
-            if(this.cfg.height) {
-                this.editorContainer.height(this.cfg.height);
-            }
-    
-            this.cfg.theme = 'snow';
-            this.cfg.modules = {
-                toolbar: this.cfg.toolbarVisible ? PrimeFaces.escapeClientId(this.id + '_toolbar') : false
-            };
-    
-            //initialize
-            this.editor = new Quill(PrimeFaces.escapeClientId(this.id) + '_editor', this.cfg);
-    
-            var events = ["keyup", "keydown", "click", "dblclick", "keypress", "mousedown", "mousemove", "mouseout",
-                          "mouseover", "mouseup"];
-    
-            $.each(events, function(index, value) {
-                $this.registerEvent(value);
-            });
-    
-            //set initial value
-            this.input.val(this.getEditorValue());
-    
-            //update input on change
-            this.editor.on('text-change', function(delta, oldDelta, source) {
-                $this.input.val($this.getEditorValue());
-                if($this.cfg.behaviors) {
-                    var changeBehavior = $this.cfg.behaviors['change'];
+        });
+        this.editor.on('selection-change', function(range, oldRange, source) {
+            if(range && !oldRange) {
+                if($this.cfg.behaviors && $this.cfg.behaviors["focus"]) {
+                    var changeBehavior = $this.cfg.behaviors["focus"];
                     if(changeBehavior) {
                         changeBehavior.call($this);
                     }
                 }
-            });
-            this.editor.on('selection-change', function(range, oldRange, source) {
-               if(range && !oldRange) {
-                   if($this.cfg.behaviors && $this.cfg.behaviors["focus"]) {
-                       var changeBehavior = $this.cfg.behaviors["focus"];
-                       if(changeBehavior) {
-                           changeBehavior.call($this);
-                       }
-                   }
-               }
-               if(!range && oldRange) {
-                   if($this.cfg.behaviors && $this.cfg.behaviors["blur"]) {
-                       var changeBehavior = $this.cfg.behaviors["blur"];
-                       if(changeBehavior) {
-                           changeBehavior.call($this);
-                       }
-                   }
-               }
-               if(range && oldRange) {
-                   if($this.cfg.behaviors && $this.cfg.behaviors["select"]) {
-                      var changeBehavior = $this.cfg.behaviors["select"];
-                      if(changeBehavior) {
-                          changeBehavior.call($this);
-                      }
-                   }
-               }
-            });
-    
-        },
-    
-        getEditorValue: function() {
-            var html = $(this.editorContainer[0]).find('p').text();
-            var value = (html == '<p><br></p>') ? '' : html;
-    
-            return value;
-        },
-    
-        clear: function() {
-            this.editor.setText('');
-        },
-    
-        registerEvent: function(event) {
-            var $this = this;
-            if(this.cfg.behaviors && this.cfg.behaviors[event]) {
-                this.editorContainer.on(event, function () {
-                    var changeBehavior = $this.cfg.behaviors[event];
+            }
+            if(!range && oldRange) {
+                if($this.cfg.behaviors && $this.cfg.behaviors["blur"]) {
+                    var changeBehavior = $this.cfg.behaviors["blur"];
                     if(changeBehavior) {
                         changeBehavior.call($this);
                     }
-                });
+                }
             }
+            if(range && oldRange) {
+                if($this.cfg.behaviors && $this.cfg.behaviors["select"]) {
+                    var changeBehavior = $this.cfg.behaviors["select"];
+                    if(changeBehavior) {
+                        changeBehavior.call($this);
+                    }
+                }
+            }
+        });
+
+    },
+
+    getEditorValue: function() {
+        var html = $(this.editorContainer[0]).find('p').text();
+        var value = (html == '<p><br></p>') ? '' : html;
+
+        return value;
+    },
+
+    clear: function() {
+        this.editor.setText('');
+    },
+
+    registerEvent: function(event) {
+        var $this = this;
+        if(this.cfg.behaviors && this.cfg.behaviors[event]) {
+            this.editorContainer.on(event, function () {
+                var changeBehavior = $this.cfg.behaviors[event];
+                if(changeBehavior) {
+                    changeBehavior.call($this);
+                }
+            });
         }
-    
-    });
+    }
+
+});
