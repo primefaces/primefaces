ece24e7dbf64999e6cceebf64bd0dd8289d2a0db
melloware <mellowaredev@gmail.com>
2017-11-28 10:43:32 -0500

    Fix #3014: Treetable colResize event with Firefox.

--- src/main/resources/META-INF/resources/primefaces/treetable/treetable.js
+++ src/main/resources/META-INF/resources/primefaces/treetable/treetable.js
@@ -1191,8 +1191,8 @@ PrimeFaces.widget.TreeTable = PrimeFaces.widget.DeferredWidget.extend({
                     params: [
                         {name: $this.id + '_colResize', value: true},
                         {name: $this.id + '_columnId', value: columnHeader.attr('id')},
-                        {name: $this.id + '_width', value: columnHeader.width()},
-                        {name: $this.id + '_height', value: columnHeader.height()}
+                        {name: $this.id + '_width', value: parseInt(columnHeader.width())},
+                        {name: $this.id + '_height', value: parseInt(columnHeader.height())}
                     ]
                 }
                 
