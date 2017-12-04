db1ccbbc9d6ae3232f15e0a26eea4df41d136f91
melloware <mellowaredev@gmail.com>
2017-12-04 08:00:00 -0500

    Fix #3005: Inplace use emptyLabel if label="" or null.

--- src/main/java/org/primefaces/component/inplace/InplaceRenderer.java
+++ src/main/java/org/primefaces/component/inplace/InplaceRenderer.java
@@ -113,7 +113,7 @@ public class InplaceRenderer extends CoreRenderer {
         String label = inplace.getLabel();
         String emptyLabel = inplace.getEmptyLabel();
 
-        if (label != null && !isValueBlank(label)) {
+        if (!isValueBlank(label)) {
             return label;
         }
         else {
