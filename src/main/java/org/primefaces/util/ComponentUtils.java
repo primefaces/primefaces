50143c2cb932020e7b67dc73ee04dbe6bd478edc
Sébastien Lepage <sebastien.lepage@outlook.com>
2017-12-19 18:47:45 +0100

    isValueBlank: better implementation
    Even better would to reuse the impl from StringUtils of Apache (but not sure if we're allowed due to license)
    ```java
        /**
         * <p>Checks if a CharSequence is whitespace, empty ("") or null.</p>
         *
         * <pre>
         * StringUtils.isBlank(null)      = true
         * StringUtils.isBlank("")        = true
         * StringUtils.isBlank(" ")       = true
         * StringUtils.isBlank("bob")     = false
         * StringUtils.isBlank("  bob  ") = false
         * </pre>
         *
         * @param cs  the CharSequence to check, may be null
         * @return {@code true} if the CharSequence is null, empty or whitespace
         * @since 2.0
         * @since 3.0 Changed signature from isBlank(String) to isBlank(CharSequence)
         */
        public static boolean isBlank(final CharSequence cs) {
            int strLen;
            if (cs == null || (strLen = cs.length()) == 0) {
                return true;
            }
            for (int i = 0; i < strLen; i++) {
                if (Character.isWhitespace(cs.charAt(i)) == false) {
                    return false;
                }
            }
            return true;
        }
    ```

--- src/main/java/org/primefaces/util/ComponentUtils.java
+++ src/main/java/org/primefaces/util/ComponentUtils.java
@@ -189,11 +189,7 @@ public class ComponentUtils {
     
 
     public static boolean isValueBlank(String value) {
-        if (value == null) {
-            return true;
-        }
-
-        return value.trim().equals("");
+        return value == null || Constants.EMPTY_STRING.equals(value.trim());
     }
 
     public static boolean isRTL(FacesContext context, RTLAware component) {
