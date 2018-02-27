a8158cf8f5510954d381a6dabc548919530333d4
cnsgithub <cnsgithub@users.noreply.github.com>
2018-02-27 07:56:05 +0100

    added overloaded iterators allowing for sorting and filtering; applied checkstyle rules

--- src/main/java/org/primefaces/model/LazyDataModel.java
+++ src/main/java/org/primefaces/model/LazyDataModel.java
@@ -23,7 +23,6 @@ import javax.faces.context.FacesContext;
 import javax.faces.model.DataModel;
 import javax.faces.model.DataModelEvent;
 import javax.faces.model.DataModelListener;
-import javax.validation.constraints.Null;
 
 /**
  * Custom lazy loading DataModel to deal with huge datasets
@@ -142,95 +141,11 @@ public abstract class LazyDataModel<T> extends DataModel<T> implements Selectabl
     }
 
     public Iterator<T> iterator(String sortField, SortOrder sortOrder, Map<String, Object> filters) {
-    	return new LazyDataModelIterator<T>(this, sortField, sortOrder, filters);
+        return new LazyDataModelIterator<T>(this, sortField, sortOrder, filters);
     }
 
-	public Iterator<T> iterator(List<SortMeta> multiSortMeta, Map<String, Object> filters) {
-		return new LazyDataModelIterator<T>(this, multiSortMeta, filters);
-	}
-    
-    private static final class LazyDataModelIterator<T> implements Iterator<T> {
-
-        private LazyDataModel<T> model;
-        private int index;
-        private Map<Integer, List<T>> pages;
-
-	    @Null
-	    private String sortField;
-	    @Null
-	    private SortOrder sortOrder;
-
-	    @Null
-        private List<SortMeta> multiSortMeta;
-        
-	    @Null
-        private Map<String, Object> filters;
-        
-        LazyDataModelIterator(LazyDataModel<T> model) {
-            this.model = model;
-            this.index = -1;
-            this.pages = new HashMap<Integer, List<T>>();
-        }
-
-	    LazyDataModelIterator(LazyDataModel<T> model, String sortField, SortOrder sortOrder, Map<String, Object> filters) {
-		    this(model);
-		    this.sortField = sortField;
-		    this.sortOrder = sortOrder;
-		    this.filters = filters;
-	    }
-        
-        LazyDataModelIterator(LazyDataModel<T> model, List<SortMeta> multiSortMeta, Map<String, Object> filters) {
-        	this(model);
-            this.multiSortMeta = multiSortMeta;
-            this.filters = filters;
-        }
-        
-        @Override
-        public boolean hasNext() {
-            int nextIndex = index + 1;
-            int pageNo = nextIndex / model.getPageSize();
-            
-            if (!pages.containsKey(pageNo)) {
-                List<T> page;
-                
-                if (sortField != null || sortOrder != null) {
-                	page = model.load(nextIndex, model.getPageSize(), sortField, sortOrder, filters); 
-                }
-                else {
-	                page = model.load(nextIndex, model.getPageSize(), multiSortMeta, filters);
-                }
-                
-                if (page == null || page.isEmpty()) {
-                    return false;
-                }
-                pages.remove(pageNo - 1);
-                pages.put(pageNo, page);
-            }
-            
-            int pageIndex = nextIndex % model.getPageSize();
-            if (pageIndex < pages.get(pageNo).size()) {
-                return true;
-            }
-            
-            return false;
-        }
-
-        @Override
-        public T next() {
-            index++;
-            int pageNo = index / model.getPageSize();
-            int pageIndex = index % model.getPageSize();
-            List<T> page = pages.get(pageNo);
-            if (page == null || pageIndex >= page.size()) {
-                throw new NoSuchElementException();
-            }
-            return page.get(pageIndex);
-        }
-
-        @Override
-        public void remove() {
-            throw new UnsupportedOperationException();
-        }
-
+    public Iterator<T> iterator(List<SortMeta> multiSortMeta, Map<String, Object> filters) {
+        return new LazyDataModelIterator<T>(this, multiSortMeta, filters);
     }
+
 }
