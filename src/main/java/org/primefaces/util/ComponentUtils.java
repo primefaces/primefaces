52dcc6773f43d27bc7f43fb17ff446b4e8ca4d64
Sébastien Lepage <sebastien.lepage@outlook.com>
2018-03-07 10:40:47 +0100

    #3427 - remove ComponentUtils.equals in favor of Objects.equals

--- src/main/java/org/primefaces/util/ComponentUtils.java
+++ src/main/java/org/primefaces/util/ComponentUtils.java
@@ -666,14 +666,4 @@ public class ComponentUtils {
         }
         return false;
     }
-
-    public static boolean equals(Object object1, Object object2) {
-        if (object1 == object2) {
-            return true;
-        }
-        if ((object1 == null) || (object2 == null)) {
-            return false;
-        }
-        return object1.equals(object2);
-    }
 }
