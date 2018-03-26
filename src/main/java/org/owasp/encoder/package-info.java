372d2961580c4559e83deac3f9e53e4b09871f81
cnsgithub <cnsgithub@users.noreply.github.com>
2018-03-26 08:04:47 +0200

    added package-info to denote version of copied owasp-java-encoder files (part of #3520)

---
+++ src/main/java/org/owasp/encoder/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * <p>This package has been introduced to fix https://github.com/primefaces/primefaces/issues/3520</p>
+ * <p>All classes contained in this package have been copied from https://github.com/OWASP/owasp-java-encoder and should be checked for updates in a regular manner.</p>
+ * 
+ * @version 1.2.1
+ */
+package org.owasp.encoder;
