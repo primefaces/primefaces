52d78424ba215176705848ff1f7c39f1fe16de65
cnsgithub <cnsgithub@users.noreply.github.com>
2018-03-26 09:02:57 +0200

    reference owasp-java-encoder javadoc to get detailed information on method usage (part of #3520)

--- src/main/java/org/primefaces/util/EscapeUtils.java
+++ src/main/java/org/primefaces/util/EscapeUtils.java
@@ -26,71 +26,122 @@ public final class EscapeUtils {
     /** no instances */
     private EscapeUtils() {
     }
-    
+
+    /**
+     * @see Encode#forHtml(String) 
+     */
     public static String forHtml(String input) {
         return Encode.forHtml(input);
     }
 
+    /**
+     * @see Encode#forHtmlContent(String)
+     */
     public static String forHtmlContent(String input) {
         return Encode.forHtmlContent(input);
     }
 
+    /**
+     * @see Encode#forHtmlAttribute(String)
+     */
     public static String forHtmlAttribute(String input) {
         return Encode.forHtmlAttribute(input);
     }
 
+    /**
+     * @see Encode#forHtmlUnquotedAttribute(String) 
+     */
     public static String forHtmlUnquotedAttribute(String input) {
         return Encode.forHtmlUnquotedAttribute(input);
     }
 
+    /**
+     * @see Encode#forCssString(String) 
+     */
     public static String forCssString(String input) {
         return Encode.forCssString(input);
     }
 
+    /**
+     * @see Encode#forCssUrl(String) 
+     */
     public static String forCssUrl(String input) {
         return Encode.forCssUrl(input);
     }
 
+    /**
+     * @see Encode#forUriComponent(String) 
+     */
     public static String forUriComponent(String input) {
         return Encode.forUriComponent(input);
     }
 
+    /**
+     * @see Encode#forXml(String) 
+     */
     public static String forXml(String input) {
         return Encode.forXml(input);
     }
 
+    /**
+     * @see Encode#forXmlContent(String) 
+     */
     public static String forXmlContent(String input) {
         return Encode.forXmlContent(input);
     }
 
+    /**
+     * @see Encode#forXmlAttribute(String) 
+     */
     public static String forXmlAttribute(String input) {
         return Encode.forXmlAttribute(input);
     }
 
+    /**
+     * @see Encode#forXmlComment(String) 
+     */
     public static String forXmlComment(String input) {
         return Encode.forXmlComment(input);
     }
 
+    /**
+     * @see Encode#forCDATA(String) 
+     */
     public static String forCDATA(String input) {
         return Encode.forCDATA(input);
     }
 
+    /**
+     * @see Encode#forJava(String) 
+     */
     public static String forJava(String input) {
         return Encode.forJava(input);
     }
 
+    /**
+     * @see Encode#forJavaScript(String) 
+     */
     public static String forJavaScript(String input) {
         return Encode.forJavaScript(input);
     }
 
+    /**
+     * @see Encode#forJavaScriptAttribute(String) 
+     */
     public static String forJavaScriptAttribute(String input) {
         return Encode.forJavaScriptAttribute(input);
     }
 
+    /**
+     * @see Encode#forJavaScriptBlock(String) 
+     */
     public static String forJavaScriptBlock(String input) {
         return Encode.forJavaScriptBlock(input);
     }
 
+    /**
+     * @see Encode#forJavaScriptSource(String) 
+     */
     public static String forJavaScriptSource(String input) {
         return Encode.forJavaScriptSource(input);
     }
