264a02743a796a665c3d74da69a01c18f1ee68c3
cnsgithub <cnsgithub@users.noreply.github.com>
2018-04-10 12:32:55 +0200

    fixes #3566 - asterisk not handled correctly in inputMask

--- src/main/java/org/primefaces/component/inputmask/InputMaskRenderer.java
+++ src/main/java/org/primefaces/component/inputmask/InputMaskRenderer.java
@@ -70,7 +70,7 @@ public class InputMaskRenderer extends InputRenderer {
         // Escape regex metacharacters first
         String regex = REGEX_METACHARS_PATTERN.matcher(mask).replaceAll("\\\\$0");
         
-        regex = regex.replace("a", "[A-Za-z]").replace("9", "[0-9]").replace("*", "[A-Za-z0-9]");
+        regex = regex.replace("a", "[A-Za-z]").replace("9", "[0-9]").replace("\\*", "[A-Za-z0-9]");
         int optionalPos = regex.indexOf("\\?");
         if (optionalPos != -1) {
             regex = regex.substring(0, optionalPos) + "(" + regex.substring(optionalPos + 2) + ")?";
