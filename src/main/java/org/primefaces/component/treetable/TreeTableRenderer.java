c4626f266d3a0d17ee40a1f863510ba3c1871a9f
Thomas Andraschko <tandraschko@apache.org>
2018-06-11 22:36:35 +0200

    refactored

--- src/main/java/org/primefaces/component/treetable/TreeTableRenderer.java
+++ src/main/java/org/primefaces/component/treetable/TreeTableRenderer.java
@@ -274,10 +274,10 @@ public class TreeTableRenderer extends DataRenderer {
         boolean hasPaginator = tt.isPaginator();
 
         if (root == null) {
-            throw new FacesException("treeTable's value must be null. ClientId: " + tt.getClientId());
+            throw new FacesException("treeTable's value must be null. ClientId: " + clientId);
         }
         if (!(root instanceof TreeNode)) {
-            throw new FacesException("treeTable's value must be an instance of " + TreeNode.class.getName() + ". ClientId: " + tt.getClientId());
+            throw new FacesException("treeTable's value must be an instance of " + TreeNode.class.getName() + ". ClientId: " + clientId);
         }
 
         if (hasPaginator) {
