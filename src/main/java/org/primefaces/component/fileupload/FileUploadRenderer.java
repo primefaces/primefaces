301d9ce6775bd2a055547fc783a44101cd1c06fe
mertsincan <sincan.mert@gmail.com>
2018-06-12 16:20:46 +0300

    Fixed #3775

--- src/main/java/org/primefaces/component/fileupload/FileUploadRenderer.java
+++ src/main/java/org/primefaces/component/fileupload/FileUploadRenderer.java
@@ -97,6 +97,7 @@ public class FileUploadRenderer extends CoreRenderer {
                     .attr("previewWidth", fileUpload.getPreviewWidth(), 80)
                     .attr("disabled", fileUpload.isDisabled(), false)
                     .attr("sequentialUploads", fileUpload.isSequential(), false)
+                    .nativeAttr("onAdd", fileUpload.getOnAdd())
                     .callback("onstart", "function()", fileUpload.getOnstart())
                     .callback("onerror", "function()", fileUpload.getOnerror())
                     .callback("oncomplete", "function(args)", fileUpload.getOncomplete());
