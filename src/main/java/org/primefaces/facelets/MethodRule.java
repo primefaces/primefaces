68fbf8a9768664e07d85804a1e35c89fc04bf1ec
Thomas Andraschko <tandraschko@apache.org>
2018-06-15 17:14:08 +0200

    #3786

--- src/main/java/org/primefaces/facelets/MethodRule.java
+++ src/main/java/org/primefaces/facelets/MethodRule.java
@@ -15,19 +15,13 @@
  */
 package org.primefaces.facelets;
 
-import java.io.Serializable;
-
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-import javax.el.ELException;
 import javax.el.MethodExpression;
-
-import javax.faces.context.FacesContext;
-import javax.faces.el.EvaluationException;
+import javax.faces.FacesException;
 import javax.faces.el.MethodBinding;
 
-import javax.faces.el.MethodNotFoundException;
 import javax.faces.view.facelets.FaceletContext;
 import javax.faces.view.facelets.MetaRule;
 import javax.faces.view.facelets.Metadata;
@@ -61,12 +55,9 @@ public class MethodRule extends MetaRule {
         if (false == name.equals(this.methodName)) {
             return null;
         }
-        
+
         if (MethodBinding.class.equals(meta.getPropertyType(name))) {
-            Method method = meta.getWriteMethod(name);
-            if (method != null) {
-                return new MethodBindingMetadata(method, attribute, this.returnTypeClass, this.params);
-            }
+            throw new FacesException(MethodBinding.class.getName() + " should not be used anymore!");
         }
         else if (MethodExpression.class.equals(meta.getPropertyType(name))) {
             Method method = meta.getWriteMethod(name);
@@ -76,36 +67,6 @@ public class MethodRule extends MetaRule {
         }
 
         return null;
-    }
-
-    private static class MethodBindingMetadata extends Metadata {
-
-        private final Method method;
-        private final TagAttribute attribute;
-        private final Class[] paramList;
-        private final Class returnType;
-
-        public MethodBindingMetadata(Method method, TagAttribute attribute, Class returnType, Class[] paramList) {
-            this.method = method;
-            this.attribute = attribute;
-            this.paramList = paramList;
-            this.returnType = returnType;
-        }
-
-        @Override
-        public void applyMetadata(FaceletContext ctx, Object instance) {
-            MethodExpression expr = this.attribute.getMethodExpression(ctx, this.returnType, this.paramList);
-
-            try {
-                this.method.invoke(instance, new Object[]{new LegacyMethodBinding(expr)});
-            }
-            catch (InvocationTargetException e) {
-                throw new TagAttributeException(this.attribute, e.getCause());
-            }
-            catch (Exception e) {
-                throw new TagAttributeException(this.attribute, e);
-            }
-        }
     }
 
     private static class MethodExpressionMetadata extends Metadata {
@@ -138,45 +99,4 @@ public class MethodRule extends MetaRule {
         }
     }
 
-    private static class LegacyMethodBinding extends MethodBinding implements Serializable {
-
-        private static final long serialVersionUID = 1L;
-
-        private final MethodExpression m;
-
-        public LegacyMethodBinding(MethodExpression m) {
-            this.m = m;
-        }
-
-        @Override
-        public Class getType(FacesContext context) throws MethodNotFoundException {
-            try {
-                return m.getMethodInfo(context.getELContext()).getReturnType();
-            }
-            catch (javax.el.MethodNotFoundException e) {
-                throw new MethodNotFoundException(e.getMessage(), e.getCause());
-            }
-            catch (ELException e) {
-                throw new EvaluationException(e.getMessage(), e.getCause());
-            }
-        }
-
-        @Override
-        public Object invoke(FacesContext context, Object[] params) throws EvaluationException, MethodNotFoundException {
-            try {
-                return m.invoke(context.getELContext(), params);
-            }
-            catch (javax.el.MethodNotFoundException e) {
-                throw new MethodNotFoundException(e.getMessage(), e.getCause());
-            }
-            catch (ELException e) {
-                throw new EvaluationException(e.getMessage(), e.getCause());
-            }
-        }
-
-        @Override
-        public String getExpressionString() {
-            return m.getExpressionString();
-        }
-    }
 }
