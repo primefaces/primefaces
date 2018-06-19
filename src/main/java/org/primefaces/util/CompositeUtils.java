3c12f964ac4a15f236d95ce9707e2230f1f67f43
Jiří Stuchlík <jistuchlik@csob.cz>
2018-06-19 12:04:52 +0200

    Fix #3097

--- src/main/java/org/primefaces/util/CompositeUtils.java
+++ src/main/java/org/primefaces/util/CompositeUtils.java
@@ -19,6 +19,7 @@ import java.beans.BeanInfo;
 import java.util.List;
 import javax.faces.FacesException;
 import javax.faces.component.ContextCallback;
+import javax.faces.component.EditableValueHolder;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.view.AttachedObjectTarget;
@@ -39,6 +40,11 @@ public class CompositeUtils {
      */
     public static void invokeOnDeepestEditableValueHolder(FacesContext context, UIComponent composite,
             final ContextCallback callback) {
+        if (composite instanceof EditableValueHolder) {
+            callback.invokeContextCallback(context, composite);
+            return;
+        }
+
         BeanInfo info = (BeanInfo) composite.getAttributes().get(UIComponent.BEANINFO_KEY);
         List<AttachedObjectTarget> targets = (List<AttachedObjectTarget>) info.getBeanDescriptor()
                 .getValue(AttachedObjectTarget.ATTACHED_OBJECT_TARGETS_KEY);
@@ -64,15 +70,10 @@ public class CompositeUtils {
                     final UIComponent child = childs.get(0);
 
                     composite.invokeOnComponent(context, composite.getClientId(context), new ContextCallback() {
-                        
+
                         @Override
                         public void invokeContextCallback(FacesContext context, UIComponent target) {
-                            if (isComposite(child)) {
-                                invokeOnDeepestEditableValueHolder(context, child, callback);
-                            }
-                            else {
-                                callback.invokeContextCallback(context, child);
-                            }
+                            invokeOnDeepestEditableValueHolder(context, child, callback);
                         }
                     });
                 }
