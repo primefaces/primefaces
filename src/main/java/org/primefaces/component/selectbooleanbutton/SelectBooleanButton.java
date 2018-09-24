cf7430bb2dbd9cceca75a764cbd37eb79b82733c
Wilber Saca <wismar25@hotmail.com>
2018-09-24 09:51:48 -0500

    Add dependency to avoid error with $.ui.keyCode

--- src/main/java/org/primefaces/component/selectbooleanbutton/SelectBooleanButton.java
+++ src/main/java/org/primefaces/component/selectbooleanbutton/SelectBooleanButton.java
@@ -24,6 +24,7 @@ import org.primefaces.util.LangUtils;
 @ResourceDependencies({
         @ResourceDependency(library = "primefaces", name = "components.css"),
         @ResourceDependency(library = "primefaces", name = "jquery/jquery.js"),
+        @ResourceDependency(library = "primefaces", name = "jquery/jquery-plugins.js"),
         @ResourceDependency(library = "primefaces", name = "core.js"),
         @ResourceDependency(library = "primefaces", name = "components.js")
 })
