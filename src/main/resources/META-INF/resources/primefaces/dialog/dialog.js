494aba48d26cd9eb291d1ce77f79934c15d4c584
mertsincan <sincan.mert@gmail.com>
2018-10-03 10:56:18 +0300

    cosmetic

--- src/main/resources/META-INF/resources/primefaces/dialog/dialog.js
+++ src/main/resources/META-INF/resources/primefaces/dialog/dialog.js
@@ -97,7 +97,7 @@ PrimeFaces.widget.Dialog = PrimeFaces.widget.DynamicOverlayWidget.extend({
 
         var margin = this.jq.outerHeight(true) - this.jq.outerHeight();
         var headerHeight = this.titlebar.outerHeight(true);
-        var contentPadding = this.content.innerHeight() - this.content.height();;
+        var contentPadding = this.content.innerHeight() - this.content.height();
         var footerHeight = this.footer.outerHeight(true) || 0;
 
         var maxHeight = windowHeight - (margin + headerHeight + contentPadding + footerHeight);
