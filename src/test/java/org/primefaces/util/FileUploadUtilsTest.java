fcfcd19f2df2420d34518f4ab4a2f6b7e367d1a8
cnsgithub <cnsgithub@users.noreply.github.com>
2018-11-15 14:55:38 +0100

    mod: added some more unit tests

--- src/test/java/org/primefaces/util/FileUploadUtilsTest.java
+++ src/test/java/org/primefaces/util/FileUploadUtilsTest.java
@@ -22,21 +22,52 @@ public class FileUploadUtilsTest {
     @Before
     public void setup() {
         fileUpload = mock(FileUpload.class);
-        when(fileUpload.isValidateContentType()).thenReturn(true);
         inputStream = mock(InputStream.class);
     }
 
     @After
     public void teardown() {
+        inputStream = null;
         fileUpload = null;
     }
     
     @Test
     public void isValidTypeFilenameCheck() {
+        when(fileUpload.getAllowTypes()).thenReturn(null);
+        Assert.assertTrue(FileUploadUtils.isValidType(fileUpload, "test.png", inputStream));
+
         when(fileUpload.getAllowTypes()).thenReturn("/\\.(gif|png|jpe?g)$/i");
         Assert.assertTrue(FileUploadUtils.isValidType(fileUpload, "test.PNG", inputStream));
+        Assert.assertTrue(FileUploadUtils.isValidType(fileUpload, "test.jpeg", inputStream));
         Assert.assertFalse(FileUploadUtils.isValidType(fileUpload, "test.bmp", inputStream));
     }
-    
+
+    @Test
+    public void isValidTypeContentTypeCheck() {
+        when(fileUpload.isValidateContentType()).thenReturn(false);
+
+        when(fileUpload.getAccept()).thenReturn("image/png");
+        Assert.assertTrue(FileUploadUtils.isValidType(fileUpload, "test.JPEG", inputStream));
+        
+        when(fileUpload.isValidateContentType()).thenReturn(true);
+
+        when(fileUpload.getAccept()).thenReturn("");
+        Assert.assertTrue(FileUploadUtils.isValidType(fileUpload, "test.png", inputStream));
+
+        when(fileUpload.getAccept()).thenReturn(".png,.bmp");
+        Assert.assertTrue(FileUploadUtils.isValidType(fileUpload, "test.png", inputStream));
+        Assert.assertTrue(FileUploadUtils.isValidType(fileUpload, "test.bmp", inputStream));
+        Assert.assertFalse(FileUploadUtils.isValidType(fileUpload, "test.jpg", inputStream));
+
+        when(fileUpload.getAccept()).thenReturn("video/*");
+        Assert.assertFalse(FileUploadUtils.isValidType(fileUpload, "test.png", inputStream));
+        Assert.assertTrue(FileUploadUtils.isValidType(fileUpload, "test.mp4", inputStream));
+
+        when(fileUpload.getAccept()).thenReturn("image/png");
+        Assert.assertTrue(FileUploadUtils.isValidType(fileUpload, "test.Png", inputStream));
+        Assert.assertFalse(FileUploadUtils.isValidType(fileUpload, "test.jpg", inputStream));
+
+        //TODO provide some tests making use of apache tika
+    }
     
 }
