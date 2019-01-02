0c3dab8b3253d9064d49562260eddc1b86f8b6a5
Thomas Andraschko <tandraschko@apache.org>
2019-01-02 22:20:05 +0100

    unused var

--- src/main/java/org/primefaces/expression/SearchExpressionFacade.java
+++ src/main/java/org/primefaces/expression/SearchExpressionFacade.java
@@ -170,7 +170,6 @@ public class SearchExpressionFacade {
         if (splittedExpressions != null && splittedExpressions.length > 0) {
 
             final char separatorChar = UINamingContainer.getSeparatorChar(context);
-            final String separatorString = String.valueOf(separatorChar);
 
             StringBuilder expressionsBuffer = SharedStringBuilder.get(context, SHARED_EXPRESSION_BUFFER_KEY);
 
@@ -310,7 +309,6 @@ public class SearchExpressionFacade {
         }
 
         final char separatorChar = UINamingContainer.getSeparatorChar(context);
-        final String separatorString = String.valueOf(separatorChar);
 
         expression = expression.trim();
 
