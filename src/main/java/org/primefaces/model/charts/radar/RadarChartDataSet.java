01ccafad0ae2585d24b16f082fdd54287387009f
melloware <mellowaredev@gmail.com>
2019-02-13 16:28:40 -0500

    Fix #4539: ChartJS add hidden option to DataSets.

--- src/main/java/org/primefaces/model/charts/radar/RadarChartDataSet.java
+++ src/main/java/org/primefaces/model/charts/radar/RadarChartDataSet.java
@@ -445,6 +445,7 @@ public class RadarChartDataSet extends ChartDataSet {
             ChartUtils.writeDataValue(fsw, "type", "radar", false);
             ChartUtils.writeDataValue(fsw, "data", this.data, true);
             ChartUtils.writeDataValue(fsw, "label", this.label, true);
+            ChartUtils.writeDataValue(fsw, "hidden", this.isHidden(), true);
             ChartUtils.writeDataValue(fsw, "backgroundColor", this.backgroundColor, true);
             ChartUtils.writeDataValue(fsw, "borderColor", this.borderColor, true);
             ChartUtils.writeDataValue(fsw, "borderWidth", this.borderWidth, true);
