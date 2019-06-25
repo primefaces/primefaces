730f0c29405a83ed1821906dcaba4cc4d85c134a
Martin Höller <martin.hoeller@xss.co.at>
2019-06-25 13:18:52 +0200

    minor cleanup as requested by meloware

--- src/main/java/org/primefaces/component/datatable/DataTableRenderer.java
+++ src/main/java/org/primefaces/component/datatable/DataTableRenderer.java
@@ -889,7 +889,7 @@ public class DataTableRenderer extends DataRenderer {
     protected void encodeFilterInput(UIColumn column, ResponseWriter writer, boolean disableTabbing,
         String filterId, String filterStyleClass, Object filterValue, String ariaLabelId) throws IOException {
 
-        if (hasFilterOptions(column)) {
+        if (!hasFilterOptions(column)) {
             encodeFilterInputText(column, writer, disableTabbing, filterId, filterStyleClass, filterValue, ariaLabelId);
         }
         else {
@@ -898,9 +898,13 @@ public class DataTableRenderer extends DataRenderer {
     }
 
     private boolean hasFilterOptions(UIColumn column) {
-        return column.getValueExpression(Column.PropertyKeys.filterOptions.toString()) == null
-            || getFilterOptions(column) == null
-            || getFilterOptions(column).length == 0;
+        boolean hasFilterOptionsVE = column.getValueExpression(Column.PropertyKeys.filterOptions.toString()) != null;
+        if (!hasFilterOptionsVE) {
+            return false;
+        }
+
+        SelectItem[] filterOptions = getFilterOptions(column);
+        return filterOptions != null && filterOptions.length != 0;
     }
 
     protected void encodeFilterInputSelect(UIColumn column, ResponseWriter writer, boolean disableTabbing,
