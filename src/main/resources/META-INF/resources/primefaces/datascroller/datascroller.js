b5baf54e42fbf7e9e40d2a0ebb5af284468254ab
mertsincan <sincan.mert@gmail.com>
2019-07-16 10:45:12 +0300

    Fixed #4995 and Fixed #4996

--- src/main/resources/META-INF/resources/primefaces/datascroller/datascroller.js
+++ src/main/resources/META-INF/resources/primefaces/datascroller/datascroller.js
@@ -6,7 +6,7 @@ PrimeFaces.widget.DataScroller = PrimeFaces.widget.BaseWidget.extend({
     init: function(cfg) {
         this._super(cfg);
         this.content = this.jq.children('div.ui-datascroller-content');
-        this.list = this.content.children('ul');
+        this.list = this.cfg.virtualScroll ? this.content.children('div').children('ul') : this.content.children('ul');
         this.loaderContainer = this.content.children('div.ui-datascroller-loader');
         this.loadStatus = $('<div class="ui-datascroller-loading"></div>');
         this.loading = false;
@@ -39,18 +39,113 @@ PrimeFaces.widget.DataScroller = PrimeFaces.widget.BaseWidget.extend({
             });
         }
         else {
-            this.content.on('scroll', function () {
-                var scrollTop = this.scrollTop,
-                scrollHeight = this.scrollHeight,
-                viewportHeight = this.clientHeight;
+            this.itemHeight = 0;
+            
+            if(this.cfg.virtualScroll) {
+                var item = this.list.children('li.ui-datascroller-item');
+                if(item) {
+                    this.itemHeight = item.outerHeight();
+                    this.content.children('div').css('height', parseFloat((this.cfg.totalSize * this.itemHeight) + 'px'));
+                }
 
-                if((scrollTop >= ((scrollHeight * $this.cfg.buffer) - (viewportHeight))) && $this.shouldLoad()) {
-                    $this.load();
+                if (this.cfg.startAtBottom) {
+                    this.blockScrollEvent = true;
+                    var pageHeight = this.itemHeight * this.cfg.chunkSize,
+                    virtualListHeight = parseFloat(this.cfg.totalSize * this.itemHeight),
+                    pageCount = (virtualListHeight / pageHeight)||1,
+                    page = (pageCount > 2) ? pageCount - 2 : 0;
+            
+                    this.list.css('top', (page * pageHeight) + 'px');
+                    this.content.scrollTop(this.content[0].scrollHeight);
+                }
+            }
+
+            this.content.on('scroll', function () {
+                if($this.cfg.virtualScroll) {
+                    if ($this.blockScrollEvent) {
+                        $this.blockScrollEvent = false;
+                        return;
+                    }
+                    
+                    var virtualScrollContent = this;
+                    
+                    clearTimeout($this.scrollTimeout);
+                    $this.scrollTimeout = setTimeout(function() {
+                        var viewportHeight = $this.content.outerHeight(),
+                        listHeight = $this.list.outerHeight() + Math.ceil(viewportHeight - $this.content.height()),
+                        pageHeight = $this.itemHeight * $this.cfg.chunkSize,
+                        virtualListHeight = parseFloat($this.cfg.totalSize * $this.itemHeight),
+                        pageCount = (virtualListHeight / pageHeight)||1;
+
+                        if(virtualScrollContent.scrollTop + viewportHeight > parseFloat($this.list.css('top')) + listHeight || virtualScrollContent.scrollTop < parseFloat($this.list.css('top'))) {
+                            var page = Math.floor((virtualScrollContent.scrollTop * pageCount) / (virtualScrollContent.scrollHeight)) + 1;
+                            $this.loadRowsWithVirtualScroll(page, function () {
+                                $this.list.css('top', ((page - 1) * pageHeight) + 'px');
+                            });
+                        }
+                    }, 200);
+                }
+                else {
+                    var scrollTop = this.scrollTop,
+                    scrollHeight = this.scrollHeight,
+                    viewportHeight = this.clientHeight;
+                
+                    if((scrollTop >= ((scrollHeight * $this.cfg.buffer) - (viewportHeight))) && $this.shouldLoad()) {
+                        $this.load();
+                    }
                 }
             });
         }
     },
 
+    loadRowsWithVirtualScroll: function(page, callback) {
+        if(this.virtualScrollActive) {
+            return;
+        }
+
+        this.virtualScrollActive = true;
+
+        var $this = this,
+        first = (page - 1) * this.cfg.chunkSize,
+        options = {
+            source: this.id,
+            process: this.id,
+            update: this.id,
+            formId: this.cfg.formId,
+            params: [{name: this.id + '_virtualScrolling', value: true},
+                     {name: this.id + '_first', value: first}],
+            onsuccess: function(responseXML, status, xhr) {
+                PrimeFaces.ajax.Response.handle(responseXML, status, xhr, {
+                    widget: $this,
+                    handle: function(content) {
+                        //insert new rows
+                        this.updateData(content);
+                        callback();
+                        this.virtualScrollActive = false;
+                    }
+                });
+
+                return true;
+            },
+            oncomplete: function(xhr, status, args) {
+                if(typeof args.totalSize !== 'undefined') {
+                    $this.cfg.totalSize = args.totalSize;
+                }
+            }
+        };
+
+        PrimeFaces.ajax.Request.handle(options);
+    },
+    
+    updateData: function(data, clear) {
+        var empty = (clear === undefined) ? true: clear;
+
+        if(empty)
+            this.list.html(data);
+        else
+            this.list.append(data);
+    },
+    
     bindManualLoader: function() {
         var $this = this;
 
@@ -80,7 +175,7 @@ PrimeFaces.widget.DataScroller = PrimeFaces.widget.BaseWidget.extend({
                 PrimeFaces.ajax.Response.handle(responseXML, status, xhr, {
                     widget: $this,
                     handle: function(content) {
-                        this.list.append(content);
+                        this.updateData(content, false);
                     }
                 });
 
