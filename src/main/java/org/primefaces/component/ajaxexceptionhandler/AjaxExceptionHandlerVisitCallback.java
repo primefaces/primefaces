039f5d75c614914fb532e536c6acf8bcf2105850
Thomas Andraschko <tandraschko@apache.org>
2019-08-10 17:24:19 +0200

    right UIData

--- src/main/java/org/primefaces/component/ajaxexceptionhandler/AjaxExceptionHandlerVisitCallback.java
+++ src/main/java/org/primefaces/component/ajaxexceptionhandler/AjaxExceptionHandlerVisitCallback.java
@@ -27,11 +27,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.faces.component.UIComponent;
+import javax.faces.component.UIData;
 import javax.faces.component.visit.VisitCallback;
 import javax.faces.component.visit.VisitContext;
 import javax.faces.component.visit.VisitResult;
 
-import org.primefaces.component.api.UIData;
 import org.primefaces.util.LangUtils;
 
 /**
