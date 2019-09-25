16bad3f9a0dfa82d50c58b847a26a872be4aba5d
Nicola Isotta <nicola.isotta@gmail.com>
2019-09-25 16:09:44 +0200

    Add support for InputNumber and additional constraints. Fixes #5165 (#5172)
    * Add support for InputNumber and additional constraints
    Fixes #5165
    
    * Line endings
    
    * Log exceptions
    
    * Improve UICalendar transforming

--- src/main/java/org/primefaces/metadata/transformer/impl/BeanValidationInputMetadataTransformer.java
+++ src/main/java/org/primefaces/metadata/transformer/impl/BeanValidationInputMetadataTransformer.java
@@ -25,25 +25,32 @@ package org.primefaces.metadata.transformer.impl;
 
 import java.io.IOException;
 import java.lang.annotation.Annotation;
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import javax.el.PropertyNotFoundException;
 import javax.faces.component.EditableValueHolder;
 import javax.faces.component.UIInput;
 import javax.faces.context.FacesContext;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Digits;
 import javax.validation.constraints.Future;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import javax.validation.metadata.ConstraintDescriptor;
+
 import org.primefaces.component.api.UICalendar;
+import org.primefaces.component.inputnumber.InputNumber;
 import org.primefaces.component.spinner.Spinner;
 import org.primefaces.context.PrimeApplicationContext;
 import org.primefaces.metadata.BeanValidationMetadataExtractor;
 import org.primefaces.metadata.transformer.AbstractInputMetadataTransformer;
+import org.primefaces.util.LangUtils;
 
 public class BeanValidationInputMetadataTransformer extends AbstractInputMetadataTransformer {
 
@@ -78,9 +85,10 @@ public class BeanValidationInputMetadataTransformer extends AbstractInputMetadat
     protected void applyConstraint(ConstraintDescriptor constraintDescriptor, UIInput input, EditableValueHolder editableValueHolder) {
 
         Annotation constraint = constraintDescriptor.getAnnotation();
+        Class<? extends Annotation> annotationType = constraint.annotationType();
 
         if (!isMaxlenghtSet(input)) {
-            if (constraint.annotationType().equals(Size.class)) {
+            if (annotationType.equals(Size.class)) {
                 Size size = (Size) constraint;
                 if (size.max() > 0) {
                     setMaxlength(input, size.max());
@@ -91,24 +99,76 @@ public class BeanValidationInputMetadataTransformer extends AbstractInputMetadat
         if (input instanceof Spinner) {
             Spinner spinner = (Spinner) input;
 
-            if (constraint.annotationType().equals(Max.class) && spinner.getMax() == Double.MAX_VALUE) {
+            if (annotationType.equals(Max.class) && spinner.getMax() == Double.MAX_VALUE) {
                 Max max = (Max) constraint;
                 spinner.setMax(max.value());
             }
-            if (constraint.annotationType().equals(Min.class) && spinner.getMin() == Double.MIN_VALUE) {
+            if (annotationType.equals(Min.class) && spinner.getMin() == Double.MIN_VALUE) {
                 Min min = (Min) constraint;
                 spinner.setMin(min.value());
+            }
+            if (annotationType.equals(DecimalMax.class) && spinner.getMax() == Double.MAX_VALUE) {
+                DecimalMax max = (DecimalMax) constraint;
+                try {
+                    spinner.setMax(Double.parseDouble(max.value()));
+                }
+                catch (NumberFormatException ex) {
+                    LOGGER.log(Level.WARNING, "Failed setting Spinner max value: " + ex.getMessage());
+                }
+            }
+            if (annotationType.equals(DecimalMin.class) && spinner.getMin() == Double.MIN_VALUE) {
+                DecimalMin min = (DecimalMin) constraint;
+                try {
+                    spinner.setMin(Double.parseDouble(min.value()));
+                }
+                catch (NumberFormatException ex) {
+                    LOGGER.log(Level.WARNING, "Failed setting Spinner min value: " + ex.getMessage());
+                }
+            }
+        }
+
+        if (input instanceof InputNumber) {
+            InputNumber inputNumber = (InputNumber) input;
+
+            if (annotationType.equals(Max.class) && LangUtils.isValueBlank(inputNumber.getMaxValue())) {
+                Max max = (Max) constraint;
+                inputNumber.setMaxValue(String.valueOf(max.value()));
+            }
+            if (annotationType.equals(Min.class) && LangUtils.isValueBlank(inputNumber.getMinValue())) {
+                Min min = (Min) constraint;
+                inputNumber.setMinValue(String.valueOf(min.value()));
+            }
+            if (annotationType.equals(DecimalMax.class) && LangUtils.isValueBlank(inputNumber.getMaxValue())) {
+                DecimalMax max = (DecimalMax) constraint;
+                inputNumber.setMaxValue(max.value());
+            }
+            if (annotationType.equals(DecimalMin.class) && LangUtils.isValueBlank(inputNumber.getMinValue())) {
+                DecimalMin min = (DecimalMin) constraint;
+                inputNumber.setMinValue(min.value());
+            }
+            if (annotationType.equals(Digits.class) && LangUtils.isValueBlank(inputNumber.getDecimalPlaces())) {
+                Digits digits = (Digits) constraint;
+                inputNumber.setDecimalPlaces(String.valueOf(digits.fraction()));
             }
         }
 
         if (input instanceof UICalendar) {
             UICalendar uicalendar = (UICalendar) input;
+            boolean hasTime = uicalendar.hasTime();
+            // for BeanValidation 2.0
+            String annotationClassName = annotationType.getSimpleName();
 
-            if (constraint.annotationType().equals(Past.class) && uicalendar.getMaxdate() == null) {
-                uicalendar.setMaxdate(new Date());
+            if (annotationType.equals(Past.class) && uicalendar.getMaxdate() == null) {
+                uicalendar.setMaxdate(hasTime ? LocalDate.now() : LocalDate.now().minusDays(1));
             }
-            if (constraint.annotationType().equals(Future.class) && uicalendar.getMindate() == null) {
-                uicalendar.setMindate(new Date());
+            if (annotationClassName.equals("PastOrPresent") && uicalendar.getMaxdate() == null) {
+                uicalendar.setMaxdate(LocalDate.now());
+            }
+            if (annotationType.equals(Future.class) && uicalendar.getMindate() == null) {
+                uicalendar.setMindate(hasTime ? LocalDate.now() : LocalDate.now().plusDays(1));
+            }
+            if (annotationClassName.equals("FutureOrPresent") && uicalendar.getMindate() == null) {
+                uicalendar.setMindate(LocalDate.now());
             }
         }
     }
