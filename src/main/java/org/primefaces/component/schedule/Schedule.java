76ee592cc34cce664c362908e5283d7fdea0d4fa
Christoph Straßer <christoph.strasser78@gmail.com>
2019-10-28 06:42:26 +0100

    #5267: fullcalendar.css --> schedule.css

--- src/main/java/org/primefaces/component/schedule/Schedule.java
+++ src/main/java/org/primefaces/component/schedule/Schedule.java
@@ -47,7 +47,7 @@ import org.primefaces.util.LocaleUtils;
 import org.primefaces.util.MapBuilder;
 
 @ResourceDependencies({
-        @ResourceDependency(library = "primefaces", name = "schedule/fullcalendar.css"),
+        @ResourceDependency(library = "primefaces", name = "schedule/schedule.css"),
         @ResourceDependency(library = "primefaces", name = "components.css"),
         @ResourceDependency(library = "primefaces", name = "moment/moment.js"),
         @ResourceDependency(library = "primefaces", name = "moment/moment-timezone-with-data.js"),
