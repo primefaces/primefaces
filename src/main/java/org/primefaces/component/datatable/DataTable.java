ab4d3c95bb8a5f1f4b6cd0026e5a8ef2e1ea24bd
melloware <mellowaredev@gmail.com>
2019-11-19 08:22:01 -0500

    Fix #608: Add touchswipe.js to resources

--- src/main/java/org/primefaces/component/datatable/DataTable.java
+++ src/main/java/org/primefaces/component/datatable/DataTable.java
@@ -62,7 +62,8 @@ import org.primefaces.util.*;
         @ResourceDependency(library = "primefaces", name = "jquery/jquery.js"),
         @ResourceDependency(library = "primefaces", name = "jquery/jquery-plugins.js"),
         @ResourceDependency(library = "primefaces", name = "core.js"),
-        @ResourceDependency(library = "primefaces", name = "components.js")
+        @ResourceDependency(library = "primefaces", name = "components.js"),
+        @ResourceDependency(library = "primefaces", name = "touch/touchswipe.js")
 })
 public class DataTable extends DataTableBase {
 
