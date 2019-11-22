86250d25a58946bfcc17e85fa6ed335a29588dcb
mertsincan <sincan.mert@gmail.com>
2019-11-22 11:31:10 +0300

    Fixed #4984

--- src/main/resources/META-INF/resources/primefaces/forms/forms.defaultcommand.js
+++ src/main/resources/META-INF/resources/primefaces/forms/forms.defaultcommand.js
@@ -40,8 +40,8 @@ PrimeFaces.widget.DefaultCommand = PrimeFaces.widget.BaseWidget.extend({
             this.scope.off('keydown.' + this.id).on('keydown.' + this.id, function (e) {
                 var keyCode = $.ui.keyCode;
                 if (e.which == keyCode.ENTER) {
-                    closestForm.trigger('keydown.' + $this.id, {scopeEnter: true});
-                    e.preventDefault();
+                    closestForm.trigger(e, {scopeEnter: true});
+                    //e.preventDefault();
                     e.stopPropagation();
                 }
             });
