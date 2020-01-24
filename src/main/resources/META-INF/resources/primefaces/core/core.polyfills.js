2811e796c4af2671cb5d645cc8c53230f8a606b8
melloware <mellowaredev@gmail.com>
2020-01-24 10:03:28 -0500

    Fix #5263: Ignore partial process for @none

--- src/main/resources/META-INF/resources/primefaces/core/core.polyfills.js
+++ src/main/resources/META-INF/resources/primefaces/core/core.polyfills.js
@@ -14,3 +14,15 @@ if (!Element.prototype.closest) {
         return null;
     };
 }
+
+if (!String.prototype.includes) {
+    String.prototype.includes = function(search, start) {
+      'use strict';
+
+      if (search instanceof RegExp) {
+        throw TypeError('first argument must not be a RegExp');
+      } 
+      if (start === undefined) { start = 0; }
+      return this.indexOf(search, start) !== -1;
+    };
+}
