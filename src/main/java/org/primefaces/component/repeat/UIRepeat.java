76d62487786912fbeeba9a5a5a6c3a01ef580023
melloware <mellowaredev@gmail.com>
2020-03-09 09:59:10 -0400

    Fix #5661: Repeat backward compatible with JSF2.2

--- src/main/java/org/primefaces/component/repeat/UIRepeat.java
+++ src/main/java/org/primefaces/component/repeat/UIRepeat.java
@@ -913,7 +913,7 @@ public class UIRepeat extends UINamingContainer {
             this.resetDataModel();
             int prevIndex = this.index;
             FacesEvent target = idxEvent.getTarget();
-            FacesContext ctx = target.getFacesContext();
+            FacesContext ctx = FacesContext.getCurrentInstance();
             UIComponent source = target.getComponent();
             UIComponent compositeParent = null;
             try {
