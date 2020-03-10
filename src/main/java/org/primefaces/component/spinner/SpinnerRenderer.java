923cc47c57c1613cce931a3223916b79bd837b6f
Melloware <mellowaredev@gmail.com>
2020-03-10 11:33:18 -0400

    Fix #5668: Spinner NPE check (#5669)

--- src/main/java/org/primefaces/component/spinner/SpinnerRenderer.java
+++ src/main/java/org/primefaces/component/spinner/SpinnerRenderer.java
@@ -46,19 +46,23 @@ public class SpinnerRenderer extends InputRenderer {
         decodeBehaviors(context, spinner);
 
         String submittedValue = context.getExternalContext().getRequestParameterMap().get(spinner.getClientId(context) + "_input");
-        String prefix = spinner.getPrefix();
-        String suffix = spinner.getSuffix();
 
-        if (prefix != null && submittedValue.startsWith(prefix)) {
-            submittedValue = submittedValue.substring(prefix.length());
+        if (submittedValue != null) {
+            String prefix = spinner.getPrefix();
+            String suffix = spinner.getSuffix();
+
+            if (prefix != null && submittedValue.startsWith(prefix)) {
+                submittedValue = submittedValue.substring(prefix.length());
+            }
+            if (suffix != null && submittedValue.endsWith(suffix)) {
+                submittedValue = submittedValue.substring(0, (submittedValue.length() - suffix.length()));
+            }
+            if (!LangUtils.isValueEmpty(spinner.getThousandSeparator())) {
+                submittedValue = submittedValue.replace(spinner.getThousandSeparator(), "");
+            }
+            submittedValue = submittedValue.replace(spinner.getDecimalSeparator(), ".");
         }
-        if (suffix != null && submittedValue.endsWith(suffix)) {
-            submittedValue = submittedValue.substring(0, (submittedValue.length() - suffix.length()));
-        }
-        if (!LangUtils.isValueEmpty(spinner.getThousandSeparator())) {
-            submittedValue = submittedValue.replace(spinner.getThousandSeparator(), "");
-        }
-        submittedValue = submittedValue.replace(spinner.getDecimalSeparator(), ".");
+
         spinner.setSubmittedValue(submittedValue);
     }
 
