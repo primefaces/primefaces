3b83f8a12fd6123bfdbe3aeccef9fa06474e6211
melloware <mellowaredev@gmail.com>
2020-03-16 08:40:30 -0400

    Fix #5698: LinkButton focus styles, enable/disable widget methods

--- src/main/resources/META-INF/resources/primefaces/forms/forms.linkbutton.js
+++ src/main/resources/META-INF/resources/primefaces/forms/forms.linkbutton.js
@@ -3,10 +3,35 @@
  */
 PrimeFaces.widget.LinkButton = PrimeFaces.widget.BaseWidget.extend({
 
-    init: function(cfg) {
+    init : function (cfg) {
         this._super(cfg);
+        this.button = this.jq;
+        this.link = this.jq.children('a');
 
-        PrimeFaces.skinButton(this.jq);
+        PrimeFaces.skinButton(this.button);
+
+        this.bindEvents();
+    },
+
+    bindEvents : function () {
+        var $this = this;
+
+        if (this.link) {
+            this.link.off().on("focus.linkbutton keydown.linkbutton", function () {
+                $this.button.addClass('ui-state-focus ui-state-active');
+            }).on("blur.linkbutton", function () {
+                $this.button.removeClass('ui-state-focus ui-state-active');
+            });
+        }
+    },
+
+    disable : function () {
+        this.button.removeClass('ui-state-hover ui-state-focus ui-state-active')
+                .addClass('ui-state-disabled').attr('disabled', 'disabled');
+    },
+
+    enable : function () {
+        this.button.removeClass('ui-state-disabled').removeAttr('disabled');
     }
 
 });
