ba15ddfa6ed977235a7915bdc826c64ccdf6ee02
GedMarc <ged_marc@hotmail.com>
2020-06-05 18:22:58 +0200

    https://github.com/primefaces/primefaces/issues/5991
    Validations 2.x support

--- src/main/java/org/primefaces/validate/bean/ConstraintDescriptorWrapper.java
+++ src/main/java/org/primefaces/validate/bean/ConstraintDescriptorWrapper.java
@@ -93,4 +93,18 @@ public class ConstraintDescriptorWrapper<T extends Annotation> implements Constr
     public ConstraintTarget getValidationAppliesTo() {
         return wrapped.getValidationAppliesTo();
     }
+
+	//BV 2
+	//@Override
+	public javax.validation.metadata.ValidateUnwrappedValue getValueUnwrapping()
+	{
+		return wrapped.getValueUnwrapping();
+	}
+
+	//BV 2
+	//@Override
+	public <U> U unwrap(Class<U> type)
+	{
+		return wrapped.unwrap(type);
+	}
 }
