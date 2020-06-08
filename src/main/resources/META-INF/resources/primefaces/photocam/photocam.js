37a508940ca5b14b49a0db53f79eaaf52ff1da7f
doleron <doleron@gmail.com>
2020-06-08 03:58:30 -0300

    doc errorObj onCameraError param

--- src/main/resources/META-INF/resources/primefaces/photocam/photocam.js
+++ src/main/resources/META-INF/resources/primefaces/photocam/photocam.js
@@ -1169,6 +1169,7 @@ PrimeFaces.widget.PhotoCam = PrimeFaces.widget.BaseWidget.extend({
     
     /**
      * default error handler webcam events
+     * @param {Webcam.Error} errorObj
      */
     onCameraError: function(errorObj) {
         var message;
