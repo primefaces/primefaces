5e83dfce5733379b9ee4f4806676f4003e5a3dcf
Melloware <mellowaredev@gmail.com>
2020-06-17 07:49:58 -0400

    Fix #6042: SelectOne remove aria-expanded from focus (#6043)

--- src/main/resources/META-INF/resources/primefaces/forms/forms.selectonemenu.js
+++ src/main/resources/META-INF/resources/primefaces/forms/forms.selectonemenu.js
@@ -901,7 +901,6 @@ PrimeFaces.widget.SelectOneMenu = PrimeFaces.widget.DeferredWidget.extend({
 
         //value before panel is shown
         this.preShowValue = this.options.filter(':selected');
-        this.focusInput.attr('aria-expanded', true);
         this.jq.attr('aria-expanded', true);
     },
 
@@ -910,7 +909,6 @@ PrimeFaces.widget.SelectOneMenu = PrimeFaces.widget.DeferredWidget.extend({
      */
     hide: function() {
         this.panel.css('z-index', '').hide();
-        this.focusInput.attr('aria-expanded', false);
         this.jq.attr('aria-expanded', false);
     },
 
