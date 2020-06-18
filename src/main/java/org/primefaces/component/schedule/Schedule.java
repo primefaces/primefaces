722c4099f9ca043dd1f7c01017138fd3ce34ae8f
melloware <mellowaredev@gmail.com>
2020-06-18 12:52:23 -0400

    Fix #5914: Proper ISO conversion from client

--- src/main/java/org/primefaces/component/schedule/Schedule.java
+++ src/main/java/org/primefaces/component/schedule/Schedule.java
@@ -83,7 +83,6 @@ public class Schedule extends ScheduleBase {
         Map<String, String> params = context.getExternalContext().getRequestParameterMap();
         String eventName = params.get(Constants.RequestParams.PARTIAL_BEHAVIOR_EVENT_PARAM);
         String clientId = getClientId(context);
-        ZoneId zoneId = CalendarUtils.calculateZoneId(this.getTimeZone());
 
         if (ComponentUtils.isRequestSource(this, context)) {
 
@@ -92,6 +91,7 @@ public class Schedule extends ScheduleBase {
 
             if (eventName.equals("dateSelect")) {
                 String selectedDateStr = params.get(clientId + "_selectedDate");
+                ZoneId zoneId = CalendarUtils.calculateZoneId(this.getTimeZone());
                 LocalDateTime selectedDate =  CalendarUtils.toLocalDateTime(zoneId, selectedDateStr);
                 SelectEvent<?> selectEvent = new SelectEvent(this, behaviorEvent.getBehavior(), selectedDate);
                 selectEvent.setPhaseId(behaviorEvent.getPhaseId());
