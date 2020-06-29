8f323ffad1a28c3515187962c03158c07f68c383
melloware <mellowaredev@gmail.com>
2020-06-29 08:07:11 -0400

    Fix #3240, #3732, #1892, #324: InputMask upgrade JS library

--- src/main/java/org/primefaces/component/inputmask/InputMask.java
+++ src/main/java/org/primefaces/component/inputmask/InputMask.java
@@ -27,9 +27,9 @@ import javax.faces.application.ResourceDependency;
 
 @ResourceDependency(library = "primefaces", name = "components.css")
 @ResourceDependency(library = "primefaces", name = "jquery/jquery.js")
-@ResourceDependency(library = "primefaces", name = "jquery/jquery-plugins.js")
 @ResourceDependency(library = "primefaces", name = "core.js")
 @ResourceDependency(library = "primefaces", name = "components.js")
+@ResourceDependency(library = "primefaces", name = "inputmask/inputmask.js")
 public class InputMask extends InputMaskBase {
 
     public static final String COMPONENT_TYPE = "org.primefaces.component.InputMask";
