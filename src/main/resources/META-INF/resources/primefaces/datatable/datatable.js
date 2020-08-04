5f8dbf7af53dab85b2c5ef0e96e3e6343577871a
Melloware <mellowaredev@gmail.com>
2020-08-04 12:03:11 -0400

    Fix #2360: Sorting in reflow mode (#6193)
    * Fix #2360: Sorting in reflow mode
    
    * Fix #2360: Sorting in reflow mode
    
    * Fix #2360: Sorting in reflow mode

--- src/main/resources/META-INF/resources/primefaces/datatable/datatable.js
+++ src/main/resources/META-INF/resources/primefaces/datatable/datatable.js
@@ -4670,7 +4670,10 @@ PrimeFaces.widget.DataTable = PrimeFaces.widget.DeferredWidget.extend({
             orderIndex = sortOrder > 0 ? 0 : 1;
 
             options.filter(':selected').prop('selected', false);
-            options.filter(':contains("' + $.escapeSelector(columnHeader.text()) + '")').filter('[value$="_' + orderIndex + '"]').prop("selected", true);
+            options.filter(function(idx, element) {
+                var header = $.escapeSelector(columnHeader.text());
+                return $.escapeSelector(element.text).startsWith(header) && element.value.endsWith("_" + orderIndex);
+            }).prop("selected", true);
         }
     },
 
