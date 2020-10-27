80fa2eff74228219173f8507b3ae3ae57a644d20
melloware <mellowaredev@gmail.com>
2020-10-27 07:59:00 -0400

    Fix #6453: InputNumber don't store default decimalPlaces

--- src/main/java/org/primefaces/component/inputnumber/InputNumberRenderer.java
+++ src/main/java/org/primefaces/component/inputnumber/InputNumberRenderer.java
@@ -138,15 +138,6 @@ public class InputNumberRenderer extends InputRenderer {
             writer.writeAttribute("style", inputNumber.getStyle(), "style");
         }
 
-        String defaultDecimalPlaces = "2";
-        if (value instanceof Long || value instanceof Integer || value instanceof Short || value instanceof BigInteger) {
-            defaultDecimalPlaces = "0";
-        }
-        String decimalPlaces = isValueBlank(inputNumber.getDecimalPlaces())
-                ? defaultDecimalPlaces
-                : inputNumber.getDecimalPlaces();
-        inputNumber.setDecimalPlaces(decimalPlaces);
-
         encodeInput(context, inputNumber, clientId, valueToRender);
         encodeHiddenInput(context, inputNumber, clientId, valueToRender);
 
@@ -193,7 +184,8 @@ public class InputNumberRenderer extends InputRenderer {
         String styleClass = createStyleClass(inputNumber, InputNumber.PropertyKeys.inputStyleClass.name(), InputText.STYLE_CLASS) ;
         String inputMode = inputNumber.getInputMode();
         if (inputMode == null) {
-            inputMode = "0".equals(inputNumber.getDecimalPlaces()) ? "numeric" : "decimal";
+            String decimalPlaces = getDecimalPlaces(inputNumber, valueToRender);
+            inputMode = "0".equals(decimalPlaces) ? "numeric" : "decimal";
         }
 
         writer.startElement("input", null);
@@ -229,6 +221,7 @@ public class InputNumberRenderer extends InputRenderer {
         String decimalSeparator = isValueBlank(inputNumber.getDecimalSeparator())
                     ? "."
                     : inputNumber.getDecimalSeparator();
+        String decimalPlaces = getDecimalPlaces(inputNumber, value);
 
         WidgetBuilder wb = getWidgetBuilder(context);
         wb.init(InputNumber.class.getSimpleName(), inputNumber.resolveWidgetVar(context), inputNumber.getClientId());
@@ -241,7 +234,7 @@ public class InputNumberRenderer extends InputRenderer {
             .attr("currencySymbolPlacement", inputNumber.getSymbolPosition(), "p")
             .attr("minimumValue", formatForPlugin(inputNumber.getMinValue()))
             .attr("maximumValue", formatForPlugin(inputNumber.getMaxValue()))
-            .attr("decimalPlaces", inputNumber.getDecimalPlaces())
+            .attr("decimalPlaces", decimalPlaces)
             .attr("emptyInputBehavior", emptyValue, "focus")
             .attr("leadingZero", inputNumber.getLeadingZero(), "deny")
             .attr("allowDecimalPadding", inputNumber.isPadControl(), true)
@@ -334,6 +327,11 @@ public class InputNumberRenderer extends InputRenderer {
         }
     }
 
+    @Override
+    protected String getHighlighter() {
+        return "inputnumber";
+    }
+
     /**
      * Format a BigDecimal value as value/minValue/maxValue for the AutoNumeric plugin.
      * @param valueToRender the value to render
@@ -343,9 +341,17 @@ public class InputNumberRenderer extends InputRenderer {
         return valueToRender.toPlainString();
     }
 
-    @Override
-    protected String getHighlighter() {
-        return "inputnumber";
+    private String getDecimalPlaces(InputNumber inputNumber, Object value) {
+        String defaultDecimalPlaces = "2";
+        if (value instanceof Long || value instanceof Integer || value instanceof Short || value instanceof BigInteger) {
+            defaultDecimalPlaces = "0";
+        }
+        String decimalPlaces = isValueBlank(inputNumber.getDecimalPlaces())
+                ? defaultDecimalPlaces
+                : inputNumber.getDecimalPlaces();
+        return decimalPlaces;
     }
+
+
 
 }
