1aabb610ef4e66b2d81fdbf3bcbfff1f863ee2bd
melloware <mellowaredev@gmail.com>
2020-10-28 09:17:29 -0400

    Fix #6456: Perf getChildCount()

--- src/main/java/org/primefaces/component/organigram/OrganigramHelper.java
+++ src/main/java/org/primefaces/component/organigram/OrganigramHelper.java
@@ -96,7 +96,7 @@ public final class OrganigramHelper {
      * @param node The root node.
      */
     public static void buildRowKeys(OrganigramNode node) {
-        if (node.getChildCount() > 0 && node.getChildren() != null) {
+        if (node.getChildren() != null && node.getChildCount() > 0) {
             for (int i = 0; i < node.getChildCount(); i++) {
                 OrganigramNode childNode = node.getChildren().get(i);
 
