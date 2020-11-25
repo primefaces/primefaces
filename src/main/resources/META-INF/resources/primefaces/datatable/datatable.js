f7b01edf54dc986f4cd85b6acf7e3728ac9bc0d4
melloware <mellowaredev@gmail.com>
2020-11-25 11:20:49 -0500

    Fix #258: After row edit must reflow

--- src/main/resources/META-INF/resources/primefaces/datatable/datatable.js
+++ src/main/resources/META-INF/resources/primefaces/datatable/datatable.js
@@ -3572,11 +3572,14 @@ PrimeFaces.widget.DataTable = PrimeFaces.widget.DeferredWidget.extend({
                             }
 
                             this.updateRow(row, content);
-                            
+
                             // #1499 enable rowReorder when done editing
                             if (this.cfg.draggableRows && $('tr.ui-row-editing').length === 0) {
                                 this.tbody.sortable("enable");
                             }
+
+                            // #258 must reflow after editing
+                            this.postUpdateData();
                         }
                     });
 
