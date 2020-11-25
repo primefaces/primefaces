a1f655e18c6d4573cb9364174ad7344666006cd4
melloware <mellowaredev@gmail.com>
2020-11-25 13:24:59 -0500

    Fix #5296: GroupHeader IndexOutOfBoundsException

--- src/main/resources/META-INF/resources/primefaces/datatable/datatable.js
+++ src/main/resources/META-INF/resources/primefaces/datatable/datatable.js
@@ -4326,7 +4326,13 @@ PrimeFaces.widget.DataTable = PrimeFaces.widget.DeferredWidget.extend({
             },
             update: function(event, ui) {
                 var fromIndex = ui.item.data('ri'),
-                toIndex = $this.paginator ? $this.paginator.getFirst() + ui.item.index(): ui.item.index();
+                itemIndex = ui.item.index();
+                
+                // #5296 must not count header group rows
+                var groupHeaders = $this.tbody.children('.ui-rowgroup-header').length;
+                itemIndex = itemIndex - groupHeaders;
+                
+                var toIndex = $this.paginator ? $this.paginator.getFirst() + itemIndex: itemIndex;
 
                 $this.syncRowParity();
 
