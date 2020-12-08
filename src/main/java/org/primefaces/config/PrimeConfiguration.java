2a411691517144774a2f875f004b26a76fc2d4c0
Sébastien Lepage <sebastien.lepage@outlook.com>
2020-12-08 12:10:43 +0100

    Fix #6602 - FileUpload agnostic to FileTypeDetector providers

--- src/main/java/org/primefaces/config/PrimeConfiguration.java
+++ src/main/java/org/primefaces/config/PrimeConfiguration.java
@@ -57,7 +57,6 @@ public class PrimeConfiguration {
     private String[] exceptionTypesToIgnoreInLogging;
     private final String multiViewStateStore;
     private final boolean markInputAsInvalidOnErrorMsg;
-    private String fileTypeDetector;
 
     // internal config
     private final boolean stringConverterAvailable;
@@ -139,8 +138,6 @@ public class PrimeConfiguration {
         markInputAsInvalidOnErrorMsg = Boolean.parseBoolean(value);
 
         cookiesSameSite = externalContext.getInitParameter(Constants.ContextParams.COOKIES_SAME_SITE);
-
-        fileTypeDetector = externalContext.getInitParameter(Constants.ContextParams.FILE_TYPE_DECTECTOR);
 
         cookiesSecure = true;
         if (externalContext.getContext() instanceof ServletContext) {
@@ -268,9 +265,5 @@ public class PrimeConfiguration {
 
     public String getCookiesSameSite() {
         return cookiesSameSite;
-    }
-
-    public String getFileTypeDetector() {
-        return fileTypeDetector;
     }
 }
