c593691355dc6416e30774457c611555ba6349ee
Sébastien Lepage <sebastien.lepage@outlook.com>
2021-01-09 11:23:17 +0100

    DataTable: clean up selection

--- src/main/java/org/primefaces/model/LazyDataModel.java
+++ src/main/java/org/primefaces/model/LazyDataModel.java
@@ -23,41 +23,75 @@
  */
 package org.primefaces.model;
 
+import javax.faces.model.ListDataModel;
 import java.io.Serializable;
-import java.util.*;
-
-import javax.faces.component.UIComponent;
-import javax.faces.context.FacesContext;
-import javax.faces.model.DataModel;
-import javax.faces.model.DataModelEvent;
-import javax.faces.model.DataModelListener;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 /**
- * Custom lazy loading DataModel to deal with huge datasets
+ * Lazy loading DataModel to deal with huge datasets
  */
-public abstract class LazyDataModel<T> extends DataModel<T> implements SelectableDataModel<T>, Serializable {
+public abstract class LazyDataModel<T> extends ListDataModel<T> implements SelectableDataModel<T>, Serializable {
 
     private static final long serialVersionUID = 1L;
-
-    private int rowIndex = -1;
 
     private int rowCount;
 
     private int pageSize;
 
-    private List<T> data;
+    /**
+     * Loads the data for the given parameters.
+     *
+     * @param first the first entry
+     * @param pageSize the page size
+     * @param sortBy a list with all sort information
+     * @param filterBy a map with all filter information
+     * @return the data
+     */
+    public abstract List<T> load(int first, int pageSize, Map<String, SortMeta> sortBy, Map<String, FilterMeta> filterBy);
 
-    public LazyDataModel() {
-        super();
+    /**
+     * Retrieve bean associated to the rowKey passed in parameter.
+     * By default, only current loaded data (current page) is supported.
+     * Override this method to support selection from different pages.
+     * @param rowKey String representation
+     * @return Bean associated with rowKey
+     */
+    @Override
+    public T getRowData(String rowKey) {
+        List<T> data = getWrappedData();
+        if (data != null) {
+            for (T o : data) {
+                if (Objects.equals(rowKey, getRowKey(o))) {
+                    return o;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Convert bean into String representation used later on to retrieve that bean.
+     * By default, Object#hashCode is used as String representation.
+     * Override if bean doesn't override Object#hashCode
+     * @param object bean
+     * @return String representation
+     */
+    @Override
+    public String getRowKey(T object) {
+        return String.valueOf(Objects.hashCode(object));
     }
 
     @Override
-    public boolean isRowAvailable() {
-        if (data == null) {
-            return false;
-        }
+    public Iterator<T> iterator() {
+        return new LazyDataModelIterator<>(this);
+    }
 
-        return rowIndex >= 0 && rowIndex < data.size();
+    public Iterator<T> iterator(Map<String, SortMeta> sortBy, Map<String, FilterMeta> filterBy) {
+        return new LazyDataModelIterator<>(this, sortBy, filterBy);
     }
 
     @Override
@@ -66,52 +100,22 @@ public abstract class LazyDataModel<T> extends DataModel<T> implements Selectabl
     }
 
     @Override
-    public T getRowData() {
-        return data.get(rowIndex);
-    }
-
-    @Override
-    public int getRowIndex() {
-        return this.rowIndex;
-    }
-
-    @Override
     public void setRowIndex(int rowIndex) {
-        int oldIndex = this.rowIndex;
-
-        if (rowIndex == -1 || pageSize == 0) {
-            this.rowIndex = -1;
-        }
-        else {
-            this.rowIndex = (rowIndex % pageSize);
-        }
-
-        if (data == null) {
-            return;
-        }
-
-        DataModelListener[] listeners = getDataModelListeners();
-        if (listeners != null && oldIndex != this.rowIndex) {
-            Object rowData = null;
-            if (isRowAvailable()) {
-                rowData = getRowData();
+        if (rowIndex != -1) {
+            if (pageSize == 0) {
+                rowIndex = -1;
             }
-
-            DataModelEvent dataModelEvent = new DataModelEvent(this, rowIndex, rowData);
-            for (int i = 0; i < listeners.length; i++) {
-                listeners[i].rowSelected(dataModelEvent);
+            else {
+                rowIndex = rowIndex % pageSize;
             }
         }
+
+        super.setRowIndex(rowIndex);
     }
 
     @Override
     public List<T> getWrappedData() {
-        return data;
-    }
-
-    @Override
-    public void setWrappedData(Object list) {
-        this.data = (List) list;
+        return (List<T>) super.getWrappedData();
     }
 
     public int getPageSize() {
@@ -124,58 +128,5 @@ public abstract class LazyDataModel<T> extends DataModel<T> implements Selectabl
 
     public void setRowCount(int rowCount) {
         this.rowCount = rowCount;
-    }
-
-    /**
-     * Loads the data for the given parameters.
-     *
-     * @param first the first entry
-     * @param pageSize the page size
-     * @param sortBy a list with all sort informations
-     * @param filterBy a map with all filter informations
-     * @return the data
-     */
-    public abstract List<T> load(int first, int pageSize, Map<String, SortMeta> sortBy, Map<String, FilterMeta> filterBy);
-
-    @Override
-    public T getRowData(String rowKey) {
-        throw new UnsupportedOperationException(
-                getMessage("getRowData(String rowKey) must be implemented by %s when basic rowKey algorithm is not used [component=%s,view=%s]."));
-    }
-
-    @Override
-    public String getRowKey(T object) {
-        throw new UnsupportedOperationException(
-                getMessage("getRowKey(T object) must be implemented by %s when basic rowKey algorithm is not used [component=%s,view=%s]."));
-    }
-
-    private String getMessage(String format) {
-        FacesContext facesContext = FacesContext.getCurrentInstance();
-        String viewId = facesContext.getViewRoot().getViewId();
-        UIComponent component = UIComponent.getCurrentComponent(facesContext);
-        String clientId = component == null ? "<unknown>" : component.getClientId(facesContext);
-        return String.format(format, getClass().getName(), clientId, viewId);
-    }
-
-    @Override
-    public Iterator<T> iterator() {
-        return new LazyDataModelIterator<>(this);
-    }
-
-    @Deprecated
-    public Iterator<T> iterator(String sortField, SortOrder sortOrder, Map<String, FilterMeta> filterBy) {
-        Map<String, SortMeta> sortBy;
-        if (sortField == null) {
-            sortBy = Collections.emptyMap();
-        }
-        else {
-            sortBy = new HashMap<>(1);
-        }
-
-        return iterator(sortBy, filterBy);
-    }
-
-    public Iterator<T> iterator(Map<String, SortMeta> sortBy, Map<String, FilterMeta> filterBy) {
-        return new LazyDataModelIterator<>(this, sortBy, filterBy);
     }
 }
