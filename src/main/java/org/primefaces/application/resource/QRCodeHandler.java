26d0756db84584d646870eed0d26cda84406db0d
jasper <jepsar@gmail.com>
2021-03-11 20:26:34 +0100

    Added SVG QR code renderer

--- src/main/java/org/primefaces/application/resource/QRCodeHandler.java
+++ src/main/java/org/primefaces/application/resource/QRCodeHandler.java
@@ -23,16 +23,16 @@
  */
 package org.primefaces.application.resource;
 
+import io.nayuki.qrcodegen.QrCode;
+import io.nayuki.qrcodegen.QrCode.Ecc;
+
 import java.io.IOException;
 import java.util.Map;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
-import net.glxn.qrgen.QRCode;
-import net.glxn.qrgen.image.ImageType;
+import javax.imageio.ImageIO;
 
 import org.primefaces.util.Constants;
-
-import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 import org.primefaces.util.LangUtils;
 
 public class QRCodeHandler extends BaseDynamicContentHandler {
@@ -49,23 +49,36 @@ public class QRCodeHandler extends BaseDynamicContentHandler {
         if (value != null) {
             boolean cache = Boolean.parseBoolean(params.get(Constants.DYNAMIC_CONTENT_CACHE_PARAM));
 
-            externalContext.setResponseStatus(200);
-            externalContext.setResponseContentType("image/png");
-
-            handleCache(externalContext, cache);
-
-            ErrorCorrectionLevel ecl = ErrorCorrectionLevel.L;
-            String errorCorrection = params.get("qrec");
-            if (!LangUtils.isValueBlank(errorCorrection)) {
-                ecl = ErrorCorrectionLevel.valueOf(errorCorrection);
+            QrCode qrCode = QrCode.encodeText(value, getErrorCorrection(params.get("qrec")));
+            if ("png".equals(params.get("fmt"))) {
+                externalContext.setResponseContentType("image/png");
+                ImageIO.write(qrCode.toImage(24, 0), "png", externalContext.getResponseOutputStream());
             }
-
-            QRCode.from(value).to(ImageType.PNG).withErrorCorrection(ecl).withCharset("UTF-8")
-                        .writeTo(externalContext.getResponseOutputStream());
-
+            else {
+                externalContext.setResponseContentType("image/svg+xml");
+                externalContext.getResponseOutputWriter().write(qrCode.toSvgString(0));
+            }
+            handleCache(externalContext, cache);
+            externalContext.setResponseStatus(200);
             externalContext.responseFlushBuffer();
             context.responseComplete();
         }
+    }
+
+    protected Ecc getErrorCorrection(final String value) {
+        if (!LangUtils.isValueBlank(value)) {
+            switch (value) {
+                case "M":
+                    return Ecc.MEDIUM;
+                case "Q":
+                    return Ecc.QUARTILE;
+                case "H":
+                    return Ecc.HIGH;
+                default:
+                    return Ecc.LOW;
+            }
+        }
+        return Ecc.LOW;
     }
 
 }
