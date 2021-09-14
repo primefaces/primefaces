94817a9e77242a33a06aeb30b40da913e75a1a8c
burak <m.buunsal@gmail.com>
2021-09-14 16:25:17 +0300

    jsdoc

--- primefaces/src/main/resources/META-INF/resources/primefaces/speeddial/speeddial.js
+++ primefaces/src/main/resources/META-INF/resources/primefaces/speeddial/speeddial.js
@@ -1,12 +1,32 @@
 /**
  * __PrimeFaces SpeedDial Widget__
  *
- * SpeedDial when pressed, a floating action button can display multiple primary actions that can be performed on a page.
+ * SpeedDial is a component that consists of many actions and a floating action button.
+ * When pressed, a floating action button can display multiple primary actions that can be performed on a page.
+ *
+ * @prop {JQuery} mask The DOM element for the mask of the speeddial.
+ * @prop {JQuery} container The DOM element for the container of the speeddial that contains item container and button.
+ * @prop {JQuery} button The DOM element for the floating action button of the speeddial.
+ * @prop {JQuery} buttonIcon The DOM element for the icon of the floating action button of the speeddial.
+ * @prop {JQuery} itemContainer The DOM element for the item container of the speeddial.
+ * @prop {JQuery} items The DOM elements for the speeddial items.
+ * @prop {number} itemsCount The number of action items.
+ * @prop {boolean} visible Whether overlay is visible or not.
  *
  * @interface {PrimeFaces.widget.SpeedDialCfg} cfg The configuration for the {@link  SpeedDial| SpeedDial widget}.
  * You can access this configuration via {@link PrimeFaces.widget.BaseWidget.cfg|BaseWidget.cfg}. Please note that this
  * configuration is usually meant to be read-only and should not be modified.
  * @extends {PrimeFaces.widget.BaseWidgetCfg} cfg
+ *
+ * @prop {boolean} cfg.visible Specifies the visibility of the overlay.
+ * @prop {string} cfg.direction Specifies the opening direction of actions.
+ * @prop {number} cfg.transitionDelay Transition delay step for each action item.
+ * @prop {string} cfg.type Specifies the opening type of actions.
+ * @prop {number} cfg.radius Radius for *circle types.
+ * @prop {boolean} cfg.mask Whether to show a mask element behind the speeddial.
+ * @prop {boolean} cfg.hideOnClickOutside Whether the actions close when clicked outside.
+ * @prop {boolean} cfg.rotateAnimation Sets rotate for show icon.
+ * @prop {string} cfg.showIcon Show icon class of the button element.
  *
  */
 PrimeFaces.widget.SpeedDial = PrimeFaces.widget.DeferredWidget.extend({
@@ -48,6 +68,22 @@ PrimeFaces.widget.SpeedDial = PrimeFaces.widget.DeferredWidget.extend({
      * @inheritdoc
      */
     _render: function() {
+        this.createItemContainerStyle();
+        this.updateItemStyles();
+
+        if (this.cfg.hideOnClickOutside) {
+            this.bindDocumentClickListener();
+        }
+
+        this.bindEvents();
+
+    },
+
+    /**
+     * Creates responsive style of the item container.
+     * @private
+     */
+    createItemContainerStyle: function () {
         if (this.cfg.type !== 'linear') {
             var button = this.button.get(0);
             var firstItem = this.items.get(0);
@@ -59,17 +95,12 @@ PrimeFaces.widget.SpeedDial = PrimeFaces.widget.DeferredWidget.extend({
                 this.itemContainer.get(0).style.setProperty('--item-diff-y', hDiff / 2 + 'px');
             }
         }
-
-        this.updateItemStyles();
-
-        if (this.cfg.hideOnClickOutside) {
-            this.bindDocumentClickListener();
-        }
-
-        this.bindEvents();
-
     },
 
+    /**
+     * Updates styles of the action items.
+     * @private
+     */
     updateItemStyles: function () {
         var $this = this;
 
@@ -95,10 +126,9 @@ PrimeFaces.widget.SpeedDial = PrimeFaces.widget.DeferredWidget.extend({
         });
     },
 
-    isVisible: function() {
-        return this.visible;//event if check
-    },
-
+    /**
+     * Shows item container of the speeddial and changes or rotates floating action button icon.
+     */
     show: function() {
         if (this.cfg.hideIcon) {
             this.buttonIcon.removeClass(this.cfg.showIcon);
@@ -122,6 +152,9 @@ PrimeFaces.widget.SpeedDial = PrimeFaces.widget.DeferredWidget.extend({
         }
     },
 
+    /**
+     * Hides item container of the speeddial and changes or rotates floating action button icon.
+     */
     hide: function() {
         if (this.cfg.hideIcon) {
             this.buttonIcon.removeClass(this.cfg.hideIcon);
@@ -145,29 +178,42 @@ PrimeFaces.widget.SpeedDial = PrimeFaces.widget.DeferredWidget.extend({
         }
     },
 
-    onClick: function(e) {
+    /**
+     * Changes visibility of the item container.
+     * @private
+     * @param {Event} event Event that occurred.
+     */
+    onClick: function(event) {
         this.container.css('z-index', PrimeFaces.nextZindex());
-        this.isVisible() ? this.hide() : this.show();
+        this.visible ? this.hide() : this.show();
 
         if(this.cfg.onClick) {
-            this.cfg.onClick.call(this, e);
+            this.cfg.onClick.call(this, event);
         }
 
         this.isItemClicked = true;
     },
 
-    onItemClick: function(e, item) {
+    /**
+     * Hides item container of the speeddial.
+     * @private
+     */
+    onItemClick: function() {
         this.hide();
 
         this.isItemClicked = true;
     },
 
+    /**
+     * Adds the outside click event listener to the document.
+     * @private
+     */
     bindDocumentClickListener: function() {
         var $this = this;
 
         if (!this.documentClickListener) {
             this.documentClickListener = function (event) {
-                if ($this.isVisible() && $this.isOutsideClicked(event)) {
+                if ($this.visible && $this.isOutsideClicked(event)) {
                     $this.hide();
                 }
 
@@ -177,18 +223,35 @@ PrimeFaces.widget.SpeedDial = PrimeFaces.widget.DeferredWidget.extend({
         }
     },
 
+    /**
+     * Returns whether outside is clicked or not.
+     * @private
+     * @return {boolean} outside is clicked.
+     */
     isOutsideClicked: function(event) {
         var containerEl = this.container.get(0);
         return containerEl && !(containerEl.isSameNode(event.target) || containerEl.contains(event.target) || this.isItemClicked);
     },
 
+    /**
+     * Calculates transition delay of the action items according to items' index.
+     * @private
+     * @param {number} index index of the action item element
+     * @return {string} point styles of the action item
+     */
     calculateTransitionDelay: function(index) {
         var length = this.itemsCount;
-        var visible = this.isVisible();
+        var visible = this.visible;
 
         return (visible ? index : length - index - 1) * this.cfg.transitionDelay;
     },
 
+    /**
+     * Calculates point styles of the action items according to items' index.
+     * @private
+     * @param {number} index index of the action item element
+     * @return {string} point styles of the action item
+     */
     calculatePointStyle: function(index) {
         var type = this.cfg.type;
 
@@ -245,6 +308,12 @@ PrimeFaces.widget.SpeedDial = PrimeFaces.widget.DeferredWidget.extend({
         return {};
     },
 
+    /**
+     * Retrieves styles of the item according to items' index.
+     * @private
+     * @param {number} index index of the action item element
+     * @return {string} styles of the action item
+     */
     getItemStyle: function(index) {
         var transitionDelay = this.calculateTransitionDelay(index);
         var pointStyle = this.calculatePointStyle(index);
