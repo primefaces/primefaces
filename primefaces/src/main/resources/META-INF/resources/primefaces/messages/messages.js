edebd4d1855f6be468a1f1498853c1ec3d52a063
Thomas Andraschko <tandraschko@apache.org>
2021-11-04 10:46:16 +0100

    #7798

--- primefaces/src/main/resources/META-INF/resources/primefaces/messages/messages.js
+++ primefaces/src/main/resources/META-INF/resources/primefaces/messages/messages.js
@@ -1,16 +1,14 @@
 /**
- * __PrimeFaces Message Widget__
+ * __PrimeFaces Messages Widget__
  * 
- * Message is a pre-skinned extended version of the standard JSF message component.
+ * Messages is a pre-skinned extended version of the standard JSF messages component.
  * 
- * @interface {PrimeFaces.widget.MessageCfg} cfg The configuration for the {@link  Message| Message widget}.
+ * @interface {PrimeFaces.widget.MessagesCfg} cfg The configuration for the {@link  Messages| Messages widget}.
  * You can access this configuration via {@link PrimeFaces.widget.BaseWidget.cfg|BaseWidget.cfg}. Please note that this
  * configuration is usually meant to be read-only and should not be modified.
  * @extends {PrimeFaces.widget.BaseWidgetCfg} cfg
- * 
- * @prop {string} cfg.target Client ID of the target for which to show this message.
  */
-PrimeFaces.widget.Message = PrimeFaces.widget.BaseWidget.extend({
+PrimeFaces.widget.Messages = PrimeFaces.widget.BaseWidget.extend({
 
     /**
      * @override
@@ -19,17 +17,22 @@ PrimeFaces.widget.Message = PrimeFaces.widget.BaseWidget.extend({
      */
     init: function(cfg) {
         this._super(cfg);
+    },
 
-        var text = this.jq.find('.ui-message-error-detail').text();
-
-        if(text) {
-           var target = $(PrimeFaces.escapeClientId(this.cfg.target));
-
-           if (this.cfg.tooltip) {
-              target.data('tooltip', text);
-           }
-
-           target.attr('aria-describedby', this.id + '_error-detail');
-        }
-   }
+    appendMessage: function(message, type) {
+        this.jq.append(
+             '<div class="ui-messages-' + type + ' ui-corner-all">' +
+                '<a href="#" class="ui-messages-close" onclick="$(this).parent().slideUp();return false;">' +
+                    '<span class="ui-icon ui-icon-close"></span>' +
+                '</a>' +
+                '<span class="ui-messages-' + type + '-icon"></span>' +
+                '<ul>' +
+                    '<li>' +
+                        '<span class="ui-messages-' + type + '-summary">' +
+                        message +
+                        '</span>' +
+                    '</li>' +
+                '</ul>' +
+            '</div>');
+    }
 });
