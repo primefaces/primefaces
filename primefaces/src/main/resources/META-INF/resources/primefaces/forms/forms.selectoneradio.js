cc80595805391bad20d93bfdc70f0e676d34d3fc
Jasper de Vries <jepsar@gmail.com>
2022-02-03 08:01:10 +0100

    Fix selected of both referenced and facet + unbind events

--- primefaces/src/main/resources/META-INF/resources/primefaces/forms/forms.selectoneradio.js
+++ primefaces/src/main/resources/META-INF/resources/primefaces/forms/forms.selectoneradio.js
@@ -174,12 +174,17 @@ PrimeFaces.widget.SelectOneRadio = PrimeFaces.widget.BaseWidget.extend({
             e.preventDefault();
         });
 
+        // for custom radio buttons, transfer the focus from original to the matching custom button
         $(this.originalInputs).on('focus.selectOneRadio', function() {
-            var radio = $this.jq.find('.ui-radiobutton-box.ui-state-active').parent().find(':radio');
+            // 1 = JSF separator, 2 = index
+            var matches = this.id.match(/^.*([^\d])(\d)+$/);
+            // first try to get the active radio button
+            var radio = $(':radio[id="' + $this.id + matches[1] + matches[2] + '_clone"]')
+                    .parent().next().filter('.ui-state-active').parent().find(':radio');
+            // else the first or last based on the index
             if (!radio.length) {
-                var first = this.id.endsWith(':0');
                 var inputs = $this.enabledInputs;
-                radio = first ? inputs.first() : inputs.last();
+                radio = matches[2] === '0' ? inputs.first() : inputs.last();
             }
             radio.trigger('focus');
         });
@@ -287,6 +292,7 @@ PrimeFaces.widget.SelectOneRadio = PrimeFaces.widget.BaseWidget.extend({
             this.inputs.off();
             this.labels.off();
             this.outputs.off();
+            $(this.originalInputs).off();
         }
     },
 
