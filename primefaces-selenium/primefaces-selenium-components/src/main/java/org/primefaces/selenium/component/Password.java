3097f0707e75f219d3ad3f59aa402bbb1fab4318
Christoph Straßer <christoph.strasser78@gmail.com>
2022-02-16 21:31:49 +0100

    figuring out which condition fails

--- primefaces-selenium/primefaces-selenium-components/src/main/java/org/primefaces/selenium/component/Password.java
+++ primefaces-selenium/primefaces-selenium-components/src/main/java/org/primefaces/selenium/component/Password.java
@@ -54,6 +54,7 @@ public abstract class Password extends InputText {
          * "Timeout Expected condition failed" on Github Actions with Selenium 4 and Firefox 96.
          */
         PrimeSelenium.waitGui().until(ExpectedConditions.and(
+                ExpectedConditions.visibilityOf(getFeedbackPanel()),
                 //PrimeExpectedConditions.documentLoaded(),
                 PrimeExpectedConditions.animationNotActive(),
                 PrimeExpectedConditions.ajaxQueueEmpty()));
