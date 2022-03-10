d7c2755cb442ec13819cab99a5e8672e89fce4ab
mertsincan <sincan.mert@gmail.com>
2022-03-10 00:25:30 +0000

    Refactor

--- primefaces/src/main/resources/META-INF/resources/primefaces/menu/menu.breadcrumb.js
+++ primefaces/src/main/resources/META-INF/resources/primefaces/menu/menu.breadcrumb.js
@@ -3,6 +3,8 @@
  * 
  * BreadCrumb provides contextual information about the page hierarchy.
  *
+ * @prop {JQuery} seoScript The script element for the seo.
+ * 
  * @interface {PrimeFaces.widget.BreadCrumbCfg} cfg The configuration for the {@link  BreadCrumb| BreadCrumb widget}.
  * You can access this configuration via {@link PrimeFaces.widget.BaseWidget.cfg|BaseWidget.cfg}. Please note that this
  * configuration is usually meant to be read-only and should not be modified.
