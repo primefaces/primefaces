03875cbf9ea1125b26f0b0b5cbe3560550c3eed9
Jesse McCormick <koan00@gmail.com>
2022-03-16 17:44:24 -0400

    Fix PF #8031. table.getColumnMeta() returns columns out of order and does not contain the priority information we need. Instead query the table directly via table.forEachColumn(). This gives us the priority and also return the columns in the order they are defined in the markup.

--- primefaces/src/main/java/org/primefaces/component/export/TableExporter.java
+++ primefaces/src/main/java/org/primefaces/component/export/TableExporter.java
@@ -24,8 +24,9 @@
 package org.primefaces.component.export;
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Consumer;
-import java.util.stream.Collectors;
+
 
 import javax.el.MethodExpression;
 import javax.faces.component.UIComponent;
@@ -95,33 +96,35 @@ public abstract class TableExporter<T extends UIComponent & UITable> extends Exp
 
         List<UIColumn> allColumns = table.getColumns();
         List<UIColumn> exportcolumns = new ArrayList<>(allColumns.size());
-        Map<String, ColumnMeta> columnMetadata = table.getColumnMeta();
         boolean visibleColumnsOnly = getExportConfiguration().isVisibleOnly();
+        AtomicBoolean hasNonDefaultSortPriorities = new AtomicBoolean(false);
+        final List<ColumnMeta> visibleColumnMetadata = new ArrayList<>(allColumns.size());
 
-        if (columnMetadata == null || columnMetadata.isEmpty()) {
-            table.forEachColumn(true, true, true, col -> {
-                if (col.isExportable() && (!visibleColumnsOnly || (visibleColumnsOnly && col.isVisible()))) {
-                    exportcolumns.add(col);
+        table.forEachColumn(true, true, true, column -> {
+            if (column.isExportable() && (!visibleColumnsOnly || (visibleColumnsOnly && column.isVisible()))) {
+                String metaColumnKey = column.getColumnKey();
+                int displayPriority = column.getDisplayPriority();
+                ColumnMeta metaCopy = new ColumnMeta(column.getColumnKey());
+                metaCopy.setDisplayPriority(displayPriority);
+                visibleColumnMetadata.add(metaCopy);
+                if (displayPriority != 0) {
+                    hasNonDefaultSortPriorities.set(true);
                 }
-                return true;
-            });
-        }
-        else {
+            }
+            return true;
+        });
+
+        if (hasNonDefaultSortPriorities.get()) {
             // sort by display priority
             Comparator<Integer> sortIntegersNaturallyWithNullsLast = Comparator.nullsLast(Comparator.naturalOrder());
-            List<ColumnMeta> columnMetas = columnMetadata.values().stream().
-                        sorted(Comparator.comparing(ColumnMeta::getDisplayPriority, sortIntegersNaturallyWithNullsLast))
-                        .collect(Collectors.toList());
+            visibleColumnMetadata.sort(Comparator.comparing(ColumnMeta::getDisplayPriority, sortIntegersNaturallyWithNullsLast));
+        }
 
-            for (ColumnMeta meta : columnMetas) {
-                String metaColumnKey = meta.getColumnKey();
-                table.invokeOnColumn(metaColumnKey, ((UIData) table).getRowIndex(), column -> {
-                    if (column.isRendered() && column.isExportable() &&
-                                (!visibleColumnsOnly || (visibleColumnsOnly && column.isVisible()))) {
-                        exportcolumns.add(column);
-                    }
-                });
-            }
+        for (ColumnMeta meta : visibleColumnMetadata) {
+            String metaColumnKey = meta.getColumnKey();
+            table.invokeOnColumn(metaColumnKey, ((UIData) table).getRowIndex(), column -> {
+                exportcolumns.add(column);
+            });
         }
 
         exportableColumns.put(table, exportcolumns);
