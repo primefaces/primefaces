40d60d231ba0749b39003acb1a7e76296343a699
Thomas Andraschko <tandraschko@apache.org>
2022-03-22 14:43:00 +0100

    #8589

---
+++ primefaces/src/main/java/org/primefaces/util/IOUtils.java
@@ -0,0 +1,73 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2009-2021 PrimeTek
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.primefaces.util;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.nio.charset.Charset;
+
+public class IOUtils {
+
+    private IOUtils() {
+
+    }
+
+    public static String toString(InputStream is, Charset charset) throws IOException {
+        char[] buffer = new char[1024];
+
+        StringBuilder out = new StringBuilder();
+        Reader in = new InputStreamReader(is, charset);
+        int read = 0;
+        while ((read = in.read(buffer, 0, buffer.length)) != -1) {
+            out.append(buffer, 0, read);
+        }
+
+        return out.toString();
+    }
+
+    public static byte[] toByteArray(InputStream input) throws IOException {
+        byte[] buffer = new byte[1024];
+
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        int read = 0;
+        while ((read = input.read(buffer)) != -1) {
+            output.write(buffer, 0, read);
+        }
+
+        return output.toByteArray();
+    }
+
+    public static void copyLarge(InputStream input, OutputStream output) throws IOException {
+        byte[] buffer = new byte[1024 * 4];
+
+        int read = 0;
+        while ((read = input.read(buffer)) != -1) {
+            output.write(buffer, 0, read);
+        }
+    }
+}
