7aed91fc87b282697701b66d4c2a77b6ae3af301
Melloware <mellowaredev@gmail.com>
2022-04-13 09:26:55 -0400

    Fix #7011: FloatLabel for SelectOne, Chips, InputMask (#8669)
    * Fix #7011: FloatLabel for SelectOne, Chips, InputMask
    
    * Fix #7011: FloatLabel for SelectOne, Chips, InputMask
    
    * Fix #7011: FloatLabel for SelectOne, Chips, InputMask
    
    * Fix #7011: FloatLabel for SelectOne, Chips, InputMask

--- primefaces/src/main/resources/META-INF/resources/primefaces/core/core.utils.js
+++ primefaces/src/main/resources/META-INF/resources/primefaces/core/core.utils.js
@@ -837,6 +837,38 @@ if (!PrimeFaces.utils) {
                 return 0;
             }
             return value || defaultValue;
+        },
+
+        /**
+         * Is this component wrapped in a float label?
+         *
+         * @param {JQuery | undefined | null} jq An element to check if wrapped in float label. 
+         * @return {boolean} true this this JQ has a float label parent
+         */
+        hasFloatLabel: function(jq) {
+            if (!jq || !jq.parent()) {
+                return false;
+            }
+            return jq.parent().hasClass('ui-float-label');
+        },
+
+        /**
+         * Handles floating label CSS if wrapped in a floating label.
+         * @private
+         * @param {JQuery | undefined} element the to add the CSS classes to
+         * @param {JQuery | undefined} input the input to check if filled
+         * @param {boolean | undefined} hasFloatLabel true if this is wrapped in a floating label
+         */
+        updateFloatLabel: function(element, input, hasFloatLabel) {
+            if (!element || !input || !hasFloatLabel) {
+                return;
+            }
+            if (input.val() !== '' || element.find('.ui-chips-token').length !== 0) {
+                element.addClass('ui-inputwrapper-filled');
+            }
+            else {
+                element.removeClass('ui-inputwrapper-filled');
+            }
         }
     };
 
