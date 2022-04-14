b7134cb01fc8b93e6580b0e6827668ed488fe7f3
Thomas Andraschko <tandraschko@apache.org>
2022-04-14 14:45:55 +0200

    avoid string concat each request

--- primefaces/src/main/java/org/primefaces/webapp/filter/NoCacheFilter.java
+++ primefaces/src/main/java/org/primefaces/webapp/filter/NoCacheFilter.java
@@ -35,7 +35,7 @@ public class NoCacheFilter extends HttpFilter {
 
     @Override
     public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
-        if (!request.getRequestURI().startsWith(request.getContextPath() + ResourceHandler.RESOURCE_IDENTIFIER)) { // Skip JSF resources (CSS/JS/Images/etc)
+        if (!request.getRequestURI().contains(ResourceHandler.RESOURCE_IDENTIFIER)) { // Skip JSF resources (CSS/JS/Images/etc)
             response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
             response.setHeader("Pragma", "no-cache"); // HTTP 1.0.
             response.setDateHeader("Expires", 0); // Proxies.
