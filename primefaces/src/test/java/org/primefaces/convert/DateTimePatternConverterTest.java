1bf1db043d96fda52228aba7fbad2f1f341af00e
tandraschko <tandraschko@apache.org>
2022-07-28 09:18:51 +0200

    fixed junit

--- primefaces/src/test/java/org/primefaces/convert/DateTimePatternConverterTest.java
+++ primefaces/src/test/java/org/primefaces/convert/DateTimePatternConverterTest.java
@@ -416,7 +416,7 @@ public class DateTimePatternConverterTest {
     }
 
     @BeforeEach
-    private void setup() {
+    public void setup() {
         this.converter = new DateTimePatternConverter();
     }
 
