7c533903c6bdbc17f38e186fc3c6891b40a22a8f
Melloware <mellowaredev@gmail.com>
2022-07-28 18:25:00 -0400

    Fix #9043: Paginator keep selected option accurate (#9045)

--- primefaces/src/main/resources/META-INF/resources/primefaces/paginator/paginator.js
+++ primefaces/src/main/resources/META-INF/resources/primefaces/paginator/paginator.js
@@ -438,6 +438,7 @@ PrimeFaces.widget.Paginator = PrimeFaces.widget.BaseWidget.extend({
      * @param {number} rpp Number of rows per page to set.
      */
     setRowsPerPage: function(rpp) {
+        this.rppSelect.find('option').removeAttr('selected');
         if (rpp === '*') {
             this.cfg.rows = this.cfg.rowCount;
             this.cfg.pageCount = 1;
@@ -462,6 +463,7 @@ PrimeFaces.widget.Paginator = PrimeFaces.widget.BaseWidget.extend({
 
             this.setPage(page);
         }
+        this.rppSelect.find('option[value="'+rpp+'"]').attr('selected', 'selected');
     },
 
     /**
