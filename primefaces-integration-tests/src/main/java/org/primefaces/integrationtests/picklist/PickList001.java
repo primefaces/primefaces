4101837b991efcf35bf9b429b972b104a95ef76d
Melloware <mellowaredev@gmail.com>
2022-08-24 15:53:56 -0400

    Fix #9135: Picklist Selenium component and tests (#9136)

---
+++ primefaces-integration-tests/src/main/java/org/primefaces/integrationtests/picklist/PickList001.java
@@ -0,0 +1,69 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2009-2022 PrimeTek
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.primefaces.integrationtests.picklist;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.primefaces.integrationtests.general.model.Driver;
+import org.primefaces.integrationtests.general.service.RealDriverService;
+import org.primefaces.model.DualListModel;
+
+import lombok.Data;
+
+@Named
+@ViewScoped
+@Data
+public class PickList001 implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Inject
+    private RealDriverService driverService;
+
+    private DualListModel<Driver> standardPickList;
+    private DualListModel<Driver> checkPickList;
+    private DualListModel<Driver> instantPickList;
+
+    @PostConstruct
+    public void init() {
+        final List<Driver> standardSource = new ArrayList<>(driverService.getDrivers());
+        final List<Driver> standardTarget = new ArrayList<>();
+        standardPickList = new DualListModel<>(standardSource, standardTarget);
+
+        final List<Driver> checkSource = new ArrayList<>(driverService.getDrivers());
+        final List<Driver> checkTarget = new ArrayList<>();
+        checkPickList = new DualListModel<>(checkSource, checkTarget);
+
+        final List<Driver> instantSource = new ArrayList<>(driverService.getDrivers());
+        final List<Driver> instantTarget = new ArrayList<>();
+        instantPickList = new DualListModel<>(instantSource, instantTarget);
+    }
+}
