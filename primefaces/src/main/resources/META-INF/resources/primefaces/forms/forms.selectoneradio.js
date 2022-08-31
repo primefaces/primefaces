c880a5634f1516efec60f4aeed871cbc5caa575e
Jasper de Vries <jepsar@gmail.com>
2022-08-31 21:30:53 +0200

    Switching branch

--- primefaces/src/main/resources/META-INF/resources/primefaces/forms/forms.selectoneradio.js
+++ primefaces/src/main/resources/META-INF/resources/primefaces/forms/forms.selectoneradio.js
@@ -46,8 +46,10 @@ PrimeFaces.widget.SelectOneRadio = PrimeFaces.widget.BaseWidget.extend({
             //labels
             var base = this.facet ? this.inputs : this.outputs;
             for(var i=0; i < base.length; i++) {
-                this.labels = this.labels.add('label[for="' +
-                        (this.facet ? base.eq(i).attr('id') : base.eq(i).parent().attr('id')) + '"]');
+                this.labels = this.labels.add('label[for="' + ( this.facet
+                    ? base.eq(i).attr('id')
+                    : this.id + ':' + base.eq(i).prev().find('.ui-radio-clone').data('itemindex') + '_clone'
+                ) + '"]');
             }
 
             //update radio state
