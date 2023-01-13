24d4410582ea9049b0c0b1a7607f448e27ff685b
Melloware <mellowaredev@gmail.com>
2023-01-13 12:33:46 -0500

    Fix #9663: Jakarta replace javax.validation constant (#9664)

--- primefaces/src/main/java/org/primefaces/validate/bean/NegativeOrZeroClientValidationConstraint.java
+++ primefaces/src/main/java/org/primefaces/validate/bean/NegativeOrZeroClientValidationConstraint.java
@@ -31,8 +31,8 @@ public class NegativeOrZeroClientValidationConstraint extends AbstractClientVali
 
     public static final String CONSTRAINT_ID = "NegativeOrZero";
     public static final String MESSAGE_METADATA = "data-p-negativeorzero-msg";
-    public static final String CONSTRAINT_CLASS_NAME = CONSTRAINT_PACKAGE  + "." + CONSTRAINT_ID;
-    public static final String MESSAGE_ID = "{" + CONSTRAINT_CLASS_NAME + ".message}";
+    public static final String CONSTRAINT_CLASS_NAME = String.format("%s.%s", javax.validation.constraints.NotNull.class.getPackage().getName(), CONSTRAINT_ID);
+    public static final String MESSAGE_ID =  String.format("{%s.%s}", CONSTRAINT_CLASS_NAME, ATTR_MESSAGE);
     public static final String MAX_VALUE = "0";
 
     public NegativeOrZeroClientValidationConstraint() {
