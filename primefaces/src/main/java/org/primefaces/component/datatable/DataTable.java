5b5255be1e16fa06c38aeb5e63c708211f44530f
Sébastien Lepage <sebastien.lepage@outlook.com>
2023-03-06 00:17:06 +0100

    Fix #3409 - Datatable: ColumnGroup refactoring

--- primefaces/src/main/java/org/primefaces/component/datatable/DataTable.java
+++ primefaces/src/main/java/org/primefaces/component/datatable/DataTable.java
@@ -1169,6 +1169,7 @@ public class DataTable extends DataTableBase {
     public List<UIColumn> collectColumns() {
         List<UIColumn> columnsTmp = new ArrayList<>();
         fetchColumnsRecursively(this, columnsTmp::add);
+        columnsTmp.sort(ColumnComparators.displayOrder(getColumnMeta()));
         return columnsTmp;
     }
 
