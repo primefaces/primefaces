c154ffe3e7bd4447a0534bd52565dac20ebf18b3
Sébastien Lepage <sebastien.lepage@outlook.com>
2023-04-12 12:57:14 +0200

    something something

--- primefaces/src/main/java/org/primefaces/util/LangUtils.java
+++ primefaces/src/main/java/org/primefaces/util/LangUtils.java
@@ -553,12 +553,4 @@ public class LangUtils {
 
         throw new IllegalArgumentException("Cannot find field " + name + " in " + clazz.getName());
     }
-
-    public static <R> R rethrow(Throwable throwable) {
-        return typeErasure(throwable);
-    }
-
-    private static <R, T extends Throwable> R typeErasure(Throwable throwable) throws T {
-        throw (T) throwable;
-    }
 }
