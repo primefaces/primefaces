3235b5df667cf56932d51850fcbb483437b8c582
Jasper de Vries <jepsar@gmail.com>
2023-06-16 09:17:51 +0200

    Revert "#10227 SelectOneRadio: remove `plain` attribute (#10237)"
    This reverts commit 217eeafc220952c892c181d3574f49b2570ac431.

--- primefaces/src/main/java/org/primefaces/component/radiobutton/RadioButton.java
+++ primefaces/src/main/java/org/primefaces/component/radiobutton/RadioButton.java
@@ -26,6 +26,7 @@ package org.primefaces.component.radiobutton;
 import javax.faces.application.ResourceDependency;
 import javax.faces.component.UINamingContainer;
 import javax.faces.context.FacesContext;
+import org.primefaces.component.selectoneradio.SelectOneRadio;
 import org.primefaces.expression.SearchExpressionFacade;
 
 @ResourceDependency(library = "primefaces", name = "components.css")
@@ -39,8 +40,11 @@ public class RadioButton extends RadioButtonBase {
     @Override
     public String getInputClientId() {
         FacesContext context = FacesContext.getCurrentInstance();
-        return SearchExpressionFacade.resolveComponent(context, this, getFor()).getClientId(context)
-                + UINamingContainer.getSeparatorChar(context) + getItemIndex() + "_clone";
+        // #10227
+        SelectOneRadio radio = (SelectOneRadio) SearchExpressionFacade.resolveComponent(context, this, getFor());
+        return radio.getClientId(context)
+                + UINamingContainer.getSeparatorChar(context) + getItemIndex()
+                + (radio.isPlain() ? "" : "_clone");
     }
 
     @Override
