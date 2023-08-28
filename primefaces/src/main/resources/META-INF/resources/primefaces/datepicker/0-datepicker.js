2519ea64a57f38b70d0b8f42cdd7c436b4fd7e63
melloware <mellowaredev@gmail.com>
2023-08-28 07:51:57 -0400

    Fixed #10561 - 13.0.1 Datepicker with autoMonthFormat is not working as expected

--- primefaces/src/main/resources/META-INF/resources/primefaces/datepicker/0-datepicker.js
+++ primefaces/src/main/resources/META-INF/resources/primefaces/datepicker/0-datepicker.js
@@ -1842,14 +1842,14 @@
         
         onInputChange: function(event) {
             // TODO: The following code block will be rearranged according to PrimeNG/React/Vue library in future versions.
-            if (this.options.autoMonthFormat && !this.options.showMinMaxRange && this.options.monthNavigator && this.options.view !== 'month') { 
+            if ((this.options.autoMonthFormat || !this.inputfield.val() || this.options.showMinMaxRange) && this.options.monthNavigator && this.options.view !== 'month') { 
                 var viewMonth = this.viewDate.getMonth();
                 viewMonth = (this.isInMaxYear() && Math.min(this.options.maxDate.getMonth(), viewMonth)) || (this.isInMinYear() && Math.max(this.options.minDate.getMonth(), viewMonth)) || viewMonth;
                 this.viewDate.setMonth(viewMonth);
             }
 
             !this.options.keepInvalid && this.inputfield.val(this.getValueToRender());
-            
+
             if (this.options.onChange) {
                 this.options.onChange.call(this, event);
             }
@@ -2828,7 +2828,7 @@
 
             this.viewDate = value;
 
-            if (this.options.autoMonthFormat && this.options.showMinMaxRange && this.options.monthNavigator && this.options.view !== 'month') {
+            if ((this.options.autoMonthFormat || !this.inputfield.val() || this.options.showMinMaxRange) && this.options.monthNavigator && this.options.view !== 'month') {
                 var viewMonth = this.viewDate.getMonth();
                 viewMonth = (this.isInMaxYear() && Math.min(this.options.maxDate.getMonth(), viewMonth)) || (this.isInMinYear() && Math.max(this.options.minDate.getMonth(), viewMonth)) || viewMonth;
                 this.viewDate.setMonth(viewMonth);
