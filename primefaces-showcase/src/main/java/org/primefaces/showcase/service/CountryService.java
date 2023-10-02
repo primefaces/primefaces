43712824e9b86b5bf4f81f53b30d9c5d888bbd93
melloware <mellowaredev@gmail.com>
2023-10-02 17:21:27 -0400

    Hebrew locale

--- primefaces-showcase/src/main/java/org/primefaces/showcase/service/CountryService.java
+++ primefaces-showcase/src/main/java/org/primefaces/showcase/service/CountryService.java
@@ -93,7 +93,7 @@ public class CountryService {
         locales.add(CountryService.toCountry(new Locale("hr", "HR")));
         locales.add(CountryService.toCountry(new Locale("ja", "JP")));
         locales.add(CountryService.toCountry(new Locale("hu", "HU")));
-        locales.add(CountryService.toCountry(new Locale("iw", "IL"), true));
+        locales.add(CountryService.toCountry(new Locale("he", "IL"), true));
         locales.add(CountryService.toCountry(new Locale("ka", "GE")));
         locales.add(CountryService.toCountry(new Locale("ky", "KG")));
         locales.add(CountryService.toCountry(new Locale("lt", "LT")));
