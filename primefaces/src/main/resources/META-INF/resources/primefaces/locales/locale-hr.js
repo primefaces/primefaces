bcd35fda2dfdc648ed208b54a5a0547daab687af
Thomas Andraschko <tandraschko@apache.org>
2023-10-31 10:45:03 +0100

    #10930

--- primefaces/src/main/resources/META-INF/resources/primefaces/locales/locale-hr.js
+++ primefaces/src/main/resources/META-INF/resources/primefaces/locales/locale-hr.js
@@ -1,30 +1,32 @@
-/** Croatian */
-PrimeFaces.locales['hr'] = {
-    monthNames: ['Siječanj', 'Veljača', 'Ožujak', 'Travanj', 'Svibanj', 'Lipanj', 'Srpanj', 'Kolovoz', 'Rujan', 'Listopad', 'Studeni', 'Prosinac'],
-    monthNamesShort: ['Sij', 'Velj', 'Ožu', 'Tra', 'Svi', 'Lip', 'Srp', 'Kol', 'Ruj', 'Lis', 'Stu', 'Pro'],
-    dayNames: ['Nedjelja', 'Ponedjeljak', 'Utorak', 'Srijeda', 'Četvrtak', 'Petak', 'Subota'],
-    dayNamesShort: ['Ne', 'Po', 'Ut', 'Sr', 'Če', 'Pe', 'Su'],
-    dayNamesMin: ['N', 'P', 'U', 'S ', 'Č', 'P ', 'S'],
-    weekHeader: 'Tjedan',
-    firstDayOfWeek: 1,
-    isRTL: false,
-    showMonthAfterYear: false,
-    yearSuffix: '',
-    timeOnlyTitle: 'Samo vrijeme',
-    timeText: 'Vrijeme',
-    hourText: 'Sat',
-    minuteText: 'Minuta',
-    secondText: 'Sekunda',
-    millisecondText: 'Milisekunda',
-    today: 'Sada',
-    month: 'Mjesec',
-    week: 'Tjedan',
-    day: 'Dan',
-    allDayText: 'Cijeli dan',
-    clear: 'Resetiraj',
-    "aria": {
-        close: 'Zatvori',
-        previous: 'Prethodni',
-        next: 'Slijedeći',
-    }
-};
+if (window.PrimeFaces) {
+    /** Croatian */
+    PrimeFaces.locales['hr'] = {
+        monthNames: ['Siječanj', 'Veljača', 'Ožujak', 'Travanj', 'Svibanj', 'Lipanj', 'Srpanj', 'Kolovoz', 'Rujan', 'Listopad', 'Studeni', 'Prosinac'],
+        monthNamesShort: ['Sij', 'Velj', 'Ožu', 'Tra', 'Svi', 'Lip', 'Srp', 'Kol', 'Ruj', 'Lis', 'Stu', 'Pro'],
+        dayNames: ['Nedjelja', 'Ponedjeljak', 'Utorak', 'Srijeda', 'Četvrtak', 'Petak', 'Subota'],
+        dayNamesShort: ['Ne', 'Po', 'Ut', 'Sr', 'Če', 'Pe', 'Su'],
+        dayNamesMin: ['N', 'P', 'U', 'S ', 'Č', 'P ', 'S'],
+        weekHeader: 'Tjedan',
+        firstDayOfWeek: 1,
+        isRTL: false,
+        showMonthAfterYear: false,
+        yearSuffix: '',
+        timeOnlyTitle: 'Samo vrijeme',
+        timeText: 'Vrijeme',
+        hourText: 'Sat',
+        minuteText: 'Minuta',
+        secondText: 'Sekunda',
+        millisecondText: 'Milisekunda',
+        today: 'Sada',
+        month: 'Mjesec',
+        week: 'Tjedan',
+        day: 'Dan',
+        allDayText: 'Cijeli dan',
+        clear: 'Resetiraj',
+        "aria": {
+            close: 'Zatvori',
+            previous: 'Prethodni',
+            next: 'Slijedeći',
+        }
+    };
+}
