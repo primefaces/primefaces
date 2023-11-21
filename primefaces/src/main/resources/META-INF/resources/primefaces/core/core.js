f8dd6e4044333bd3f298556019428bb3451cfc96
Thomas Andraschko <tandraschko@apache.org>
2023-11-21 13:56:43 +0100

    fixed JS doc generation

--- primefaces/src/main/resources/META-INF/resources/primefaces/core/core.js
+++ primefaces/src/main/resources/META-INF/resources/primefaces/core/core.js
@@ -382,7 +382,7 @@
          * Applies the inline AJAX status (ui-state-loading) to the given widget / button.
          * @param {PrimeFaces.widget.BaseWidget} [widget] the widget.
          * @param {JQuery} [button] The button DOM element.
-         * @param {(widget, settings) => boolean} [isXhrSource] Callback that checks if the widget is the source of the current AJAX request.
+         * @param {(PrimeFaces.widget.BaseWidget, JQuery.AjaxSettings) => boolean} [isXhrSource] Callback that checks if the widget is the source of the current AJAX request.
          */
         bindButtonInlineAjaxStatus: function(widget, button, isXhrSource) {
             if (!isXhrSource) {
