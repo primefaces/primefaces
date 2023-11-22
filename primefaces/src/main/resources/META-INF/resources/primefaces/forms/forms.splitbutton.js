a9fa9ff2649d825f938c6735754507a28c806925
Thomas Andraschko <tandraschko@apache.org>
2023-11-22 10:10:45 +0100

    Fix #10996 - FileUpload: add Inline ajax status (#10999)
    * #10838 fixed null check while testing applications
    
    * #10981
    
    * #10996
    
    * #10996
    
    * fixed JS doc generation
    
    * fixed JS doc generation
    
    * fixed JS doc generation

--- primefaces/src/main/resources/META-INF/resources/primefaces/forms/forms.splitbutton.js
+++ primefaces/src/main/resources/META-INF/resources/primefaces/forms/forms.splitbutton.js
@@ -29,7 +29,7 @@
  * @prop {PrimeFaces.CssTransitionHandler | null} [transition] Handler for CSS transitions used by this widget.
  * @prop {PrimeFaces.UnbindCallback} [resizeHandler] Unbind callback for the resize handler.
  * @prop {PrimeFaces.UnbindCallback} [scrollHandler] Unbind callback for the scroll handler.
- * @prop {number} [ajaxCount] Number of concurrent active Ajax requests.
+ * @forcedProp {number} [ajaxCount] Number of concurrent active Ajax requests.
  *
  * @interface {PrimeFaces.widget.SplitButtonCfg} cfg The configuration for the {@link  SplitButton| SplitButton widget}.
  * You can access this configuration via {@link PrimeFaces.widget.BaseWidget.cfg|BaseWidget.cfg}. Please note that this
@@ -172,38 +172,17 @@ PrimeFaces.widget.SplitButton = PrimeFaces.widget.BaseWidget.extend({
             }
         });
 
-        $this.ajaxCount = 0;
-        $(document).on('pfAjaxSend.' + this.id, function(e, xhr, settings) {
-            if ($this.isXhrSource(settings)) {
-                $this.ajaxCount++;
-                if ($this.ajaxCount > 1) {
-                    return;
-                }
-                $this.button.addClass('ui-state-loading');
-                if ($this.cfg.disableOnAjax !== false) {
-                    $this.disable();
-                }
-                var loadIcon = $('<span class="ui-icon-loading ui-icon ui-c pi pi-spin pi-spinner"></span>');
-                var uiIcon = $this.button.find('.ui-icon');
-                if (uiIcon.length) {
-                    var prefix = 'ui-button-icon-';
-                    loadIcon.addClass(prefix + uiIcon.attr('class').includes(prefix + 'left') ? 'left' : 'right');
-                }
-                $this.button.prepend(loadIcon);
+        PrimeFaces.bindButtonInlineAjaxStatus($this, $this.button, function(widget, settings) {
+            // Checks whether the ID of the button, or one if its menu items equals the source ID from the provided settings.
+            var sourceId = PrimeFaces.ajax.Utils.getSourceId(settings);
+            if (sourceId === null) {
+                return false;
             }
-        }).on('pfAjaxComplete.' + this.id, function(e, xhr, settings, args) {
-            if ($this.isXhrSource(settings)) {
-                $this.ajaxCount--;
-                if ($this.ajaxCount > 0 || args.redirect) {
-                    return;
-                }
-                $this.button.removeClass('ui-state-loading');
-                if ($this.cfg.disableOnAjax !== false && !$this.cfg.disabledAttr) {
-                    $this.enable();
-                }
-                $this.button.find('.ui-icon-loading').remove();
+            if ($this.id === sourceId) {
+                return true;
             }
-        });        
+            return $this.menuitems.find('[id="' + sourceId + '"]').length;
+        });
 
         if(this.cfg.filter) {
             this.setupFilterMatcher();
@@ -212,25 +191,6 @@ PrimeFaces.widget.SplitButton = PrimeFaces.widget.BaseWidget.extend({
 
             this.bindFilterEvents();
         }
-    },
-
-    /**
-     * Checks whether the ID of the button, or one if its menu items equals the source ID from the provided settings.
-     *
-     * @param {JQuery.AjaxSettings} settings containing source ID.
-     * @returns {boolean} `true` if the ID of the button, or one if its menu items equals the source ID from the
-     * provided settings.
-     * @private
-     */
-    isXhrSource: function(settings) {
-        var sourceId = PrimeFaces.ajax.Utils.getSourceId(settings);
-        if (sourceId === null) {
-            return false;
-        }
-        if (this.id === sourceId) {
-            return true;
-        }
-        return this.menuitems.find('[id="' + sourceId + '"]').length;
     },
 
     /**
