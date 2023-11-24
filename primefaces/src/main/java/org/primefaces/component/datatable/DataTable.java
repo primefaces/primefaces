828155b9d113819491d1ebedfe3b9e6782df13b5
Sébastien Lepage <sebastien.lepage@outlook.com>
2023-11-24 11:38:28 +0100

    fix missing import

--- primefaces/src/main/java/org/primefaces/component/datatable/DataTable.java
+++ primefaces/src/main/java/org/primefaces/component/datatable/DataTable.java
@@ -57,10 +57,7 @@ import org.primefaces.event.data.FilterEvent;
 import org.primefaces.event.data.PageEvent;
 import org.primefaces.event.data.SortEvent;
 import org.primefaces.model.*;
-import org.primefaces.util.ComponentUtils;
-import org.primefaces.util.Constants;
-import org.primefaces.util.ELUtils;
-import org.primefaces.util.MapBuilder;
+import org.primefaces.util.*;
 
 @ResourceDependency(library = "primefaces", name = "components.css")
 @ResourceDependency(library = "primefaces", name = "jquery/jquery.js")
