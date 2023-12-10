95a8452728ae125072b69006362d1f83b5fad738
Jasper de Vries <jepsar@gmail.com>
2023-12-10 13:16:51 +0100

    Add StaticMessageCfg.

--- primefaces/src/main/resources/META-INF/resources/primefaces/staticmessage/staticmessage.js
+++ primefaces/src/main/resources/META-INF/resources/primefaces/staticmessage/staticmessage.js
@@ -2,6 +2,11 @@
  * __PrimeFaces StaticMessage Widget__
  * 
  * StaticMessage widget to handle events.
+ *
+ * @interface {PrimeFaces.widget.StaticMessageCfg} cfg The configuration for the {@link StaticMessage| StaticMessage widget}.
+ * You can access this configuration via {@link PrimeFaces.widget.BaseWidget.cfg|BaseWidget.cfg}. Please note that this
+ * configuration is usually meant to be read-only and should not be modified.
+ * @extends {PrimeFaces.widget.BaseWidgetCfg} cfg
  */
 PrimeFaces.widget.StaticMessage = PrimeFaces.widget.BaseWidget.extend({
 
