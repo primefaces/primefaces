6f3bb75008bdf3fcdfacb8c70de2bac549462533
melloware <mellowaredev@gmail.com>
2023-12-13 07:54:48 -0500

    Fix #11115: DatePicker consistent hide()

--- primefaces/src/main/resources/META-INF/resources/primefaces/datepicker/1-datepicker.js
+++ primefaces/src/main/resources/META-INF/resources/primefaces/datepicker/1-datepicker.js
@@ -531,7 +531,7 @@ PrimeFaces.widget.DatePicker = PrimeFaces.widget.BaseWidget.extend({
      * Hide the popup panel.
      */
     hide: function() {
-        this.jq.data().primeDatePicker.hideOverlay();
+        this.jq.data().primeDatePicker.onEscapeKey();
     },
 
     /**
