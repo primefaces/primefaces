de8b511e90c31fa86a1b64d3f18fea4bc8f42dd4
Melloware <mellowaredev@gmail.com>
2023-12-19 11:10:24 -0500

    Fix #11148: 13.0.5 Message fix for multiple messages (#11153)
    * Fix #11148: 13.0.5 Message integration test
    
    * Update message001.xhtml

---
+++ primefaces-integration-tests/src/main/java/org/primefaces/integrationtests/message/Message001.java
@@ -0,0 +1,59 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2009-2023 PrimeTek Informatics
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.primefaces.integrationtests.message;
+
+import lombok.Data;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Named
+@ViewScoped
+@Data
+public class Message001 implements Serializable {
+
+    private static final long serialVersionUID = -3664548553854145624L;
+
+    private String val1;
+
+    @PostConstruct
+    public void init() {
+    }
+
+    public void action1() {
+        FacesContext.getCurrentInstance().addMessage("form:val1",
+                new FacesMessage(FacesMessage.SEVERITY_ERROR, "Action 1", "Action 1 - some error"));
+    }
+
+    public void action2() {
+        FacesContext.getCurrentInstance().addMessage("form:val1",
+                new FacesMessage(FacesMessage.SEVERITY_INFO, "info summary", "info detail"));
+        FacesContext.getCurrentInstance().addMessage("form:val1",
+                new FacesMessage(FacesMessage.SEVERITY_ERROR, "error summary", "error detail"));
+    }
+}
