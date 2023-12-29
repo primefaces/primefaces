1002dfa8dded785132e6736ed89b809602091822
Jasper de Vries <jepsar@gmail.com>
2023-12-29 16:34:46 +0100

    Fix copy paste fail

--- primefaces/src/main/resources/META-INF/resources/primefaces/forms/forms.commandlink.js
+++ primefaces/src/main/resources/META-INF/resources/primefaces/forms/forms.commandlink.js
@@ -4,7 +4,7 @@
  * CommandLink is an extended version of standard commandLink with AJAX and theming.
  *
  * @prop {number} [ajaxCount] Number of concurrent active Ajax requests.
- * @prop {number} [ajaxCount] Timestamp of the Ajax request that started the animation.
+ * @prop {number} [ajaxStart] Timestamp of the Ajax request that started the animation.
  * 
  * @interface {PrimeFaces.widget.CommandLinkCfg} cfg The configuration for the {@link  CommandLink| CommandLink widget}.
  * You can access this configuration via {@link PrimeFaces.widget.BaseWidget.cfg|BaseWidget.cfg}. Please note that this
