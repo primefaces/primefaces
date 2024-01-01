0d849e7df6096e9869045f390aeedf5e750fac1e
melloware <mellowaredev@gmail.com>
2024-01-01 08:41:39 -0500

    2024!

--- primefaces-integration-tests/src/test/java/org/primefaces/integrationtests/fileupload/FileUpload012Test.java
+++ primefaces-integration-tests/src/test/java/org/primefaces/integrationtests/fileupload/FileUpload012Test.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright (c) 2009-2023 PrimeTek Informatics
+ * Copyright (c) 2009-2024 PrimeTek Informatics
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -21,7 +21,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-
 package org.primefaces.integrationtests.fileupload;
 
 import org.junit.jupiter.api.Tag;
