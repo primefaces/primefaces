3794371c28c337dc5bfa5560296f43181450157c
melloware <mellowaredev@gmail.com>
2024-01-04 15:54:43 -0500

    Fix #2478: ColumnTogglerEvent giving access to UIColumn

---
+++ primefaces/src/main/java/org/primefaces/event/ColumnToggleEvent.java
@@ -0,0 +1,45 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2009-2024 PrimeTek Informatics
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.primefaces.event;
+
+import javax.faces.component.UIComponent;
+import javax.faces.component.behavior.Behavior;
+import org.primefaces.component.api.UIColumn;
+import org.primefaces.model.Visibility;
+
+public class ColumnToggleEvent extends ToggleEvent {
+
+    private static final long serialVersionUID = 1L;
+
+    private transient UIColumn column;
+
+    public ColumnToggleEvent(UIComponent component, Behavior behavior, UIColumn column, Visibility visibility, Object data) {
+        super(component, behavior, visibility, data);
+        this.column = column;
+    }
+
+    public UIColumn getColumn() {
+        return column;
+    }
+}
