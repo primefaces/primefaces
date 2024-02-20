4d4e116498ec2e49c4514ea940f88dfac1e1cbcc
Can Türker <tuerker@fgcz.ethz.ch>
2024-02-20 19:25:17 +0100

    Extend invalid file type message by printing allowed types value

--- primefaces/src/main/java/org/primefaces/component/fileupload/FileUpload.java
+++ primefaces/src/main/java/org/primefaces/component/fileupload/FileUpload.java
@@ -141,7 +141,8 @@ public class FileUpload extends FileUploadBase {
         String accept = isValidateContentType() ? getAccept() : null;
         if (!FileUploadUtils.isValidType(appContext, uploadedFile, getAllowTypes(), accept)) {
             throw new ValidatorException(
-                    MessageFactory.getFacesMessage(FileValidator.ALLOW_TYPES_MESSAGE_ID, FacesMessage.SEVERITY_ERROR, uploadedFile.getFileName()), getAllowTypes());
+                    MessageFactory.getFacesMessage(FileValidator.ALLOW_TYPES_MESSAGE_ID, FacesMessage.SEVERITY_ERROR, uploadedFile.getFileName()
+                            , getAllowTypes()));
         }
 
         if (isVirusScan()) {
