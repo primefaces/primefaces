3d3d7027edaf845c827bf9cf7233e6c5041a377a
melloware <mellowaredev@gmail.com>
2024-02-29 14:25:23 -0500

    Fix #11540: Picklist trigger input changes on transfer

--- primefaces/src/main/resources/META-INF/resources/primefaces/picklist/picklist.js
+++ primefaces/src/main/resources/META-INF/resources/primefaces/picklist/picklist.js
@@ -1168,6 +1168,10 @@ PrimeFaces.widget.PickList = PrimeFaces.widget.BaseWidget.extend({
             $($this.jqId + ' ul').sortable('enable');
             $this.updateButtonsState();
         }
+
+        // #11540: trigger change events on inputs for form handling
+        this.targetInput.trigger("change");
+        this.sourceInput.trigger("change")?
     },
 
     /**
