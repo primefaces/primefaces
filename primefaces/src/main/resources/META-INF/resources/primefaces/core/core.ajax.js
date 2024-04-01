6dc094b9b8ae9a2a384c5a7417621dec08bc8dfe
melloware <mellowaredev@gmail.com>
2024-04-01 14:14:19 -0400

    Fix #11702: DOM leaks on AJAX update

--- primefaces/src/main/resources/META-INF/resources/primefaces/core/core.ajax.js
+++ primefaces/src/main/resources/META-INF/resources/primefaces/core/core.ajax.js
@@ -326,7 +326,9 @@ if (!PrimeFaces.ajax) {
                         PrimeFaces.warn("DOM element with id '" + id + "' cant be found; skip update...");
                     }
                     else {
-                        target.replaceWith(content);
+                        var removedContent = target.replaceWith(content);
+                        // detach all handlers and data to clean up DOM
+                        PrimeFaces.utils.cleanseDomElement(removedContent);
                     }
                 }
             }
@@ -1352,7 +1354,7 @@ if (!PrimeFaces.ajax) {
 
                         try {
                             delete PrimeFaces.widgets[widgetVar];
-                            delete widget;
+                            widget = null;
                         } catch (e) { }
                     }
                 }
