e9073c8730bdb23a9da912ef77c8d42109d6ec9a
Sébastien Lepage <sebastien.lepage@outlook.com>
2024-04-14 14:13:04 +0200

    Move IOBiConsumer into org.primefaces.util

--- primefaces/src/main/java/org/primefaces/util/IOBiConsumer.java
+++ primefaces/src/main/java/org/primefaces/util/IOBiConsumer.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package org.primefaces.functional;
+package org.primefaces.util;
 
 import java.io.IOException;
 import java.util.Objects;
