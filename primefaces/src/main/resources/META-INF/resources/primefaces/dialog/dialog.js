92fc32b48268146b75a645af0732121e02237b9c
Melloware <mellowaredev@gmail.com>
2024-04-25 15:56:03 -0400

    Fix #11818: Dialog focus from link (#11819)

--- primefaces/src/main/resources/META-INF/resources/primefaces/dialog/dialog.js
+++ primefaces/src/main/resources/META-INF/resources/primefaces/dialog/dialog.js
@@ -370,8 +370,12 @@ PrimeFaces.widget.Dialog = PrimeFaces.widget.DynamicOverlayWidget.extend({
      * @protected
      */
     applyFocus: function() {
-        if (this.cfg.focus)
-            PrimeFaces.expressions.SearchExpressionFacade.resolveComponentsAsSelector(this.jq, this.cfg.focus).trigger('focus');
+        if (this.cfg.focus) {
+            var $this = this;
+            PrimeFaces.queueTask(function() {
+                PrimeFaces.expressions.SearchExpressionFacade.resolveComponentsAsSelector($this.jq, $this.cfg.focus).trigger('focus')
+            }, 100);
+        }
         else
             PrimeFaces.focus(null, this.id);
     },
