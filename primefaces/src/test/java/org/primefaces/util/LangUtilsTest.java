9f4b3c60585abb7039e681079f6b2b97bb361f1d
melloware <mellowaredev@gmail.com>
2024-10-01 08:58:15 -0400

    Merge branch 'master' into PF-12511

--- primefaces/src/test/java/org/primefaces/util/LangUtilsTest.java
+++ primefaces/src/test/java/org/primefaces/util/LangUtilsTest.java
@@ -23,14 +23,11 @@
  */
 package org.primefaces.util;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class LangUtilsTest {
 
