c92f0fbc90b2c6238853a45e0e4d9c3ca0077d53
Gandulf Kohlweiss <gako@ilogs.com>
2024-10-17 14:47:09 +0200

    Fix #12732: If the menuitem has an icon defined use the icon instead of the index number (#12733)
    * If the menuitem has an icon defined use the icon instead of the calculated index number
    
    * use LangUtils.isNotEmpty and fix code styles
    
    * add example of new icon feature for steps component
    
    ---------
    
    Co-authored-by: Gandulf Kohlweiss <gandulf.kohlweiss@ilogs.com>

--- primefaces/src/main/java/org/primefaces/component/steps/StepsRenderer.java
+++ primefaces/src/main/java/org/primefaces/component/steps/StepsRenderer.java
@@ -27,6 +27,7 @@ import org.primefaces.component.menu.AbstractMenu;
 import org.primefaces.component.menu.BaseMenuRenderer;
 import org.primefaces.model.menu.MenuElement;
 import org.primefaces.model.menu.MenuItem;
+import org.primefaces.util.LangUtils;
 
 import java.io.IOException;
 import java.util.List;
@@ -150,7 +151,14 @@ public class StepsRenderer extends BaseMenuRenderer {
 
         writer.startElement("span", steps);
         writer.writeAttribute("class", Steps.STEP_NUMBER_CLASS, null);
-        writer.writeText((index + 1), null);
+        if (LangUtils.isNotEmpty(menuitem.getIcon())) {
+            writer.startElement("span", null);
+            writer.writeAttribute("class", Steps.STEP_NUMBER_ICON_CLASS + " " + menuitem.getIcon(), null);
+            writer.endElement("span");
+        }
+        else {
+            writer.writeText((index + 1), null);
+        }
         writer.endElement("span");
 
         Object value = menuitem.getValue();
