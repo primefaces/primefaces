01b51b100600b9963538181a5b670fe06368598f
Melloware <mellowaredev@gmail.com>
2024-10-24 08:39:36 -0400

    Fix #12771: 14.0.7  AJAX resolving clientIds of currently unrendered should not throw a exception (#12776)

--- primefaces/src/main/java/org/primefaces/expression/WidgetVarSearchKeywordResolver.java
+++ primefaces/src/main/java/org/primefaces/expression/WidgetVarSearchKeywordResolver.java
@@ -25,13 +25,13 @@ package org.primefaces.expression;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 import javax.faces.FacesException;
 import javax.faces.component.UIComponent;
 import javax.faces.component.search.SearchExpressionContext;
 import javax.faces.component.search.SearchKeywordContext;
 import javax.faces.component.search.SearchKeywordResolver;
 import javax.faces.component.visit.VisitContext;
-import org.primefaces.util.ComponentUtils;
 
 public class WidgetVarSearchKeywordResolver extends SearchKeywordResolver {
 
@@ -51,8 +51,7 @@ public class WidgetVarSearchKeywordResolver extends SearchKeywordResolver {
 
                 WidgetVarVisitCallback visitCallback = new WidgetVarVisitCallback(matcher.group(1));
                 context.getSearchExpressionContext().getFacesContext().getViewRoot().visitTree(
-                        VisitContext.createVisitContext(context.getSearchExpressionContext().getFacesContext(), null,
-                                ComponentUtils.VISIT_HINTS_SKIP_UNRENDERED),
+                    VisitContext.createVisitContext(context.getSearchExpressionContext().getFacesContext(), null, null),
                         visitCallback);
 
                 if (visitCallback.getComponent() != null) {
--- primefaces/src/main/java/org/primefaces/util/AjaxRequestBuilder.java
+++ primefaces/src/main/java/org/primefaces/util/AjaxRequestBuilder.java
@@ -23,8 +23,20 @@
  */
 package org.primefaces.util;
 
-import java.util.*;
+import org.primefaces.component.api.AjaxSource;
+import org.primefaces.component.api.ClientBehaviorRenderingMode;
+import org.primefaces.config.PrimeConfiguration;
+import org.primefaces.context.PrimeApplicationContext;
+import org.primefaces.expression.SearchExpressionUtils;
+
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.logging.Logger;
+
 import javax.faces.application.ProjectStage;
 import javax.faces.component.UIComponent;
 import javax.faces.component.UIForm;
@@ -32,12 +44,6 @@ import javax.faces.component.UIParameter;
 import javax.faces.component.search.SearchExpressionHint;
 import javax.faces.context.FacesContext;
 import javax.faces.view.facelets.FaceletException;
-
-import org.primefaces.component.api.AjaxSource;
-import org.primefaces.component.api.ClientBehaviorRenderingMode;
-import org.primefaces.config.PrimeConfiguration;
-import org.primefaces.context.PrimeApplicationContext;
-import org.primefaces.expression.SearchExpressionUtils;
 
 /**
  * Helper to generate javascript code of an ajax call
@@ -135,7 +141,7 @@ public class AjaxRequestBuilder {
     }
 
     public AjaxRequestBuilder process(UIComponent component, String expressions, boolean ignoreNoResult) {
-        addExpressions(component, expressions, "p", ignoreNoResult ? HINTS_PROCESS_IGNORE_NO_RESULT : HINTS_PROCESS, false);
+        addExpressions(component, expressions, "p", ignoreNoResult ? HINTS_PROCESS_IGNORE_NO_RESULT : HINTS_PROCESS);
 
         return this;
     }
@@ -145,16 +151,14 @@ public class AjaxRequestBuilder {
     }
 
     public AjaxRequestBuilder update(UIComponent component, String expressions, boolean ignoreNoResult) {
-        addExpressions(component, expressions, "u", ignoreNoResult ? HINTS_UPDATE_IGNORE_NO_RESULT : HINTS_UPDATE, true);
+        addExpressions(component, expressions, "u", ignoreNoResult ? HINTS_UPDATE_IGNORE_NO_RESULT : HINTS_UPDATE);
 
         return this;
     }
 
-    private AjaxRequestBuilder addExpressions(UIComponent component, String expressions, String key, Set<SearchExpressionHint> hints,
-            boolean skipUnredered) {
+    private AjaxRequestBuilder addExpressions(UIComponent component, String expressions, String key, Set<SearchExpressionHint> hints) {
         if (LangUtils.isNotBlank(expressions)) {
-            String resolvedExpressions = SearchExpressionUtils.resolveClientIdsAsString(context, component, expressions, hints,
-                    skipUnredered ? ComponentUtils.VISIT_HINTS_SKIP_UNRENDERED : null);
+            String resolvedExpressions = SearchExpressionUtils.resolveClientIdsAsString(context, component, expressions, hints, null);
             buffer.append(",").append(key).append(":\"").append(resolvedExpressions).append("\"");
         }
 
