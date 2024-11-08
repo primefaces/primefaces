81afba93f920b924c050e50ffc6803fdf9966be2
Andre Wachsmuth <awa@xima.de>
2024-11-09 00:03:48 +0100

    Fix duplicate code in inputmask

--- primefaces/src/main/frontend/bundles/components/inputmask.ts
+++ primefaces/src/main/frontend/bundles/components/inputmask.ts
@@ -3,5 +3,5 @@ import "../../src/inputmask/1-inputmask.js";
 
 // Expose InputMask to the global scope
 // Not needed for our code, but might already be used by external code
-import Inputmask from "inputmask/dist/inputmask.es6.js";
+import Inputmask from "inputmask/lib/inputmask.js";
 Object.assign(window, { Inputmask });
