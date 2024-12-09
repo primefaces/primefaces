b5b1668c31d98c063d7cfe9aa40701b32a30b179
Melloware <mellowaredev@gmail.com>
2024-12-09 10:24:36 -0500

    Fix #13026: Crud showcase fix (#13028)

--- primefaces-showcase/src/main/java/org/primefaces/showcase/domain/Product.java
+++ primefaces-showcase/src/main/java/org/primefaces/showcase/domain/Product.java
@@ -25,6 +25,7 @@ package org.primefaces.showcase.domain;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Objects;
 
 public class Product implements Serializable {
 
@@ -153,10 +154,7 @@ public class Product implements Serializable {
 
     @Override
     public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((code == null) ? 0 : code.hashCode());
-        return result;
+        return Objects.hashCode(code);
     }
 
     @Override
@@ -171,12 +169,7 @@ public class Product implements Serializable {
             return false;
         }
         Product other = (Product) obj;
-        if (code == null) {
-            return other.code == null;
-        }
-        else {
-            return code.equals(other.code);
-        }
+        return Objects.equals(getCode(), other.getCode());
     }
 
 }
