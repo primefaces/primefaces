0bdf9c7be5e571aec87f016c1b8247eb88699946
Christoph Straßer <christoph.strasser78@gmail.com>
2025-02-09 18:55:17 +0100

    #13278: Textarea, countBytesAsChars, Integrationtests (#13309)

--- primefaces-integration-tests/src/test/java/org/primefaces/integrationtests/inputtextarea/InputTextArea003Test.java
+++ primefaces-integration-tests/src/test/java/org/primefaces/integrationtests/inputtextarea/InputTextArea003Test.java
@@ -25,8 +25,10 @@ package org.primefaces.integrationtests.inputtextarea;
 
 import org.primefaces.selenium.AbstractPrimePage;
 import org.primefaces.selenium.AbstractPrimePageTest;
+import org.primefaces.selenium.PrimeSelenium;
 import org.primefaces.selenium.component.CommandButton;
 import org.primefaces.selenium.component.InputTextarea;
+import org.primefaces.selenium.component.SelectBooleanCheckbox;
 
 import org.json.JSONObject;
 import org.junit.jupiter.api.DisplayName;
@@ -61,6 +63,45 @@ class InputTextArea003Test extends AbstractPrimePageTest {
 
     @Test
     @Order(2)
+    @DisplayName("InputTextarea: MaxLength using less than max # of characters, including one Emoji, countBytesAsChars=true")
+    void maxLengthLessThan_WithEmoji(Page page) {
+        // Arrange
+        InputTextarea inputText = page.inputtext;
+        assertEquals("", inputText.getValue());
+
+        // Act
+        PrimeSelenium.executeScript("document.getElementById('form\\:inputtext').value = 'four\uD83D\uDE00';");
+        page.button.click();
+
+        // Assert
+        assertEquals("MaxLength Counter", inputText.getAssignedLabelText());
+        assertEquals("four\uD83D\uDE00", inputText.getValue());
+        assertEquals("4 characters remaining.", page.display.getText());
+        assertConfiguration(inputText.getWidgetConfiguration());
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("InputTextarea: MaxLength using less than max # of characters, including one Emoji, countBytesAsChars=true")
+    void maxLengthLessThan_WithEmoji_CountBytesAsChar(Page page) {
+        // Arrange
+        InputTextarea inputText = page.inputtext;
+        assertEquals("", inputText.getValue());
+        page.checkboxCountBytesAsChars.check();
+
+        // Act
+        PrimeSelenium.executeScript("document.getElementById('form\\:inputtext').value = 'four\uD83D\uDE00';");
+        page.button.click();
+
+        // Assert
+        assertEquals("MaxLength Counter", inputText.getAssignedLabelText());
+        assertEquals("four\uD83D\uDE00", inputText.getValue());
+        assertEquals("2 characters remaining.", page.display.getText());
+        assertConfiguration(inputText.getWidgetConfiguration());
+    }
+
+    @Test
+    @Order(10)
     @DisplayName("InputTextarea: MaxLength using more than max # of characters")
     void maxLengthMoreThan(Page page) {
         // Arrange
@@ -87,6 +128,9 @@ class InputTextArea003Test extends AbstractPrimePageTest {
     }
 
     public static class Page extends AbstractPrimePage {
+        @FindBy(id = "form:checkboxCountBytesAsChars")
+        SelectBooleanCheckbox checkboxCountBytesAsChars;
+
         @FindBy(id = "form:inputtext")
         InputTextarea inputtext;
 
