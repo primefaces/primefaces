93ca378edb1fcb2b2ff7a12d843ef88cfffcab19
Andre Wachsmuth <sensenmann5@gmail.com>
2025-03-02 17:08:22 +0100

    Remove patch for jquery-ui and add fix to our file with a long list of legacy patches

--- primefaces/src/main/frontend/packages/calendar/calendar/src/jquery.ui.datetimepicker.pfextensions.js
+++ primefaces/src/main/frontend/packages/calendar/calendar/src/jquery.ui.datetimepicker.pfextensions.js
@@ -174,7 +174,21 @@
         setTimeout( function() {
         	$.datepicker._base_disableDatepicker(target);
         }, 0);
-    }
+    };
+
+    // This patch fixes a focusing issue with the datepicker, when used
+    // in combination with the `inputmask` library. The JQuery datepicker
+    // would trigger a focus event on every `keyup`, triggering a focusing
+    // event. This makes `inputmask` think that the user just entered the
+    // input field, and it consequently saves the new value as the initial
+    // value. Then, when the user leaves the input field and the browser
+    // fires a blur event, `inputmask` does not see any changes and does
+    // not trigger a `change` event. Finally, the result of this missing
+    // change event is that no AJAX behavior is triggered either, i.e.
+    // `<p:ajax event="change">` won't work.
+    $.datepicker._shouldFocusInput = function( inst ) {
+        return inst.input && inst.input.is( ":visible" ) && !inst.input.is( ":disabled" ) && !inst.input.is( ":focus" );
+    };
 })();
 
 
