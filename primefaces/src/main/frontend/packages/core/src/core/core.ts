a1ffb3e94ea07d0a0492670bb193fb686a4cabd5
Melloware <mellowaredev@gmail.com>
2025-03-17 07:36:37 -0400

    Fix #13538: SplitButton aria menu button fixes (#13561)

--- primefaces/src/main/frontend/packages/core/src/core/core.ts
+++ primefaces/src/main/frontend/packages/core/src/core/core.ts
@@ -129,6 +129,7 @@ const LocaleEnUs: PrimeType.Locale = {
         "jumpToPageDropdownLabel": "Jump to Page Dropdown",
         "jumpToPageInputLabel": "Jump to Page Input",
         "lastPageLabel": "Last Page",
+        "listLabel": "Options List",
         "listView": "List View",
         "moveAllToSource": "Move All to Source",
         "moveAllToTarget": "Move All to Target",
