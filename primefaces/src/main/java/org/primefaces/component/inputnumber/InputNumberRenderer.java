eb0ea6001cede4879428cb4eda598ef490352f17
melloware <mellowaredev@gmail.com>
2025-03-20 16:24:46 -0400

    Fix #13597: 15.0.2 InputNumber use default range for numeric type

--- primefaces/src/main/java/org/primefaces/component/inputnumber/InputNumberRenderer.java
+++ primefaces/src/main/java/org/primefaces/component/inputnumber/InputNumberRenderer.java
@@ -48,8 +48,8 @@ import javax.faces.convert.ConverterException;
 public class InputNumberRenderer extends InputRenderer {
 
     // Default values for "minValue"/"maxValue" properties of the AutoNumeric Plugin
-    private static final BigDecimal DEFAULT_MIN_VALUE = new BigDecimal("-10000000000000");
-    private static final BigDecimal DEFAULT_MAX_VALUE = new BigDecimal("10000000000000");
+    private static final String DEFAULT_MIN_VALUE = "-10000000000000";
+    private static final String DEFAULT_MAX_VALUE = "10000000000000";
 
     @Override
     public Object getConvertedValue(FacesContext context, UIComponent component, Object submittedValue)
@@ -89,7 +89,7 @@ public class InputNumberRenderer extends InputRenderer {
             else {
                 // Coerce submittedValue to (effective) range of [minValue, maxValue]
                 BigDecimal value = new BigDecimal(submittedValue);
-                submittedValue = coerceValueInRange(value, inputNumber).toString();
+                submittedValue = coerceValueInRange(context, value, inputNumber).toString();
             }
         }
         catch (NumberFormatException ex) {
@@ -118,7 +118,7 @@ public class InputNumberRenderer extends InputRenderer {
             catch (Exception e) {
                 throw new IllegalArgumentException("Error converting  [" + valueToRender + "] to a decimal value;", e);
             }
-            valueToRender = formatForPlugin(coerceValueInRange(decimalToRender, inputNumber));
+            valueToRender = formatForPlugin(coerceValueInRange(context, decimalToRender, inputNumber));
         }
 
         encodeMarkup(context, inputNumber, value, valueToRender);
@@ -240,8 +240,8 @@ public class InputNumberRenderer extends InputRenderer {
             .attr("currencySymbol", inputNumber.getSymbol())
             .attr("currencySymbolPlacement", inputNumber.getSymbolPosition(), "p")
             .attr("negativePositiveSignPlacement", inputNumber.getSignPosition(), null)
-            .attr("minimumValue", getMinimum(isIntegral, inputNumber))
-            .attr("maximumValue", getMaximum(isIntegral, inputNumber))
+            .attr("minimumValue", getMinimum(context, isIntegral, inputNumber))
+            .attr("maximumValue", getMaximum(context, isIntegral, inputNumber))
             .attr("emptyInputBehavior", emptyValue, "focus")
             .attr("leadingZero", inputNumber.getLeadingZero(), "deny")
             .attr("allowDecimalPadding", inputNumber.getPadControl(), "true")
@@ -262,13 +262,17 @@ public class InputNumberRenderer extends InputRenderer {
 
     /**
      * Get the effective minimum Value (as interpreted in the AutoNumeric plugin)
-     * @param inputNumber the InputNumber component
+     * @param context faces context
+     * @param component the InputNumber component
      * @return the minimumValue property as BigDecimal, or the AutoNumeric default value if empty
      */
-    private BigDecimal getEffectiveMinValue(InputNumber inputNumber) {
-        String minimumValue = inputNumber.getMinValue();
+    private BigDecimal getEffectiveMinValue(FacesContext context, InputNumber component) {
+        String minimumValue = component.getMinValue();
         if (minimumValue == null) {
-            return DEFAULT_MIN_VALUE;
+            minimumValue = determineMinimumFromType(context, component);
+        }
+        if (minimumValue == null) {
+            minimumValue = DEFAULT_MIN_VALUE;
         }
         try {
             return new BigDecimal(minimumValue);
@@ -280,14 +284,19 @@ public class InputNumberRenderer extends InputRenderer {
 
     /**
      * Get the effective maximum Value (as interpreted in the AutoNumeric plugin)
-     * @param inputNumber the InputNumber component
+     * @param context faces context
+     * @param component the InputNumber component
      * @return the maximumValue property as BigDecimal, or the AutoNumeric default value if empty
      */
-    private BigDecimal getEffectiveMaxValue(InputNumber inputNumber) {
-        String maximumValue = inputNumber.getMaxValue();
+    private BigDecimal getEffectiveMaxValue(FacesContext context, InputNumber component) {
+        String maximumValue = component.getMaxValue();
         if (maximumValue == null) {
-            return DEFAULT_MAX_VALUE;
+            maximumValue = determineMaximumFromType(context, component);
         }
+        if (maximumValue == null) {
+            maximumValue = DEFAULT_MAX_VALUE;
+        }
+
         try {
             return new BigDecimal(maximumValue);
         }
@@ -299,11 +308,11 @@ public class InputNumberRenderer extends InputRenderer {
     /**
      * Coerce the provided value to the range defined by the effective minimum and maximum numbers.
      * @param value the value to render
-     * @param inputNumber the component for which the minValue and maxValue properties define the range
+     * @param component the component for which the minValue and maxValue properties define the range
      * @return the value if inside the range, or else the nearest boundary that is still inside the range
      */
-    private BigDecimal coerceValueInRange(BigDecimal value, InputNumber inputNumber) {
-        return coerceValueInRange(value, getEffectiveMinValue(inputNumber), getEffectiveMaxValue(inputNumber));
+    private BigDecimal coerceValueInRange(FacesContext context, BigDecimal value, InputNumber component) {
+        return coerceValueInRange(value, getEffectiveMinValue(context, component), getEffectiveMaxValue(context, component));
     }
 
     /**
@@ -369,31 +378,139 @@ public class InputNumberRenderer extends InputRenderer {
         return "2";
     }
 
+    private String determineMinimumFromType(FacesContext context, InputNumber component) {
+        Object value = component.getValue();
+        if (value != null) {
+            if (value instanceof Long) {
+                return Long.toString(Long.MIN_VALUE);
+            }
+            else if (value instanceof Integer) {
+                return Integer.toString(Integer.MIN_VALUE);
+            }
+            else if (value instanceof Double) {
+                return Double.toString(Double.MIN_VALUE);
+            }
+            else if (value instanceof Short) {
+                return Short.toString(Short.MIN_VALUE);
+            }
+            else if (value instanceof Byte) {
+                return Byte.toString(Byte.MIN_VALUE);
+            }
+            else if (value instanceof Float) {
+                return Float.toString(Float.MIN_VALUE);
+            }
+        }
+
+        Class<?> type = getTypeFromValueExpression(context, component);
+        if (type != null) {
+            if (type.isAssignableFrom(Long.class)) {
+                return Long.toString(Long.MIN_VALUE);
+            }
+            else if (type.isAssignableFrom(Integer.class)) {
+                return Integer.toString(Integer.MIN_VALUE);
+            }
+            else if (type.isAssignableFrom(Double.class)) {
+                return Double.toString(Double.MIN_VALUE);
+            }
+            else if (type.isAssignableFrom(Short.class)) {
+                return Short.toString(Short.MIN_VALUE);
+            }
+            else if (type.isAssignableFrom(Byte.class)) {
+                return Byte.toString(Byte.MIN_VALUE);
+            }
+            else if (type.isAssignableFrom(Float.class)) {
+                return Float.toString(Float.MIN_VALUE);
+            }
+        }
+
+        return null;
+    }
+
     /**
      * If using @Positive annotation and this is an Integer default to 0 instead of 0.0001.
+     * @param context faces context
      * @param isIntegral flag whether the (expected) value is integral
-     * @param inputNumber the component
+     * @param component the component
      * @return the minimum value of the component
      */
-    private String getMinimum(boolean isIntegral, InputNumber inputNumber) {
-        String minimum = inputNumber.getMinValue();
+    private String getMinimum(FacesContext context, boolean isIntegral, InputNumber component) {
+        String minimum = component.getMinValue();
         if (isIntegral && PositiveClientValidationConstraint.MIN_VALUE.equals(minimum)) {
             minimum = "0";
         }
+
+        if (minimum == null) {
+            minimum = determineMinimumFromType(context, component);
+        }
+
         return formatForPlugin(minimum);
+    }
+
+    private String determineMaximumFromType(FacesContext context, InputNumber component) {
+        Object value = component.getValue();
+        if (value != null) {
+            if (value instanceof Long) {
+                return Long.toString(Long.MAX_VALUE);
+            }
+            else if (value instanceof Integer) {
+                return Integer.toString(Integer.MAX_VALUE);
+            }
+            else if (value instanceof Double) {
+                return Double.toString(Double.MAX_VALUE);
+            }
+            else if (value instanceof Short) {
+                return Short.toString(Short.MAX_VALUE);
+            }
+            else if (value instanceof Byte) {
+                return Byte.toString(Byte.MAX_VALUE);
+            }
+            else if (value instanceof Float) {
+                return Float.toString(Float.MAX_VALUE);
+            }
+        }
+
+        Class<?> type = getTypeFromValueExpression(context, component);
+        if (type != null) {
+            if (type.isAssignableFrom(Long.class)) {
+                return Long.toString(Long.MAX_VALUE);
+            }
+            else if (type.isAssignableFrom(Integer.class)) {
+                return Integer.toString(Integer.MAX_VALUE);
+            }
+            else if (type.isAssignableFrom(Double.class)) {
+                return Double.toString(Double.MAX_VALUE);
+            }
+            else if (type.isAssignableFrom(Short.class)) {
+                return Short.toString(Short.MAX_VALUE);
+            }
+            else if (type.isAssignableFrom(Byte.class)) {
+                return Byte.toString(Byte.MAX_VALUE);
+            }
+            else if (type.isAssignableFrom(Float.class)) {
+                return Float.toString(Float.MAX_VALUE);
+            }
+        }
+
+        return null;
     }
 
     /**
      * If using @Negative annotation and this is an Integer default to 0 instead of -0.0001.
+     * @param context faces context
      * @param isIntegral flag whether the (expected) value is integral
-     * @param inputNumber the component
+     * @param component the component
      * @return the maximum value of the component
      */
-    private String getMaximum(boolean isIntegral, InputNumber inputNumber) {
-        String maximum = inputNumber.getMaxValue();
+    private String getMaximum(FacesContext context, boolean isIntegral, InputNumber component) {
+        String maximum = component.getMaxValue();
         if (isIntegral && NegativeClientValidationConstraint.MAX_VALUE.equals(maximum)) {
             maximum = "0";
         }
+
+        if (maximum == null) {
+            maximum = determineMaximumFromType(context, component);
+        }
+
         return formatForPlugin(maximum);
     }
 
