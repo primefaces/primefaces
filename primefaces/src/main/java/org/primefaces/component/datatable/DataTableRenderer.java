62edef19e186ee409b2f47f4e571c7ccf77f45bd
Melloware <mellowaredev@gmail.com>
2025-06-30 13:05:37 -0400

    Update DataTableRenderer.java

--- primefaces/src/main/java/org/primefaces/component/datatable/DataTableRenderer.java
+++ primefaces/src/main/java/org/primefaces/component/datatable/DataTableRenderer.java
@@ -678,7 +678,7 @@ public class DataTableRenderer extends DataRenderer {
         writer.writeAttribute("id", clientId, null);
         writer.writeAttribute("class", columnClass, null);
         writer.writeAttribute("scope", "col", null);
-        if (LangUtils.isNotBlank(column.getAriaHeaderText())) {
+        if (LangUtils.isNotBlank(ariaHeaderLabel)) {
             writer.writeAttribute(HTML.ARIA_LABEL, ariaHeaderLabel, null);
         }
         if (component != null) {
