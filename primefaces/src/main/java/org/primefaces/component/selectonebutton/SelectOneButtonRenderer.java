9a3a120c111c91717c0268d84c29f9a582272e3b
Melloware <mellowaredev@gmail.com>
2025-07-11 07:35:12 -0400

    Fix #13989: 15.0.6 SelectOneButton ARIA role (#13995)

--- primefaces/src/main/java/org/primefaces/component/selectonebutton/SelectOneButtonRenderer.java
+++ primefaces/src/main/java/org/primefaces/component/selectonebutton/SelectOneButtonRenderer.java
@@ -124,7 +124,9 @@ public class SelectOneButtonRenderer extends SelectOneRenderer {
         //button
         writer.startElement("div", null);
         writer.writeAttribute("class", buttonStyle, null);
+        writer.writeAttribute(HTML.ARIA_ROLE, "radio", null);
         writer.writeAttribute("tabindex", button.getTabindex(), null);
+
         if (option.getDescription() != null) {
             writer.writeAttribute("title", option.getDescription(), null);
         }
