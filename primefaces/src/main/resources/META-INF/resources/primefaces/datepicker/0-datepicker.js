5831f70d80d6bcbad3313010cc1d8a5d973af3e1
Melloware <mellowaredev@gmail.com>
2025-07-24 07:24:24 -0400

    Fix #14007: 15.0.7 DatePicker improve min/max date handling (#14029)

--- primefaces/src/main/resources/META-INF/resources/primefaces/datepicker/0-datepicker.js
+++ primefaces/src/main/resources/META-INF/resources/primefaces/datepicker/0-datepicker.js
@@ -2389,7 +2389,7 @@
         },
 
         onInputChange: function(event) {
-            if ((this.options.autoMonthFormat || !this.inputfield.val() || this.options.showMinMaxRange) && this.options.monthNavigator && this.options.view !== 'month') { 
+            if ((this.options.autoMonthFormat || !this.inputfield.val()) && this.options.monthNavigator && this.options.view !== 'month') { 
                 var viewMonth = this.viewDate.getMonth();
                 viewMonth = (this.isInMaxYear() && Math.min(this.options.maxDate.getMonth(), viewMonth)) || (this.isInMinYear() && Math.max(this.options.minDate.getMonth(), viewMonth)) || viewMonth;
                 this.viewDate.setMonth(viewMonth);
@@ -3580,7 +3580,7 @@
 
             this.viewDate = value;
 
-            if ((this.options.autoMonthFormat || !this.inputfield.val() || this.options.showMinMaxRange) && this.options.monthNavigator && this.options.view !== 'month') {
+            if ((this.options.autoMonthFormat || !this.inputfield.val()) && this.options.monthNavigator && this.options.view !== 'month') {
                 var viewMonth = this.viewDate.getMonth();
                 viewMonth = (this.isInMaxYear() && Math.min(this.options.maxDate.getMonth(), viewMonth)) || (this.isInMinYear() && Math.max(this.options.minDate.getMonth(), viewMonth)) || viewMonth;
                 this.viewDate.setMonth(viewMonth);
