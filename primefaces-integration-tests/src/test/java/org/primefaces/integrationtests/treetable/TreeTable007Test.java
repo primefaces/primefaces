a6e8c26a376c58a4ff6ab5ca0a2c9e48526a6521
Christoph Straßer <christoph.strasser78@gmail.com>
2025-08-05 22:09:29 +0200

    Fix #14045: Integration test: TreeTable007Test.cellEdit broken since Firefox 141.0 (vs 140.0.4)

--- primefaces-integration-tests/src/test/java/org/primefaces/integrationtests/treetable/TreeTable007Test.java
+++ primefaces-integration-tests/src/test/java/org/primefaces/integrationtests/treetable/TreeTable007Test.java
@@ -24,6 +24,7 @@
 package org.primefaces.integrationtests.treetable;
 
 import org.primefaces.selenium.AbstractPrimePage;
+import org.primefaces.selenium.PrimeExpectedConditions;
 import org.primefaces.selenium.PrimeSelenium;
 import org.primefaces.selenium.component.CommandButton;
 import org.primefaces.selenium.component.InputText;
@@ -83,6 +84,7 @@ class TreeTable007Test extends AbstractTreeTableTest {
         PrimeSelenium.guardAjax(input).sendKeys(Keys.ESCAPE);
 
         // Assert
+        PrimeSelenium.waitGui().until(PrimeExpectedConditions.elementToBeClickable(treeTable.getRow(1).getCell(3).getWebElement().findElement(By.cssSelector("button"))));
         PrimeSelenium.guardAjax(treeTable.getRow(1).getCell(3).getWebElement().findElement(By.cssSelector("button"))).click();
         assertMessage(page.messages, 0, "selected document", "Spanish - IT;10kb;Folder");
 
