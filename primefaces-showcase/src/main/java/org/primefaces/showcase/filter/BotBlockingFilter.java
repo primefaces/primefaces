07fa3a25593adf5563b3ef85499f610c0afb1427
tandraschko <tandraschko@apache.org>
2025-11-08 15:06:31 +0100

    block bots

--- primefaces-showcase/src/main/java/org/primefaces/showcase/filter/BotBlockingFilter.java
+++ primefaces-showcase/src/main/java/org/primefaces/showcase/filter/BotBlockingFilter.java
@@ -23,12 +23,17 @@
  */
 package org.primefaces.showcase.filter;
 
-import jakarta.servlet.*;
+import java.io.IOException;
+import java.util.regex.Pattern;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.regex.Pattern;
 
 @WebFilter(filterName = "BotBlockingFilter", urlPatterns = "/*")
 public class BotBlockingFilter implements Filter {
