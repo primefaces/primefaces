c1031ec1d3939340ead84416c85a0fc10c832bbe
Melloware <mellowaredev@gmail.com>
2025-11-24 09:21:53 -0500

    Update datatable.widget.js

--- primefaces/src/main/frontend/packages/components/src/datatable/datatable.widget.js
+++ primefaces/src/main/frontend/packages/components/src/datatable/datatable.widget.js
@@ -5854,8 +5854,8 @@ PrimeFaces.widget.DataTable = class DataTable extends PrimeFaces.widget.Deferred
             this.updateExpandedRowsColspan();
         }
 
-        // reset the column widths if the table has an auto layout
-        if(this.scrollBody && this.isAutoLayout()) {
+        // reset the position and column widths if scrollable
+        if(this.scrollBody) {
             // reset scroll position to left-top.
             this.scrollBody.scrollTop(0).scrollLeft(0); 
 
