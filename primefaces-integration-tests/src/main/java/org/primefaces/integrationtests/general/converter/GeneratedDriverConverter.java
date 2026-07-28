60f22e72a0b2ba93d53cbe7721926a9b7cc86943
melloware <mellowaredev@gmail.com>
2026-07-28 10:16:27 -0400

    Fix #15082 Update bean discovery mode in beans.xml

--- primefaces-integration-tests/src/main/java/org/primefaces/integrationtests/general/converter/GeneratedDriverConverter.java
+++ primefaces-integration-tests/src/main/java/org/primefaces/integrationtests/general/converter/GeneratedDriverConverter.java
@@ -26,6 +26,7 @@ package org.primefaces.integrationtests.general.converter;
 import org.primefaces.integrationtests.general.model.Driver;
 import org.primefaces.integrationtests.general.service.GeneratedDriverService;
 
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.faces.application.FacesMessage;
 import jakarta.faces.component.UIComponent;
 import jakarta.faces.context.FacesContext;
@@ -36,6 +37,7 @@ import jakarta.inject.Inject;
 import jakarta.inject.Named;
 
 @Named
+@ApplicationScoped
 @FacesConverter(value = "generatedDriverConverter", managed = true)
 public class GeneratedDriverConverter implements Converter {
 
