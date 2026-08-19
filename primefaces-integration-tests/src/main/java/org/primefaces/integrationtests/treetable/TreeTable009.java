f55d5f2a4c814f6601f6f29e0880795924f8e245
jxmai <jxmai@users.noreply.github.com>
2026-08-19 02:58:05 -0500

    #9254: Fix TreeTable rendering issue after clearFilters() (#15120)
    * #9254: Fix TreeTable rendering issue after clearFilters()
    
    * #9254: clean up unused import
    
    * #9254: add comment explaining noActiveFilters check
    
    * #9254: add more info to the comments
    
    * #9254: use property key instead hardcoded string
    
    * #9254: remove .name() for consistency
    
    * #9254: include integration test for clearFilters bug

---
+++ primefaces-integration-tests/src/main/java/org/primefaces/integrationtests/treetable/TreeTable009.java
@@ -0,0 +1,60 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2009-2026 PrimeFaces
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.primefaces.integrationtests.treetable;
+
+import org.primefaces.model.TreeNode;
+
+import java.io.Serializable;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+
+import lombok.Data;
+
+@Named
+@ViewScoped
+@Data
+public class TreeTable009 implements Serializable {
+
+    private static final long serialVersionUID = -3049074201208091930L;
+
+    private TreeNode<Document> root;
+    private boolean otherRoot;
+
+    @Inject
+    private DocumentService service;
+
+    @PostConstruct
+    public void init() {
+        root = service.createDocuments();
+    }
+
+    public void switchRoot() {
+        root = otherRoot ? service.createDocuments() : service.createOtherDocuments();
+        otherRoot = !otherRoot;
+    }
+
+}
