0342683f2ae6a953472c48ceb0d600cd7dcca9c9
Nicola Isotta <nicola.isotta@gmail.com>
2026-09-03 16:47:20 +0200

    Dialog: remove incorrect implicitDefaultValue on appendTo (#15168)
    Dialog does not default to '@(body)' in runtime (DynamicOverlayWidget
    explicitly excludes Dialog and leaves the element in-place in the DOM).
    Remove implicitDefaultValue to prevent generating incorrect VDL documentation.

--- primefaces/src/main/java/org/primefaces/component/dialog/DialogBase.java
+++ primefaces/src/main/java/org/primefaces/component/dialog/DialogBase.java
@@ -127,7 +127,7 @@ public abstract class DialogBase extends UIPanel implements Widget, RTLAware, St
     @Property(description = "Client-side callback to execute when dialog is shown.")
     public abstract String getOnShow();
 
-    @Property(implicitDefaultValue = "@(body)", description = "Append dialog to the element with the given identifier.")
+    @Property(description = "Append dialog to the element with the given identifier.")
     public abstract String getAppendTo();
 
     @Property(defaultValue = "true", description = "Renders dialog header.")
